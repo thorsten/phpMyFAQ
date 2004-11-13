c1f21c42ebbfd7dd3694fe9a601ca466582055d0
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2004-11-13 18:51:54 +0000

    Some code improvements

--- phpmyfaq/add.php
+++ phpmyfaq/add.php
@@ -1,38 +1,39 @@
 <?php
-/******************************************************************************
- * Datei:				add.php
- * Autor:				Thorsten Rinne <thorsten@phpmyfaq.de>
- * Datum:				2002-09-16
- * Letzte Änderung:		2004-05-29
- * Copyright:           (c) 2001-2004 Thorsten Rinne
- * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- ******************************************************************************/
+/**
+* $Id: add.php,v 1.2 2004-11-13 18:51:54 thorstenr Exp $
+*
+* This is the page there a user can add a FAQ record.
+*
+* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
+* @since        2002-09-16
+* @copyright    (c) 2001-2004 Thorsten Rinne
+*
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in
+* compliance with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+*/
 
 Tracking("newentrypage",0);
 
 if (isset($_GET["question"])) {
-	$question = strip_tags($_REQUEST["question"]);
+    $question = strip_tags($_REQUEST["question"]);
     $readonly = " readonly=\"readonly\"";
-	}
-else {
+} else {
 	$question = "";
     $readonly = "";
-	}
+}
+
 if (isset($_GET["cat"])) {
 	$category = $_GET["cat"];
-	}
-else {
+} else {
 	$category = "";
-	}
+}
 
 $tree->buildTree();
 
@@ -53,6 +54,5 @@ $tpl->processTemplate ("writeContent", array(
 				"copyright_eintrag" => unhtmlentities($PMF_CONF["copyright_eintrag"]),
 				"msgNewContentSubmit" => $PMF_LANG["msgNewContentSubmit"],
 				));
-
 $tpl->includeTemplate("writeContent", "index");
 ?>
