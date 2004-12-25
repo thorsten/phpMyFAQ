3fd9848669c4782859bdc08cd54a9fe9198da8da
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2004-12-25 05:52:48 +0000

    added language selection for categories

--- phpmyfaq/admin/category.main.php
+++ phpmyfaq/admin/category.main.php
@@ -4,7 +4,7 @@
  * Description:			list all categories
  * Author:				Thorsten Rinne <thorsten@phpmyfaq.de>
  * Date:				2003-12-20
- * Last change:			2004-07-26
+ * Last change:			2004-12-10
  * Copyright:           (c) 2001-2004 Thorsten Rinne
  * 
  * The contents of this file are subject to the Mozilla Public License
@@ -19,17 +19,17 @@
  ******************************************************************************/
 
 print "<h2>".$PMF_LANG["ad_categ_existing"]."</h2>\n";
- 
+
 if ($permission["editcateg"]) {
     
     if (isset($_POST['language'])) {
         $lang = $_POST['language'];
     } else {
-        $lang = '';
+        $lang = $LANGCODE;
     }
 ?>
 
-    <form action="" enctype="multipart/form-data" method="POST">
+    <form action="<?php print $_SERVER['PHP_SELF']; ?>?aktion=category" enctype="multipart/form-data" method="POST">
     <fieldset>
     <legend>Select category language</legend>
     <select name="language" id="language">
@@ -47,19 +47,19 @@ if ($permission["editcateg"]) {
         $indent = "";
         for ($i = 0; $i < $cat["indent"]; $i++) {
             $indent .= "&nbsp;&nbsp;&nbsp;";
-            }
+        }
         print $indent."<strong style=\"vertical-align: top;\">&middot; ".$cat["name"]."</strong> ";
         print "<a href=\"".$_SERVER["PHP_SELF"].$linkext."&amp;aktion=addcategory&amp;cat=".$cat["id"]."\" title=\"".$PMF_LANG["ad_kateg_add"]."\"><img src=\"images/add.gif\" width=\"17\" height=\"18\" alt=\"".$PMF_LANG["ad_kateg_add"]."\" title=\"".$PMF_LANG["ad_kateg_add"]."\" border=\"0\" /></a>";
         print "<a href=\"".$_SERVER["PHP_SELF"].$linkext."&amp;aktion=editcategory&amp;cat=".$cat["id"]."\" title=\"".$PMF_LANG["ad_kateg_rename"]."\"><img src=\"images/edit.gif\" width=\"18\" height=\"18\" border=\"0\" /></a>";
         if (count($tree->getChilds($cat["id"])) == 0) {
             print "<a href=\"".$_SERVER["PHP_SELF"].$linkext."&amp;aktion=deletecategory&amp;cat=".$cat["id"]."\" title=\"".$PMF_LANG["ad_categ_delete"]."\"><img src=\"images/delete.gif\" width=\"17\" height=\"18\" alt=\"".$PMF_LANG["ad_categ_delete"]."\" title=\"".$PMF_LANG["ad_categ_delete"]."\" border=\"0\" /></a>";
-            }
+        }
         print "<a href=\"".$_SERVER["PHP_SELF"].$linkext."&amp;aktion=cutcategory&amp;cat=".$cat["id"]."\" title=\"".$PMF_LANG["ad_categ_cut"]."\"><img src=\"images/cut.gif\" width=\"16\" height=\"16\" alt=\"".$PMF_LANG["ad_categ_cut"]."\" border=\"0\" title=\"".$PMF_LANG["ad_categ_cut"]."\" /></a>\n";
         if ($cat["parent_id"] == 0) {
             print "<a href=\"".$_SERVER["PHP_SELF"].$linkext."&amp;aktion=movecategory&amp;cat=".$cat["id"]."\" title=\"".$PMF_LANG["ad_categ_move"]."\"><img src=\"images/move.gif\" width=\"16\" height=\"16\" alt=\"".$PMF_LANG["ad_categ_move"]."\" border=\"0\" title=\"".$PMF_LANG["ad_categ_move"]."\" /></a>\n";
-            }
-        print "<br />";
         }
+        print "<br />";
+    }
 ?>
 	<p><img src="images/arrow.gif" width="11" height="11" alt="" border="0"> <a href="<?php print $_SERVER["PHP_SELF"].$linkext."&amp;aktion=addcategory"; ?>"><?php print $PMF_LANG["ad_kateg_add"]; ?></a></p>
 	<p><?php print $PMF_LANG["ad_categ_remark"]; ?></p>
