5ccbb3b86c42bb9a98c4ea3a8a4d5339075e827e
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2004-12-31 12:51:24 +0000

    new feature: add FAQ URL in the mail to the admin bugfix: unhtmlentities() to revert entitiies for text emails

--- phpmyfaq/save.php
+++ phpmyfaq/save.php
@@ -1,23 +1,23 @@
 <?php
-/******************************************************************************
- * $Id: save.php,v 1.6 2004-12-16 13:31:47 thorstenr Exp $
- *
- * Datei:				save.php
- * Autor:				Thorsten Rinne <thorsten@phpmyfaq.de>
- * Datum:				2002-09-16
- * Letzte Änderung:		2004-10-31
- * Copyright:           (c) 2001-2004 Thorsten Rinne
- * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- ******************************************************************************/
+/**
+* $Id: save.php,v 1.7 2004-12-31 12:51:24 thorstenr Exp $
+*
+* Saves a user FAQ record and sends an email to the user
+*
+* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
+* @since        2002-09-16
+* @copyright    (c) 2001-2004 phpMyFAQ Team
+* 
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in
+* compliance with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+*/
 
 if (isset($_REQUEST["username"]) && isset($_REQUEST["rubrik"]) && isset($_REQUEST["thema"]) && $_REQUEST["thema"] != "" && isset($_REQUEST["content"]) && $_REQUEST["content"] != "" && isset($_REQUEST["usermail"]) && $_REQUEST["usermail"] != "" && IPCheck($_SERVER["REMOTE_ADDR"])) {
 	Tracking("savenewentry",0);
@@ -45,7 +45,7 @@ if (isset($_REQUEST["username"]) && isset($_REQUEST["rubrik"]) && isset($_REQUES
 	$author = $_REQUEST["username"];
 	$db->query("INSERT INTO ".SQLPREFIX."faqdata (id, lang, active, thema, content, keywords, author, email, comment, datum) VALUES (".$db->nextID(SQLPREFIX."faqdata", "id").", '".$lang."', 'no', '".$thema."', '".$content."', '".$keywords."', '".$author."', '".$_REQUEST["usermail"]."', 'y', '".$datum."')");
 	$db->query("INSERT INTO ".SQLPREFIX."faqvisits (id, lang, visits, last_visit) VALUES (".$db->insert_id('faqdata', 'id').", '".$lang."', '1', ".time().")");
-	mail($IDN->encode($PMF_CONF["adminmail"]), $PMF_CONF["title"], $PMF_LANG["msgMailCheck"], "From: ".$IDN->encode($_REQUEST["usermail"]));
+	mail($IDN->encode($PMF_CONF["adminmail"]), unhtmlentities($PMF_CONF["title"]), unhtmlentities($PMF_LANG["msgMailCheck"])."\n".unhtmlentities($PMF_CONF["title"]).": http://".$_SERVER["SERVER_NAME"].dirname($_SERVER["PHP_SELF"]), "From: ".$IDN->encode($_REQUEST["usermail"]));
 	$tpl->processTemplate ("writeContent", array(
 				"msgNewContentHeader" => $PMF_LANG["msgNewContentHeader"],
 				"Message" => $PMF_LANG["msgNewContentThanks"]
