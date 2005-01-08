40175135c083f1d85a102da8b6d03c857b9aa664
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2005-01-08 10:13:12 +0000

    2004->2005

--- phpmyfaq/admin/footer.php
+++ phpmyfaq/admin/footer.php
@@ -1,29 +1,30 @@
 <?php
-/******************************************************************************
- * File:				footer.php
- * Description:			footer of the admin area
- * Authors:				Thorsten Rinne <thorsten@phpmyfaq.de>
- * Date:				2003-02-26
- * Last change:			2004-06-09
- * Copyright:           (c) 2001-2004 Thorsten Rinne
- * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- ******************************************************************************/
+/**
+* $Id: footer.php,v 1.2 2005-01-08 10:13:12 thorstenr Exp $
+*
+* Footer of the admin area
+*
+* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
+* @since        2003-02-26
+* @copyright    (c) 2001-2005 phpMyFAQ Team
+* 
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in
+* compliance with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+*/
 ?>
 </div>
     
 <!-- Footer -->
 <div>
     <div id="footer">
-        <div>phpMyFAQ Version <?php print $PMF_CONF["version"]; ?> | &copy; 2001-2004 <a href="http://www.phpmyfaq.de/" target="_blank">phpMyFAQ-Team</a></div>
+        <div>phpMyFAQ Version <?php print $PMF_CONF["version"]; ?> | &copy; 2001-2005 <a href="http://www.phpmyfaq.de/" target="_blank">phpMyFAQ-Team</a></div>
     </div>
 </div>
 
