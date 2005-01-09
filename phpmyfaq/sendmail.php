e61aae6fb5e399ef723bd6e888eecb6e5333950b
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2005-01-09 12:15:24 +0000

    More input validation

--- phpmyfaq/sendmail.php
+++ phpmyfaq/sendmail.php
@@ -1,28 +1,36 @@
 <?php
-/******************************************************************************
- * Datei:				sendmail.php
- * Autor:				Thorsten Rinne <thorsten@phpmyfaq.de>
- * Datum:				2002-09-16
- * Letzte Änderung:		2004-07-17
- * Copyright:           (c) 2001-2004 Thorsten Rinne
- * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- ******************************************************************************/
+/**
+* $Id: sendmail.php,v 1.2 2005-01-09 12:15:24 thorstenr Exp $
+*
+* The 'send an email from the contact page' page
+*
+* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
+* @since        2002-09-16
+* @copyright    (c) 2001-2005 phpMyFAQ Team
+* 
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in
+* compliance with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+*/
 
 Tracking("s2fmail",0);
 
-if ($_POST["name"] && $_POST["email"] && checkEmail($_POST["email"]) && $_POST["question"]) {
+if (isset($_POST["name"]) && $_POST["name"] != '' && isset($_POST["email"]) && checkEmail($_POST["email"]) && isset($_POST["question"]) && $_POST["question"] != '') {
+    
 	list($user, $host) = explode("@", $_POST["email"]);
     if (gethostbyname($host) != "64.94.110.11") {
-        mail($IDN->encode($PMF_CONF["adminmail"]), "Feedback: ".$PMF_CONF["title"], stripslashes($_POST["question"]), "From: ".encode_iso88591($_POST["name"]). " <".$IDN->encode($_POST["email"]).">");
+        $question = stripslashes($_POST["question"]);
+        $name = encode_iso88591($_POST["name"]);
+        $sender = $IDN->encode($_POST["email"]);
+        
+        mail($IDN->encode($PMF_CONF["adminmail"]), "Feedback: ".$PMF_CONF["title"], $question, "From: ".$name. " <".$sender.">");
+        
         $tpl->processTemplate ("writeContent", array(
                 "msgContact" => $PMF_LANG["msgContact"],
                 "Message" => $PMF_LANG["msgMailContact"]
