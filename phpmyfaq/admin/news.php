d2c85be14fb843cb976cfdbf9d56da7328352d1f
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2005-01-09 11:15:02 +0000

    removed unnecessary code

--- phpmyfaq/admin/news.php
+++ phpmyfaq/admin/news.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* $Id: news.php,v 1.6 2005-01-04 15:52:47 thorstenr Exp $
+* $Id: news.php,v 1.7 2005-01-09 11:15:02 thorstenr Exp $
 *
 * The main administration file for the news
 *
@@ -90,7 +90,7 @@ if (isset($_REQUEST["do"]) && $_REQUEST["do"] == "write" && $permission["addnews
         <dt><strong><?php print $PMF_LANG["ad_news_header"]; ?></strong></dt>
 	    <dd><input class="admin" type="text" style="width: 525px;" name="header" value="<?php print $row->header ?>" /></dd>
         <dt><strong><?php print $PMF_LANG["ad_news_text"]; ?></strong></dt>
-        <dd><noscript>Please enable JavaScript to use the WYSIWYG editor!</noscript><textarea class="admin" id="content" name="content"><?php if (isset($row->artikel)) { print htmlspecialchars(stripslashes(br2nl($row->artikel)), ENT_QUOTES); } ?></textarea></dd>
+        <dd><noscript>Please enable JavaScript to use the WYSIWYG editor!</noscript><textarea class="admin" id="content" name="content"><?php if (isset($row->artikel)) { print htmlspecialchars(stripslashes($row->artikel), ENT_QUOTES); } ?></textarea></dd>
 	    <dt><strong><?php print $PMF_LANG["ad_news_link_url"]; ?></strong></dt>
         <dd><input class="admin" type="text" style="width: 525px;" name="link" value="<?php print $row->link; ?>" /></dd>
 	    <dt><strong><?php print $PMF_LANG["ad_news_link_title"]; ?></strong></dt>
