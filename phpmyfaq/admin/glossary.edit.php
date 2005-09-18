27af04ba06745d3951f2badaf5594ed4fc9cf15b
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2005-09-18 19:55:38 +0000

    Added form for editing a glossary item

--- phpmyfaq/admin/glossary.edit.php
+++ phpmyfaq/admin/glossary.edit.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* $Id: glossary.edit.php,v 1.1 2005-09-15 20:13:26 thorstenr Exp $
+* $Id: glossary.edit.php,v 1.2 2005-09-18 19:55:38 thorstenr Exp $
 *
 * Displays a form to edit an extisting glossary item
 *
@@ -23,8 +23,30 @@ print sprintf('<h2>%s</h2>', $PMF_LANG['ad_menu_glossary']);
 
 if ($permission['editglossary']) {
     
+    $id = (int)$_GET['id'];
     
+    require_once('../inc/glossary.php');
+    $glossary = new PMF_Glossary($db, $LANGCODE);
+    $glossaryItem = $glossary->getGlossaryItem($id);
+?>
+<form action="<?php print $_SERVER['PHP_SELF'].$linkext; ?>" method="post">
+<fieldset>
+    <legend><?php print $PMF_LANG['ad_glossary_edit']; ?></legend>
     
+    <input type="hidden" name="aktion" value="updateglossary" />
+    <input type="hidden" name="id" value="<?php print $glossaryItem['id']; ?>" />
+    
+    <label class="left" for="item"><?php print $PMF_LANG['ad_glossary_item']; ?>:</label>
+    <input class="admin" type="text" name="item" id="item" size="50" value="<?php print $glossaryItem['item']; ?>" /><br />
+    
+    <label class="left" for="definition"><?php print $PMF_LANG['ad_glossary_definition']; ?>:</label>
+    <textarea class="admin" name="definition" id="definition" cols="50" rows="3"><?php print $glossaryItem['definition']; ?></textarea><br />
+    
+    <input class="submit" type="submit" value="<?php print $PMF_LANG['ad_glossary_save']; ?>" />
+
+</fieldset>
+</form>
+<?php
 } else {
     print $PMF_LANG["err_NotAuth"];
 }
