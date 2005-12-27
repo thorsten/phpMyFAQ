0c72a0be97b619529a6214fde038f1c490fde6b9
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2005-12-27 19:00:44 +0000

    - added new tables - removed old tables - reordered create statements alphabetically

--- phpmyfaq/install/mysql.sql.php
+++ phpmyfaq/install/mysql.sql.php
@@ -1,13 +1,14 @@
 <?php
 /**
-* $Id: mysql.sql.php,v 1.16 2005-12-12 14:10:51 b33blebr0x Exp $
+* $Id: mysql.sql.php,v 1.17 2005-12-27 19:00:44 thorstenr Exp $
 *
 * CREATE TABLE instruction for MySQL database
 *
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
 * @author       Tom Rochester <tom.rochester@gmail.com>
+* @author       Lars Tiedemann <php@larstiedemann.de>
 * @since        2004-09-18
-* @copyright    (c) 2001-2004 phpMyFAQ Team
+* @copyright    (c) 2001-2005 phpMyFAQ Team
 * 
 * The contents of this file are subject to the Mozilla Public License
 * Version 1.1 (the "License"); you may not use this file except in
@@ -27,38 +28,20 @@ $uninst[] = "DROP TABLE ".$sqltblpre."faqcategoryrelations";
 $uninst[] = "DROP TABLE ".$sqltblpre."faqchanges";
 $uninst[] = "DROP TABLE ".$sqltblpre."faqcomments";
 $uninst[] = "DROP TABLE ".$sqltblpre."faqdata";
-$uninst[] = "DROP TABLE ".$sqltblpre."faqfragen";
-$uninst[] = "DROP TABLE ".$sqltblpre."faqnews";
-$uninst[] = "DROP TABLE ".$sqltblpre."faqvoting";
-$uninst[] = "DROP TABLE ".$sqltblpre."faqsessions";
-//$uninst[] = "DROP TABLE ".$sqltblpre."faquser";
-$uninst[] = "DROP TABLE ".$sqltblpre."faqvisits";
+$uninst[] = "DROP TABLE ".$sqltblpre."faqquestions";
 $uninst[] = "DROP TABLE ".$sqltblpre."faqglossary";
 $uninst[] = "DROP TABLE ".$sqltblpre."faqgroup";
 $uninst[] = "DROP TABLE ".$sqltblpre."faqgroup_right";
+$uninst[] = "DROP TABLE ".$sqltblpre."faqnews";
 $uninst[] = "DROP TABLE ".$sqltblpre."faqright";
+$uninst[] = "DROP TABLE ".$sqltblpre."faqsessions";
 $uninst[] = "DROP TABLE ".$sqltblpre."faquser";
 $uninst[] = "DROP TABLE ".$sqltblpre."faquserdata";
 $uninst[] = "DROP TABLE ".$sqltblpre."faquserlogin";
 $uninst[] = "DROP TABLE ".$sqltblpre."faquser_group";
 $uninst[] = "DROP TABLE ".$sqltblpre."faquser_right";
-
-//faqdata
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqdata (
-id int(11) NOT NULL,
-lang varchar(5) NOT NULL,
-active char(3) NOT NULL,
-keywords text NOT NULL,
-thema text NOT NULL,
-content longtext NOT NULL,
-author varchar(255) NOT NULL,
-email varchar(255) NOT NULL,
-comment enum('y','n') NOT NULL default 'y',
-datum varchar(15) NOT NULL,
-linkState VARCHAR(7) NOT NULL,
-linkCheckDate INT(11) DEFAULT '0' NOT NULL,
-FULLTEXT (keywords,thema,content),
-PRIMARY KEY (id, lang)) TYPE = MYISAM";
+$uninst[] = "DROP TABLE ".$sqltblpre."faqvisits";
+$uninst[] = "DROP TABLE ".$sqltblpre."faqvoting";
 
 //faqadminlog
 $query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqadminlog (
@@ -117,15 +100,47 @@ datum int(15) NOT NULL,
 helped text NOT NULL,
 PRIMARY KEY (id_comment))";
 
-//faqfragen
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqfragen (
-id int(11) unsigned NOT NULL,
-ask_username varchar(100) NOT NULL,
-ask_usermail varchar(100) NOT NULL,
-ask_rubrik varchar(100) NOT NULL,
-ask_content text NOT NULL,
-ask_date varchar(20) NOT NULL,
-PRIMARY KEY (id))";
+//faqdata
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqdata (
+id int(11) NOT NULL,
+lang varchar(5) NOT NULL,
+active char(3) NOT NULL,
+keywords text NOT NULL,
+thema text NOT NULL,
+content longtext NOT NULL,
+author varchar(255) NOT NULL,
+email varchar(255) NOT NULL,
+comment enum('y','n') NOT NULL default 'y',
+datum varchar(15) NOT NULL,
+linkState VARCHAR(7) NOT NULL,
+linkCheckDate INT(11) DEFAULT '0' NOT NULL,
+FULLTEXT (keywords,thema,content),
+PRIMARY KEY (id, lang)) TYPE = MYISAM";
+
+//faqglossary
+$query[] = "CREATE TABLE ".$sqltblpre."faqglossary (
+id INT(11) NOT NULL ,
+lang VARCHAR(2) NOT NULL ,
+item VARCHAR(255) NOT NULL ,
+definition TEXT NOT NULL,
+PRIMARY KEY (id, lang))";
+
+//faqgroup
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqgroup (
+group_id INT(11) UNSIGNED NOT NULL,
+name VARCHAR(25) NULL,
+description TEXT NULL,
+auto_join INT(1) UNSIGNED NULL,
+PRIMARY KEY(group_id),
+UNIQUE INDEX name(name)
+)";
+
+//faqgroup_right
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqgroup_right (
+group_id INT(11) UNSIGNED NOT NULL,
+right_id INT(11) UNSIGNED NOT NULL,
+PRIMARY KEY(group_id, right_id)
+)";
 
 //faqnews
 $query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqnews (
@@ -138,15 +153,25 @@ linktitel varchar(255) NOT NULL,
 target varchar(255) NOT NULL,
 PRIMARY KEY (id))";
 
-//faqvoting
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqvoting (
+//faqquestions
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqquestions (
 id int(11) unsigned NOT NULL,
-artikel int(11) unsigned NOT NULL,
-vote int(11) unsigned NOT NULL,
-usr int(11) unsigned NOT NULL,
-datum varchar(20) NOT NULL default '',
-ip varchar(15) NOT NULL default '',
+ask_username varchar(100) NOT NULL,
+ask_usermail varchar(100) NOT NULL,
+ask_rubrik varchar(100) NOT NULL,
+ask_content text NOT NULL,
+ask_date varchar(20) NOT NULL,
 PRIMARY KEY (id))";
+
+//faqright
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqright (
+right_id INT(11) UNSIGNED NOT NULL,
+name VARCHAR(50) NULL,
+description TEXT NULL,
+for_users INT(1) UNSIGNED NULL DEFAULT 1,
+for_groups INT(1) UNSIGNED NULL DEFAULT 1,
+PRIMARY KEY(right_id)
+)";
 
 //faqsessions
 $query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqsessions (
@@ -154,6 +179,51 @@ sid int(11) NOT NULL,
 ip text NOT NULL,
 time int(11) NOT NULL,
 PRIMARY KEY sid (sid))";
+
+//faquser
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquser (
+user_id INT(11) UNSIGNED NOT NULL,
+login VARCHAR(25) NOT NULL,
+session_id VARCHAR(150) NULL,
+session_timestamp INT(11) UNSIGNED NOT NULL,
+ip VARCHAR(15) NULL,
+account_status VARCHAR(50) NULL,
+last_login TIMESTAMP(14) NULL,
+auth_source VARCHAR(100) NULL,
+member_since TIMESTAMP(14) NULL,
+PRIMARY KEY(user_id),
+UNIQUE INDEX session(session_id),
+UNIQUE INDEX login(login)
+)";
+
+//faquserdata
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquserdata (
+user_id INT(11) UNSIGNED NOT NULL,
+last_modified TIMESTAMP(14) NULL,
+display_name VARCHAR(50) NULL,
+email VARCHAR(100) NULL
+)";
+
+//faquserlogin
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquserlogin (
+login VARCHAR(25) NOT NULL,
+pass VARCHAR(150) NULL,
+PRIMARY KEY(login)
+)";
+
+/faquser_group
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquser_group (
+user_id INT(11) UNSIGNED NOT NULL,
+group_id INT(11) UNSIGNED NOT NULL,
+PRIMARY KEY(user_id, group_id)
+)";
+
+//faquser_login
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquser_right (
+user_id INT(11) UNSIGNED NOT NULL,
+right_id INT(11) UNSIGNED NOT NULL,
+PRIMARY KEY(user_id, right_id)
+)";
 
 //faqvisits
 $query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqvisits (
@@ -163,86 +233,12 @@ visits int(11) NOT NULL,
 last_visit int(15) NOT NULL,
 PRIMARY KEY (id, lang))";
 
-// faqglossary
-$query[] = "CREATE TABLE ".$sqltblpre."faqglossary (
-id INT(11) NOT NULL ,
-lang VARCHAR(2) NOT NULL ,
-item VARCHAR(255) NOT NULL ,
-definition TEXT NOT NULL,
-PRIMARY KEY (id, lang))";
-
-//faquser
-/*$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquser (
-id int(2) NOT NULL,
-name text NOT NULL,
-pass varchar(64) BINARY NOT NULL,
-realname varchar(255) NOT NULL default '',
-email varchar(255) NOT NULL default '',
-rights varchar(255) NOT NULL,
+//faqvoting
+$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqvoting (
+id int(11) unsigned NOT NULL,
+artikel int(11) unsigned NOT NULL,
+vote int(11) unsigned NOT NULL,
+usr int(11) unsigned NOT NULL,
+datum varchar(20) NOT NULL default '',
+ip varchar(15) NOT NULL default '',
 PRIMARY KEY (id))";
-
-$query[] = "INSERT INTO ".$sqltblpre."faquser (id, name, pass, realname, email, rights) VALUES (1, 'admin', '".md5($password)."', '".$realname."', '".$email."', '1111111111111111111111111')";*/
-
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqgroup (
-  group_id INT(11) UNSIGNED NOT NULL,
-  name VARCHAR(25) NULL,
-  description TEXT NULL,
-  auto_join INT(1) UNSIGNED NULL,
-  PRIMARY KEY(group_id),
-  UNIQUE INDEX name(name)
-)";
-
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqgroup_right (
-  group_id INT(11) UNSIGNED NOT NULL,
-  right_id INT(11) UNSIGNED NOT NULL,
-  PRIMARY KEY(group_id, right_id)
-)";
-
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faqright (
-  right_id INT(11) UNSIGNED NOT NULL,
-  name VARCHAR(50) NULL,
-  description TEXT NULL,
-  for_users INT(1) UNSIGNED NULL DEFAULT 1,
-  for_groups INT(1) UNSIGNED NULL DEFAULT 1,
-  PRIMARY KEY(right_id)
-)";
-
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquser (
-  user_id INT(11) UNSIGNED NOT NULL,
-  login VARCHAR(25) NOT NULL,
-  session_id VARCHAR(150) NULL,
-  session_timestamp INT(11) UNSIGNED NOT NULL,
-  ip VARCHAR(15) NULL,
-  account_status VARCHAR(50) NULL,
-  last_login TIMESTAMP(14) NULL,
-  auth_source VARCHAR(100) NULL,
-  member_since TIMESTAMP(14) NULL,
-  PRIMARY KEY(user_id),
-  UNIQUE INDEX session(session_id),
-  UNIQUE INDEX login(login)
-)";
-
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquserdata (
-  user_id INT(11) UNSIGNED NOT NULL,
-  last_modified TIMESTAMP(14) NULL,
-  display_name VARCHAR(50) NULL,
-  email VARCHAR(100) NULL
-)";
-
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquserlogin (
-  login VARCHAR(25) NOT NULL,
-  pass VARCHAR(150) NULL,
-  PRIMARY KEY(login)
-)";
-
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquser_group (
-  user_id INT(11) UNSIGNED NOT NULL,
-  group_id INT(11) UNSIGNED NOT NULL,
-  PRIMARY KEY(user_id, group_id)
-)";
-
-$query[] = "CREATE TABLE IF NOT EXISTS ".$sqltblpre."faquser_right (
-  user_id INT(11) UNSIGNED NOT NULL,
-  right_id INT(11) UNSIGNED NOT NULL,
-  PRIMARY KEY(user_id, right_id)
-)";
