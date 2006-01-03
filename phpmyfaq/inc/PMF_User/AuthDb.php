08dfafb661dca846343f9e9c9a015efab2cdddff
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-01-03 15:09:03 +0000

    Code cleanup

--- phpmyfaq/inc/PMF_User/AuthDb.php
+++ phpmyfaq/inc/PMF_User/AuthDb.php
@@ -1,149 +1,115 @@
 <?php
-
-error_reporting(E_ALL);
-
 /**
- * manages user authentication with PostgreSQL databases.
- *
- * @author Lars Tiedemann, <php@larstiedemann.de>
- * @package PMF
- */
+* $Id: AuthDb.php,v 1.6 2006-01-03 15:09:03 thorstenr Exp $
+*
+* manages user authentication with databases.
+*
+* @author       Lars Tiedemann <php@larstiedemann.de>
+* @since        2005-09-30
+* @copyright    (c) 2005-2006 phpMyFAQ Team
+* 
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in
+* compliance with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+*/
 
-if (0 > version_compare(PHP_VERSION, '4')) {
-    die('This file was generated for PHP 4');
-}
-
-/**
- * manages user authentication. 
- *
- * Subclasses of Auth implement authentication functionality with different
- * types. The class AuthLdap for expamle provides authentication functionality
- * LDAP-database access, AuthMysql with MySQL-database access.
- *
- * Authentication functionality includes creation of a new login-and-password
- * deletion of an existing login-and-password combination and validation of
- * given by a user. These functions are provided by the database-specific
- * see documentation of the database-specific authentication classes AuthMysql,
- * or AuthLdap for further details.
- *
- * Passwords are usually encrypted before stored in a database. For
- * and security, a password encryption method may be chosen. See documentation
- * Enc class for further details.
- *
- * Instead of calling the database-specific subclasses directly, the static
- * selectDb(dbtype) may be called which returns a valid database-specific
- * object. See documentation of the static method selectDb for further details.
- *
- * @author Lars Tiedemann <php@larstiedemann.de>
- * @since 2005-09-30
- * @version 0.1
- */
-//require_once('PMF/Auth.php');
-
-/* user defined includes */
-// section -64--88-1-10-36edbf7a:106a832a030:-7fdb-includes begin
 require_once dirname(__FILE__).'/Auth.php';
-// section -64--88-1-10-36edbf7a:106a832a030:-7fdb-includes end
-
-/* user defined constants */
-// section -64--88-1-10-36edbf7a:106a832a030:-7fdb-constants begin
-// section -64--88-1-10-36edbf7a:106a832a030:-7fdb-constants end
 
 /**
- * manages user authentication with PostgreSQL databases.
- *
- * @access public
- * @author Lars Tiedemann, <php@larstiedemann.de>
- * @package PMF
- */
-class PMF_AuthDb
-    extends PMF_Auth
+* manages user authentication with databases.
+*
+* @access   public
+* @author   Lars Tiedemann, <php@larstiedemann.de>
+* @package  PMF_Auth
+*/
+class PMF_AuthDb extends PMF_Auth
 {
-    // --- ATTRIBUTES ---
-
     /**
-     * Short description of attribute db
-     *
-     * @access private
-     * @var mixed
-     */
+    * Short description of attribute db
+    *
+    * @access private
+    * @var mixed
+    */
     var $_db = null;
 
     /**
-     * Short description of attribute tablename
-     *
-     * @access private
-     * @var string
-     */
+    * Short description of attribute tablename
+    *
+    * @access private
+    * @var string
+    */
     var $_tablename = '';
 
     /**
-     * Short description of attribute login_column
-     *
-     * @access private
-     * @var string
-     */
+    * Short description of attribute login_column
+    *
+    * @access private
+    * @var string
+    */
     var $_login_column = '';
 
     /**
-     * Short description of attribute password_column
-     *
-     * @access private
-     * @var string
-     */
+    * Short description of attribute password_column
+    *
+    * @access private
+    * @var string
+    */
     var $_password_column = '';
 
-    // --- OPERATIONS ---
+
 
     /**
      * constructor
      *
-     * @access public
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @param bool
-     * @param bool
-     * @return void
+     * @access  public
+     * @author  Lars Tiedemann, <php@larstiedemann.de>
+     * @param   string
+     * @param   bool
+     * @return  void
      */
     function PMF_AuthDb($enctype = 'none', $read_only = false)
     {
-        // section -64--88-1-10-36edbf7a:106a832a030:-7fd8 begin
 		$this->selectEncType($enctype);
 		$this->read_only($read_only);
-        // section -64--88-1-10-36edbf7a:106a832a030:-7fd8 end
     }
 
     /**
      * destructor
      *
-     * @access public
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @return void
+     * @access  public
+     * @author  Lars Tiedemann, <php@larstiedemann.de>
+     * @return  void
      */
     function __destruct()
     {
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fdd begin
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fdd end
     }
 
     /**
-     * adds a new user account to the authentication table.
-     *
-     * Returns true on success, otherwise false. 
-     *
-     * Error messages are added to the array errors.
-     *
-     * @access public
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @param string
-     * @return void
-     */
+    * add()
+    *
+    * Adds a new user account to the authentication table.
+    *
+    * Returns true on success, otherwise false. 
+    *
+    * Error messages are added to the array errors.
+    *
+    * @access   public
+    * @author   Lars Tiedemann, <php@larstiedemann.de>
+    * @param    string
+    * @param    string
+    * @return   void
+    */
     function add($login, $pass)
     {
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fdb begin
-        if (!$this->_db()) 
+        if (!$this->_db()) {
         	return false;
+        }
         // check if $login already exists
         if ($this->checkLogin($login) > 0) {
         	$this->errors[] = PMF_USERERROR_ADD . PMF_USERERROR_LOGIN_NOT_UNIQUE;
@@ -168,27 +134,29 @@ class PMF_AuthDb
 			return false;
 		}
 		return true;
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fdb end
     }
 
     /**
-     * changes the password for the account specified by login. 
-     *
-     * Returns true on success, otherwise false. 
-     *
-     * Error messages are added to the array errors.
-     *
-     * @access public
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @param string
-     * @return void
-     */
+    * changePassword()
+    *
+    * changes the password for the account specified by login. 
+    *
+    * Returns true on success, otherwise false. 
+    *
+    * Error messages are added to the array errors.
+    *
+    * @access   public
+    * @author   Lars Tiedemann, <php@larstiedemann.de>
+    * @param    string
+    * @param    string
+    * @return   void
+    */
     function changePassword($login, $pass)
     {
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd9 begin
-        if (!$this->_db()) 
+
+        if (!$this->_db()) {
         	return false;
+        }
         $change = "
           UPDATE
             ".$this->_tablename()."
@@ -208,28 +176,27 @@ class PMF_AuthDb
 			return false;
 		}
 		return true;
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd9 end
     }
 
     /**
-     * deletes the user account specified by login. 
-     *
-     * Returns true on success, otherwise false. 
-     *
-     * Error messages are added to the array errors.
-     *
-     * @access public
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @return bool
-     */
+    * delete()
+    *
+    * Deletes the user account specified by login. 
+    *
+    * Returns true on success, otherwise false. 
+    *
+    * Error messages are added to the array errors.
+    *
+    * @access public
+    * @author Lars Tiedemann, <php@larstiedemann.de>
+    * @param string
+    * @return bool
+    */
     function delete($login)
     {
-        $returnValue = (bool) false;
-
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd7 begin
-        if (!$this->_db()) 
+        if (!$this->_db()) {
         	return false;
+        }
         $delete = "
           DELETE FROM
             ".$this->_tablename()."
@@ -247,29 +214,28 @@ class PMF_AuthDb
 			return false;
 		}
 		return true;
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd7 end
-
-        return (bool) $returnValue;
     }
 
     /**
-     * checks the password for the given user account. 
-     *
-     * Returns true if the given password for the user account specified by
-     * is correct, otherwise false.
-     * Error messages are added to the array errors.
-     *
-     * @access public
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @param string
-     * @return void
-     */
+    * checkPassword()
+    *
+    * checks the password for the given user account. 
+    *
+    * Returns true if the given password for the user account specified by
+    * is correct, otherwise false.
+    * Error messages are added to the array errors.
+    *
+    * @access public
+    * @author Lars Tiedemann, <php@larstiedemann.de>
+    * @param string
+    * @param string
+    * @return void
+    */
     function checkPassword($login, $pass)
     {
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd5 begin
-        if (!$this->_db()) 
+        if (!$this->_db()) {
         	return false;
+        }
         $check = "
           SELECT 
             ".$this->_login_column().",
@@ -303,48 +269,49 @@ class PMF_AuthDb
 		}
 		$this->errors[] = PMF_USERERROR_INCORRECT_PASSWORD;
 		return false;
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd5 end
     }
 
     /**
-     * establishes a connection to a MySQL server using the given parameters. 
-     *
-     * Selects the database and tries to connect to the specified MySQL server.
-     * the MySQL link resource on success, otherwise false.
-     *
-     * Error messages are added to the array errors.
-     *
-     * @access public
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param object
-     * @param string
-     * @param string
-     * @param string
-     * @return void
-     */
+    * connect()
+    *
+    * Establishes a connection to a MySQL server using the given parameters. 
+    *
+    * Selects the database and tries to connect to the specified MySQL server.
+    * the MySQL link resource on success, otherwise false.
+    *
+    * Error messages are added to the array errors.
+    *
+    * @access   public
+    * @author   Lars Tiedemann, <php@larstiedemann.de>
+    * @param    object
+    * @param    string
+    * @param    string
+    * @param    string
+    * @return   void
+    */
     function connect($db, $table = '', $login_column = '', $password_column = '')
     {
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd3 begin
         $this->_db($db);
         $this->_tablename($table);
         $this->_login_column($login_column);
         $this->_password_column($password_column);
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd3 end
     }
 
     /**
-     * Short description of method checkLogin
-     *
-     * @access public
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @return void
-     */
+    * checkLogin()
+    *
+    * Short description of method checkLogin
+    *
+    * @access   public
+    * @author   Lars Tiedemann, <php@larstiedemann.de>
+    * @param    string
+    * @return   void
+    */
     function checkLogin($login)
     {
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd1 begin
-        if (!$this->_db()) 
+        if (!$this->_db()) {
         	return false;
+        }
         $check = "
           SELECT 
             ".$this->_login_column."
@@ -357,137 +324,123 @@ class PMF_AuthDb
         $error = $this->_db->error();
 		if (strlen($error) > 0) {
 		    $this->errors[] = $error;
-			return (int) 0;
+			return 0;
 		} 
         return $this->_db->num_rows($check);
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fd1 end
     }
 
     /**
-     * sets or returns the table variable. 
-     *
-     * If this method is called without parameter, the object property table is
-     * If the property table is not set, an empty string is returned and an
-     * message is added to the array errors.
-     *
-     * If a string table is passed to this method, the object property table
-     * be updated. The previous value of the property table is returned.
-     *
-     * @access private
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @return string
-     */
+    * _tablename()
+    *
+    * sets or returns the table variable. 
+    *
+    * If this method is called without parameter, the object property table is
+    * If the property table is not set, an empty string is returned and an
+    * message is added to the array errors.
+    *
+    * If a string table is passed to this method, the object property table
+    * be updated. The previous value of the property table is returned.
+    *
+    * @access   private
+    * @author   Lars Tiedemann, <php@larstiedemann.de>
+    * @param    string
+    * @return   string
+    */
     function _tablename($table = '')
     {
-        $returnValue = (string) '';
-
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fc8 begin
         if ($table != '') { 
         	$old_table = $this->_tablename;
 		    $this->_tablename = $table;
 		    return $old_table;
         }
         if (!$this->_tablename) {
-        	$this->errors[] = PMF_UNDEFINED_PARAMETER."PMF_AuthMysql->_tablename";
+        	$this->errors[] = PMF_UNDEFINED_PARAMETER."PMF_AuthDb->_tablename";
         	$this->_tablename = '';
         }
-        return (string) $this->_tablename;
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fc8 end
-
-        return (string) $returnValue;
+        return $this->_tablename;
     }
 
     /**
-     * sets or returns the login_column variable. 
-     *
-     * If this method is called without parameter, the object property
-     * is returned. If the property login_column is not set, an empty string is
-     * and an error message is added to the array errors.
-     *
-     * If a string login_column is passed to this method, the object property
-     * will be updated. The previous value of the property login_column is
-     *
-     * @access private
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @return string
-     */
+    * _login_column()
+    *
+    * sets or returns the login_column variable. 
+    *
+    * If this method is called without parameter, the object property
+    * is returned. If the property login_column is not set, an empty string is
+    * and an error message is added to the array errors.
+    *
+    * If a string login_column is passed to this method, the object property
+    * will be updated. The previous value of the property login_column is
+    *
+    * @access   private
+    * @author   Lars Tiedemann, <php@larstiedemann.de>
+    * @param    string
+    * @return   string
+    */
     function _login_column($login_column = '')
     {
-        $returnValue = (string) '';
-
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fc4 begin
         if ($login_column != '') { 
         	$old_login_column = $this->_login_column;
 		    $this->_login_column = $login_column;
 		    return $old_login_column;
         }
         if (!$this->_login_column) {
-        	$this->errors[] = PMF_UNDEFINED_PARAMETER."PMF_AuthMysql->_login_column";
+        	$this->errors[] = PMF_UNDEFINED_PARAMETER."PMF_AuthDb->_login_column";
         	$this->_login_column = '';
         }
-        return (string) $this->_login_column;
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fc4 end
-
-        return (string) $returnValue;
+        return $this->_login_column;
     }
 
     /**
-     * sets or returns the password_column variable. 
-     *
-     * If this method is called without parameter, the object property
-     * is returned. If the property password_column is not set, an empty string
-     * returned and an error message is added to the array errors.
-     *
-     * If a string password_column is passed to this method, the object property
-     * will be updated. The previous value of the property password_column is
-     *
-     * @access private
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param string
-     * @return string
-     */
+    * _password_column()
+    *
+    * sets or returns the password_column variable. 
+    *
+    * If this method is called without parameter, the object property
+    * is returned. If the property password_column is not set, an empty string
+    * returned and an error message is added to the array errors.
+    *
+    * If a string password_column is passed to this method, the object property
+    * will be updated. The previous value of the property password_column is
+    *
+    * @access   private
+    * @author   Lars Tiedemann, <php@larstiedemann.de>
+    * @param    string
+    * @return   string
+    */
     function _password_column($password_column = '')
     {
-        $returnValue = (string) '';
-
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fc1 begin
         if ($password_column != '') { 
         	$old_password_column = $this->_password_column;
 		    $this->_password_column = $password_column;
 		    return $old_password_column;
         }
         if (!$this->_password_column) {
-        	$this->errors[] = PMF_UNDEFINED_PARAMETER."PMF_AuthMysql->_password_column";
+        	$this->errors[] = PMF_UNDEFINED_PARAMETER."PMF_AuthDb->_password_column";
         	$this->_password_column = '';
         }
         return (string) $this->_password_column;
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fc1 end
-
-        return (string) $returnValue;
     }
 
     /**
-     * sets or returns the db variable.
-     *
-     * If this method is called without parameter, the database object db is
-     * If db is invalid, false is returned and an error message is added to the
-     * errors.
-     *
-     * If a valid database object is passed to this method, db will be updated.
-     * previous value of db is returned.
-     *
-     * @access private
-     * @author Lars Tiedemann, <php@larstiedemann.de>
-     * @param object
-     * @return object
-     */
+    * _db()
+    *
+    * sets or returns the db variable.
+    *
+    * If this method is called without parameter, the database object db is
+    * If db is invalid, false is returned and an error message is added to the
+    * errors.
+    *
+    * If a valid database object is passed to this method, db will be updated.
+    * previous value of db is returned.
+    *
+    * @access   private
+    * @author   Lars Tiedemann, <php@larstiedemann.de>
+    * @param    object
+    * @return   object
+    */
     function _db($db = null)
     {
-        $returnValue = null;
-
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fba begin
         if ($db !== null) {
         	$old_db = $this->_db;
         	if (!PMF_User::checkDb($db)) {
@@ -503,9 +456,6 @@ class PMF_AuthDb
         	return false;
         }
         return $this->_db;
-        // section -64--88-1-10--64a6e3e6:106b6331e8d:-7fba end
-
-        return $returnValue;
     }
 
 } /* end of class PMF_AuthDb */
