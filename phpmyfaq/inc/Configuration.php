a2eeda6e11abe0d0658b9bcf00e45f6972b66ebc
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-01-04 14:27:49 +0000

    Added configuration class

---
+++ phpmyfaq/inc/Configuration.php
@@ -0,0 +1,115 @@
+<?php
+/**
+* $Id: Configuration.php,v 1.1 2006-01-04 14:27:49 thorstenr Exp $
+*
+* The main class for fetching the configuration, update and delete items.
+*
+* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
+* @package      phpMyFAQ
+* @since        2006-01-04
+* @copyright    (c) 2006 phpMyFAQ Team
+* 
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in
+* compliance with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+*/
+
+class PMF_Configuration
+{
+    /**
+    * DB handle
+    *
+    * @var  object
+    */
+    var $db;
+    
+    /**
+    * Configuration array
+    *
+    * @var  array
+    */
+    var $config = array();
+    
+    /**
+    * Constructor
+    *
+    * @param    object
+    * @return   void
+    * @author   Thorsten Rinne <thorsten@phpmyfaq.de>
+    */
+    function PMF_Configuration($db)
+    {
+        $this->db = $db;
+    }
+    
+    /**
+    * getAll()
+    *
+    * Fetches all configuration items into an array
+    *
+    * @return   void
+    * @access   public
+    * @author   Thorsten Rinne <thorsten@phpmyfaq.de>
+    */
+    function getAll()
+    {
+        $result = $this->db->query(sprintf("SELECT config_name, config_value FROM %sfaqconfig", SQLPREFIX));
+        while ($row = $this->db->fetch_object($result)) {
+            $this->config[$row->config_name] = $row->config_value;
+        }
+    } // end func fetchAll()
+    
+    /**
+    * get()
+    *
+    * Returns a configuration item
+    *
+    * @param    string
+    * @return   mixed
+    * @access   public
+    * @author   Thorsten Rinne <thorsten@phpmyfaq.de>
+    */
+    function get($item)
+    {
+        if (!isset($this->config[$item])) {
+            $this->getAll();
+        }
+        return $this->config[$item];
+    } // end func get()
+    
+    /**
+    * update()
+    *
+    * Updates all configuration items
+    *
+    * @param    mixed
+    * @return   bool
+    * @access   public
+    * @author   Thorsten Rinne <thorsten@phpmyfaq.de>
+    */
+    function update($newconfig)
+    {
+        if (is_array($newconfig)) {
+            foreach ($newconfig as $name => $value) {
+                $query = sprintf("
+                    UPDATE
+                        %sfaqconfig
+                    SET
+                        config_value = '%s'
+                    WHERE
+                        config_name = '%s'",
+                    SQLPREFIX, $value, $name);
+                $this->db->query($query);
+            }
+            return true;
+        }
+        return false;
+    } // end func update()
+    
+}
