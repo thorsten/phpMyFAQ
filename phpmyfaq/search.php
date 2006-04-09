846f9fd098c37e6df93677d571d2695da26d5e4d
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-04-09 12:14:35 +0000

    MFB: Added missing features

--- phpmyfaq/search.php
+++ phpmyfaq/search.php
@@ -1,10 +1,11 @@
 <?php
 /**
-* $Id: search.php,v 1.7 2006-01-02 16:51:26 thorstenr Exp $
+* $Id: search.php,v 1.8 2006-04-09 12:14:35 thorstenr Exp $
 *
 * The fulltext search page
 *
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
+* @author       Periklis Tsirakidis <tsirakidis@phpdevel.de>
 * @since        2002-09-16
 * @copyright    (c) 2001-2006 phpMyFAQ Team
 * 
@@ -26,25 +27,54 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
 
 Tracking("fulltext_search",0);
 
-if (isset($_POST["suchbegriff"]) || isset($_GET["search"])) {
-	if (isset($_POST["suchbegriff"])) {
-		$suchbegriff = safeSQL(strip_tags($_POST["suchbegriff"]));
+// HACK: (re)evaluate the Category object w/o passing the user language
+//       so the result set of a Search will have the Category Path
+//       for any of the multilanguage faq records and the Category list
+//       on the left pane will not be affected
+$tree = new Category();
+$tree->transform(0);
+
+if (isset($_POST['suchbegriff']) || isset($_GET['search'])) {
+	if (isset($_POST['suchbegriff'])) {
+		$suchbegriff = $db->escape_string(strip_tags($_POST['suchbegriff']));
+		$searchcategory = isset($_POST['searchcategory']) ? $db->escape_string(strip_tags($_POST['searchcategory'])) : '%';
 	}
-	if (isset($_GET["search"])) {
-		$suchbegriff = safeSQL(strip_tags($_GET["search"]));
+	if (isset($_GET['search'])) {
+		$suchbegriff = $db->escape_string(strip_tags(urldecode($_GET['search'])));
+		$searchcategory = isset($_POST['searchcategory']) ? $db->escape_string(strip_tags($_POST['searchcategory'])) : '%';
 	}
-	$printResult = searchEngine($suchbegriff);
+	$printResult = searchEngine($suchbegriff, $searchcategory);
 } else {
-	$printResult = $PMF_LANG["help_search"];
-    $suchbegriff = "";
+	$printResult = $PMF_LANG['help_search'];
+    $suchbegriff = '';
 }
 
-$tpl->processTemplate ("writeContent", array(
-				"msgSearch" => $PMF_LANG["msgSearch"],
-                "searchString" => $suchbegriff,
-				"writeSendAdress" => $_SERVER["PHP_SELF"]."?".$sids."action=search",
-				"msgSearchWord" => $PMF_LANG["msgSearchWord"],
-				"printResult" => $printResult
-				));
+$tree->buildTree();
 
-$tpl->includeTemplate("writeContent", "index");
+if (!file_exists(dirname(__FILE__).'/'.$_SERVER['SERVER_NAME'].'.pmfsearch.src')) {
+    $tpl->processTemplate('writeContent', array(
+    				      'msgSearch' => $PMF_LANG['msgSearch'],
+                          'searchString' => $suchbegriff,
+                          'selectCategories' => $PMF_LANG['msgSelectCategories'],
+                          'allCategories' => $PMF_LANG['msgAllCategories'],
+                          'printCategoryOptions' => $tree->printCategoryOptions(0),
+    				      'writeSendAdress' => $_SERVER['PHP_SELF'].'?'.$sids.'action=search',
+    				      'msgSearchWord' => $PMF_LANG['msgSearchWord'],
+    				      'printResult' => $printResult,
+                          'msgFirefoxPluginTitle' => ''
+    				));
+} else {
+    $tpl->processTemplate('writeContent', array(
+    				      'msgSearch' => $PMF_LANG['msgSearch'],
+                          'searchString' => $suchbegriff,
+                          'selectCategories' => $PMF_LANG['msgSelectCategories'],
+                          'allCategories' => $PMF_LANG['msgAllCategories'],
+                          'printCategoryOptions' => $tree->printCategoryOptions(0),
+    				      'writeSendAdress' => $_SERVER['PHP_SELF'].'?'.$sids.'action=search',
+    				      'msgSearchWord' => $PMF_LANG['msgSearchWord'],
+    				      'printResult' => $printResult,
+                          'msgFirefoxPluginTitle' => '<p><a id="searchplugin" href="javascript:addEngine(\'http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']).'\', \''.$_SERVER['SERVER_NAME'].'.pmfsearch\', \'png\', \'Web\')">'.$PMF_LANG['ad_search_plugin_install'].'</a></p>'
+    				));
+}
+
+$tpl->includeTemplate('writeContent', 'index');
