bd3327843a03bc1cb75f52b28d75128fd141cf1e
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-06-01 09:22:44 +0000

    Code cleanup

--- phpmyfaq/main.php
+++ phpmyfaq/main.php
@@ -1,17 +1,19 @@
 <?php
 /**
-* $Id: main.php,v 1.9 2006-01-02 16:51:26 thorstenr Exp $
+* $Id: main.php,v 1.10 2006-06-01 09:22:44 thorstenr Exp $
+*
+* The main start page with the Top10 and the latest messages
 *
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
 * @since        2002-08-23
 * @copyright    (c) 2001-2006 phpMyFAQ Team
-* 
+*
 * The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the 'License'); you may not use this file except in
+* Version 1.1 (the "License""); you may not use this file except in
 * compliance with the License. You may obtain a copy of the License at
 * http://www.mozilla.org/MPL/
-* 
-* Software distributed under the License is distributed on an 'AS IS'
+*
+* Software distributed under the License is distributed on an "AS IS"
 * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 * License for the specific language governing rights and limitations
 * under the License.
@@ -23,12 +25,12 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
 }
 
 $tpl->processTemplate ('writeContent', array(
-                       'writeNewsHeader' => $PMF_CONF['title'].$PMF_LANG['msgNews'],
-                       'writeNews' => generateNews($LANGCODE),
-                       'writeNumberOfArticles' => $PMF_LANG['msgHomeThereAre'].$faq->getNumberOfRecords($LANGCODE).$PMF_LANG['msgHomeArticlesOnline'],
-                       'writeTopTenHeader' => $PMF_LANG['msgTopTen'],
-                       'writeTopTenRow' => $faq->getTopTen(),
-                       'writeNewestHeader' => $PMF_LANG['msgLatestArticles'],
-                       'writeNewestRow' => $faq->getFiveLatest()));
+    'writeNewsHeader' 		=> $PMF_CONF['title'].$PMF_LANG['msgNews'],
+    'writeNews' 			=> generateNews($LANGCODE),
+    'writeNumberOfArticles' => $PMF_LANG['msgHomeThereAre'].$faq->getNumberOfRecords($LANGCODE).$PMF_LANG['msgHomeArticlesOnline'],
+    'writeTopTenHeader' 	=> $PMF_LANG['msgTopTen'],
+    'writeTopTenRow' 		=> $faq->getTopTen(),
+    'writeNewestHeader' 	=> $PMF_LANG['msgLatestArticles'],
+    'writeNewestRow' 		=> $faq->getFiveLatest()));
 
 $tpl->includeTemplate('writeContent', 'index');
