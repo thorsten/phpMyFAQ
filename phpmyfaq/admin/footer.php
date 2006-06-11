3e635c01e1cb932485acdfc12eeeed895ef3928e
matteo <matteo@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-06-11 15:09:36 +0000

    MFB: (trivial) Copyright alignment

--- phpmyfaq/admin/footer.php
+++ phpmyfaq/admin/footer.php
@@ -1,18 +1,18 @@
 <?php
 /**
-* $Id: footer.php,v 1.5 2006-01-02 16:51:26 thorstenr Exp $
+* $Id: footer.php,v 1.6 2006-06-11 15:09:36 matteo Exp $
 *
 * Footer of the admin area
 *
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
 * @since        2003-02-26
 * @copyright    (c) 2001-2006 phpMyFAQ Team
-* 
+*
 * The contents of this file are subject to the Mozilla Public License
 * Version 1.1 (the "License"); you may not use this file except in
 * compliance with the License. You may obtain a copy of the License at
 * http://www.mozilla.org/MPL/
-* 
+*
 * Software distributed under the License is distributed on an "AS IS"
 * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 * License for the specific language governing rights and limitations
@@ -25,11 +25,11 @@ if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
 }
 ?>
 </div>
-    
+
 <!-- Footer -->
 <div>
     <div id="footer">
-        <div><a href="http://www.phpmyfaq.de/" target="_blank">phpMyFAQ</a> <?php print $PMF_CONF["version"]; ?> | &copy; 2001-2005 <a href="http://www.phpmyfaq.de/impressum.php" target="_blank">phpMyFAQ Team</a></div>
+        <div id="copyright"><strong>phpMyFAQ <?php print $PMF_CONF["version"]; ?></strong> | &copy; 2001-2006 <a href="http://www.phpmyfaq.de/" target="_blank">phpMyFAQ Team</a></div>
     </div>
 </div>
 
