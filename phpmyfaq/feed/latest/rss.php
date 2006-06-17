f871be329924e518401d842286a811939caff418
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-06-17 22:21:09 +0000

    Use the FAQ class methods

--- phpmyfaq/feed/latest/rss.php
+++ phpmyfaq/feed/latest/rss.php
@@ -1,19 +1,19 @@
 <?php
 /**
-* $Id: rss.php,v 1.11 2006-06-17 13:01:25 matteo Exp $
+* $Id: rss.php,v 1.12 2006-06-17 22:21:09 thorstenr Exp $
 *
 * The RSS feed with the latest five records
 *
 * @package      phpMyFAQ
 * @access       public
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @copyright    (c) 2004 - 2005 phpMyFAQ Team
+* @copyright    (c) 2004 - 2006 phpMyFAQ Team
 *
 * The contents of this file are subject to the Mozilla Public License
 * Version 1.1 (the "License"); you may not use this file except in
 * compliance with the License. You may obtain a copy of the License at
 * http://www.mozilla.org/MPL/
-* 
+*
 * Software distributed under the License is distributed on an "AS IS"
 * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 * License for the specific language governing rights and limitations
@@ -24,29 +24,43 @@ define('PMF_ROOT_DIR', dirname(dirname(dirname(__FILE__))));
 require_once(PMF_ROOT_DIR.'/inc/Init.php');
 PMF_Init::cleanRequest();
 
-require_once (PMF_ROOT_DIR."/lang/".$PMF_CONF["language"]);
+//
+// get language (default: english)
+//
+$pmf = new PMF_Init();
+$LANGCODE = $pmf->setLanguage((isset($PMF_CONF['detection']) ? true : false), $PMF_CONF['language']);
+// Preload English strings
+require_once ('lang/language_en.php');
 
-$query = 'SELECT DISTINCT '.SQLPREFIX.'faqdata.id, '.SQLPREFIX.'faqdata.lang, '.SQLPREFIX.'faqcategoryrelations.category_id, '.SQLPREFIX.'faqdata.thema, '.SQLPREFIX.'faqdata.datum, '.SQLPREFIX.'faqvisits.visits FROM '.SQLPREFIX.'faqvisits, '.SQLPREFIX.'faqdata LEFT JOIN '.SQLPREFIX.'faqcategoryrelations ON '.SQLPREFIX.'faqdata.id = '.SQLPREFIX.'faqcategoryrelations.record_id AND '.SQLPREFIX.'faqdata.lang = '.SQLPREFIX.'faqcategoryrelations.record_lang WHERE '.SQLPREFIX.'faqdata.id = '.SQLPREFIX.'faqvisits.id AND '.SQLPREFIX.'faqdata.lang = '.SQLPREFIX.'faqvisits.lang AND '.SQLPREFIX.'faqdata.active = \'yes\' ORDER BY '.SQLPREFIX.'faqdata.datum DESC';
-$result = $db->query($query);
+if (isset($LANGCODE) && PMF_Init::isASupportedLanguage($LANGCODE)) {
+    // Overwrite English strings with the ones we have in the current language
+    require_once('lang/language_'.$LANGCODE.'.php');
+} else {
+    $LANGCODE = 'en';
+}
 
-$rss = "<?xml version=\"1.0\" encoding=\"".$PMF_LANG["metaCharset"]."\" standalone=\"yes\" ?>\n<rss version=\"2.0\">\n<channel>\n";
-$rss .= "<title>".$PMF_CONF["title"]."</title>\n";
-$rss .= "<description>".$PMF_CONF["metaDescription"]."</description>\n";
-$rss .= "<link>http://".$_SERVER["HTTP_HOST"]."</link>";
-if ($num = $db->num_rows($result) > 0) {
-    
-    $counter = 0;
-    while ( ($row = $db->fetch_object($result)) && $counter < 5) {
-        $counter++;
-        
-        $rss .= "\t<item>\n";
-        $rss .= "\t\t<title><![CDATA[ ".stripslashes(htmlspecialchars(makeShorterText($row->thema, 8)))." ... ]]></title>\n";
-        $rss .= "\t\t<description><![CDATA[ ".stripslashes(htmlspecialchars(makeShorterText($row->thema, 8)))." (".$row->visits." ".$PMF_LANG["msgViews"].") ]]></description>\n";
-        $rss .= "\t\t<link>http://".$_SERVER["HTTP_HOST"].str_replace("feed/latest/rss.php", "index.php", $_SERVER["PHP_SELF"])."?action=artikel&amp;cat=".$row->category_id."&amp;id=".$row->id."&amp;artlang=".$row->lang."</link>\n";
-        $rss .= "\t</item>\n";
-        
+$faq = new FAQ($db, $LANGCODE);
+$rssData = $faq->getLatestData();
+$num = count($rssData);
+
+$rss =
+	"<?xml version=\"1.0\" encoding=\"".$PMF_LANG["metaCharset"]."\" standalone=\"yes\" ?>\n" .
+	"<rss version=\"2.0\">\n<channel>\n" .
+	"<title>" . $PMF_CONF["title"] . "</title>\n" .
+	"<description>" . $PMF_CONF["metaDescription"] . "</description>\n" .
+	"<link>http://" . $_SERVER["HTTP_HOST"]."</link>";
+if ($num > 0) {
+	foreach ($rssData as $rssKey => $rssValue) {
+        $rss .= "\t<item>\n" .
+        	"\t\t<title><![CDATA[ " .
+        	stripslashes(htmlspecialchars(makeShorterText($row['thema'], 8))) .
+			" ... ]]></title>\n" .
+			"\t\t<description><![CDATA[ " .
+        	stripslashes(htmlspecialchars(makeShorterText($row['thema'], 8))) .
+			" (".$row['visits']." ".$PMF_LANG["msgViews"].") ]]></description>\n" .
+			"\t\t<link>".$row['url']."</link>\n" .
+			"\t</item>\n";
     }
-    
 }
 
 $rss .= "</channel>\n</rss>";
@@ -56,4 +70,3 @@ header("Content-Length: ".strlen($rss));
 print $rss;
 
 $db->dbclose();
-?>
