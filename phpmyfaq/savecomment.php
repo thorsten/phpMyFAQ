94f8f3a85a07620e66992bdd210d53c81e0a49bc
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-06-18 09:43:04 +0000

    Using FAQ::addComment instead of inner queries

--- phpmyfaq/savecomment.php
+++ phpmyfaq/savecomment.php
@@ -1,18 +1,18 @@
 <?php
 /**
-* $Id: savecomment.php,v 1.12 2006-04-09 12:07:16 thorstenr Exp $
+* $Id: savecomment.php,v 1.13 2006-06-18 09:43:04 thorstenr Exp $
 *
 * Saves the posted comment
 *
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
 * @since        2002-08-29
 * @copyright    (c) 2001-2006 phpMyFAQ Team
-* 
+*
 * The contents of this file are subject to the Mozilla Public License
 * Version 1.1 (the "License"); you may not use this file except in
 * compliance with the License. You may obtain a copy of the License at
 * http://www.mozilla.org/MPL/
-* 
+*
 * Software distributed under the License is distributed on an "AS IS"
 * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 * License for the specific language governing rights and limitations
@@ -35,14 +35,15 @@ if (    isset($_POST['user']) && $_POST['user'] != ''
 
     $id = (isset($_POST["id"])) ? (int)$_POST["id"] : 0;
     Tracking("save_comment", $id);
-	
-	$helped = ""; // not used in this version - maybe in the future
-	$datum = date("YmdHis");
-    $comment = nl2br($db->escape_string(safeHTML($_POST["comment"])));
-    $comment_by_user = $db->escape_string(safeHTML($_POST["user"]));
-    $comment_by_mail = $db->escape_string(safeHTML($_POST["mail"]));
-	
-    $result = $db->query("INSERT INTO ".SQLPREFIX."faqcomments (id_comment, id, usr, email, comment, datum, helped) VALUES (".$db->nextID(SQLPREFIX."faqcomments", "id_comment").", ".$id.", '".$comment_by_user."', '".$comment_by_mail."', '".$comment."', ".$datum.", '".$helped."')");
+
+	$commentData = array(
+		'record_id'	=> $id,
+		'username'	=> $db->escape_string(safeHTML($_POST["user"])),
+		'usermail' 	=> $db->escape_string(safeHTML($_POST["mail"])),
+		'comment'	=> nl2br($db->escape_string(safeHTML($_POST["comment"]))),
+		'date'		=> date('YmdHis'),
+		'helped'	=> '');
+	$faq->addComment($commentData);
 
     $tpl->processTemplate ("writeContent", array(
     "msgCommentHeader" => $PMF_LANG["msgWriteComment"],
