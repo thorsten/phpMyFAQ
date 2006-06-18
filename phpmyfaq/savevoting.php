c57c979a1e316900458930579cf675fcec53cbdd
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-06-18 10:51:43 +0000

    Using PMF_Faq methods instead of inner queries

--- phpmyfaq/savevoting.php
+++ phpmyfaq/savevoting.php
@@ -1,18 +1,18 @@
 <?php
 /**
-* $Id: savevoting.php,v 1.15 2006-04-09 12:12:47 thorstenr Exp $
+* $Id: savevoting.php,v 1.16 2006-06-18 10:51:43 thorstenr Exp $
 *
 * Saves a user voting
 *
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
 * @since        2002-09-16
 * @copyright    (c) 2001-2006 phpMyFAQ Team
-* 
+*
 * The contents of this file are subject to the Mozilla Public License
 * Version 1.1 (the "License"); you may not use this file except in
 * compliance with the License. You may obtain a copy of the License at
 * http://www.mozilla.org/MPL/
-* 
+*
 * Software distributed under the License is distributed on an "AS IS"
 * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 * License for the specific language governing rights and limitations
@@ -24,46 +24,37 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
     exit();
 }
 
-$record = (isset($_POST["artikel"])) ? intval($_POST["artikel"]) : '';
-$vote = (isset($_POST["vote"])) ? intval($_POST["vote"]) : 0;
-$userip = (isset($_POST["userip"])) ? strip_tags($_POST["userip"]) : '';
+$record_id   = (isset($_POST['artikel'])) ? intval($_POST['artikel']) : '';
+$voting      = (isset($_POST['vote'])) ? intval($_POST['vote']) : 0;
+$user_ip     = (isset($_POST['userip'])) ? strip_tags($_POST['userip']) : '';
 
-if (isset($vote) && $vote != "" && votingCheck($record, $userip) && intval($_POST["vote"]) > 0 && intval($_POST["vote"]) < 6) {
-    
-	$noUser = "0";
-	$datum = date("YmdHis");
-	Tracking("save_voting", $record);
-    
-	if ($result = $db->query("SELECT usr FROM ".SQLPREFIX."faqvoting WHERE artikel = ".$record)) {
-		while ($row = $db->fetch_object($result)) {
-			$noUser = $row->usr;
-		}
-	}
-    
-	if ($noUser == "0" || $noUser == "") {
-		$db->query("INSERT INTO ".SQLPREFIX."faqvoting (id, artikel, vote, usr, datum, ip) VALUES (".$db->nextID(SQLPREFIX."faqvoting", "id").", ".$record.", ".$vote.", '1', ".time().", '".$userip."');");
-	}  else {
-		$db->query("UPDATE ".SQLPREFIX."faqvoting SET vote = vote + ".$vote.", usr = usr + 1, datum = ".time().", ip = '".$userip."' where artikel = ".$record);
-	}
-    
-	$tpl->processTemplate ("writeContent", array(
-				"msgVoteThanks" => $PMF_LANG["msgVoteThanks"]
-				));
-    
-} elseif (isset($_POST["vote"])  && !votingCheck($record, $userip)) {
-    
-    Tracking("error_save_voting", $record);
-	$tpl->processTemplate ("writeContent", array(
-				"msgVoteThanks" => $PMF_LANG["err_VoteTooMuch"]
-				));
-    
+if (isset($voting) && votingCheck($record_id, $user_ip) && $voting > 0 && $voting < 6) {
+    Tracking('save_voting', $record);
+
+    $votingData = array(
+        'record_id'  => $record_id,
+        'vote'       => $voting,
+        'user_ip'    => $user_ip);
+
+    if ($faq->getNumberOfVotings($record)) {
+        $faq->addVoting($votingData);
+    }  else {
+        $faq->updateVoting($votingData);
+    }
+
+    $tpl->processTemplate ('writeContent', array(
+        'msgVoteThanks' => $PMF_LANG['msgVoteThanks']));
+
+} elseif (isset($voting) && !votingCheck($record, $userip)) {
+    Tracking('error_save_voting', $record);
+    $tpl->processTemplate('writeContent', array(
+        'msgVoteThanks' => $PMF_LANG['err_VoteTooMuch']));
+
 } else {
-    
-	Tracking("error_save_voting", $record);
-	$tpl->processTemplate ("writeContent", array(
-				"msgVoteThanks" => $PMF_LANG["err_noVote"]
-				));
-    
+    Tracking('error_save_voting', $record);
+    $tpl->processTemplate ('writeContent', array(
+        'msgVoteThanks' => $PMF_LANG['err_noVote']));
+
 }
 
-$tpl->includeTemplate("writeContent", "index");
+$tpl->includeTemplate('writeContent', 'index');
