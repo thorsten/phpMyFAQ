cca5d7115f8776c6162c6a2923a0c74898979c2a
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-07-01 15:16:45 +0000

    Added standard TinyMCE call, need configuration

--- phpmyfaq/admin/footer.php
+++ phpmyfaq/admin/footer.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* $Id: footer.php,v 1.8 2006-06-19 18:54:33 matteo Exp $
+* $Id: footer.php,v 1.9 2006-07-01 15:16:45 thorstenr Exp $
 *
 * Footer of the admin area
 *
@@ -36,6 +36,60 @@ if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
 </div>
 
 <iframe id="keepPMFSessionAlive" src="session.keepalive.php<?php print $linkext; ?>&amp;lang=<?php print $LANGCODE; ?>" frameBorder="no" width="0" height="0"></iframe>
+
+<script language="javascript" type="text/javascript" src="editor/tiny_mce.js"></script>
+<script language="javascript" type="text/javascript">
+    tinyMCE.init({
+        mode : "textareas",
+        theme : "advanced",
+        plugins : "table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,zoom,flash,searchreplace,print,paste,directionality,fullscreen,noneditable,contextmenu",
+        theme_advanced_buttons1_add_before : "save,newdocument,separator",
+        theme_advanced_buttons1_add : "fontselect,fontsizeselect",
+        theme_advanced_buttons2_add : "separator,insertdate,inserttime,preview,zoom,separator,forecolor,backcolor,liststyle",
+        theme_advanced_buttons2_add_before: "cut,copy,paste,pastetext,pasteword,separator,search,replace,separator",
+        theme_advanced_buttons3_add_before : "tablecontrols,separator",
+        theme_advanced_buttons3_add : "emotions,iespell,flash,advhr,separator,print,separator,ltr,rtl,separator,fullscreen",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        plugin_insertdate_dateFormat : "%Y-%m-%d",
+        plugin_insertdate_timeFormat : "%H:%M:%S",
+        extended_valid_elements : "hr[class|width|size|noshade]",
+        file_browser_callback : "fileBrowserCallBack",
+        paste_use_dialog : false,
+        theme_advanced_resizing : true,
+        theme_advanced_resize_horizontal : false,
+        theme_advanced_link_targets : "_something=My somthing;_something2=My somthing2;_something3=My somthing3;",
+        apply_source_formatting : true
+    });
+
+    function fileBrowserCallBack(field_name, url, type, win) {
+        var connector = "../../filemanager/browser.html?Connector=connectors/php/connector.php";
+        var enableAutoTypeSelection = true;
+
+        var cType;
+        tinyfck_field = field_name;
+        tinyfck = win;
+
+        switch (type) {
+            case "image":
+                cType = "Image";
+                break;
+            case "flash":
+                cType = "Flash";
+                break;
+            case "file":
+                cType = "File";
+                break;
+        }
+
+        if (enableAutoTypeSelection && cType) {
+            connector += "&Type=" + cType;
+        }
+
+        window.open(connector, "tinyfck", "modal,width=600,height=400");
+    }
+</script>
 
 </body>
 </html>
