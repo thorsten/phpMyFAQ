1ec967bfaf768a566bd265252f15c03c5a6b82cc
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-07-02 09:49:49 +0000

    Using the new PMF_Faq methods

--- phpmyfaq/admin/record.add.php
+++ phpmyfaq/admin/record.add.php
@@ -1,18 +1,18 @@
 <?php
 /**
-* $Id: record.add.php,v 1.29 2006-01-17 19:43:04 thorstenr Exp $
+* $Id: record.add.php,v 1.30 2006-07-02 09:49:49 thorstenr Exp $
 *
 * Adds a record in the database
 *
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
 * @since        2003-02-23
 * @copyright    (c) 2001-2006 phpMyFAQ Team
-* 
+*
 * The contents of this file are subject to the Mozilla Public License
 * Version 1.1 (the "License"); you may not use this file except in
 * compliance with the License. You may obtain a copy of the License at
 * http://www.mozilla.org/MPL/
-* 
+*
 * Software distributed under the License is distributed on an "AS IS"
 * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 * License for the specific language governing rights and limitations
@@ -25,45 +25,43 @@ if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
 }
 
 if ($permission["editbt"]) {
-	$submit = $_REQUEST["submit"];
-	
-	if (isset($submit[1]) && isset($_REQUEST["thema"]) && $_REQUEST["thema"] != "" && isset($_REQUEST['rubrik']) && is_array($_REQUEST['rubrik'])) {
-		// new entry
-		adminlog("Beitragcreatesave");
-        print "<h2>".$PMF_LANG["ad_entry_aor"]."</h2>\n";
-        $lang = $_REQUEST["language"];
-		$thema = $db->escape_string($_REQUEST["thema"]);
-		$content = $db->escape_string($_REQUEST["content"]);
-		$keywords = $db->escape_string($_REQUEST["keywords"]);
-		$author = $db->escape_string($_REQUEST["author"]);
-        if (isset($_REQUEST["comment"])) {
-            $comment = $db->escape_string($_REQUEST["comment"]);
+    $submit = $_REQUEST["submit"];
+
+    if (isset($submit[1]) && isset($_REQUEST["thema"]) && $_REQUEST["thema"] != "" && isset($_REQUEST['rubrik']) && is_array($_REQUEST['rubrik'])) {
+        // new entry
+        adminlog("Beitragcreatesave");
+        printf("<h2>%s</h2>\n", $PMF_LANG['ad_entry_aor']);
+
+        $recordData = array(
+            'lang'      => $db->escape_string($_POST['language']),
+            'active'    => $db->escape_string($_POST['active']),
+            'thema'     => $db->escape_string($_POST['thema']),
+            'content'   => $db->escape_string($_POST['content']),
+            'keywords'  => $db->escape_string($_POST['keywords']),
+            'author'    => $db->escape_string($_POST['author']),
+            'email'     => $db->escape_string($_POST['active']),
+            'comment'   => (isset($_POST['comment']) ? 'y' : 'n'),
+            'date'      => date('YmdHis')
+        );
+
+        $rubrik = $_POST['rubrik'];
+
+        // Add new record and get that ID
+        $nextID = $faq->addRecord($recordData);
+        if ($nextID) {
+            print $PMF_LANG["ad_entry_savedsuc"];
+            link_ondemand_javascript($nextID, $lang);
         } else {
-			$comment = "n";
-	    }
-		$datum = date("YmdHis");
-        $rubrik = $_REQUEST['rubrik'];
-		
-        $nextID = $db->nextID(SQLPREFIX."faqdata", "id");
-        
-        $result_record = $db->query("INSERT INTO ".SQLPREFIX."faqdata (id, lang, thema, content, keywords, author, active, datum, email, comment) VALUES (".$nextID.", '".$lang."', '".$thema."', '".$content."', '".$keywords."', '".$author."', '".$_REQUEST["active"]."', '".$datum."', '".$_REQUEST["email"]."', '".$comment."')");
-        
-        $result_visits = $db->query("INSERT INTO ".SQLPREFIX."faqvisits (id, lang, visits, last_visit) VALUES (".$nextID.", '".$lang."', 1, ".time().")");
-    	
-        if ($result_record) {
-    		print $PMF_LANG["ad_entry_savedsuc"];
-			link_ondemand_javascript($nextID, $lang);
-        } else {
-    		print $PMF_LANG["ad_entry_savedfail"].$db->error();
+            print $PMF_LANG["ad_entry_savedfail"].$db->error();
         }
-        
-        // insert the new category relations
-        foreach ($rubrik as $categories) {
-            $db->query('INSERT INTO '.SQLPREFIX.'faqcategoryrelations VALUES ('.$categories.', \''.$_REQUEST["language"].'\', '.$nextID.', \''.$_REQUEST["language"].'\')');
-        }
-	} elseif (isset($submit[2]) && isset($_REQUEST["thema"]) && $_REQUEST["thema"] != "" && isset($_REQUEST['rubrik']) && is_array($_REQUEST['rubrik'])) {
-		// Preview
-	    $rubrik = $_REQUEST["rubrik"];
+        // Create the visit entry
+        $faq->createNewVisit($newID, $recordData['lang']);
+        // Insert the new category relations
+        $faq->addCategoryRelation($categories, $newID, $recordData['lang']);
+
+    } elseif (isset($submit[2]) && isset($_REQUEST["thema"]) && $_REQUEST["thema"] != "" && isset($_REQUEST['rubrik']) && is_array($_REQUEST['rubrik'])) {
+        // Preview
+        $rubrik = $_REQUEST["rubrik"];
         $cat = new Category;
         $cat->transform(0);
         $categorylist = '';
@@ -77,12 +75,12 @@ if ($permission["editbt"]) {
         }
         $content = $_POST['content'];
 ?>
-	<h3><strong><em><?php print $categorylist; ?></em>
+    <h3><strong><em><?php print $categorylist; ?></em>
     <?php print $_REQUEST["thema"]; ?></strong></h3>
     <?php print $content; ?>
     <p class="little"><?php print $PMF_LANG["msgLastUpdateArticle"].makeDate(date("YmdHis")); ?><br />
     <?php print $PMF_LANG["msgAuthor"].' '.$_REQUEST["author"]; ?></p>
-    
+
     <form action="<?php print $_SERVER["PHP_SELF"].$linkext; ?>&amp;aktion=editpreview" method="post">
     <input type="hidden" name="id" value="<?php print $id; ?>" />
     <input type="hidden" name="thema" value="<?php print htmlspecialchars($_REQUEST["thema"]); ?>" />
@@ -104,8 +102,8 @@ if ($permission["editbt"]) {
 <?php
     } else {
         print "<h2>".$PMF_LANG["ad_entry_aor"]."</h2>\n";
-		print "<p>".$PMF_LANG["ad_entryins_fail"]."</p>";
-	    $rubrik = isset($_POST['rubrik']) ? $_POST['rubrik'] : null;
+        print "<p>".$PMF_LANG["ad_entryins_fail"]."</p>";
+        $rubrik = isset($_POST['rubrik']) ? $_POST['rubrik'] : null;
 ?>
     <form action="<?php print $_SERVER['PHP_SELF'].$linkext; ?>&amp;aktion=editpreview" method="post">
     <input type="hidden" name="thema" value="<?php print htmlspecialchars($_POST['thema']); ?>" />
@@ -127,8 +125,8 @@ if ($permission["editbt"]) {
     <p align="center"><input class="submit" type="submit" name="submit" value="<?php print $PMF_LANG['ad_entry_back']; ?>" /></p>
     </form>
 <?php
-	}
+    }
 } else {
-	print $PMF_LANG["err_NotAuth"];
+    print $PMF_LANG["err_NotAuth"];
 }
 ?>
