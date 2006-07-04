574b943fe8ed33c9af804550165a9addcb981746
matteo <matteo@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-07-04 22:34:46 +0000

    Fix: typo: $newID -> $nextID + MFB

--- phpmyfaq/admin/record.add.php
+++ phpmyfaq/admin/record.add.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* $Id: record.add.php,v 1.32 2006-07-04 21:35:15 matteo Exp $
+* $Id: record.add.php,v 1.33 2006-07-04 22:34:46 matteo Exp $
 *
 * Adds a record in the database
 *
@@ -25,9 +25,12 @@ if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
 }
 
 if ($permission["editbt"]) {
-    $submit = $_REQUEST["submit"];
+    $submit = $_POST["submit"];
 
-    if (isset($submit[1]) && isset($_REQUEST["thema"]) && $_REQUEST["thema"] != "" && isset($_REQUEST['rubrik']) && is_array($_REQUEST['rubrik'])) {
+    if (    isset($submit[1])
+         && isset($_POST["thema"]) && $_POST["thema"] != ""
+         && isset($_POST['rubrik']) && is_array($_POST['rubrik'])
+       ) {
         // new entry
         adminlog("Beitragcreatesave");
         printf("<h2>%s</h2>\n", $PMF_LANG['ad_entry_aor']);
@@ -55,13 +58,16 @@ if ($permission["editbt"]) {
             print $PMF_LANG["ad_entry_savedfail"].$db->error();
         }
         // Create the visit entry
-        $faq->createNewVisit($newID, $recordData['lang']);
+        $faq->createNewVisit($nextID, $recordData['lang']);
         // Insert the new category relations
         $faq->addCategoryRelation($categories, $nextID, $recordData['lang']);
 
-    } elseif (isset($submit[2]) && isset($_REQUEST["thema"]) && $_REQUEST["thema"] != "" && isset($_REQUEST['rubrik']) && is_array($_REQUEST['rubrik'])) {
+    } elseif (    isset($submit[2])
+               && isset($_POST["thema"]) && $_POST["thema"] != ""
+               && isset($_POST['rubrik']) && is_array($_POST['rubrik'])
+             ) {
         // Preview
-        $rubrik = $_REQUEST["rubrik"];
+        $rubrik = $_POST["rubrik"];
         $cat = new PMF_Category;
         $cat->transform(0);
         $categorylist = '';
@@ -76,27 +82,29 @@ if ($permission["editbt"]) {
         $content = $_POST['content'];
 ?>
     <h3><strong><em><?php print $categorylist; ?></em>
-    <?php print $_REQUEST["thema"]; ?></strong></h3>
+    <?php print $_POST["thema"]; ?></strong></h3>
     <?php print $content; ?>
     <p class="little"><?php print $PMF_LANG["msgLastUpdateArticle"].makeDate(date("YmdHis")); ?><br />
-    <?php print $PMF_LANG["msgAuthor"].' '.$_REQUEST["author"]; ?></p>
+    <?php print $PMF_LANG["msgAuthor"].' '.$_POST["author"]; ?></p>
 
-    <form action="<?php print $_SERVER["PHP_SELF"].$linkext; ?>&amp;aktion=editpreview" method="post">
-    <input type="hidden" name="id" value="<?php print $id; ?>" />
-    <input type="hidden" name="thema" value="<?php print htmlspecialchars($_REQUEST["thema"]); ?>" />
-    <input type="hidden" name="content" value="<?php print htmlspecialchars($_POST['content']); ?>" />
-    <input type="hidden" name="lang" value="<?php print $_REQUEST["language"]; ?>" />
-    <input type="hidden" name="keywords" value="<?php print $_REQUEST["keywords"]; ?>" />
-    <input type="hidden" name="author" value="<?php print $_REQUEST["author"]; ?>" />
-    <input type="hidden" name="email" value="<?php print $_REQUEST["email"]; ?>" />
+    <form action="<?php print $_SERVER['PHP_SELF'].$linkext; ?>&amp;aktion=editpreview" method="post">
+    <input type="hidden" name="id"       value="<?php print $id; ?>" />
+    <input type="hidden" name="thema"    value="<?php print htmlspecialchars($_POST["thema"]); ?>" />
+    <input type="hidden" name="content"  value="<?php print htmlspecialchars($_POST['content']); ?>" />
+    <input type="hidden" name="lang"     value="<?php print $_POST["language"]; ?>" />
+    <input type="hidden" name="keywords" value="<?php print $_POST["keywords"]; ?>" />
+    <input type="hidden" name="author"   value="<?php print $_POST["author"]; ?>" />
+    <input type="hidden" name="email"    value="<?php print $_POST["email"]; ?>" />
 <?php
         foreach ($rubrik as $key => $categories) {
             print '    <input type="hidden" name="rubrik['.$key.']" value="'.$categories.'" />';
         }
 ?>
-    <input type="hidden" name="active" value="<?php print $_REQUEST["active"]; ?>" />
-    <input type="hidden" name="changed" value="<?php print $_REQUEST["changed"]; ?>" />
-    <input type="hidden" name="comment" value="<?php print $_REQUEST["comment"]; ?>" />
+    <input type="hidden" name="solution_id" value="<?php print $_POST["solution_id"]; ?>" />
+    <input type="hidden" name="revision"    value="<?php print $_POST["revision"]; ?>" />
+    <input type="hidden" name="active"      value="<?php print $_POST["active"]; ?>" />
+    <input type="hidden" name="changed"     value="<?php print $_POST["changed"]; ?>" />
+    <input type="hidden" name="comment"     value="<?php print $_POST["comment"]; ?>" />
     <p align="center"><input class="submit" type="submit" name="submit" value="<?php print $PMF_LANG["ad_entry_back"]; ?>" /></p>
     </form>
 <?php
@@ -106,12 +114,12 @@ if ($permission["editbt"]) {
         $rubrik = isset($_POST['rubrik']) ? $_POST['rubrik'] : null;
 ?>
     <form action="<?php print $_SERVER['PHP_SELF'].$linkext; ?>&amp;aktion=editpreview" method="post">
-    <input type="hidden" name="thema" value="<?php print htmlspecialchars($_POST['thema']); ?>" />
-    <input type="hidden" name="content" value="<?php print htmlspecialchars($_POST['content']); ?>" />
-    <input type="hidden" name="lang" value="<?php print $_POST['language']; ?>" />
+    <input type="hidden" name="thema"    value="<?php print htmlspecialchars($_POST['thema']); ?>" />
+    <input type="hidden" name="content"  value="<?php print htmlspecialchars($_POST['content']); ?>" />
+    <input type="hidden" name="lang"     value="<?php print $_POST['language']; ?>" />
     <input type="hidden" name="keywords" value="<?php print $_POST['keywords']; ?>" />
-    <input type="hidden" name="author" value="<?php print $_POST['author']; ?>" />
-    <input type="hidden" name="email" value="<?php print $_POST['email']; ?>" />
+    <input type="hidden" name="author"   value="<?php print $_POST['author']; ?>" />
+    <input type="hidden" name="email"    value="<?php print $_POST['email']; ?>" />
 <?php
         if (is_array($rubrik)) {
             foreach ($rubrik as $key => $categories) {
@@ -119,9 +127,11 @@ if ($permission["editbt"]) {
             }
         }
 ?>
-    <input type="hidden" name="active" value="<?php print $_POST['active']; ?>" />
-    <input type="hidden" name="changed" value="<?php print $_POST['changed']; ?>" />
-    <input type="hidden" name="comment" value="<?php print $_POST['comment']; ?>" />
+    <input type="hidden" name="solution_id" value="<?php print $_POST["solution_id"]; ?>" />
+    <input type="hidden" name="revision"    value="<?php print $_POST["revision"]; ?>" />
+    <input type="hidden" name="active"      value="<?php print $_POST['active']; ?>" />
+    <input type="hidden" name="changed"     value="<?php print $_POST['changed']; ?>" />
+    <input type="hidden" name="comment"     value="<?php print isset($_POST['comment']) ? $_POST['comment'] : ''; ?>" />
     <p align="center"><input class="submit" type="submit" name="submit" value="<?php print $PMF_LANG['ad_entry_back']; ?>" /></p>
     </form>
 <?php
@@ -129,4 +139,3 @@ if ($permission["editbt"]) {
 } else {
     print $PMF_LANG["err_NotAuth"];
 }
-?>
