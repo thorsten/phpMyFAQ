259f67fdf5b593f649b5d5ff990e67fb08ea56e5
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-08-10 19:50:20 +0000

    Added the first methods for PMF_Tags

--- phpmyfaq/inc/Tags.php
+++ phpmyfaq/inc/Tags.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* $Id: Tags.php,v 1.1 2006-08-10 19:27:34 thorstenr Exp $
+* $Id: Tags.php,v 1.2 2006-08-10 19:50:20 thorstenr Exp $
 *
 * The main Tags class
 *
@@ -50,6 +50,103 @@ class PMF_Tags
         $this->language = $language;
     }
     
+    /**
+     * Returns all tags for a FAQ record
+     *
+     * @param   integer $reord_id
+     * @array   array   $tags
+     * @access  public
+     * @since   2006-08-10
+     * @author  Thorsten Rinne <thorsten@phpmyfaq.de>
+     */
+    function getAllTagsById($record_id)
+    {
+        $tags = array();
+        
+        
+        
+        return $tags;
+    }
     
+    /**
+     * Returns the FAQ record IDs where all tags are included
+     *
+     * @param   array   $arrayOfTags
+     * @return  array   $records
+     * @access  public
+     * @since   2006-08-10
+     * @author  Thorsten Rinne <thorsten@phpmyfaq.de>
+     */
+    function getRecordsByIntersectionTags($arrayOfTags)
+    {
+        if (!is_array($arrayOfTags)) {
+            return false;
+        }
+        
+        $query = sprintf("
+            SELECT
+                d.record_id AS record_id
+            FROM
+                %sfaqdata_tags d, %sfaqtags t
+            WHERE
+                t.tagging_id = d.tagging_id
+            AND
+                (t.tagging_name IN ('%s'))
+            GROUP BY
+                d.record_id
+            HAVING
+                COUNT(d.record_id) = %d",
+            SQLPREFIX,
+            SQLPREFIX,
+            implode("', '", $arrayOfTags),
+            count($arrayOfTags));
+        
+        $records = array();
+        $result = $this->db->query($query);
+        while ($row = $this->db->fetch_object($result)) {
+            $records[] = $row->record_id;
+        }
+        
+        return $records;
+    }
+
+    /**
+     * Returns all FAQ record IDs where all tags are included
+     *
+     * @param   array   $arrayOfTags
+     * @return  array   $records
+     * @access  public
+     * @since   2006-08-10
+     * @author  Thorsten Rinne <thorsten@phpmyfaq.de>
+     */
+    function getRecordsByUnionTags($arrayOfTags)
+    {
+        if (!is_array($arrayOfTags)) {
+            return false;
+        }
+        
+        $query = sprintf("
+            SELECT
+                d.record_id AS record_id
+            FROM
+                %sfaqdata_tags d, %sfaqtags t
+            WHERE
+                t.tagging_id = d.tagging_id
+            AND
+                (t.tagging_name IN ('%s'))
+            GROUP BY
+                d.record_id",
+            SQLPREFIX,
+            SQLPREFIX,
+            implode("', '", $arrayOfTags));
+        
+        $records = array();
+        $result = $this->db->query($query);
+        while ($row = $this->db->fetch_object($result)) {
+            $records[] = $row->record_id;
+        }
+        
+        return $records;
+    }
     
 }
