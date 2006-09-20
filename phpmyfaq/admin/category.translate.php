4f328a78d18a07c5b2411689605328c65553e34d
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-09-20 05:35:50 +0000

    Minor fix done in the morning train...

--- phpmyfaq/admin/category.translate.php
+++ phpmyfaq/admin/category.translate.php
@@ -1,24 +1,24 @@
 <?php
 /**
-* NEW - Name category.translate.php
-*
-* translates a category
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @author       Rudi Ferrari <bookcrossers@gmx.de>
-* @since        2006-09-10
-* @copyright    (c) 2003-2006 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * $Id: category.translate.php,v 1.5 2006-09-20 05:35:50 thorstenr Exp $
+ *
+ * translates a category
+ *
+ * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @author       Rudi Ferrari <bookcrossers@gmx.de>
+ * @since        2006-09-10
+ * @copyright    (c) 2006 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
@@ -55,7 +55,7 @@ if ($permission["editcateg"]) {
         <?php print $user->getAllUserOptions($cat->categories[$id]["user_id"]); ?>
         </select><br />
 
-        <input class="submit" style="margin-left: 190px;" type="submit" name="submit" value="<?php print $PMF_LANG["ad_categ_translatecateg"]; ?>" /> 
+        <input class="submit" style="margin-left: 190px;" type="submit" name="submit" value="<?php print $PMF_LANG["ad_categ_translatecateg"]; ?>" />
         <br /><hr />
         <?php
            print '<strong>'.$PMF_LANG["ad_categ_transalready"].'</strong><br />';
