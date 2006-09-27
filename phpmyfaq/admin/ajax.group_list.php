090fa0baec448fa67d7513eeecdbdddbca2c8b76
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-09-27 18:36:44 +0000

    Added group permissions

--- phpmyfaq/admin/ajax.group_list.php
+++ phpmyfaq/admin/ajax.group_list.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* $Id: ajax.group_list.php,v 1.12 2006-09-27 18:21:29 thorstenr Exp $
+* $Id: ajax.group_list.php,v 1.13 2006-09-27 18:36:44 thorstenr Exp $
 *
 * AJAX: lists all registered users
 *
@@ -24,69 +24,71 @@ if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     exit();
 }
 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-header("Content-type: text/xml");
-header("Vary: Negotiate,Accept");
-header("Content-type: text/xml; charset=".$PMF_LANG['metaCharset']);
+if ($permission['adduser'] || $permission['edituser'] || $permission['deluser']) {
 
-require_once(PMF_ROOT_DIR.'/inc/PMF_User/User.php');
-
-$user = new PMF_User();
-$user->addDb($db);
-$userList = $user->getAllUsers();
-$groupList = is_a($user->perm, "PMF_PermMedium") ? $user->perm->getAllGroups() : array();
-$data = array(
-    'name' => "Name:",
-    'description' => "Description:",
-    'auto_join' => "Auto-join:",
-);
-$perm = $user->perm;
-$all_rights = $perm->getAllRightsData();
-
-if (count(ob_list_handlers()) > 0) {
-    ob_clean();
-}
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+    header("Cache-Control: no-store, no-cache, must-revalidate");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+    header("Content-type: text/xml");
+    header("Vary: Negotiate,Accept");
+    header("Content-type: text/xml; charset=".$PMF_LANG['metaCharset']);
+    
+    require_once(PMF_ROOT_DIR.'/inc/PMF_User/User.php');
+    
+    $user = new PMF_User();
+    $user->addDb($db);
+    $userList = $user->getAllUsers();
+    $groupList = is_a($user->perm, "PMF_PermMedium") ? $user->perm->getAllGroups() : array();
+    $data = array(
+        'name' => "Name:",
+        'description' => "Description:",
+        'auto_join' => "Auto-join:",
+    );
+    $perm = $user->perm;
+    $all_rights = $perm->getAllRightsData();
+    
+    if (count(ob_list_handlers()) > 0) {
+        ob_clean();
+    }
 ?>
 <xml>
     <rightlist>
 <?php
-foreach ($all_rights as $right_data) {
-    $right_id = $right_data['right_id'];
-    // right is not for groups!
-    if (!$right_data['for_groups'])
-        continue;
+    foreach ($all_rights as $right_data) {
+        $right_id = $right_data['right_id'];
+        // right is not for groups!
+        if (!$right_data['for_groups'])
+            continue;
 ?>
         <right id="<?php print $right_id; ?>">
             <name><?php print isset($PMF_LANG['rightsLanguage'][$right_data['name']]) ? PMF_htmlentities($PMF_LANG['rightsLanguage'][$right_data['name']]) : $right_data['name']; ?></name>
             <description><?php print $right_data['description']; ?></description>
         </right>
 <?php
-    } /* end foreach ($all_rights) */
+        } /* end foreach ($all_rights) */
 ?>
     </rightlist>
     <userlist>
         <select_class>ad_select_user</select_class>
 <?php
-foreach ($userList as $user_id) {
-    $user_object = new PMF_User();
-    $user_object->getUserById($user_id);
+    foreach ($userList as $user_id) {
+        $user_object = new PMF_User();
+        $user_object->getUserById($user_id);
 ?>
         <user id="<?php print $user_id; ?>">
             <login><?php print $user_object->getLogin(); ?></login>
         </user>
 <?php
-} /* end foreach ($userList) */
+    } /* end foreach ($userList) */
 ?>
     </userlist>
     <grouplist>
         <select_class>ad_select_group</select_class>
 <?php
-foreach ($groupList as $group_id) {
-    $groupData = $perm->getGroupData($group_id);
+    foreach ($groupList as $group_id) {
+        $groupData = $perm->getGroupData($group_id);
 ?>
         <group id="<?php print $groupData['group_id']; ?>">
             <name><?php print $groupData['name']; ?></name>
@@ -94,34 +96,36 @@ foreach ($groupList as $group_id) {
             <auto_join><?php print $groupData['auto_join']; ?></auto_join>
             <group_rights>
 <?php
-    foreach ($all_rights as $right_data) {
-        $right_id = $right_data['right_id'];
-        // right is not for groups!
-        if (!$right_data['for_groups'])
-            continue;
-        // right is a group right!
-        if ($perm->checkGroupRight($group_id, $right_id)) {
+        foreach ($all_rights as $right_data) {
+            $right_id = $right_data['right_id'];
+            // right is not for groups!
+            if (!$right_data['for_groups'])
+                continue;
+            // right is a group right!
+            if ($perm->checkGroupRight($group_id, $right_id)) {
 ?>
                 <right id="<?php print $right_id; ?>"></right>
 <?php
-        } /* end if ($perm->checkGroupRight()) */
-    } /* end foreach ($all_rights) */
+            } /* end if ($perm->checkGroupRight()) */
+        } /* end foreach ($all_rights) */
 ?>
             </group_rights>
             <group_members>
 <?php
-    foreach ($perm->getGroupMembers($group_id) as $member_id) {
-        $member = new PMF_User();
-        $member->getUserById($member_id);
+        foreach ($perm->getGroupMembers($group_id) as $member_id) {
+            $member = new PMF_User();
+            $member->getUserById($member_id);
 ?>
-                <user id="<?php print $member->getUserId(); ?>"></user>
+            <user id="<?php print $member->getUserId(); ?>"></user>
 <?php
-    } /* end $perm->getGroupMembers($group_id) as $member_id) */
+        } /* end $perm->getGroupMembers($group_id) as $member_id) */
 ?>
             </group_members>
         </group>
 <?php
-} /* end foreach ($groupList) */
+    } /* end foreach ($groupList) */
 ?>
     </grouplist>
 </xml>
+<?php
+}
