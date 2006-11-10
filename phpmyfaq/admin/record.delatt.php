fb53fb442bf19f7b0eb3d3feda776214b01f61f3
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-11-10 21:02:36 +0000

    Code cleanup

--- phpmyfaq/admin/record.delatt.php
+++ phpmyfaq/admin/record.delatt.php
@@ -1,38 +1,44 @@
 <?php
 /**
-* $Id: record.delatt.php,v 1.8 2006-09-19 21:39:39 matteo Exp $
-*
-* Delete an attachment
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2003-02-24
-* @copyright    (c) 2003-2006 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * $Id: record.delatt.php,v 1.9 2006-11-10 21:02:36 thorstenr Exp $
+ *
+ * Deletes an attachment
+ *
+ * @author      Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since       2003-02-24
+ * @copyright   (c) 2003-2006 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
     exit();
 }
 
-print "<h2>".$PMF_LANG["ad_entry_aor"]."</h2>\n";
+printf("<h2>%s</h2>\n", $PMF_LANG['ad_entry_aor']);
+
 if ($permission["delatt"]) {
-    if (@unlink(PMF_ROOT_DIR."/attachments/".$_REQUEST["id"]."/".$_REQUEST["which"])) {
-        print "<p>".$PMF_LANG["ad_att_delsuc"]."</p>\n";
+    
+    $record_id   = (int)$_GET['id'];
+    $record_lang = $_GET['lang'];
+    $which_file  = $_GET['which'];
+    
+    if (@unlink(PMF_ROOT_DIR."/attachments/".$record_id."/".$which_file)) {
+        printf("<p>%s</p>\n", $PMF_LANG['ad_att_delsuc']);
     } else {
-        print "<p>".$PMF_LANG["ad_att_delfail"]."</p>\n";
+        printf("<p>%s</p>\n", $PMF_LANG['ad_att_delfail']);
     }
-    print "<p><a href=\"".$_SERVER["PHP_SELF"].$linkext."&amp;action=editentry&amp;id=".$_REQUEST["id"]."&amp;lang=".$_REQUEST["lang"]."\">".$PMF_LANG["ad_entry_back"]."</a></p>";
+    printf('<p><a href="%s&amp;action=editentry&amp;id=%d&amp;lang=%s">%s</a></p>',
+        $linkext, $record_id, $record_lang, $PMF_LANG['ad_entry_back']);
 } else {
-    print $PMF_LANG["err_NotAuth"];
+    print $PMF_LANG['err_NotAuth'];
 }
-?>
