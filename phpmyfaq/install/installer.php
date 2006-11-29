9b96515f3a8c9e6e606a4b442a66fe1d2aea8577
johannes <johannes@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2006-11-29 14:51:54 +0000

    - Disable MySQLi support with zend.ze1_compatiblity_mode = On
    23:04 <@chinstrap> yes zend.ze1_compatibility_mode changes the = Opertor to
                       make copies instead of references on objects.
    23:04 <@chinstrap> Now MySQLi uses objects as resources
    23:07 <@chinstrap> having zend.ze1_compatibility_mode on the Engine tries to use
                       a copy (aka clone) of that object but mysqli doesn't want to
                       be cloned.

--- phpmyfaq/install/installer.php
+++ phpmyfaq/install/installer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Id: installer.php,v 1.80 2006-11-16 22:05:31 matteo Exp $
+ * $Id: installer.php,v 1.81 2006-11-29 14:51:54 johannes Exp $
  *
  * The main phpMyFAQ Installer
  *
@@ -315,6 +315,11 @@ if (!isset($_POST["sql_server"]) && !isset($_POST["sql_user"]) && !isset($_POST[
     // check what extensions are loaded in PHP
     foreach ($supported_databases as $extension => $database) {
         if (extension_loaded($extension) && version_compare(PHP_VERSION, $database[0]) >= 0) {
+            // prevent MySQLi with zend.ze1_compatibility_mode enabled due to a few cloning isssues
+            if ($extension = 'mysqli' && ini_get('zend.ze1_compatibility_mode')) {
+                continue;
+            }
+
             printf('<option value="%s">%s</option>', $extension, $database[1]);
         }
     }
