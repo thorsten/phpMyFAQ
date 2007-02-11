73898cfb1354b26346ff43ec5f7f0ebe53f09062
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2007-02-11 17:42:35 +0000

    MFB: Added another mail check

--- phpmyfaq/mailsend2friend.php
+++ phpmyfaq/mailsend2friend.php
@@ -1,30 +1,30 @@
 <?php
 /**
-* $Id: mailsend2friend.php,v 1.15 2006-10-03 20:27:48 matteo Exp $
-*
-* Sends the emails to your friends
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2002-09-16
-* @copyright    (c) 2001-2006 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * $Id: mailsend2friend.php,v 1.16 2007-02-11 17:42:35 thorstenr Exp $
+ *
+ * Sends the emails to your friends
+ *
+ * @author      Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since       2002-09-16
+ * @copyright   (c) 2001-2007 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
     exit();
 }
 
-Tracking("sendmail_send2friend",0);
+Tracking('sendmail_send2friend', 0);
 
 $captcha = new PMF_Captcha($db, $sids, $pmf->language);
 
@@ -44,7 +44,7 @@ if (   isset($_POST['name']) && $_POST['name'] != ''
     list($user, $host) = explode("@", $mailto[0]);
     if (checkEmail($mailfrom)) {
         foreach($mailto as $mail) {
-            $mail = $db->escape_string(strip_tags($mail));
+            $mail = checkEmail($db->escape_string(strip_tags($mail)));
             if ($mail != "") {
                 mail(
                     $IDN->encode($mail),
