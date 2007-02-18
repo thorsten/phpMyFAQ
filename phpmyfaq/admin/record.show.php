6e5374d623c87ca04ed988b9e42ec6155b9acbef
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2007-02-18 15:46:27 +0000

    Improved code and fixed a lot of minor issues

--- phpmyfaq/admin/record.show.php
+++ phpmyfaq/admin/record.show.php
@@ -1,24 +1,24 @@
 <?php
 /**
-* $Id: record.show.php,v 1.37 2007-02-11 13:59:26 thorstenr Exp $
-*
-* Shows the list of records ordered by categories
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @author       Minoru TODA <todam@netjapan.co.jp>
-* @since        2003-02-23
-* @copyright    (c) 2001-2007 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * $Id: record.show.php,v 1.38 2007-02-18 15:46:27 thorstenr Exp $
+ *
+ * Shows the list of records ordered by categories
+ *
+ * @author      Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @author      Minoru TODA <todam@netjapan.co.jp>
+ * @since       2003-02-23
+ * @copyright   (c) 2003-2007 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
@@ -29,50 +29,50 @@ printf("<h2>%s</h2>\n", $PMF_LANG['ad_entry_aor']);
 
 if ($permission["editbt"] || $permission["delbt"]) {
     // (re)evaluate the Category object w/o passing the user language
-    $tree = new PMF_Category();
-    $tree->transform(0);
-    $tree->buildTree();
+    $category = new PMF_Category();
+    $category->transform(0);
+    $category->buildTree();
 
     $linkverifier = new PMF_Linkverifier($db, $user->getLogin());
     if ($linkverifier->isReady()) {
         link_verifier_javascript();
     }
 
-    $cond = array();
-    $internalSearch = '';
+    $comment = new PMF_Comment($db, $LANGCODE);
 
-    if (isset($_REQUEST["linkstate"])) {
-        $cond[SQLPREFIX."faqdata.links_state"] = "linkbad";
-        $linkState = " checked ";
-        $internalSearch .= "&amp;linkstate=linkbad";
-    } else {
-        $linkState = "";
+    $cond             = array();
+    $numCommentsByFaq = array();
+    $internalSearch   = '';
+    $linkState        = '';
+    $searchcat        = 0;
+    $searchterm       = '';
+
+    if (isset($_REQUEST['linkstate'])) {
+        $cond[SQLPREFIX.'faqdata.links_state'] = 'linkbad';
+        $linkState = ' checked="checked" ';
+        $internalSearch .= '&amp;linkstate=linkbad';
     }
 
-    if (isset($_REQUEST["searchcat"]) && is_numeric($_REQUEST["searchcat"]) && $_REQUEST["searchcat"] != "0" ) {
-        $searchcat = safeSQL($_REQUEST["searchcat"]);
+    if (isset($_POST['searchcat']) && is_numeric($_POST['searchcat']) && $_POST['searchcat'] != 0) {
+        $searchcat = (int)($_POST['searchcat']);
         $internalSearch .= "&amp;searchcat=".$searchcat;
-        $cond[SQLPREFIX."faqcategoryrelations.category_id"] = array_merge(array($searchcat),$tree->getChildNodes($searchcat));
-    } else {
-        $searchcat = 0;
+        $cond[SQLPREFIX.'faqcategoryrelations.category_id'] = array_merge(array($searchcat), $category->getChildNodes($searchcat));
     }
-    
-    if (isset($_REQUEST["suchbegriff"])) {
-        $begriff = safeSQL($_REQUEST["suchbegriff"]);
-    } else {
-        $begriff = "";
+
+    if (isset($_POST['searchterm'])) {
+        $searchterm = safeSQL($_POST['searchterm']);
     }
 ?>
-    <form action="<?php print $_SERVER["PHP_SELF"].$linkext; ?>&amp;action=view" method="post">
+    <form action="?action=view" method="post">
     <fieldset>
     <legend><?php print $PMF_LANG["msgSearch"]; ?></legend>
         <table class="admin">
         <tr>
             <td><strong><?php print $PMF_LANG["msgSearchWord"]; ?>:</strong></td>
-            <td><input class="admin" type="text" name="suchbegriff" size="50" value="<?php print $begriff; ?>"></td>
+            <td><input class="admin" type="text" name="searchterm" size="50" value="<?php print $searchterm; ?>" /></td>
             <td>
             <?php if ($linkverifier->isReady() == true) { ?>
-            <input class="admin" type="checkbox" name="linkstate" value="linkbad" <?php print $linkState; ?>><?php print $PMF_LANG['ad_linkcheck_searchbadonly']; ?>
+            <input class="admin" type="checkbox" name="linkstate" value="linkbad" <?php print $linkState; ?> /><?php print $PMF_LANG['ad_linkcheck_searchbadonly']; ?>
             <?php } ?>
             </td>
         </tr>
@@ -80,9 +80,9 @@ if ($permission["editbt"] || $permission["delbt"]) {
             <td><strong><?php print $PMF_LANG["msgCategory"]; ?>:</strong></td>
             <td><select class="admin" name="searchcat">
             <option value="0"><?php print $PMF_LANG["msgShowAllCategories"]; ?></option>
-            <?php print $tree->printCategoryOptions($searchcat); ?>
+            <?php print $category->printCategoryOptions($searchcat); ?>
             </select></td>
-            <td><input class="submit" type="submit" name="submit" value="<?php print $PMF_LANG["msgSearch"]; ?>"></td>
+            <td><input class="submit" type="submit" name="submit" value="<?php print $PMF_LANG["msgSearch"]; ?>" /></td>
         </tr>
         </table>
     </fieldset>
@@ -91,33 +91,13 @@ if ($permission["editbt"] || $permission["delbt"]) {
     <legend><?php print ((isset($_REQUEST['action']) && 'accept' == $_REQUEST['action']) ? $PMF_LANG['ad_menu_entry_aprove'] : $PMF_LANG['ad_menu_entry_edit']); ?></legend>
 <?php
     // 1. Count the comments for each faq
-    $resultComments = $db->query("SELECT count(id) as anz, id FROM ".SQLPREFIX."faqcomments GROUP BY id ORDER BY id;");
-    if ($db->num_rows($resultComments) > 0) {
-        while ($row = $db->fetch_object($resultComments)) {
-            $numCommentsByFaq[$row->id] = $row->anz;
-        }
-    }
+    $numCommentsByFaq = $comment->getNumberOfComments();
 
     // FIXME: Count "comments"/"entries" for each category also within a search context. Now the count is broken.
     // FIXME: we are not considering 'faqdata.links_state' for filtering the faqs.
-    if (!(isset($_REQUEST["suchbegriff"]) && $_REQUEST["suchbegriff"] != "")) {
-        $matrix = array();
-        // 2. Count the comments for each category
-        // 2.1 Create a matrix for representing categories and faq records
-        $query  = 'SELECT '.SQLPREFIX.'faqcategoryrelations.category_id AS id_cat, '.SQLPREFIX.'faqdata.id AS id'
-             .' FROM '.SQLPREFIX.'faqdata'
-             .' INNER JOIN '.SQLPREFIX.'faqcategoryrelations'
-             .' ON '.SQLPREFIX.'faqdata.id='.SQLPREFIX.'faqcategoryrelations.record_id'
-             .' AND '.SQLPREFIX.'faqdata.lang='.SQLPREFIX.'faqcategoryrelations.category_lang'
-             .' ORDER BY '.SQLPREFIX.'faqcategoryrelations.category_id, '.SQLPREFIX.'faqdata.id';
-        $resultFaq = $db->query($query);
-        if ($db->num_rows($resultFaq) > 0) {
-            while ($row = $db->fetch_object($resultFaq)) {
-                $matrix[$row->id_cat][$row->id] = true;
-            }
-        }
+    if (!(isset($_POST["searchterm"]) && $_REQUEST["searchterm"] != "")) {
 
-        // 2.2 Evaluate the comments for each category using the matrix above
+        $matrix = $category->getCategoryRecordsMatrix();
         foreach ($matrix as $catkey => $value) {
             $numCommentsByCat[$catkey] = 0;
             foreach ($value as $faqkey => $value) {
@@ -128,7 +108,7 @@ if ($permission["editbt"] || $permission["delbt"]) {
         }
 
         // 3. Count the entries
-        if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'accept') {
+        if (isset($_GET['action']) && $_GET['action'] == 'accept') {
             $active = 'no';
         } else {
             $active = 'yes';
@@ -140,7 +120,7 @@ if ($permission["editbt"] || $permission["delbt"]) {
             ." AND ".SQLPREFIX."faqcategoryrelations.record_lang = ".SQLPREFIX."faqdata.lang"
             ." AND ".SQLPREFIX."faqdata.active = '".$active."'"
             ." GROUP BY ".SQLPREFIX."faqcategoryrelations.category_id";
-        
+
         $result = $db->query($query);
         if ($db->num_rows($result) > 0) {
             while ($row = $db->fetch_object($result)) {
@@ -149,7 +129,7 @@ if ($permission["editbt"] || $permission["delbt"]) {
         }
     }
 
-    if (isset($_REQUEST["action"]) && $_REQUEST["action"] == "view" && !(isset($_REQUEST["suchbegriff"]) && $_REQUEST["suchbegriff"] != "")) {
+    if (isset($_REQUEST["action"]) && $_REQUEST["action"] == "view" && !(isset($_REQUEST["searchterm"]) && $_REQUEST["searchterm"] != "")) {
         // No search requested
         $where = "";
         foreach ($cond as $field => $data) {
@@ -171,13 +151,13 @@ if ($permission["editbt"] || $permission["delbt"]) {
         $result = $db->query($query);
         $laction = 'view';
         $internalSearch = '';
-    } else if (isset($_REQUEST["action"]) && $_REQUEST["action"] == "view" && isset($_REQUEST["suchbegriff"]) && $_REQUEST["suchbegriff"] != "") {
+    } else if (isset($_REQUEST["action"]) && $_REQUEST["action"] == "view" && isset($_REQUEST["searchterm"]) && $_REQUEST["searchterm"] != "") {
         // Search for:
         // a. solution id
         // b. full text search
         // TODO: Decide if the search will be performed upon all entries or upon the active ones.
-        $begriff = strip_tags($_REQUEST["suchbegriff"]);
-        if (is_numeric($begriff)) {
+        $searchterm = strip_tags($_REQUEST["searchterm"]);
+        if (is_numeric($searchterm)) {
             // a. solution id
             $result = $db->search(SQLPREFIX.'faqdata',
                         array(SQLPREFIX.'faqdata.id AS id',
@@ -189,7 +169,7 @@ if ($permission["editbt"] || $permission["delbt"]) {
                         array(SQLPREFIX.'faqdata.id = '.SQLPREFIX.'faqcategoryrelations.record_id',
                             SQLPREFIX.'faqdata.lang = '.SQLPREFIX.'faqcategoryrelations.record_lang'),
                         array(SQLPREFIX.'faqdata.solution_id'),
-                        $begriff);
+                        $searchterm);
         } else {
             // b. full text search
             $result = $db->search(SQLPREFIX."faqdata",
@@ -204,13 +184,13 @@ if ($permission["editbt"] || $permission["delbt"]) {
                         array(SQLPREFIX.'faqdata.thema',
                             SQLPREFIX.'faqdata.content',
                             SQLPREFIX.'faqdata.keywords'),
-                        $begriff,
+                        $searchterm,
                         array(),
                         array(SQLPREFIX.'faqcategoryrelations.category_id',  SQLPREFIX.'faqdata.id')
                         );
         }
         $laction = 'view';
-        $internalSearch = '&amp;search='.$begriff;
+        $internalSearch = '&amp;search='.$searchterm;
         $wasSearch = true;
 
     } elseif (isset($_REQUEST["action"]) && $_REQUEST["action"] == "accept") {
@@ -246,24 +226,35 @@ if ($permission["editbt"] || $permission["delbt"]) {
             if ($cid != $old) {
                 if ($old == 0) {
 ?>
-    <!--<a name="cat_<?php print $cid; ?>" />--><div class="categorylisting"><a href="#cat_<?php print $cid; ?>" onclick="showhideCategory('category_<?php print $cid; ?>');"><img src="../images/more.gif" width="11" height="11" alt="" /> <?php print $tree->getPath($cid); ?></a><?php print $catInfo;?></div>
+    <!--<a name="cat_<?php print $cid; ?>" />--><div class="categorylisting"><a href="#cat_<?php print $cid; ?>" onclick="showhideCategory('category_<?php print $cid; ?>');"><img src="../images/more.gif" width="11" height="11" alt="" /> <?php print $category->getPath($cid); ?></a><?php print $catInfo;?></div>
     <div id="category_<?php print $cid; ?>" class="categorybox" style="display: none;">
     <table class="listrecords">
     <thead>
+    <tr>
         <th class="listhead"><a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=id&amp;sortby=desc">&uarr;</a>&nbsp;<a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=id&amp;sortby=asc">&darr;</a></th>
         <th class="listhead"><a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=lang&amp;sortby=desc">&uarr;</a>&nbsp;<a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=lang&amp;sortby=asc">&darr;</a></th>
         <th class="listhead"><a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=title&amp;sortby=desc">&uarr;</a>&nbsp;<a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=title&amp;sortby=asc">&darr;</a></th>
         <th class="listhead"><a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=date&amp;sortby=desc">&uarr;</a>&nbsp;<a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=date&amp;sortby=asc">&darr;</a></th>
         <th class="listhead" colspan="2">&nbsp;</th>
+    </tr>
     </thead>
 <?php
                 } else {
 ?>
     </table>
     </div>
-    <!--<a name="cat_<?php print $cid; ?>" />--><div class="categorylisting"><a href="#cat_<?php print $cid; ?>" onclick="showhideCategory('category_<?php print $cid; ?>');"><img src="../images/more.gif" width="11" height="11" alt="" /> <?php print $tree->getPath($cid); ?></a><?php print $catInfo;?></div>
+    <!--<a name="cat_<?php print $cid; ?>" />--><div class="categorylisting"><a href="#cat_<?php print $cid; ?>" onclick="showhideCategory('category_<?php print $cid; ?>');"><img src="../images/more.gif" width="11" height="11" alt="" /> <?php print $category->getPath($cid); ?></a><?php print $catInfo;?></div>
     <div id="category_<?php print $cid; ?>" class="categorybox" style="display: none;">
     <table class="listrecords">
+    <thead>
+    <tr>
+        <th class="listhead"><a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=id&amp;sortby=desc">&uarr;</a>&nbsp;<a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=id&amp;sortby=asc">&darr;</a></th>
+        <th class="listhead"><a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=lang&amp;sortby=desc">&uarr;</a>&nbsp;<a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=lang&amp;sortby=asc">&darr;</a></th>
+        <th class="listhead"><a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=title&amp;sortby=desc">&uarr;</a>&nbsp;<a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=title&amp;sortby=asc">&darr;</a></th>
+        <th class="listhead"><a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=date&amp;sortby=desc">&uarr;</a>&nbsp;<a href="?action=view&amp;category=<?php print $cid; ?>&amp;orderby=date&amp;sortby=asc">&darr;</a></th>
+        <th class="listhead" colspan="2">&nbsp;</th>
+    </tr>
+    </thead>
 <?php
                 }
 ?>
@@ -283,7 +274,7 @@ if ($permission["editbt"] || $permission["delbt"]) {
         </td>
         <td class="list"></td>
         <td class="list" width="100"><?php print $linkverifier->getEntryStateHTML($row->id, $row->lang); ?></td>
-        <td class="list" width="17"><a href="?action=saveentry&amp;id=<?php print $row->id; ?>&amp;language=<?php print $row->lang; ?>&amp;submit[0]=<?php print $PMF_LANG["ad_entry_delete"]; ?>" title="<?php print $PMF_LANG["ad_user_delete"]; ?> '<?php print str_replace("\"", "´", $row->thema); ?>'"><img src="images/delete.gif" width="17" height="18" alt="<?php print $PMF_LANG["ad_entry_delete"]; ?>" /></a></td>
+        <td class="list" width="17"><a href="?action=saveentry&amp;id=<?php print $row->id; ?>&amp;language=<?php print $row->lang; ?>&amp;submit%5B0%5D=<?php print urlencode($PMF_LANG["ad_entry_delete"]); ?>" title="<?php print $PMF_LANG["ad_user_delete"]; ?> '<?php print str_replace("\"", "´", $row->thema); ?>'"><img src="images/delete.gif" width="17" height="18" alt="<?php print $PMF_LANG["ad_entry_delete"]; ?>" /></a></td>
     </tr>
 <?php
             $old = $cid;
@@ -296,8 +287,8 @@ if ($permission["editbt"] || $permission["delbt"]) {
     </form>
 <?php
     } else {
-        print $PMF_LANG["err_nothingFound"];
+        print $PMF_LANG['err_nothingFound'];
     }
 } else {
-    print $PMF_LANG["err_NotAuth"];
+    print $PMF_LANG['err_NotAuth'];
 }
