63a2ffb38197ff0549bad3a4cb7159637ba6705f
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2007-02-26 19:01:09 +0000

    Fix: Due to permissions we don't want all categories

--- phpmyfaq/open.php
+++ phpmyfaq/open.php
@@ -1,21 +1,21 @@
 <?php
 /**
-* $Id: open.php,v 1.10 2007-02-04 19:27:50 thorstenr Exp $
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2002-09-17
-* @copyright    (c) 2001-2007 phpMyFAQ Team
-* 
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the 'License'); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-* 
-* Software distributed under the License is distributed on an 'AS IS'
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * $Id: open.php,v 1.11 2007-02-26 19:01:09 thorstenr Exp $
+ *
+ * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since        2002-09-17
+ * @copyright    (c) 2001-2007 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the 'License'); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an 'AS IS'
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
@@ -24,19 +24,11 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
 
 Tracking('open_questions', 0);
 
-// HACK: (re)evaluate the Category object w/o passing the user language
-//       so the result set (the Open Uqestions) will have the Category Name
-//       for any of the multilanguage categories and the Category list
-//       on the left pane will not be affected
-$category = new PMF_Category();
-$category->transform(0);
-
 $tpl->processTemplate ('writeContent', array(
-                        'msgOpenQuestions' => $PMF_LANG['msgOpenQuestions'],
-                        'msgQuestionText' => $PMF_LANG['msgQuestionText'],
-                        'msgDate_User' => $PMF_LANG['msgDate_User'],
-                        'msgQuestion2' => $PMF_LANG['msgQuestion2'],
-                        'printOpenQuestions' => printOpenQuestions()
-                        ));
+    'msgOpenQuestions'      => $PMF_LANG['msgOpenQuestions'],
+    'msgQuestionText'       => $PMF_LANG['msgQuestionText'],
+    'msgDate_User'          => $PMF_LANG['msgDate_User'],
+    'msgQuestion2'          => $PMF_LANG['msgQuestion2'],
+    'printOpenQuestions'    => printOpenQuestions()));
 
 $tpl->includeTemplate('writeContent', 'index');
