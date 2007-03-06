f52ab3c35f68c350393184e3f8b1494eb81406e2
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2007-03-06 21:21:55 +0000

    Improved security

--- phpmyfaq/sitemap.php
+++ phpmyfaq/sitemap.php
@@ -1,24 +1,24 @@
 <?php
 /**
-* $Id: sitemap.php,v 1.13 2006-09-19 21:39:38 matteo Exp $
-*
-* Shows the whole FAQ articles
-*
-* @author       Thomas Zeithaml <seo@annatom.de>
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2005-08-21
-* @copyright    (c) 2001-2006 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * $Id: sitemap.php,v 1.14 2007-03-06 21:21:55 thorstenr Exp $
+ *
+ * Shows the whole FAQ articles
+ *
+ * @author      Thomas Zeithaml <seo@annatom.de>
+ * @author      Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since       2005-08-21
+ * @copyright   (c) 2005-2007 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
@@ -28,7 +28,7 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
 Tracking('sitemap', 0);
 
 if (isset($_REQUEST['letter']) && (is_string($_REQUEST['letter']))  && (1 == strlen($_REQUEST['letter']))) {
-    $currentLetter = strtoupper($db->escape_string($_REQUEST['letter']));
+    $currentLetter = strtoupper($db->escape_string(substr($_REQUEST['letter'], 0, 1)));
 } else {
     $currentLetter = 'A';
 }
