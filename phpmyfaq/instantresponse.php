74e6194e9cba4ac9bd89ed0fd0e726de14ceaf61
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2007-03-18 18:11:40 +0000

    Added first lines of code for Instant Response

---
+++ phpmyfaq/instantresponse.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * $Id: search.php,v 1.20 2007/02/04 19:27:50 thorstenr Exp $
+ *
+ * The Ajax powered instant response page
+ *
+ * @author      Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since       2007-03-18
+ * @copyright   (c) 2007 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+
+if (!defined('IS_VALID_PHPMYFAQ')) {
+    header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
+    exit();
+}
+
+Tracking('instantresponse', 0);
+
+
+
+$tpl->processTemplate(
+    'writeContent', array(
+        'msgInstantResponse'    => $PMF_LANG['msgInstantResponse'],
+        'searchString'          => $suchbegriff,
+        'searchOnAllLanguages'  => $PMF_LANG['msgSearchOnAllLanguages'],
+        'checkedAllLanguages'   => $allLanguages ? ' checked="checked"' : '',
+        'selectCategories'      => $PMF_LANG['msgSelectCategories'],
+        'allCategories'         => $PMF_LANG['msgAllCategories'],
+        'printCategoryOptions'  => $category->printCategoryOptions($searchCategory),
+        'writeSendAdress'       => $_SERVER['PHP_SELF'].'?'.$sids.'action=search',
+        'msgSearchWord'         => $PMF_LANG['msgSearchWord'],
+        'printInstantResponse'  => $printInstantResponse));
+
+$tpl->includeTemplate('writeContent', 'index');
