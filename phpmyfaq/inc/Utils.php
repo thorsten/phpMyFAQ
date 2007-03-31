3aa2a1ae1df035057fc89e908ec4e9609f795963
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2007-03-31 14:48:26 +0000

    Removed unused constructor

--- phpmyfaq/inc/Utils.php
+++ phpmyfaq/inc/Utils.php
@@ -1,24 +1,24 @@
 <?php
 /**
-* $Id: Utils.php,v 1.2 2006-07-29 10:34:01 matteo Exp $
-*
-* Utilities - Functions and Classes common to the whole phpMyFAQ architecture
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @author       Matteo Scaramuccia <matteo@scaramuccia.com>
-* @since        2005-11-01
-* @copyright    (c) 2005-2006 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * $Id: Utils.php,v 1.3 2007-03-31 14:48:26 thorstenr Exp $
+ *
+ * Utilities - Functions and Classes common to the whole phpMyFAQ architecture
+ *
+ * @author      Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @author      Matteo Scaramuccia <matteo@scaramuccia.com>
+ * @since       2005-11-01
+ * @copyright   (c) 2005-2007 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 // {{{ Constants
 /**#@+
@@ -42,10 +42,6 @@ define("HTTP_PARAMS_GET_TYPE", "type");
  */
 class PMF_Utils
 {
-    function PMF_Utils()
-    {
-    }
-
     function getPMFDate($unixTime = NULL)
     {
         if (!isset($unixTime)) {
