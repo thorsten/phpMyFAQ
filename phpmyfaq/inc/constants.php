c92beda4be67f3f088c66990190059c335e93ee0
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2007-04-06 11:23:52 +0000

    Added blacklist for searchbots

--- phpmyfaq/inc/constants.php
+++ phpmyfaq/inc/constants.php
@@ -1,24 +1,24 @@
 <?php
 /**
-* $Id: constants.php,v 1.30 2007-03-18 18:07:12 thorstenr Exp $
-*
-* Constants for phpMyFAQ
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @author       Matteo Scaramuccia <matteo@scaramuccia.com>
-* @since        2003-12-10
-* @copyright    (c) 2003-2007 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License Version
-* 1.1 (the "License"); you may not use this file except in compliance with
-* the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS" basis,
-* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-* for the specific language governing rights and limitations under the
-* License.
-*/
+ * $Id: constants.php,v 1.31 2007-04-06 11:23:52 thorstenr Exp $
+ *
+ * Constants for phpMyFAQ
+ *
+ * @author      Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @author      Matteo Scaramuccia <matteo@scaramuccia.com>
+ * @since       2003-12-10
+ * @copyright   (c) 2003-2007 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ */
 
 /**
  * PHP 6 script encoding
@@ -137,6 +137,25 @@ define("PMF_TAGS_AUTOCOMPLETE_RESULT_SET_SIZE", 20);
  */
 $denyUploadExts = array();
 
+/**
+ * Bot Blacklist
+ *
+ * @var array
+ */
+$botBlacklist = array(
+    'nustcrape',
+    'webpost',
+    'google',
+    'crawler',
+    'scooter',
+    'bravobrian',
+    'archiver',
+    'w3c',
+    'control',
+    'wget',
+    'bot',
+    'spider',
+    'Yahoo! Slurp');
 
 /****************************************************************************
  *                  DO NOT CHANGE ANYTHING BELOW THIS LINE!                 *
