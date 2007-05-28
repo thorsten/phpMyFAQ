86228051fe0fdcc19c908036a333da680327189e
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2007-05-28 08:40:49 +0000

    MFB: Fixed bug [#1402]: We used wrong POST variables for saving and updating the permissions

--- phpmyfaq/admin/category.main.php
+++ phpmyfaq/admin/category.main.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Id: category.main.php,v 1.43 2007-05-02 17:58:03 thorstenr Exp $
+ * $Id: category.main.php,v 1.44 2007-05-28 08:40:49 thorstenr Exp $
  *
  * List all categories in the admin section
  *
@@ -53,10 +53,10 @@ if ($permission['editcateg']) {
 
         $userperm       = isset($_POST['userpermission']) ?
                           $db->escape_string($_POST['userpermission']) : 'all';
-        $user_allowed   = ('all' == $userperm) ? -1 : (int)$_POST['userpermission'];
+        $user_allowed   = ('all' == $userperm) ? -1 : (int)$_POST['restricted_users'];
         $groupperm      = isset($_POST['grouppermission']) ?
                           $db->escape_string($_POST['grouppermission']) : 'all';
-        $group_allowed  = ('all' == $groupperm) ? -1 : (int)$_POST['grouppermission'];
+        $group_allowed  = ('all' == $groupperm) ? -1 : (int)$_POST['restricted_groups'];
 
         $category_id = $category->addCategory($category_data, $parent_id);
         if ($category_id) {
@@ -83,10 +83,10 @@ if ($permission['editcateg']) {
 
         $userperm       = isset($_POST['userpermission']) ?
                           $db->escape_string($_POST['userpermission']) : 'all';
-        $user_allowed   = ('all' == $userperm) ? -1 : (int)$_POST['userpermission'];
+        $user_allowed   = ('all' == $userperm) ? -1 : (int)$_POST['restricted_users'];
         $groupperm      = isset($_POST['grouppermission']) ?
                           $db->escape_string($_POST['grouppermission']) : 'all';
-        $group_allowed  = ('all' == $groupperm) ? -1 : (int)$_POST['grouppermission'];
+        $group_allowed  = ('all' == $groupperm) ? -1 : (int)$_POST['restricted_groups'];
 
         if (!$category->checkLanguage($category_data['id'], $category_data['lang'])) {
             if ($category->addCategory($category_data, $parent_id, $category_data['id']) &&
