7c45d8e51734267b5c523f9ce0f1cafeab046ccd
thorstenr <thorstenr@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2008-01-26 08:23:06 +0000

    Improved XHTML

--- phpmyfaq/admin/backup.main.php
+++ phpmyfaq/admin/backup.main.php
@@ -1,18 +1,18 @@
 <?php
 /**
-* $Id: backup.main.php,v 1.9 2006-09-19 21:39:38 matteo Exp $
+* $Id: backup.main.php,v 1.10 2008-01-26 08:23:06 thorstenr Exp $
 *
 * Frontend for Backup and Restore
 *
 * @author       Thorsten Rinne <thorsten@phpmyfaq.de>
 * @since        2003-02-24
 * @copyright    (c) 2001-2006 phpMyFAQ Team
-* 
+*
 * The contents of this file are subject to the Mozilla Public License
 * Version 1.1 (the "License"); you may not use this file except in
 * compliance with the License. You may obtain a copy of the License at
 * http://www.mozilla.org/MPL/
-* 
+*
 * Software distributed under the License is distributed on an "AS IS"
 * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 * License for the specific language governing rights and limitations
@@ -24,7 +24,7 @@ if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     exit();
 }
 
-printf('<h2>%s</h2>', $PMF_LANG['ad_csv_backup']);    
+printf('<h2>%s</h2>', $PMF_LANG['ad_csv_backup']);
 
 if ($permission['backup']) {
 ?>
@@ -33,7 +33,7 @@ if ($permission['backup']) {
         <p><?php print $PMF_LANG["ad_csv_make"]; ?></p>
         <p align="center"><a href="attachment.php?action=sicherdaten"><?php print $PMF_LANG["ad_csv_linkdat"]; ?></a> | <a href="attachment.php?action=sicherlog"><?php print $PMF_LANG["ad_csv_linklog"]; ?></a></p>
     </fieldset>
-    <p>&nbsp;</p>
+
     <form method="post" action="?action=restore" enctype="multipart/form-data">
     <fieldset>
         <legend><?php print $PMF_LANG["ad_csv_head2"]; ?></legend>
