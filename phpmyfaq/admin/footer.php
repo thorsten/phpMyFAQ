50573fec00f25878a9cff850541bf9f0408636a0
matteo <matteo@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-01-07 15:03:33 +0000

    Fix: (trivial) Fixed @copyright.

--- phpmyfaq/admin/footer.php
+++ phpmyfaq/admin/footer.php
@@ -2,11 +2,12 @@
 /**
  * Footer of the admin area
  *
- * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
- * @author    Matteo Scaramuccia <matteo@phpmyfaq.de>
- * @since     2003-02-26
- * @copyright 2003-2008 phpMyFAQ Team
- * @version   SVN: $Id$
+ * @package     phpMyFAQ 
+ * @author      Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @author      Matteo Scaramuccia <matteo@phpmyfaq.de>
+ * @since       2003-02-26
+ * @copyright   2003-2009 phpMyFAQ Team
+ * @version     SVN: $Id$
  *
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
