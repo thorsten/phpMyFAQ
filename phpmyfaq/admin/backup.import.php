b144a03360865889111bf980d9a64c94fbb5d672
matteo <matteo@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-01-07 15:20:44 +0000

    Fix: backups must be marked as "pmf25".

--- phpmyfaq/admin/backup.import.php
+++ phpmyfaq/admin/backup.import.php
@@ -1,23 +1,23 @@
 <?php
 /**
-* $Id: backup.import.php,v 1.18 2007-05-27 20:41:15 matteo Exp $
-*
-* The import function to import the phpMyFAQ backups
-*
-* @author       Thorsten Rinne <thorsten@rinne.info>
-* @since        2003-02-24
-* @copyright    (c) 2001-2006 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * The import function to import the phpMyFAQ backups
+ *
+ * @package     phpMyFAQ 
+ * @author      Thorsten Rinne <thorsten@rinne.info>
+ * @since       2003-02-24
+ * @copyright   (c) 2003-2009 phpMyFAQ Team
+ * @version     SVN: $Id$ 
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
@@ -32,7 +32,7 @@ if ($permission["restore"]) {
         $fp = fopen($_FILES["userfile"]["tmp_name"], "r");
         $dat = fgets($fp, 65536);
 
-        if (substr($dat, 0, 9) != '-- pmf2.0') {
+        if (substr($dat, 0, 9) != '-- pmf2.5') {
             print $PMF_LANG["ad_csv_no"];
             $ok = 0;
         } else {
