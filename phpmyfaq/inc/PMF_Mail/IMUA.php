416d1d1f8be1e9c2e0111010a4a5ae850fe25b29
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-01-24 14:14:07 +0000

    PHPDoc fixes

--- phpmyfaq/inc/PMF_Mail/IMUA.php
+++ phpmyfaq/inc/PMF_Mail/IMUA.php
@@ -2,11 +2,12 @@
 /**
  * MUA (Mail User Agent) interface.
  *
- * @package   phpMyFAQ
- * @author    Matteo Scaramuccia <matteo@phpmyfaq.de>
- * @since     2009-09-11
- * @version   SVN: $Id$
- * @copyright 2009 phpMyFAQ Team
+ * @package    phpMyFAQ
+ * @subpackage Mail
+ * @author     Matteo Scaramuccia <matteo@phpmyfaq.de>
+ * @since      2009-09-11
+ * @version    SVN: $Id$
+ * @copyright  2009 phpMyFAQ Team
  *
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
@@ -26,21 +27,20 @@
 declare(encoding='latin1');
  
  /**
- * MUA (Mail User Agent) interface.
- *
- * @package phpMyFAQ 
- * @access public
- */ 
+  * MUA (Mail User Agent) interface.
+  *
+  * @package phpMyFAQ 
+  * @access public
+  */ 
 interface PMF_Mail_IMUA
 {
     /**
      * Send the message using an e-mail.
-     *
-     * @access public
-     * @param string $recipients Recipients of the e-mail as a comma-separated list
-     *                           of RFC 2822 compliant elements
-     * @param array $headers Headers of the e-mail
-     * @param string $body Body of the e-mail
+     * 
+     * @param  string $recipients Recipients of the e-mail as a comma-separated list
+     *                            of RFC 2822 compliant elements
+     * @param  array  $headers    Headers of the e-mail
+     * @param  string $body       Body of the e-mail
      * @return bool True if successful, false otherwise.     
      */
     public function send($recipients, $headers, $body);
