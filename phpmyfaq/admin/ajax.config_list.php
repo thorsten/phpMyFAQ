314575a5db06fff42bcdab270d4704de7b9ba909
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-01-26 18:14:32 +0000

    Don't show uneditable configuration items

--- phpmyfaq/admin/ajax.config_list.php
+++ phpmyfaq/admin/ajax.config_list.php
@@ -1,23 +1,24 @@
 <?php
 /**
-* $Id: ajax.config_list.php,v 1.19 2007-04-21 10:31:10 thorstenr Exp $
-*
-* AJAX: lists the complete configuration items
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2005-12-26
-* @copyright    (c) 2005-2007 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * AJAX: lists the complete configuration items
+ * 
+ * @package    phpMyFAQ
+ * @subpackage Administration
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2005-12-26
+ * @copyright  2005-2009 phpMyFAQ Team
+ * @version    SVN: $Id$
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
@@ -87,17 +88,13 @@ function printInputFieldByType($key, $type)
             if ($faqconfig->get($key)) {
                 print ' checked="checked"';
             }
-            printf(' />&nbsp;%s', $PMF_LANG["ad_entry_active"]);
-            printf("<br />\n");
+            print " /><br />\n";
             break;
-
         case 'print':
 
-            print $faqconfig->get($key);
             printf('<input type="hidden" name="edit[%s]" size="80" value="%s" />',
                     $key,
                     str_replace('"', '&quot;', $faqconfig->get($key)));
-            printf("<br />\n");
             break;
     }
 }
