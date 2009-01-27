32d0451973c83e4100b1ea8ed0f889ebce23b541
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-01-27 20:17:09 +0000

    Code cleanup

--- phpmyfaq/admin/backup.main.php
+++ phpmyfaq/admin/backup.main.php
@@ -1,23 +1,24 @@
 <?php
 /**
-* $Id: backup.main.php,v 1.10 2008-01-26 08:23:06 thorstenr Exp $
-*
-* Frontend for Backup and Restore
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2003-02-24
-* @copyright    (c) 2001-2006 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * Frontend for Backup and Restore
+ *
+ * @package    phpMyFAQ
+ * @subpackage Administration
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2003-02-24
+ * @copyright  2003-2009 phpMyFAQ Team
+ * @version    SVN: $Id$
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
@@ -31,7 +32,7 @@ if ($permission['backup']) {
 	<fieldset>
         <legend><?php print $PMF_LANG["ad_csv_head"]; ?></legend>
         <p><?php print $PMF_LANG["ad_csv_make"]; ?></p>
-        <p align="center"><a href="attachment.php?action=sicherdaten"><?php print $PMF_LANG["ad_csv_linkdat"]; ?></a> | <a href="attachment.php?action=sicherlog"><?php print $PMF_LANG["ad_csv_linklog"]; ?></a></p>
+        <p align="center"><a href="attachment.php?action=savecontent"><?php print $PMF_LANG["ad_csv_linkdat"]; ?></a> | <a href="attachment.php?action=savelogs"><?php print $PMF_LANG["ad_csv_linklog"]; ?></a></p>
     </fieldset>
 
     <form method="post" action="?action=restore" enctype="multipart/form-data">
