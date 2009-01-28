4c37db053c8a0b20723deca828d95a7a6a7b0f07
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-01-28 18:26:49 +0000

    Init

---
+++ phpmyfaq/inc/PMF_Filter.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * ext/filter wrapper class
+ *
+ * @package    phpMyFAQ
+ * @subpackage Filter
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2009-01-28
+ * @copyright  2009 phpMyFAQ Team
+ * @version    SVN: $Id$
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+
+/**
+ * PMF_Filter
+ *
+ * @package    phpMyFAQ
+ * @subpackage Filter
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2009-01-28
+ * @copyright  2009 phpMyFAQ Team
+ * @version    SVN: $Id$
+ */
+class PMF_Filter
+{
+	/**
+	 * Static wrapper method for filter_input()
+	 *
+	 * @param integer $type          Filter type
+	 * @param string  $variable_name Variable name
+	 * @param integer $filter        Filter 
+	 * @param mixed   $default       Default value
+	 * @return mixed
+	 */
+	public static function filterInput($type, $variable_name, $filter, $default = null)
+	{
+		$return = filter_input($type, $variable_name, $filter);
+		
+		return is_null($returnValue) ? $default : $return;
+	}
+}
