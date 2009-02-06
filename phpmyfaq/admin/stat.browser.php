cacb2815b22a088ff01470a4bcb1a4be452cf3f7
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-02-06 16:12:31 +0000

    Code cleanup

--- phpmyfaq/admin/stat.browser.php
+++ phpmyfaq/admin/stat.browser.php
@@ -1,32 +1,33 @@
 <?php
-/******************************************************************************
- * File:				stat.browser.php
- * Description:			sessionbrowser
- * Authors:				Thorsten Rinne <thorsten@phpmyfaq.de>
- * Date:				2003-02-24
- * Last change:			2004-06-18
- * Copyright:           (c) 2001-2006 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+/**
+ * Sessionbrowser
+ * 
+ * @package    phpMyFAQ
+ * @subpackage Administration
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2003-02-24
+ * @copyright  2003-2009 phpMyFAQ Team
+ * @version    SVN: $Id$
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
     exit();
 }
 if ($permission['viewlog']) {
-    require_once(PMF_ROOT_DIR.'/inc/Session.php');
 
 	$perpage   = 50;
-	$day       = (int)$_POST['day'];
+	$day       = PMF_Filter::filterInput(INPUT_POST, 'day', FILTER_VALIDATE_INT);
 	$firstHour = mktime (0, 0, 0, date('m', $day), date('d', $day), date('Y', $day));
 	$lastHour  = mktime (23, 59, 59, date('m', $day), date('d', $day), date('Y', $day));
 
