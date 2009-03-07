c44d7eddfe283e63dde594f788cd3038e96d5ccc
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-03-07 12:05:06 +0000

    Added order by tag name

--- phpmyfaq/inc/Tags.php
+++ phpmyfaq/inc/Tags.php
@@ -1,25 +1,24 @@
 <?php
 /**
-* $Id: Tags.php,v 1.37.2.2 2007-08-12 13:55:48 thorstenr Exp $
-*
-* The main Tags class
-*
-* @package      phpMyFAQ
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @author       Matteo Scaramuccia <matteo@scaramuccia.com>
-* @since        2006-08-10
-* @copyright    (c) 2006-2007 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * The main Tags class
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @author    Matteo Scaramuccia <matteo@scaramuccia.com>
+ * @since     2006-08-10
+ * @copyright 2006-2009 phpMyFAQ Team
+ * @version   SVN: $Id$
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 class PMF_Tags
 {
@@ -143,7 +142,8 @@ class PMF_Tags
             WHERE
                 dt.record_id = %d
             AND
-                dt.tagging_id = t.tagging_id",
+                dt.tagging_id = t.tagging_id
+            ORDER BY t.tagging_name",
             SQLPREFIX,
             SQLPREFIX,
             $record_id
