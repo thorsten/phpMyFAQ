e1340b180158d4e1cee2c96ca85bc3a817f8a96f
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-03-15 10:40:10 +0000

    Fixed fatal error

--- phpmyfaq/inc/PMF_DB/Mysqli.php
+++ phpmyfaq/inc/PMF_DB/Mysqli.php
@@ -196,7 +196,7 @@ class PMF_DB_Mysqli implements PMF_DB_Driver
      * @author  Matteo scaramuccia <matteo@scaramuccia.com>
      * @since   2005-02-21
      */
-    public function search($table, $assoc, $joinedTable = '', $joinAssoc = array(), $match = array(), $string = '', $cond = array(), $orderBy = array())
+    public function search($table, Array $assoc, $joinedTable = '', Array $joinAssoc = array(), $match = array(), $string = '', Array $cond = array(), Array $orderBy = array())
     {
         $string = $this->escape_string(trim($string));
         $fields = "";
