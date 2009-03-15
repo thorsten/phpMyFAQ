86afc759ab2e70510926efa7de2357cac3727393
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-03-15 12:56:51 +0000

    Code cleanup

--- phpmyfaq/opensearch.php
+++ phpmyfaq/opensearch.php
@@ -1,32 +1,33 @@
 <?php
 /**
-* $Id: opensearch.php,v 1.5 2007-05-29 15:55:22 thorstenr Exp $
-*
-* This is XML code for OpenSearch
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2006-11-19
-* @copyright:   (c) 2006-2007 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * This is XML code for OpenSearch
+ *
+ * @package    phpMyFAQ
+ * @subpackage Frontend
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2006-11-19
+ * @version    SVN: $Id$
+ * @copyright: 2006-2009 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 define('PMF_ROOT_DIR', dirname(__FILE__));
 
-require_once(PMF_ROOT_DIR.'/inc/Init.php');
-require_once(PMF_ROOT_DIR.'/inc/Link.php');
+require_once PMF_ROOT_DIR.'/inc/Init.php';
+require_once PMF_ROOT_DIR.'/inc/Link.php';
 
-$plugin_icon    = $_SERVER['HTTP_HOST'].'.pmfsearch.png';
+$plugin_icon    = $_SERVER['HTTP_HOST'] . '.pmfsearch.png';
 $baseUrl        = PMF_Link::getSystemUri('/opensearch.php');
-$search_url     = $baseUrl.'/index.php?action=search';
+$search_url     = $baseUrl . '/index.php?action=search';
 $src_url        = $baseUrl;
 
 $opensearch     = "<?xml version=\"1.0\" encoding=\"".$PMF_LANG['metaCharset']."\"?>
