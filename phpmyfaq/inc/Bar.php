9c2a812a3283a5725382db9a509c8c4e2fbc99a7
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-03-30 14:40:39 +0000

    Init

---
+++ phpmyfaq/inc/Bar.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Bar image generation
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since     2009-03-30
+ * @version   SVN: $Id$
+ * @copyright 2009 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+
+/**
+ * PMF_Bar
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since     2009-03-30
+ * @version   SVN: $Id$
+ * @copyright 2009 phpMyFAQ Team
+ */
+class PMF_Bar
+{
+	/**
+	 * Image resource
+	 * 
+	 * @var resource
+	 */
+	private $image = null;
+	
+	/**
+	 * Number
+	 * 
+	 * @var float
+	 */
+	private $number = 0;
+	
+	/**
+	 * Colored image
+	 * 
+	 * @var boolean
+	 */
+	private $colored = false;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param  float   $number  Number for the bar
+	 * @param  boolean $colored Image colored? default: false
+	 * @return void
+	 */
+	public function __construct($number, $colored = false)
+	{
+		$this->number  = $number;
+		$this->colored = $colored;
+	}
+	
+	/**
+	 * Rendering of the image
+	 * 
+	 * @return boolean
+	 */
+	public function renderImage()
+	{
+		header ('Content-type: image/png');
+        imagepng($this->image);
+	}
+}
