474f1ff0ed0e959ec23ec3f321bb9f7fccc7334a
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-03-30 18:03:10 +0000

    Code cleanup

--- phpmyfaq/send2friend.php
+++ phpmyfaq/send2friend.php
@@ -1,23 +1,24 @@
 <?php
 /**
-* $Id: send2friend.php,v 1.17 2008-05-23 13:06:06 thorstenr Exp $
-*
-* The send2friend page
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2002-09-16
-* @copyright    (c) 2002-2007 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * The send2friend page
+ *
+ * @package    phpMyFAQ
+ * @subpackage Frontend
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2002-09-16
+ * @version    SVN: $Id$
+ * @copyright  2002-2009 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
@@ -45,21 +46,21 @@ $send2friendLink = sprintf('http://%s%s?action=artikel&amp;cat=%d&amp;id=%d&amp;
     urlencode($artlang));
 
 $tpl->processTemplate ('writeContent', array(
-    'msgSend2Friend' => $PMF_LANG['msgSend2Friend'],
-    'writeSendAdress' => $_SERVER['PHP_SELF'].'?'.$sids.'action=mailsend2friend',
-    'msgS2FReferrer' => 'link',
-    'msgS2FName' => $PMF_LANG['msgS2FName'],
-    'msgS2FEMail' => $PMF_LANG['msgS2FEMail'],
+    'msgSend2Friend'     => $PMF_LANG['msgSend2Friend'],
+    'writeSendAdress'    => $_SERVER['PHP_SELF'].'?'.$sids.'action=mailsend2friend',
+    'msgS2FReferrer'     => 'link',
+    'msgS2FName'         => $PMF_LANG['msgS2FName'],
+    'msgS2FEMail'        => $PMF_LANG['msgS2FEMail'],
     'defaultContentMail' => ($user instanceof PMF_User_CurrentUser) ? $user->getUserData('email') : '',
     'defaultContentName' => ($user instanceof PMF_User_CurrentUser) ? $user->getUserData('display_name') : '',
-    'msgS2FFriends' => $PMF_LANG['msgS2FFriends'],
-    'msgS2FEMails' => $PMF_LANG['msgS2FEMails'],
-    'msgS2FText' => $PMF_LANG['msgS2FText'],
-    'send2friend_text' => PMF_htmlentities($PMF_CONF['main.send2friendText'], ENT_QUOTES, $PMF_LANG['metaCharset']),
-    'msgS2FText2' => $PMF_LANG['msgS2FText2'],
-    'send2friendLink' => $send2friendLink,
-    'msgS2FMessage' => $PMF_LANG['msgS2FMessage'],
-    'captchaFieldset' => printCaptchaFieldset($PMF_LANG['msgCaptcha'], $captcha->printCaptcha('send2friend'), $captcha->caplength),
-    'msgS2FButton' => $PMF_LANG['msgS2FButton']));
+    'msgS2FFriends'      => $PMF_LANG['msgS2FFriends'],
+    'msgS2FEMails'       => $PMF_LANG['msgS2FEMails'],
+    'msgS2FText'         => $PMF_LANG['msgS2FText'],
+    'send2friend_text'   => PMF_htmlentities($PMF_CONF['main.send2friendText'], ENT_QUOTES, $PMF_LANG['metaCharset']),
+    'msgS2FText2'        => $PMF_LANG['msgS2FText2'],
+    'send2friendLink'    => $send2friendLink,
+    'msgS2FMessage'      => $PMF_LANG['msgS2FMessage'],
+    'captchaFieldset'    => printCaptchaFieldset($PMF_LANG['msgCaptcha'], $captcha->printCaptcha('send2friend'), $captcha->caplength),
+    'msgS2FButton'       => $PMF_LANG['msgS2FButton']));
 
 $tpl->includeTemplate('writeContent', 'index');
