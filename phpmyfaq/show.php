15a1a8d273dc3cce6e17c4bab500884082c90dbc
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-03-30 18:06:30 +0000

    Removed direct $_REQUEST access

--- phpmyfaq/show.php
+++ phpmyfaq/show.php
@@ -1,32 +1,33 @@
 <?php
 /**
-* $Id: show.php,v 1.16 2008-05-23 13:06:06 thorstenr Exp $
-*
-* @author       Thorsten Rinne <thorsten@phpmyfaq.de>
-* @since        2002-08-27
-* @copyright    (c) 2002-2007 phpMyFAQ Team
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.1 (the 'License'); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an 'AS IS'
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-*/
+ * Frontend for categories or list of records
+ *
+ * @package    phpMyFAQ
+ * @subpackage Frontend
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2002-08-27
+ * @version    SVN: $Id$
+ * @copyright  2002-2009 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the 'License'); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an 'AS IS'
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
     header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
     exit();
 }
 
-if (isset($_REQUEST['cat']) && is_numeric($_REQUEST['cat'])) {
-    $currentCategory = (int)$_REQUEST['cat'];
-}
+$currentCategory = PMF_Filter::filterInput(INPUT_GET, 'cat', FILTER_VALIDATE_INT);
 
-if (isset($currentCategory) && $currentCategory != 0 && isset($category->categoryName[$currentCategory])) {
+if (!is_null($currentCategory) && isset($category->categoryName[$currentCategory])) {
 
     $faqsession->userTracking('show_category', $currentCategory);
     $parent  = $category->categoryName[$currentCategory]['parent_id'];
@@ -52,18 +53,17 @@ if (isset($currentCategory) && $currentCategory != 0 && isset($category->categor
     }
 
     $tpl->processTemplate('writeContent', array(
-                        'writeCategory' => $PMF_LANG['msgEntriesIn'].$name,
-                        'writeThemes' => $records,
-                        'writeOneThemeBack' => $up));
+        'writeCategory'     => $PMF_LANG['msgEntriesIn'].$name,
+        'writeThemes'       => $records,
+        'writeOneThemeBack' => $up));
     $tpl->includeTemplate('writeContent', 'index');
 
 } else {
 
     $faqsession->userTracking('show_all_categories', 0);
     $tpl->processTemplate('writeContent', array(
-                          'writeCategory' => $PMF_LANG['msgFullCategories'],
-                          'writeThemes' => $category->viewTree(),
-                          'writeOneThemeBack' => ''));
+        'writeCategory'     => $PMF_LANG['msgFullCategories'],
+        'writeThemes'       => $category->viewTree(),
+        'writeOneThemeBack' => ''));
     $tpl->includeTemplate('writeContent', 'index');
-
 }
