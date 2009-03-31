709309e1ad4624ea9aa19764478ce251f05efc97
anatoliy <anatoliy@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-03-31 09:22:05 +0000

    using new bar functionality also with rating statistics

--- phpmyfaq/admin/stat.bar.php
+++ phpmyfaq/admin/stat.bar.php
@@ -27,30 +27,7 @@ PMF_Init::cleanRequest();
 session_name(PMF_COOKIE_NAME_AUTH . trim($faqconfig->get('main.phpMyFAQToken')));
 session_start();
 
-$image           = @imagecreate (50, 15) or die ("Sorry, but phpMyFAQ cannot initialize a new image stream.");
-$backgroundColor = imagecolorallocate ($image, 211, 211, 211);
-$textColor       = imagecolorallocate ($image, 0, 0, 0);
-$num             = PMF_Filter::filterInput(INPUT_GET, 'num', FILTER_VALIDATE_FLOAT);
-
-if (!is_null($num)) {
-    $num = round($num * 20);
-    if ($num < 25) {
-        $textColor = imagecolorallocate ($image, 255, 255, 255);
-        $barColor  = imagecolorallocate ($image, 255, 0, 0);
-        imagefilledrectangle ($image, 0, 0, round(($num/100)*50), 15, $barColor);
-    } elseif ($num > 75) {
-        $textColor = imagecolorallocate ($image, 255, 255, 255);
-        $barColor  = imagecolorallocate ($image, 0, 128, 0);
-        imagefilledrectangle ($image, 0, 0, round(($num/100)*50), 15, $barColor);
-    } elseif ($num <= 75 && $num >= 25) {
-        $textColor = imagecolorallocate ($image, 255, 255, 255);
-        $barColor  = imagecolorallocate ($image, 150, 150, 150);
-        imagefilledrectangle ($image, 0, 0, round(($num/100)*50), 15, $barColor);
-    }
-    imagestring ($image, 2, 1, 1, $num . '%', $textColor);
-} else {
-    imagestring ($image, 1, 5, 5, 'n/a', $textColor);
-}
-
-header ('Content-type: image/png');
-imagepng($image);
+$num = PMF_Filter::filterInput(INPUT_GET, 'num', FILTER_VALIDATE_FLOAT);
+$bar = new PMF_Bar($num*20, true);
+$bar->renderImage();
+exit;
