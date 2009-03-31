c19308cd259d510670e48fb4e64908902a52d5f8
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-03-31 10:35:46 +0000

    Init

---
+++ phpmyfaq/admin/ajax.records.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * AJAX: handling of Ajax record calls
+ * 
+ * @package    phpMyFAQ
+ * @subpackage Administration
+ * @author     Anatoliy Belsky <anatoliy.belsky@mayflower.de>
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2009-03-31
+ * @copyright  2009 phpMyFAQ Team
+ * @version    SVN: $Id$
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+
+if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
+    header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
+    exit();
+}
+
+$ajax_action = PMF_Filter::filterInput(INPUT_POST, 'ajaxaction', FILTER_SANITIZE_STRING);
+
+if ('save_sticky_records' == $ajax_action && $permission['delcomment']) {
+    
+}
