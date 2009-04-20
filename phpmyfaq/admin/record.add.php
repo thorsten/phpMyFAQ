0e1e93477c5172996d392c1d4b7e8e17d14940e2
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-04-20 19:08:01 +0000

    Removed direct access to $_POST

--- phpmyfaq/admin/record.add.php
+++ phpmyfaq/admin/record.add.php
@@ -29,17 +29,38 @@ if (!defined('IS_VALID_PHPMYFAQ_ADMIN')) {
 // Re-evaluate $user
 $user = PMF_User_CurrentUser::getFromSession($faqconfig->get('main.ipCheck'));
 
-// Evaluate the passed validity range, if any
-$dateStart = PMF_Filter::filterInput(INPUT_POST, 'dateStart', FILTER_SANITIZE_STRING, '00000000000000');
-$dateEnd   = PMF_Filter::filterInput(INPUT_POST, 'dateEnd', FILTER_SANITIZE_STRING, '99991231235959');
-
-if ($permission["editbt"]) {
-    $submit = $_POST["submit"];
-
-    if (    isset($submit[1])
-         && isset($_POST["thema"]) && $_POST["thema"] != ""
-         && isset($_POST['rubrik']) && is_array($_POST['rubrik'])
-       ) {
+if ($permission['editbt']) {
+	
+	// Get submit action
+    $submit        = PMF_Filter::filterInputArray(INPUT_POST, array('submit' => array('filter' => FILTER_VALIDATE_INT,
+                                                                                      'flags'  => FILTER_REQUIRE_ARRAY)));
+    // FAQ data
+    $dateStart     = PMF_Filter::filterInput(INPUT_POST, 'dateStart', FILTER_SANITIZE_STRING);
+    $dateEnd       = PMF_Filter::filterInput(INPUT_POST, 'dateEnd', FILTER_SANITIZE_STRING);
+    $question      = PMF_Filter::filterInput(INPUT_POST, 'thema', FILTER_SANITIZE_STRING);
+    $categories    = PMF_Filter::filterInputArray(INPUT_POST, array('rubrik' => array('filter' => FILTER_VALIDATE_INT,
+                                                                                      'flags'  => FILTER_REQUIRE_ARRAY)));
+    $language      = PMF_Filter::filterInput(INPUT_POST, 'language', FILTER_SANITIZE_STRING);
+    $tags          = PMF_Filter::filterInput(INPUT_POST, 'tags', FILTER_SANITIZE_STRING);
+    $active        = PMF_Filter::filterInput(INPUT_POST, 'active', FILTER_SANITIZE_STRING);
+    $sticky        = PMF_Filter::filterInput(INPUT_POST, 'sticky', FILTER_SANITIZE_STRING);
+    $content       = PMF_Filter::filterInput(INPUT_POST, 'content', FILTER_SANITIZE_STRING);
+    $keywords      = PMF_Filter::filterInput(INPUT_POST, 'keywords', FILTER_SANITIZE_STRING);
+    $author        = PMF_Filter::filterInput(INPUT_POST, 'author', FILTER_SANITIZE_STRING);
+    $email         = PMF_Filter::filterInput(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $comment       = PMF_Filter::filterInput(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
+    $record_id     = PMF_Filter::filterInput(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+    $solution_id   = PMF_Filter::filterInput(INPUT_POST, 'solution_id', FILTER_VALIDATE_INT);
+    $revision_id   = PMF_Filter::filterInput(INPUT_POST, 'revision_id', FILTER_VALIDATE_INT);
+    $changed       = PMF_Filter::filterInput(INPUT_POST, 'changed', FILTER_SANITIZE_STRING);
+    
+    // Permissions
+    $userperm      = PMF_Filter::filterInput(INPUT_POST, 'userpermission', FILTER_SANITIZE_STRING);
+    $user_allowed  = ('all' == $userperm) ? -1 : PMF_Filter::filterInput(INPUT_POST, 'restricted_users', FILTER_VALIDATE_INT);
+    $groupperm     = PMF_Filter::filterInput(INPUT_POST, 'grouppermission', FILTER_SANITIZE_STRING);
+    $group_allowed = ('all' == $groupperm) ? -1 : PMF_Filter::filterInput(INPUT_POST, 'restricted_groups', FILTER_VALIDATE_INT);
+            
+    if (isset($submit['submit'][1]) && !is_null($question) && !is_null($categories)) {
         // new entry
         adminlog("Beitragcreatesave");
         printf("<h2>%s</h2>\n", $PMF_LANG['ad_entry_aor']);
@@ -47,49 +68,38 @@ if ($permission["editbt"]) {
         $category = new PMF_Category($current_admin_user, $current_admin_groups, false);
         $tagging  = new PMF_Tags();
 
-        // Get the data
-        $categories     = $_POST['rubrik'];
-        $tags           = $db->escape_string(trim($_POST['tags']));
-        $userperm       = isset($_POST['userpermission']) ?
-                          $db->escape_string($_POST['userpermission']) : 'all';
-        $user_allowed   = ('all' == $userperm) ? -1 : (int)$_POST['restricted_users'];
-        $groupperm      = isset($_POST['grouppermission']) ?
-                          $db->escape_string($_POST['grouppermission']) : 'all';
-        $group_allowed  = ('all' == $groupperm) ? -1 : (int)$_POST['restricted_groups'];
-
         $recordData     = array(
-            'lang'          => $db->escape_string($_POST['language']),
-            'active'        => $db->escape_string($_POST['active']),
-            'sticky'        => (int)isset($_POST['sticky']) && 'on' == $_POST['sticky'],
-            'thema'         => $db->escape_string($_POST['thema']),
-            'content'       => $db->escape_string($_POST['content']),
-            'keywords'      => $db->escape_string($_POST['keywords']),
-            'author'        => $db->escape_string($_POST['author']),
-            'email'         => $db->escape_string($_POST['email']),
-            'comment'       => (isset($_POST['comment']) ? 'y' : 'n'),
+            'lang'          => $language,
+            'active'        => $active,
+            'sticky'        => (int)$sticky,
+            'thema'         => $question,
+            'content'       => $content,
+            'keywords'      => $keywords,
+            'author'        => $author,
+            'email'         => $email,
+            'comment'       => (!is_null($comment) ? 'y' : 'n'),
             'date'          => date('YmdHis'),
-            'dateStart'     => $db->escape_string($dateStart),
-            'dateEnd'       => $db->escape_string($dateEnd),
+            'dateStart'     => (empty($dateStart) ? '00000000000000' : $dateStart),
+            'dateEnd'       => (empty($dateEnd) ? '99991231235959' : $dateEnd),
             'linkState'     => '',
-            'linkDateCheck' => 0
-        );
-
+            'linkDateCheck' => 0);
 
         // Add new record and get that ID
         $record_id = $faq->addRecord($recordData);
 
         if ($record_id) {
             // Create ChangeLog entry
-            $faq->createChangeEntry($record_id, $user->getUserId(), nl2br($db->escape_string($_POST['changed'])), $recordData['lang']);
+            $faq->createChangeEntry($record_id, $user->getUserId(), nl2br($changed), $recordData['lang']);
             // Create the visit entry
             $visits = PMF_Visits::getInstance();
             $visits->add($record_id, $recordData['lang']);
             // Insert the new category relations
-            $faq->addCategoryRelations($categories, $record_id, $recordData['lang']);
+            $faq->addCategoryRelations($categories['rubrik'], $record_id, $recordData['lang']);
             // Insert the tags
             if ($tags != '') {
                 $tagging->saveTags($record_id, explode(',',$tags));
             }
+            
             // Add user permissions
             $faq->addPermission('user', $record_id, $user_allowed);
             $category->addPermission('user', $categories, $user_allowed);
@@ -107,91 +117,80 @@ if ($permission["editbt"]) {
             print $PMF_LANG['ad_entry_savedfail'].$db->error();
         }
 
-    } elseif (    isset($submit[2])
-               && isset($_POST['thema']) && $_POST['thema'] != ""
-               && isset($_POST['rubrik']) && is_array($_POST['rubrik'])
-             ) {
+    } elseif (isset($submit['submit'][2]) && !is_null($question) && !is_null($categories)) {
         // Preview
-        $rubrik = $_POST['rubrik'];
         $cat = new PMF_Category($current_admin_user, $current_admin_groups, false);
         $cat->transform(0);
         $categorylist = '';
-        foreach ($rubrik as $categories) {
-            $categorylist .= $cat->getPath($categories).'<br />';
+        foreach ($categories['rubrik'] as $_categories) {
+            $categorylist .= $cat->getPath($_categories).'<br />';
         }
-        if (isset($_REQUEST['id']) && $_REQUEST['id'] != '') {
-            $id = $_REQUEST['id'];
-        } else {
-            $id = '';
-        }
-        $content = $_POST['content'];
 ?>
     <h3><strong><em><?php print $categorylist; ?></em>
-    <?php print $_POST["thema"]; ?></strong></h3>
+    <?php print $question; ?></strong></h3>
     <?php print $content; ?>
     <p class="little"><?php print $PMF_LANG["msgLastUpdateArticle"].makeDate(date("YmdHis")); ?><br />
     <?php print $PMF_LANG["msgAuthor"].' '.$_POST["author"]; ?></p>
 
     <form action="?action=editpreview" method="post">
     <input type="hidden" name="id"                  value="<?php print $id; ?>" />
-    <input type="hidden" name="thema"               value="<?php print htmlspecialchars($_POST['thema']); ?>" />
-    <input type="hidden" name="content" class="mceNoEditor" value="<?php print htmlspecialchars($_POST['content']); ?>" />
-    <input type="hidden" name="lang"                value="<?php print $_POST['language']; ?>" />
-    <input type="hidden" name="keywords"            value="<?php print $_POST['keywords']; ?>" />
-    <input type="hidden" name="tags"                value="<?php print $_POST['tags']; ?>" />
-    <input type="hidden" name="author"              value="<?php print $_POST['author']; ?>" />
-    <input type="hidden" name="email"               value="<?php print $_POST['email']; ?>" />
-    <input type="hidden" name="sticky"              value="<?php print (isset($_REQUEST['sticky']) ? $_REQUEST['sticky'] : ''); ?>" />
+    <input type="hidden" name="thema"               value="<?php print htmlspecialchars($question); ?>" />
+    <input type="hidden" name="content" class="mceNoEditor" value="<?php print htmlspecialchars($content); ?>" />
+    <input type="hidden" name="lang"                value="<?php print $language; ?>" />
+    <input type="hidden" name="keywords"            value="<?php print $keywords; ?>" />
+    <input type="hidden" name="tags"                value="<?php print $tags; ?>" />
+    <input type="hidden" name="author"              value="<?php print $author; ?>" />
+    <input type="hidden" name="email"               value="<?php print $email; ?>" />
+    <input type="hidden" name="sticky"              value="<?php print (!is_null($sticky) ? $sticky : ''); ?>" />
 <?php
-        foreach ($rubrik as $key => $categories) {
-            print '    <input type="hidden" name="rubrik['.$key.']" value="'.$categories.'" />';
+        foreach ($categories['rubrik'] as $key => $_categories) {
+            print '    <input type="hidden" name="rubrik['.$key.']" value="'.$_categories.'" />';
         }
 ?>
-    <input type="hidden" name="solution_id"         value="<?php print (int)$_POST['solution_id']; ?>" />
-    <input type="hidden" name="revision"            value="<?php print (isset($_POST['revision']) ? (int)$_POST['revision'] : ''); ?>" />
-    <input type="hidden" name="active"              value="<?php print $_POST['active']; ?>" />
-    <input type="hidden" name="changed"             value="<?php print $_POST['changed']; ?>" />
-    <input type="hidden" name="comment"             value="<?php print (isset($_POST['comment']) ? $_POST['comment'] : ''); ?>" />
+    <input type="hidden" name="solution_id"         value="<?php print $solution_id; ?>" />
+    <input type="hidden" name="revision"            value="<?php print $revision_id; ?>" />
+    <input type="hidden" name="active"              value="<?php print $active; ?>" />
+    <input type="hidden" name="changed"             value="<?php print $changed; ?>" />
+    <input type="hidden" name="comment"             value="<?php print $comment; ?>" />
     <input type="hidden" name="dateStart"           value="<?php print $dateStart; ?>" />
     <input type="hidden" name="dateEnd"             value="<?php print $dateEnd; ?>" />
-    <input type="hidden" name="userpermission"      value="<?php print $_POST['userpermission']; ?>" />
-    <input type="hidden" name="restricted_users"    value="<?php print $_POST['restricted_users']; ?>" />
-    <input type="hidden" name="grouppermission"     value="<?php print $_POST['grouppermission']; ?>" />
-    <input type="hidden" name="restricted_group"    value="<?php print $_POST['restricted_group']; ?>" />
+    <input type="hidden" name="userpermission"      value="<?php print $user_permission; ?>" />
+    <input type="hidden" name="restricted_users"    value="<?php print $restricted_users; ?>" />
+    <input type="hidden" name="grouppermission"     value="<?php print $group_permission; ?>" />
+    <input type="hidden" name="restricted_group"    value="<?php print $restricted_groups; ?>" />
     <p align="center"><input class="submit" type="submit" name="submit" value="<?php print $PMF_LANG["ad_entry_back"]; ?>" /></p>
     </form>
 <?php
     } else {
         printf("<h2>%s</h2>\n", $PMF_LANG['ad_entry_aor']);
         printf("<p>%s</p>", $PMF_LANG['ad_entryins_fail']);
-        $rubrik = isset($_POST['rubrik']) ? $_POST['rubrik'] : null;
 ?>
     <form action="?action=editpreview" method="post">
-    <input type="hidden" name="thema"               value="<?php print htmlspecialchars($_POST['thema']); ?>" />
-    <input type="hidden" name="content" class="mceNoEditor" value="<?php print htmlspecialchars($_POST['content']); ?>" />
-    <input type="hidden" name="lang"                value="<?php print $_POST['language']; ?>" />
-    <input type="hidden" name="keywords"            value="<?php print $_POST['keywords']; ?>" />
-    <input type="hidden" name="tags"                value="<?php print $_POST['tags']; ?>" />
-    <input type="hidden" name="author"              value="<?php print $_POST['author']; ?>" />
-    <input type="hidden" name="email"               value="<?php print $_POST['email']; ?>" />
+    <input type="hidden" name="thema"               value="<?php print htmlspecialchars($question); ?>" />
+    <input type="hidden" name="content" class="mceNoEditor" value="<?php print htmlspecialchars($content); ?>" />
+    <input type="hidden" name="lang"                value="<?php print $language; ?>" />
+    <input type="hidden" name="keywords"            value="<?php print $keywords; ?>" />
+    <input type="hidden" name="tags"                value="<?php print $tags; ?>" />
+    <input type="hidden" name="author"              value="<?php print $author; ?>" />
+    <input type="hidden" name="email"               value="<?php print $email; ?>" />
 <?php
-        if (is_array($rubrik)) {
-            foreach ($rubrik as $key => $categories) {
-                print '    <input type="hidden" name="rubrik['.$key.']" value="'.$categories.'" />';
+        if (is_array($categories['rubrik'])) {
+            foreach ($categories['rubrik'] as $key => $_categories) {
+                print '    <input type="hidden" name="rubrik['.$key.']" value="'.$_categories.'" />';
             }
         }
 ?>
-    <input type="hidden" name="solution_id"         value="<?php print $_POST['solution_id']; ?>" />
-    <input type="hidden" name="revision"            value="<?php print $_POST['revision']; ?>" />
-    <input type="hidden" name="active"              value="<?php print $_POST['active']; ?>" />
-    <input type="hidden" name="changed"             value="<?php print $_POST['changed']; ?>" />
-    <input type="hidden" name="comment"             value="<?php print isset($_POST['comment']) ? $_POST['comment'] : ''; ?>" />
+    <input type="hidden" name="solution_id"         value="<?php print $solution_id; ?>" />
+    <input type="hidden" name="revision"            value="<?php print $revision_id; ?>" />
+    <input type="hidden" name="active"              value="<?php print $active; ?>" />
+    <input type="hidden" name="changed"             value="<?php print $changed; ?>" />
+    <input type="hidden" name="comment"             value="<?php print $comment; ?>" />
     <input type="hidden" name="dateStart"           value="<?php print $dateStart; ?>" />
     <input type="hidden" name="dateEnd"             value="<?php print $dateEnd; ?>" />
-    <input type="hidden" name="userpermission"      value="<?php print $_POST['userpermission']; ?>" />
-    <input type="hidden" name="restricted_users"    value="<?php print $_POST['restricted_users']; ?>" />
-    <input type="hidden" name="grouppermission"     value="<?php print $_POST['grouppermission']; ?>" />
-    <input type="hidden" name="restricted_group"    value="<?php print $_POST['restricted_group']; ?>" />
+    <input type="hidden" name="userpermission"      value="<?php print $user_permission; ?>" />
+    <input type="hidden" name="restricted_users"    value="<?php print $restricted_users; ?>" />
+    <input type="hidden" name="grouppermission"     value="<?php print $group_permission; ?>" />
+    <input type="hidden" name="restricted_group"    value="<?php print $restricted_groups; ?>" />
     <p align="center"><input class="submit" type="submit" name="submit" value="<?php print $PMF_LANG['ad_entry_back']; ?>" /></p>
     </form>
 <?php
