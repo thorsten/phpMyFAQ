a0adbd49118c975139996b8ad31cd2b7d746520a
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-07-21 09:36:55 +0000

    Removed relative urls and added phpMyFAQ stylesheet in Editor

--- phpmyfaq/admin/footer.php
+++ phpmyfaq/admin/footer.php
@@ -67,21 +67,21 @@ tinyMCE.init({
     theme_advanced_toolbar_align : "left",
     theme_advanced_statusbar_location : "bottom",
     theme_advanced_resizing : true,
-    
-    remove_linebreaks : false, 
+    relative_urls           : false,
+    remove_linebreaks       : false, 
     extended_valid_elements : "code",
 
     // Ajax-based file manager
     file_browser_callback : "ajaxfilemanager",
 
     // Example content CSS (should be your site CSS)
-    content_css : "css/example.css",
+    content_css : "../template/style.css",
 
     // Drop lists for link/image/media/template dialogs
     template_external_list_url : "js/template_list.js",
-    external_link_list_url : "js/link_list.js",
-    external_image_list_url : "js/image_list.js",
-    media_external_list_url : "js/media_list.js",
+    external_link_list_url     : "js/link_list.js",
+    external_image_list_url    : "js/image_list.js",
+    media_external_list_url    : "js/media_list.js",
 
     // Replace values for the template plugin
     template_replace_values : {
