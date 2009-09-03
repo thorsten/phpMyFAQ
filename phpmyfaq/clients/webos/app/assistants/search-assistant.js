d2df0881d0d19ee6e14a1d5ffea870069cc67fe6
max <max@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-09-03 14:34:13 +0000

    added layout

--- phpmyfaq/clients/webos/app/assistants/search-assistant.js
+++ phpmyfaq/clients/webos/app/assistants/search-assistant.js
@@ -2,6 +2,43 @@ function SearchAssistant() {
 }
 
 SearchAssistant.prototype.setup = function(){
+    this.searchField = {
+            hintText:         'Searchkey',
+            textFieldName:    'search', 
+            modelProperty:    'original', 
+            multiline:        false,
+            disabledProperty: 'disabled',
+            focus:            true, 
+            modifierState:    Mojo.Widget.capsLock,
+            //autoResize:     automatically grow or shrink the textbox horizontally,
+            //autoResizeMax:  how large horizontally it can get
+            //enterSubmits:   when used in conjunction with multline, if this is set, then enter will submit rather than newline
+            limitResize:      false, 
+            holdToEnable:     false, 
+            focusMode:        Mojo.Widget.focusSelectMode,
+            changeOnKeyPress: true,
+            textReplacement:  false,
+            maxLength:        30,
+            requiresEnterKey: false
+        };
+    this.submitField = {
+            //type : 'Activity'
+    };
+    
+    this.searchModel = {
+            value:    "",
+            disabled: false
+    };
+
+    this.submitModel = {
+            buttonLabel : 'Search', 
+            buttonClass : '',
+            disable : false
+    };
+
+    this.controller.setupWidget('search', this.searchField, this.searchModel);
+    this.controller.setupWidget('submit', this.submitField, this.submitModel);
+
 }
 SearchAssistant.prototype.handleButtonPress = function(event){
 }
