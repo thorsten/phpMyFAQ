fdd73c902a161bc5249f520f20b84805be486c45
thorsten <thorsten@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-09-03 11:58:56 +0000

    Init

---
+++ phpmyfaq/restservice.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The REST Services interface
+ *
+ * @package    phpMyFAQ 
+ * @subpackage PMF_Service
+ * @author     Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since      2009-09-03
+ * @copyright  2009 phpMyFAQ Team
+ * @version    SVN: $Id$
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+
+//
+// Prepend and start the PHP session
+//
+require_once 'inc/Init.php';
+define('IS_VALID_PHPMYFAQ', null);
+PMF_Init::cleanRequest();
+session_name(PMF_COOKIE_NAME_AUTH . trim($faqconfig->get('main.phpMyFAQToken')));
+session_start();
