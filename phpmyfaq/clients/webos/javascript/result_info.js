da5fb071f5845eca0ac19c9da4dfca96761c14cd
noon <noon@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-09-03 13:59:36 +0000

    add result demo array

--- phpmyfaq/clients/webos/javascript/result_info.js
+++ phpmyfaq/clients/webos/javascript/result_info.js
@@ -1,1 +1,29 @@
 _APP_Name = 'phpMyFAQ WebOS'
+
+    
+_Result = [
+ { 
+     id: '1',
+     string: 'Result1'
+ },
+ { 
+	 id: '2',
+     string: 'Result1'
+ },
+ { 
+	 id: '3',
+     string: 'Result1'
+ },
+ { 
+	 id: '4',
+     string: 'Result1'
+ },
+ { 
+	 id: '5',
+     string: 'Result1'
+ },
+ { 
+	 id: '6',
+     string: 'Result1'
+     },
+ ];
