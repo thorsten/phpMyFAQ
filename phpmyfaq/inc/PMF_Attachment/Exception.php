eca5ca2876823516dcccdb61a94785baed3bd444
anatoliy <anatoliy@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-09-06 10:41:12 +0000

    added PMF_Attachment_Exception

---
+++ phpmyfaq/inc/PMF_Attachment/Exception.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Attachment exception
+ *
+ * @package    phpMyFAQ
+ * @license    MPL
+ * @author     Anatoliy Belsky <ab@php.net>
+ * @since      2009-08-21
+ * @version    SVN: $Id: Exception.php 4459 2009-06-10 15:57:47Z thorsten $
+ * @copyright  2009 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+/**
+ * PMF_Atachment_Exception
+ * 
+ * @package    phpMyFAQ
+ * @license    MPL
+ * @author     Anatoliy Belsky <ab@php.net>
+ * @since      2009-08-21
+ * @version    SVN: $Id: Exception.php 4459 2009-06-10 15:57:47Z thorsten $
+ * @copyright  2009 phpMyFAQ Team
+ */
+class PMF_Attachment_Exception extends Exception
+{
+    
+}
