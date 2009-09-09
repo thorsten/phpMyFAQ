855d52f672a26f357f9744e3cc3a957108b80a9c
anatoliy <anatoliy@838f1ae0-46ae-11dd-8317-05e2edd5b3f7>
2009-09-09 18:10:40 +0000

    added new attachment configs

--- phpmyfaq/lang/language_en.php
+++ phpmyfaq/lang/language_en.php
@@ -1039,6 +1039,13 @@ $PMF_LANG["rightsLanguage"]['edittranslation'] = "edit translation";
 $PMF_LANG["rightsLanguage"]['deltranslation'] = "delete translation";
 $PMF_LANG["rightsLanguage"]['approverec'] = "approve records";
 
+// added 2.6.0-alpha - 2009-09-9 by Anatoliy Belsky
+$LANG_CONF["main.enableAttachmentEncryption"] = array(0 => "checkbox", 1 => "Enable attachment encryption <br><small>Ignored when attachments is disabled</small>");
+$LANG_CONF["main.defaultAttachmentEncKey"] = array(0 => "input", 1 => 'Default attachment encryption key <br><small>Ignored if attachment encryption is disabled</small><br><small><font color="red">WARNING: Do not change this once set and enabled file encryption!!!</font></small>');
+$LANG_CONF["main.attachmentsStorageType"] = array(0 => "select", 1 => "Attachment storage type");
+$PMF_LANG['att_storage_type'][0] = 'Filesystem';
+//$PMF_LANG['att_storage_type'][1] = 'Database';
+
 // added 2.6.0-alpha - 2009-09-06 by Thorsten
 $PMF_LANG['ad_menu_upgrade'] = 'Upgrade';
 $PMF_LANG['ad_you_shouldnt_update'] = 'You have the latest version of phpMyFAQ. You do not need to upgrade.';
