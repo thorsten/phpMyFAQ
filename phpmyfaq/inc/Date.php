ab6c1baf0ffc4353e99dc4bd741fd44bc76a6446
Thorsten Rinne <thorsten@phpmyfaq.de>
2009-09-24 07:42:47 +0200

    Added skeleton class for new PMF_Date class

---
+++ phpmyfaq/inc/Date.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * phpMyFAQ Date class
+ *
+ * @category  phpMyFAQ
+ * @package   PMF_Date
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since     2009-09-24
+ * @version   git: $Id$
+ * @copyright 2009 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+
+/**
+ * PMF_Date
+ *
+ * @category  phpMyFAQ
+ * @package   PMF_Date
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since     2009-09-24
+ * @copyright 2009 phpMyFAQ Team
+ */
+class PMF_Date
+{
+	
+}
