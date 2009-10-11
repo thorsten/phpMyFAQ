b801ae1be51fede1505726fd53bca1ca8f0e72db
Thorsten Rinne <thorsten@phpmyfaq.de>
2009-10-11 11:52:57 +0200

    Added new XHTML generation

--- phpmyfaq/inc/PMF_Export/Xhtml.php
+++ phpmyfaq/inc/PMF_Export/Xhtml.php
@@ -33,5 +33,94 @@
  */
 class PMF_Export_Xhtml extends PMF_Export 
 {
+    /**
+     * XMLWriter object
+     * 
+     * @var XMLWriter
+     */
+    private $xml = null;
     
+    /**
+     * Constructor
+     * 
+     * @param PMF_Faq      $faq      PMF_Faq object
+     * @param PMF_Category $category PMF_Category object 
+     * 
+     * return PMF_Export_Xhtml
+     */
+    public function __construct(PMF_Faq $faq, PMF_Category $category)
+    {
+        $this->faq      = $faq;
+        $this->category = $category;
+        $this->xml      = new XMLWriter();
+        
+        $this->xml->openMemory();
+    }
+    
+    /**
+     * Generates the export
+     * 
+     * @param integer $categoryId Category Id
+     * @param boolean $downwards  If true, downwards, otherwise upward ordering
+     * @param string  $language   Language
+     * 
+     * @return string
+     */
+    public function generate($categoryId = 0, $downwards = true, $language = '')
+    {
+    	global $PMF_LANG;
+    	
+        // Initialize categories
+        $this->category->transform($categoryId);
+        
+        $faqdata = $this->faq->get(FAQ_QUERY_TYPE_EXPORT_XML, $categoryId, $downwards, $language);
+        $version = PMF_Configuration::getInstance()->get('main.currentVersion');
+        $comment = sprintf('XHTML output by phpMyFAQ %s | Date: %s', 
+          $version, 
+          PMF_Date::createIsoDate(date("YmdHis")));
+        
+        $this->xml->startDocument('1.0', 'utf-8');
+        $this->xml->writeDtd('html', 
+                             '-//W3C//DTD XHTML 1.0 Transitional//EN', 
+                             'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd');
+        $this->xml->startElement('html');
+        $this->xml->writeAttribute('xmlns', 'http://www.w3.org/1999/xhtml');
+        $this->xml->writeAttribute('xml:lang', $language);
+        $this->xml->writeComment($comment);
+        
+        $this->xml->startElement('head');
+        $this->xml->writeElement('title', PMF_Configuration::getInstance()->get('main.titleFAQ'));
+        $this->xml->startElement('meta');
+        $this->xml->writeAttribute('http-equiv', 'Content-Type');
+        $this->xml->writeAttribute('content', 'application/xhtml+xml; charset=utf-8');
+        $this->xml->endElement();
+        $this->xml->endElement(); // </head>
+        
+        $this->xml->startElement('body');
+        $this->xml->writeAttribute('dir', $PMF_LANG['dir']);
+        
+        if (count($faqdata)) {
+        	$lastCategory = 0;
+            foreach ($faqdata as $data) {
+            
+                if ($data['category_id'] != $lastCategory) {
+                    $this->xml->writeElement('h1', $this->category->getPath($data['category_id'], ' >> '));
+                }
+                
+                $this->xml->writeElement('h2', strip_tags($data['topic']));
+                $this->xml->writeElement('p', $data['content']);
+                $this->xml->writeElement('p', $PMF_LANG['msgAuthor'] . ': ' .$data['author_email']);
+                $this->xml->writeElement('p', $PMF_LANG['msgLastUpdateArticle'] . 
+                                              PMF_Date::createIsoDate($data['lastmodified']));
+                
+                $lastCategory = $data['category_id'];
+            }
+        }
+        
+        $this->xml->endElement(); // </body>
+        $this->xml->endElement(); // </html>
+        
+        header('Content-type: text/html'); 
+        return $this->xml->outputMemory();
+    }
 }
