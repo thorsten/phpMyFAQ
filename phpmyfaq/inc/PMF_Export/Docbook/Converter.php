46bfaea61e6bce3b67c684e7d7f9f03db3b10004
Thorsten Rinne <thorsten@phpmyfaq.de>
2009-10-13 21:04:24 +0200

    Init

---
+++ phpmyfaq/inc/PMF_Export/Docbook/Converter.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * XHTML 2 Docbook XML Convert class
+ *
+ * @category  phpMyFAQ
+ * @package   PMF_Export
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since     2009-10-13
+ * @license   Mozilla Public License 1.1
+ * @copyright 2009 phpMyFAQ Team
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+
+
+/**
+ * PMF_Export_Docbook_Converter
+ *
+ * @category  phpMyFAQ
+ * @package   PMF_Export
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @since     2009-10-13
+ * @license   Mozilla Public License 1.1
+ * @copyright 2009 phpMyFAQ Team
+ */
+class PMF_Export_Docbook_Converter extends PMF_Export_Docbook_Abstract
+{
+	
+}
