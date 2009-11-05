9f1a2e17a33310b6d9f6a86fc5ffb950eb49f679
Thorsten Rinne <thorsten@phpmyfaq.de>
2009-11-05 16:23:04 +0100

    PHPDoc fixes

--- phpmyfaq/add.php
+++ phpmyfaq/add.php
@@ -2,12 +2,8 @@
 /**
  * This is the page there a user can add a FAQ record.
  *
- * @category  phpMyFAQ
- * @package   Frontend
- * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
- * @since     2002-09-16
- * @copyright 2002-2009 phpMyFAQ Team
- *
+ * PHP Version 5.2.0
+ * 
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
@@ -17,6 +13,14 @@
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific language governing rights and limitations
  * under the License.
+ * 
+ * @category  phpMyFAQ
+ * @package   Frontend
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2002-2009 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2002-09-16
  */
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
