344dfa5ea08904429f6a10eff9d416505236334c
David Soria Parra <dsp@php.net>
2009-11-10 11:36:50 +0100

    Add new changelog creation script

---
+++ phpmyfaq/scripts/git2changelog.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+. scripts/version.sh
+
+tags=`git describe --tags --abbrev=0`
+content=`cat docs/CHANGEDFILES.txt`
+filelist=`git log --pretty=format:"" --name-only $(git describe --tags --abb$rev=0)..HEAD | sed 's,^phpmyfaq,.,' | sort -u`
+
+cat <<EOF
+CHANGED FILES FROM $tags -> $PMF_VERSION
+$filelist
+
+
+$content
+EOF
