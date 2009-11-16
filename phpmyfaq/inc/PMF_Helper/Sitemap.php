8df2c0d4254761bca0a873a605f7f11af343436c
Thorsten Rinne <thorsten@phpmyfaq.de>
2009-11-16 15:14:36 +0100

    Add skeleton class for Sitemap helper methods

---
+++ phpmyfaq/inc/PMF_Helper/Sitemap.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * The Sitemap Helper class provides pre-rendered data
+ *
+ * PHP Version 5.2.0
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * @category  phpMyFAQ
+ * @package   PMF_Helper
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2009 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2009-11-16
+ */
+
+/**
+ * PMF_Helper_Sitemap 
+ *
+ * @category  phpMyFAQ
+ * @package   PMF_Helper
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2009 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2009-11-16
+ */
+class PMF_Helper_Sitemap extends PMF_Helper
+{
+    /**
+     * Constructor
+     * 
+     * @return 
+     */
+    private function __construct()
+    {
+        
+    }
+    
+    /**
+     * Returns the single instance
+     *
+     * @access static
+     * @return PMF_Helper_Search
+     */
+    public static function getInstance()
+    {
+        if (null == self::$instance) {
+            $className = __CLASS__;
+            self::$instance = new $className();
+        }
+        return self::$instance;
+    }
+   
+    /**
+     * __clone() Magic method to prevent cloning
+     * 
+     * @return void
+     */
+    private function __clone()
+    {
+        
+    }
+    
+    /**
+     * Returns all available first letters with entries
+     * 
+     * @param array $letters Array with all available first letters
+     * 
+     * @return string
+     */
+    public function renderAllFirstLetters(Array $letters)
+    {
+    	
+    }
+    
+    /**
+     * Returns a list of all records from one chosen letter
+     * 
+     * @param string $letter Letter
+     * 
+     * @return void
+     */
+    public function renderAllRecordsFromLetter($letter)
+    {
+    	
+    }
+}
