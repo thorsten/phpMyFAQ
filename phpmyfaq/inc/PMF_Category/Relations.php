c6a8025968f3d7631192ba6acd75aa83835dcdae
Thorsten Rinne <thorsten@phpmyfaq.de>
2010-01-05 19:09:15 +0100

    Added PMF_Category_Relations class

---
+++ phpmyfaq/inc/PMF_Category/Relations.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Relation class for categories and FAQ entries
+ *
+ * PHP Version 5.2.0
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * @category  phpMyFAQ
+ * @package   PMF_Category
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2010 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2001-01-05
+ */
+
+/**
+ * PMF_Category_Relations
+ * 
+ * @category  phpMyFAQ
+ * @package   PMF_Category
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2010 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2001-01-05
+ */
+class PMF_Category_Relations extends PMF_Category_Abstract implements PMF_Category_Interface
+{
+    /**
+     * Constructor
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+/**
+     * Creates a new entry
+     *
+     * @param array   $data Array of data
+     * 
+     * @return boolean
+     * @throws PMF_Category_Exception
+     */
+    public function create(Array $data)
+    {
+        $query =sprintf("
+            INSERT INTO
+                %sfaqcategoryrelations
+            VALUES
+                (%d, '%s', %d, '%s')",
+            SQLPREFIX,
+            $data['category_id'],
+            $data['category_lang'],
+            $data['record_id'],
+            $data['record_lang']);
+            
+        $result = $this->db->query($query);
+            
+        if (!$result) {
+            throw new PMF_Exception($this->db->error());
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * Updates an existing entry
+     *
+     * @param integer $id   ID
+     * @param array   $data Array of data
+     * 
+     * @return boolean
+     * @throws PMF_Category_Exception
+     */
+    public function update($id, Array $data)
+    {
+        
+    }
+    
+    /**
+     * Deletes an entry
+     *
+     * @param integer $id ID
+     * 
+     * @return boolean
+     * @throws PMF_Category_Exception
+     */
+    public function delete($id)
+    {
+        
+    }
+    
+    /**
+     * Fetches one entry
+     *
+     * @param integer $id ID
+     * 
+     * @return array
+     * @throws PMF_Category_Exception
+     */
+    public function fetch($id)
+    {
+        
+    }
+    
+    /**
+     * Fetches all entries, if parameter = null, otherwise all from the given
+     * array like array(1, 2, 3)
+     *
+     * @param array $ids Array of IDs
+     * 
+     * @return array
+     * @throws PMF_Category_Exception
+     */
+    public function fetchAll(Array $ids = null)
+    {
+        
+    }
+}
