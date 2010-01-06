26706d3e59ce88d42a7e97781c3b41497f69a993
Thorsten Rinne <thorsten@phpmyfaq.de>
2010-01-06 18:35:09 +0100

    Added classes by Johannes

---
+++ phpmyfaq/inc/PMF_Category/Tree/DataProvider/Interface.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Dataprovider of PMF_Category_Tree class
+ *
+ * PHP Version 5.2.0
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * @category  phpMyFAQ
+ * @package   PMF_Category
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2010 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2001-01-05
+ */
+
+/**
+ * PMF_Category_Tree_DataProvider
+ * 
+ * @category  phpMyFAQ
+ * @package   PMF_Category
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2010 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2001-01-05
+ */
+interface PMF_Category_Tree_DataProvider_Interface
+{
+    /**
+     * Fetches all category data from database
+     *
+     * @param integer $parentId Parent ID
+     */
+    public function getData($parentId = 0);
+    
+    public function getPath($id);
+}
