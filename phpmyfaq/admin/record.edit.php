d2ae6ad9b4f17b8a6606470b03a04617de5b1acd
Thorsten Rinne <thorsten@phpmyfaq.de>
2010-08-02 05:55:24 +0200

    WS fixes

--- phpmyfaq/admin/record.edit.php
+++ phpmyfaq/admin/record.edit.php
@@ -211,7 +211,7 @@ if ($permission["editbt"] && !PMF_Db::checkOnEmptyTable('faqcategories')) {
 
     <label class="lefteditor" for="rubrik"><?php print $PMF_LANG["ad_entry_category"]; ?></label>
     <select name="rubrik[]" id="rubrik" size="5" multiple="multiple">
-<?php print $helper->renderCategoryOptions($categories); ?>
+    <?php print $helper->renderCategoryOptions($categories); ?>
     </select><br />
 
     <label for="thema"><?php print $PMF_LANG["ad_entry_theme"]; ?></label>
@@ -220,7 +220,7 @@ if ($permission["editbt"] && !PMF_Db::checkOnEmptyTable('faqcategories')) {
     <label for="content"><?php print $PMF_LANG["ad_entry_content"]; ?></label>
     <noscript>Please enable JavaScript to use the WYSIWYG editor!</noscript>
     <textarea id="content" name="content" cols="84" rows="16" style="width: 720px; height: 480px;">
-        <?php if (isset($faqData['content'])) { print trim(PMF_String::htmlentities($faqData['content'])); } ?>
+    <?php if (isset($faqData['content'])) { print trim(PMF_String::htmlentities($faqData['content'])); } ?>
     </textarea><br />
 
 <?php
