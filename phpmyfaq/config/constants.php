a55b46e28ccad13f06afdabf42b232da9232e2f3
Thorsten Rinne <thorsten@phpmyfaq.de>
2010-08-15 12:23:18 +0200

    Removed Oracle database support

--- phpmyfaq/config/constants.php
+++ phpmyfaq/config/constants.php
@@ -1,12 +1,8 @@
 <?php
 /**
- * Constants for phpMyFAQ.
- *
- * @package   phpMyFAQ 
- * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
- * @author    Matteo Scaramuccia <matteo@phpmyfaq.de>
- * @since     2003-12-10
- * @copyright 2003-2010 phpMyFAQ Team
+ * Constants for phpMyFAQ
+ * 
+ * PHP Version 5.2
  *
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 (the "License"); you may not use this file except in compliance with
@@ -17,13 +13,16 @@
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- */
-
-/**
- * PHP 6 script encoding
  *
+ * @category  phpMyFAQ
+ * @package   Setup
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @author    Matteo Scaramuccia <matteo@phpmyfaq.de>
+ * @since     2003-12-10
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @copyright 2003-2010 phpMyFAQ Team
  */
-declare(encoding='latin1');
 
 /**
  * Set the string below according to your users (main) timezone.
@@ -185,7 +184,6 @@ $supported_databases = array(
     'sqlite'  => array('5.2.0', 'SQLite'),
     'ibm_db2' => array('5.2.0', 'IBM DB2 Universal Database 8.2 / 9.1'),
     'sqlsrv'  => array('5.2.0', 'SQL Server Driver for PHP (experimental)'),
-    'oci8'    => array('5.2.0', 'Oracle 8i and later (experimental)'),
     'ibase'   => array('5.2.0', 'Interbase/Firebird (experimental)')
 );
 
