6604bc0641a7a3e2cf8ea77b6b3d85a3a682bbf9
Thorsten Rinne <thorsten@phpmyfaq.de>
2010-09-05 12:20:41 +0200

    Added skeleton classes for Facebook and Twitter support

---
+++ phpmyfaq/inc/PMF_Services/Facebook.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Service class for Facebook support
+ * 
+ * PHP version 5.2
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * @category  phpMyFAQ
+ * @package   PMF_Services
+ * @author    Thorsten Rinne
+ * @copyright 2010 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2010-09-05
+ */
+
+/**
+ * PMF_Services_Facebook
+ * 
+ * @category  phpMyFAQ
+ * @package   PMF_Services
+ * @author    Thorsten Rinne
+ * @copyright 2010 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
+ * @link      http://www.phpmyfaq.de
+ * @since     2010-09-05
+ */
+class PMF_Services_Facebook extends PMF_Services
+{
+    
+}
