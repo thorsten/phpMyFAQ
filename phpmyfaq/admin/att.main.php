0abc212c19ae096123e3c4ad59aa66478e1ffb4f
Anatoliy Belsky <point@silent.(none)>
2010-12-20 20:35:35 +0100

    added pagination to the attachment listing

--- phpmyfaq/admin/att.main.php
+++ phpmyfaq/admin/att.main.php
@@ -28,11 +28,30 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
     exit();
 }
 
-printf('<h2>%s</h2>', $PMF_LANG['ad_menu_attachment_admin']);
+$page = PMF_Filter::filterInput(INPUT_GET, 'page', FILTER_VALIDATE_INT);
+$page = 1 > $page ? 1 : $page;
 
 $fa = new PMF_Attachment_Collection;
+$itemsPerPage = 32;
+$allCrumbs = $fa->getBreadcrumbs();
 
-$c = $fa->getBreadcrumbs();
+$crumbs   = array_slice(
+    $allCrumbs, 
+    ($page-1)*$itemsPerPage,
+    $itemsPerPage
+);
+
+$pagination = new PMF_Pagination(
+    array(
+        'baseUrl'   => '?' . str_replace('&', '&amp;', $_SERVER['QUERY_STRING']),
+        'total'     => count($allCrumbs),
+        'perPage'   => $itemsPerPage,
+        'layoutTpl' => '<p align="center"><strong>{LAYOUT_CONTENT}</strong></p>',
+    )
+);
+
+printf('<h2>%s</h2>', $PMF_LANG['ad_menu_attachment_admin']);
+
 ?>
 <table cellspacing="30">
 	<thead>
@@ -46,7 +65,7 @@ $c = $fa->getBreadcrumbs();
 	</thead>
 	<tbody>
 <?php
-    foreach($c as $item) {
+    foreach($crumbs as $item) {
         print <<<ROW
  		<tr>
  			<td>{$item->filename}</td>
@@ -61,4 +80,34 @@ ROW;
     }
 ?>
 	</tbody>
+	<tfoot>
+		<tr>
+			<td><?php echo $pagination->render(); ?></td>
+		</tr>
+	</tfoot>
 </table>
+<script type="text/javascript">
+/**
+ * Ajax call for deleting attachments
+ *
+ * @param  integer record_id   Record id
+ * @param  string  record_lang Record language
+ * @return void
+ */
+function deleteRecord(record_id, record_lang)
+{
+/*    if (confirm('Are you sure that the topic should be deleted?')) {
+        $('#saving_data_indicator').html('<img src="images/indicator.gif" /> deleting ...');
+        $.ajax({
+            type:    "POST",
+            url:     "index.php?action=ajax&ajax=records&ajaxaction=delete_record",
+            data:    "record_id=" + record_id + "&record_lang=" + record_lang,
+            success: function(msg) {
+                $('.record_' + record_id + '_' + record_lang).fadeOut('slow');
+                $('.record_' + record_id + '_' + record_lang).after('<tr><td colspan="8">' + msg + '</td></tr>');
+                $('#saving_data_indicator').html('Issue <strong>successfully</strong> deleted.');
+            }
+        });
+    }*/
+} 
+</script>
