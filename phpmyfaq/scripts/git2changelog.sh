592adf32acc6d97f3755bfde438cdc012f78db1b
Thorsten Rinne <thorsten@phpmyfaq.de>
2011-03-23 20:29:53 +0100

    Prepare release

--- phpmyfaq/scripts/git2changelog.sh
+++ phpmyfaq/scripts/git2changelog.sh
@@ -2,7 +2,7 @@
 . scripts/version.sh
 
 tags=`git describe --tags --abbrev=0`
-content=`cat docs/CHANGEDFILES`
+content=`cat CHANGEDFILES`
 filelist=`git log --pretty=format:"" --name-only $(git describe --tags --abb$rev=0)..HEAD | sed 's,^phpmyfaq,.,' | sort -u`
 
 cat <<EOF
