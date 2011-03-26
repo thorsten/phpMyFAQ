823038350157bc66fc56552585bf12c6d2ba8209
Thorsten Rinne <thorsten@phpmyfaq.de>
2011-03-26 14:15:09 +0100

    Only use local jQuery library

--- phpmyfaq/admin/attachment.php
+++ phpmyfaq/admin/attachment.php
@@ -111,8 +111,7 @@ if (is_null($currentAction) || !is_null($currentSave)) {
     <link rel="stylesheet" href="style/admin.css?v=1">
 
     <script src="../inc/js/modernizr.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-    <script>!window.jQuery && document.write('<script src="../inc/js/jquery.min.js"><\/script>')</script>
+    <script src="../inc/js/jquery.min.js"></script>
     <script src="../inc/js/functions.js"></script>
     
     <link rel="shortcut icon" href="../template/<?php print PMF_Template::getTplSetName(); ?>/favicon.ico">
--- phpmyfaq/template/default/index.tpl
+++ phpmyfaq/template/default/index.tpl
@@ -31,8 +31,7 @@
     <link rel="stylesheet" media="print" href="template/{tplSetName}/css/print.css?v=1">
 
     <script src="inc/js/modernizr.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
-    <script>!window.jQuery && document.write('<script src="inc/js/jquery.min.js"><\/script>')</script>
+    <script src="inc/js/jquery.min.js"></script>
     <script src="inc/js/functions.js"></script>
 
     <link rel="shortcut icon" href="template/{tplSetName}/favicon.ico">
