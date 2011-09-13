1e3223728c96a78611df3a39dce92d9ae4c6345c
Jason Berger <jlberger78@yahoo.com>
2011-09-13 02:59:02 -0300

    Edited phpmyfaq/inc/Language.php via GitHub

--- phpmyfaq/inc/Language.php
+++ phpmyfaq/inc/Language.php
@@ -19,7 +19,7 @@
  * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
  * @author    Matteo scaramuccia <matteo@phpmyfaq.de>
  * @author    Aurimas Fišeras <aurimas@gmail.com>
- * @copyright 2009-2010 phpMyFAQ Team
+ * @copyright 2009-2011 phpMyFAQ Team
  * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
  * @link      http://www.phpmyfaq.de
  * @since     2009-05-14
