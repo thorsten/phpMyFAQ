1f6777265e172752744a976918cec66e5b9bcd82
Jason Berger <jlberger78@yahoo.com>
2011-09-13 03:02:15 -0300

    Edited phpmyfaq/inc/Relation.php via GitHub

--- phpmyfaq/inc/Relation.php
+++ phpmyfaq/inc/Relation.php
@@ -18,7 +18,7 @@
  * @package   PMF_Relation
  * @author    Marco Enders <marco@minimarco.de>
  * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
- * @copyright 2006-2010 phpMyFAQ Team
+ * @copyright 2006-2011 phpMyFAQ Team
  * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
  * @link      http://www.phpmyfaq.de
  * @since     2006-06-18
