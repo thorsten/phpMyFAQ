33cfda6becb11f17eb471d01e612b81184e7ac81
Jason Berger <jlberger78@yahoo.com>
2011-09-13 04:58:36 -0300

    Edited phpmyfaq/inc/PMF_User/UserData.php via GitHub

--- phpmyfaq/inc/PMF_User/UserData.php
+++ phpmyfaq/inc/PMF_User/UserData.php
@@ -17,7 +17,7 @@
  * @category  phpMyFAQ
  * @package   PMF_User
  * @author    Lars Tiedemann <php@larstiedemann.de>
- * @copyright 2005-2010 phpMyFAQ Team
+ * @copyright 2005-2011 phpMyFAQ Team
  * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
  * @link      http://www.phpmyfaq.de
  * @since     2005-09-18
