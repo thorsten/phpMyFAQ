f63b3cd5393ea073240c262d4b84dcb405f96575
Thorsten Rinne <thorsten@phpmyfaq.de>
2012-01-05 11:46:38 +0100

    Moved TTF fonts for captchas

--- phpmyfaq/inc/Captcha.php
+++ phpmyfaq/inc/Captcha.php
@@ -19,7 +19,7 @@
  * @author    Thomas Zeithaml <seo@annatom.de>
  * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
  * @author    Matteo Scaramuccia <matteo@scaramuccia.com>
- * @copyright 2006-2011 phpMyFAQ Team
+ * @copyright 2006-2012 phpMyFAQ Team
  * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
  * @link      http://www.phpmyfaq.de
  * @since     2006-02-04
@@ -37,7 +37,7 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
  * @author    Thomas Zeithaml <seo@annatom.de>
  * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
  * @author    Matteo Scaramuccia <matteo@scaramuccia.com>
- * @copyright 2006-2011 phpMyFAQ Team
+ * @copyright 2006-2012 phpMyFAQ Team
  * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
  * @link      http://www.phpmyfaq.de
  * @since     2006-02-04
@@ -571,6 +571,6 @@ class PMF_Captcha
      */
     private function getFonts()
     {
-        return glob(dirname(dirname(__FILE__)) . '/font/*.ttf');
+        return glob(dirname(__FILE__) . '/fonts/*.ttf');
     }
 }
