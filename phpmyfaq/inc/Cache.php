d3b7164f0617069393682928602997208d469289
Thorsten Rinne <thorsten@phpmyfaq.de>
2012-03-06 22:09:06 +0100

    CS fixes

--- phpmyfaq/inc/Cache.php
+++ phpmyfaq/inc/Cache.php
@@ -17,7 +17,7 @@
  * @category  phpMyFAQ
  * @package   PMF_Cache
  * @author    Anatoliy Belsky <ab@php.net>
- * @copyright 2002-2011 phpMyFAQ Team
+ * @copyright 2002-2012 phpMyFAQ Team
  * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
  * @link      http://www.phpmyfaq.de
  * @since     2011-09-23
@@ -33,32 +33,43 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
  * @category  phpMyFAQ
  * @package   PMF_Cache
  * @author    Anatoliy Belsky <ab@php.net>
- * @copyright 2002-2011 phpMyFAQ Team
+ * @copyright 2002-2012 phpMyFAQ Team
  * @license   http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License Version 1.1
  * @link      http://www.phpmyfaq.de
  * @since     2011-09-23
  */
 class PMF_Cache
 {
-	protected static $instance = NULL;
+    /**
+     * @var PMF_Cache
+     */
+    protected static $instance = NULL;
 
-	public static function init(PMF_Configuration $faqConfig)
-	{
-		$config = array();
-		if ($faqConfig->get('cache.varnishEnable')) {
-			$config[VARNISH_CONFIG_PORT]    = $faqConfig->get('cache.varnishPort');
-			$config[VARNISH_CONFIG_SECRET]  = $faqConfig->get('cache.varnishSecret');
-			$config[VARNISH_CONFIG_TIMEOUT] = $faqConfig->get('cache.varnishTimeout');
-			$config[VARNISH_CONFIG_HOST]    = $faqConfig->get('cache.varnishHost');
+    /**
+     * @static
+     * @param PMF_Configuration $faqConfig
+     */
+    public static function init(PMF_Configuration $faqConfig)
+    {
+        $config = array();
+        if ($faqConfig->get('cache.varnishEnable')) {
+            $config[VARNISH_CONFIG_PORT]    = $faqConfig->get('cache.varnishPort');
+            $config[VARNISH_CONFIG_SECRET]  = $faqConfig->get('cache.varnishSecret');
+            $config[VARNISH_CONFIG_TIMEOUT] = $faqConfig->get('cache.varnishTimeout');
+            $config[VARNISH_CONFIG_HOST]    = $faqConfig->get('cache.varnishHost');
 
-			self::$instance = new PMF_Cache_Varnish($config);
-		} else {
-			self::$instance = new PMF_Cache_Dummy($config);
-		}
-	}
+            self::$instance = new PMF_Cache_Varnish($config);
+        } else {
+            self::$instance = new PMF_Cache_Dummy($config);
+        }
+    }
 
-	public static function getInstance()
-	{
-		return self::$instance;
-	}
+    /**
+     * @static
+     * @return null|PMF_Cache
+     */
+    public static function getInstance()
+    {
+        return self::$instance;
+    }
 }
