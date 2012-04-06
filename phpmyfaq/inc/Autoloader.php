bcf92883ad1a2c8ebcd2827dcae57156cea4ebb4
Thorsten Rinne <thorsten@phpmyfaq.de>
2012-04-06 17:47:38 +0200

    Autoloader is now a class

--- phpmyfaq/inc/Autoloader.php
+++ phpmyfaq/inc/Autoloader.php
@@ -25,7 +25,7 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
 }
 
 /**
- * phpMyFAQ __autoload() implementation
+ * phpMyFAQ spl_autoload_register() implementation
  *
  * @category  phpMyFAQ
  * @package   Core
@@ -36,43 +36,69 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
  * @link      http://www.phpmyfaq.de
  * @since     2009-01-07
  */
-function PMF_Autoloader($class)
+class PMF_Autoloader
 {
-    // Class/interface name paranoid cleanup i.e.:
-    // - avoid path traversal issues;
-    // - remove any *NIX/WIN filename reserved chars.
-    $invalidChars = array(
-        '.', '\\', '/', ':', '*', '?', '"', '<', '>', "'", '|'
-    );
+    /**
+     * Constructor
+     *
+     * @return PMF_Autoloader
+     */
+    public function __construct()
+    {
+        spl_autoload_register(array($this, 'loader'));
+    }
 
-    $class   = str_replace($invalidChars, '', $class);
-    $rootDir = defined('PMF_ROOT_DIR') ? PMF_ROOT_DIR : '.';
+    /**
+     * Class loader
+     *
+     * @param string $className
+     *
+     * @return void
+     */
+    private function loader($className)
+    {
+        // Class/interface name paranoid cleanup i.e.:
+        // - avoid path traversal issues;
+        // - remove any *NIX/WIN filename reserved chars.
+        $invalidChars = array(
+            '.', '\\', '/', ':', '*', '?', '"', '<', '>', "'", '|'
+        );
 
-    // Try to load the class/interface declaration using its name if splittable by '_'
-    // Note: using include instead of require give us the possibility to echo failures
-    $classParts      = explode('_', $class);
-    $classPartsCount = count($classParts);
-    $includeDir      = $rootDir . DIRECTORY_SEPARATOR . 'inc'. DIRECTORY_SEPARATOR;
-    if (2 == $classPartsCount) {
-        $path = $includeDir . $classParts[1] . '.php';
-    } else {
-        $path = $includeDir . $classParts[1];
-        for ($i = 2; $i < $classPartsCount; $i++) {
-            $path .= DIRECTORY_SEPARATOR . $classParts[$i];
+        $className = str_replace($invalidChars, '', $className);
+        $rootDir   = defined('PMF_ROOT_DIR') ? PMF_ROOT_DIR : '.';
+
+        // Try to load the class/interface declaration using its name if splittable by '_'
+        // Note: using include instead of require give us the possibility to echo failures
+        $classParts      = explode('_', $className);
+        $classPartsCount = count($classParts);
+        $includeDir      = $rootDir . DIRECTORY_SEPARATOR . 'inc'. DIRECTORY_SEPARATOR;
+        if (2 == $classPartsCount) {
+            $path = $includeDir . $classParts[1] . '.php';
+        } else {
+            $path = $includeDir . $classParts[1];
+            for ($i = 2; $i < $classPartsCount; $i++) {
+                $path .= DIRECTORY_SEPARATOR . $classParts[$i];
+            }
+            $path .= '.php';
         }
-        $path .= '.php';
-    }
 
-    if (file_exists($path)) {
-        include $path;
-    } else {
-        printf("<br /><b>PMF Autoloader</b>: unable to find a suitable file declaring '%s'.", $class);
-    }
+        if (file_exists($path)) {
+            include $path;
+        } else {
+            printf(
+                "<br /><b>PMF Autoloader</b>: unable to find a suitable file declaring '%s'.",
+                $className
+            );
+        }
 
-    // Sanity check
-    if (!class_exists($class, false) && !interface_exists($class, false)) {
-        printf("<br /><b>PMF Autoloader</b>: unable to define '%s' as a class/interface.<br />", $class);
+        // Sanity check
+        if (!class_exists($className, false) && !interface_exists($className, false)) {
+            printf(
+                "<br /><b>PMF Autoloader</b>: unable to define '%s' as a class/interface.<br />",
+                $className
+            );
+        }
     }
 }
 
-spl_autoload_register('PMF_Autoloader');
+$faqAutoloader = new PMF_Autoloader();
