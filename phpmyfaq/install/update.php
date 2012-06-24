45094155b9c38cc648dce4b058641e89c0c3381d
Thorsten Rinne <thorsten@rinne.info>
2012-06-24 14:09:21 +0200

    Fixed links (#481)

--- phpmyfaq/install/update.php
+++ phpmyfaq/install/update.php
@@ -68,8 +68,8 @@ if ((@ini_get('safe_mode') != 'On' || @ini_get('safe_mode') !== 1)) {
 
 <nav>
     <ul>
-        <li><a target="_blank" href="http://www.phpmyfaq.de//documentation.php">Documentation</a></li>
-        <li><a target="_blank" href="http://www.phpmyfaq.de//support.php">Support</a></li>
+        <li><a target="_blank" href="http://www.phpmyfaq.de/documentation.php">Documentation</a></li>
+        <li><a target="_blank" href="http://www.phpmyfaq.de/support.php">Support</a></li>
         <li><a target="_blank" href="http://forum.phpmyfaq.de/">Forums</a></li>
         <li><a target="_blank" href="http://faq.phpmyfaq.de/">FAQ</a></li>
     </ul>
