73c4687bd26aaa695e32f07083428738590c2197
Anatoliy Belsky <ab@php.net>
2012-07-07 18:32:43 +0200

    implemented posting the faq form via ajax

--- phpmyfaq/assets/js/autosave.js
+++ phpmyfaq/assets/js/autosave.js
@@ -19,10 +19,39 @@ function pmf_auto_save()
 {
 	var ed = tinyMCE.activeEditor;
 	if (ed.isDirty()) {
-		var content = ed.getContent();
-		/* XXX need ajax here */
-		console.log('autosaved');
-		ed.isNotDirty = true;
+		var formData = {};
+		formData.revision_id = $('#revision_id').attr('value');
+		formData.record_id = $('#record_id').attr('value');
+		formData.csrf = $('#csrf').attr('value');
+		formData.openQuestionId = $('#openQuestionId').attr('value');
+		formData.question = $('#question').attr('value');
+		formData.answer = ed.getContent();
+		formData.keywords = $('#keywords').attr('value');
+		formData.tags = $('#tags').attr('value');
+		formData.author = $('#author').attr('value');
+		formData.email = $('#email').attr('value');
+		formData.lang = $('#lang').attr('value');
+		formData.solution_id = $('#solution_id').attr('value');
+
+		$.ajax({
+			url: pmf_auto_save_action(),
+			type: 'POST',
+			data: formData,
+			success: function(msg) {
+				$('#saving_data_indicator').html('Item was autosaved at rev ' + msg);
+				ed.isNotDirty = true;
+			}
+		});
 	}
+}
+
+function pmf_auto_save_action()
+{
+	var act;
+	var fa = $('#faqEditor').attr('action');
+
+	act = '?action=ajax&ajax=autosave&' + fa.substr(1).replace(/action=/, 'do=');
+
+	return act;
 }
 
