4e1e56a39cedfa1e610384b80d4e45fd69ab9b2e
Thorsten Rinne <thorsten@phpmyfaq.de>
2012-08-30 20:54:56 +0200

    Init (#500)

---
+++ phpmyfaq/inc/Notification.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The notification class for phpMyFAQ
+ *
+ * PHP Version 5.3
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * @category  phpMyFAQ
+ * @package   Notification
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2012 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      http://www.phpmyfaq.de
+ * @since     2012-08-30
+ */
+
+if (!defined('IS_VALID_PHPMYFAQ')) {
+    exit();
+}
+
+/**
+ * PMF_Notification
+ *
+ * @category  phpMyFAQ
+ * @package   Notification
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2012 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      http://www.phpmyfaq.de
+ * @since     2012-08-30
+ */
+
+class PMF_Notification
+{
+    /**
+     * @var PMF_Configuration
+     */
+    private $config;
+
+    /**
+     * Mail object
+     *
+     * @var PMF_Mail
+     */
+    private $mail;
+
+    /**
+     * Constructor
+     *
+     * @param PMF_Configuration
+     *
+     * @return PMF_Notification
+     */
+    public function __construct(PMF_Configuration $config)
+    {
+        $this->config  = $config;
+    }
+
+    /**
+     * Sets the Mail class
+     *
+     * @param \PMF_Mail $mail
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * Returns the mail class
+     *
+     * @return \PMF_Mail
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+}
