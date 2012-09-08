2089049adfb896f22cb57014fafb3e452b4eb054
Thorsten Rinne <t.rinne@yatego.com>
2012-09-08 12:49:43 +0200

    Fixed issue with English only configuration

--- phpmyfaq/admin/ajax.config_list.php
+++ phpmyfaq/admin/ajax.config_list.php
@@ -23,7 +23,6 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
     exit();
 }
 
-require PMF_ROOT_DIR . '/lang/language_en.php';
 require PMF_ROOT_DIR . '/inc/libs/twitteroauth/twitteroauth.php';
 
 if (!empty($_SESSION['access_token'])) {
