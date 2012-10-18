9e71ffb730fbe30db12006506c1527ed04a8ed60
Thorsten Rinne <thorsten@phpmyfaq.de>
2012-10-18 21:53:24 +0200

    Fixed license

--- phpmyfaq/config/constants.php
+++ phpmyfaq/config/constants.php
@@ -4,14 +4,9 @@
  *
  * PHP Version 5.3
  *
- *  Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
  *
  * @category  phpMyFAQ
  * @package   Setup
