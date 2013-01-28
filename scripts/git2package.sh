ab779aaec77a10250e5f939dd05874e40aca80bc
Thorsten Rinne <thorsten@phpmyfaq.de>
2013-01-28 18:56:37 +0100

    Fixed build script

--- scripts/git2package.sh
+++ scripts/git2package.sh
@@ -51,14 +51,13 @@ composer install
 
 # Add missing directories
 mkdir -p $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/inc/libs/phpseclib/Crypt
-rm $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/admin/assets/font/
 mkdir -p $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/admin/assets/font/
 
 # copy dependencies
 cp -r $cwd/vendor/phpseclib/phpseclib/Crypt $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/inc/libs/phpseclib/Crypt
 cp -r $cwd/vendor/twitteroauth/twitteroauth $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/inc/libs/twitteroauth
 cp -r $cwd/vendor/symfony/class-loader/* $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/inc/libs/
-cp -r $cwd/vendor/Font-Awesome/font/* $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/admin/assets/font/
+cp -r $cwd/vendor/Font-Awesome/font $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/admin/assets/font
 
 # prepare packaging
 mkdir $cwd/build/${PMF_PACKAGE_FOLDER}/phpmyfaq/phpmyfaq
