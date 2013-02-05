d97d64373593824d030598cf3f676d8071c6f7c2
Alexander M. Turek <me@derrabus.de>
2013-02-05 21:04:39 +0100

    Removed unused methods.

--- phpmyfaq/inc/PMF/Helper/Http.php
+++ phpmyfaq/inc/PMF/Helper/Http.php
@@ -71,49 +71,4 @@ class PMF_Helper_Http extends PMF_Helper
         header('Vary: Negotiate,Accept');
         header('Content-type: ' . $this->contentType);
     }
-
-    /**
-     * Sends any kind of data with optional HTTP headers as JSON.
-     *
-     * @param mixed $payload What to send
-     * @param string|array $headers Which headers to send
-     *
-     * @return string
-     */
-    public function sendJsonWithHeaders($payload, $headers = '')
-    {
-        return $this->sendWithHeaders($payload, $headers, true);
-    }
-
-    /**
-     * Sends any kind of data with optional HTTP headers as text or JSON.
-     *
-     * @param mixed $payload What to send
-     * @param string|array $headers Which headers to send
-     * @param bool $isJson Send as JSON?
-     *
-     * @return string
-     */
-    private function sendWithHeaders($payload, $headers = '', $isJson = false)
-    {
-        $validHeaders = array();
-        if (is_string($headers) && strlen($headers) > 0) {
-            $validHeaders[] = $headers;
-        } elseif(is_array($headers)) {
-            foreach ($headers as $header) {
-                if (strlen($header) > 0) {
-                    $validHeaders[] = $header;
-                }
-            }
-        }
-        foreach ($validHeaders as $header) {
-            header($header);
-        }
-        if ($isJson) {
-            header('Content-Type: application/json');
-            print json_encode($payload);
-        } else {
-            print $payload;
-        }
-    }
 }
