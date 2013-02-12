79f6978a6a8fcb9dc4a1a597c35c439fb57ad791
Thorsten Rinne <thorsten@rinne.info>
2013-02-12 18:31:04 +0100

    Fixed validation issue, closes #598

--- phpmyfaq/inc/Category.php
+++ phpmyfaq/inc/Category.php
@@ -138,6 +138,7 @@ class PMF_Category
      * @param  integer $user     User
      * @param  array   $groups   Group
      * @param  boolean $withperm With or without permission check
+     *
      * @return void
      */
     public function __construct($user = null, $groups = null, $withperm = true)
@@ -657,7 +658,7 @@ class PMF_Category
                 $num_entries = '';
             } else {
                 $totFaqRecords += $number[$parent];
-                $num_entries    = '<span id="rssCategoryLink"> ('.$plr->GetMsg('plmsgEntries',$number[$parent]);
+                $num_entries    = '<span class="rssCategoryLink"> ('.$plr->GetMsg('plmsgEntries',$number[$parent]);
                 $num_entries   .= sprintf(
                     ' <a href="feed/category/rss.php?category_id=%d&category_lang=%s" target="_blank"><img id="category_%d_RSS" src="%s/images/feed.png" width="16" height="16" alt="RSS" border="0" /></a>',
                     $parent,
