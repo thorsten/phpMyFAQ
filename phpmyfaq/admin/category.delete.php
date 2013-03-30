faca40e4dcf5b35595bded417efe87b1bc7732b4
Alexander M. Turek <me@derrabus.de>
2013-03-30 21:56:49 +0100

    Migrated category.delete.php to Twig

--- phpmyfaq/admin/category.delete.php
+++ phpmyfaq/admin/category.delete.php
@@ -18,61 +18,30 @@
  */
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
-    header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']));
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']));
     exit();
 }
 
-?>
-
-        <header>
-            <h2><i class="icon-list"></i> <?php print $PMF_LANG['ad_categ_deletesure']; ?></h2>
-        </header>
-
-<?php
 if ($permission['delcateg']) {
     $category = new PMF_Category($faqConfig, array(), false);
     $category->setUser($currentAdminUser);
     $category->setGroups($currentAdminGroups);
     $categories = $category->getAllCategories();
     $id         = PMF_Filter::filterInput(INPUT_GET, 'cat', FILTER_VALIDATE_INT, 0);
-    ?>
-        <form class="form-horizontal" action="?action=removecategory" method="post">
-            <input type="hidden" name="cat" value="<?php print $id; ?>" />
-            <input type="hidden" name="lang" value="<?php print $LANGCODE; ?>" />
-            <input type="hidden" name="csrf" value="<?php print $user->getCsrfTokenFromSession(); ?>" />
 
-            <div class="control-group">
-                <label class="control-label"><?php print $PMF_LANG['ad_categ_titel']; ?>:</label>
-                <div class="controls">
-                    <?php print $categories[$id]['name']; ?>
-                </div>
-            </div>
+    $twig->loadTemplate('category/delete.twig')
+        ->display(
+            array(
+                'LANGCODE'            => $LANGCODE,
+                'PMF_LANG'            => $PMF_LANG,
+                'categoryDescription' => $categories[$id]['description'],
+                'categoryName'        => $categories[$id]['name'],
+                'csrfToken'           => $user->getCsrfTokenFromSession(),
+                'id'                  => $id
+            )
+        );
 
-            <div class="control-group">
-                <label class="control-label"><?php print $PMF_LANG['ad_categ_desc']; ?>:</label>
-                <div class="controls">
-                    <?php print $categories[$id]['description']; ?>
-                    <label class="radio">
-                        <input type="radio" checked name="deleteall" value="yes" />
-                        <?php print $PMF_LANG['ad_categ_deletealllang']; ?>
-                    </label>
-                    <label class="radio">
-                        <input type="radio" name="deleteall" value="no" />
-                        <?php print $PMF_LANG['ad_categ_deletethislang']; ?>
-                    </label>
-                </div>
-            </div>
-
-            <div class="form-actions">
-                <button class="btn btn-danger" type="submit" name="submit">
-                    <?php print $PMF_LANG['ad_categ_del_yes']; ?>
-                </button>
-                <a class="btn btn-success" onclick="javascript:history.back();">
-                    <?php print $PMF_LANG['ad_categ_del_no']; ?>
-                </a>
-            </div>
-        </form>
-<?php
+    unset($category, $categories, $id);
 } else {
-    print $PMF_LANG['err_NotAuth'];
+    require 'noperm.php';
 }
