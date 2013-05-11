25973b1b2333838c4106f21d9f3f21af31a755c6
Thorsten Rinne <thorsten@phpmyfaq.de>
2013-05-11 16:41:13 +0200

    Fixed validation issue (#598)

--- phpmyfaq/inc/PMF/Category.php
+++ phpmyfaq/inc/PMF/Category.php
@@ -661,7 +661,7 @@ class PMF_Category
                 $num_entries = '';
             } else {
                 $totFaqRecords += $number[$parent];
-                $num_entries    = '<span id="rssCategoryLink"> ('.$plr->GetMsg('plmsgEntries',$number[$parent]);
+                $num_entries    = '<span class="rssCategoryLink"> ('.$plr->GetMsg('plmsgEntries',$number[$parent]);
                 $num_entries   .= sprintf(
                     ' <a href="feed/category/rss.php?category_id=%d&category_lang=%s" target="_blank"><img id="category_%d_RSS" src="assets/img/feed.png" width="16" height="16" alt="RSS" border="0" /></a>',
                     $parent,
