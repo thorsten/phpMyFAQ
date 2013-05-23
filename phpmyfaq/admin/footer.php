c70faa7142e666af9b97466f3615381db9395c55
Thorsten Rinne <thorsten@phpmyfaq.de>
2013-05-23 18:08:50 +0200

    Let the user resize the editor textarea

--- phpmyfaq/admin/footer.php
+++ phpmyfaq/admin/footer.php
@@ -90,6 +90,7 @@ tinyMCE.init({
     theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
     theme_advanced_statusbar_location : "bottom",
+    theme_advanced_resizing : true,
     relative_urls           : false,
     convert_urls            : false,
     remove_linebreaks       : false, 
