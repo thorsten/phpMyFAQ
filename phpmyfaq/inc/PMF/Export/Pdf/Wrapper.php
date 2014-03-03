5cac8463db90621b197bc5e0dff68d6257cff35d
Thorsten Rinne <thorsten@phpmyfaq.de>
2014-03-03 15:37:48 +0100

    Fixed support for Czech PDF export, closes #769

--- phpmyfaq/inc/PMF/Export/Pdf/Wrapper.php
+++ phpmyfaq/inc/PMF/Export/Pdf/Wrapper.php
@@ -256,6 +256,7 @@ class PMF_Export_Pdf_Wrapper extends TCPDF
         'tw' => 'arialunicid0',
         'ja' => 'arialunicid0',
         'ko' => 'arialunicid0',
+        'cs' => 'arialunicid0',
         'default' => 'helvetica'
     );
 
