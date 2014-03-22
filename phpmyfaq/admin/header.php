ba8786577859c0437af0f707b4414ac9ebc440bd
Thorsten Rinne <thorsten@phpmyfaq.de>
2014-03-22 16:43:54 +0100

    Removed conditional comments for IE6 and IE7

--- phpmyfaq/admin/header.php
+++ phpmyfaq/admin/header.php
@@ -163,8 +163,6 @@ switch ($action) {
 }
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7 ]> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js ie7"> <![endif]-->
 <!--[if IE 8 ]> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js ie8"> <![endif]-->
 <!--[if IE 9 ]> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js"> <!--<![endif]-->
