4bc119d73d39256930e5b17be8422f92af2f39ba
Thorsten Rinne <thorsten@phpmyfaq.de>
2014-03-23 07:01:04 +0100

    Refacotring the admin system info (#745)

--- phpmyfaq/admin/system.php
+++ phpmyfaq/admin/system.php
@@ -30,34 +30,40 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
 if ($permission['editconfig']) {
     $faqSystem = new PMF_System();
 ?>
-<header>
-    <h2><i class="icon-wrench"></i> <?php echo $PMF_LANG['ad_system_info']; ?></h2>
-</header>
+    <header class="row">
+        <div class="col-lg-12">
+            <h2><i class="fa fa-wrench fa-fw"></i> <?php echo $PMF_LANG['ad_system_info'] ?></h2>
+        </div>
+    </header>
 
-<table class="table table-striped">
-    <tbody>
-    <?php
-    $systemInformation = array(
-        'phpMyFAQ Version'           => $faqSystem->getVersion(),
-        'Server Software'            => $_SERVER['SERVER_SOFTWARE'],
-        'Server Document root'       => $_SERVER['DOCUMENT_ROOT'],
-        'phpMyFAQ installation path' => dirname(dirname($_SERVER['SCRIPT_FILENAME'])),
-        'PHP Version'                => PHP_VERSION,
-        'Webserver Interface'        => strtoupper(PHP_SAPI),
-        'PHP Extensions'             => implode(', ', get_loaded_extensions()),
-        'PHP Session path'           => session_save_path(),
-        'Database Server'            => PMF_Db::getType(),
-        'Database Server Version'    => $faqConfig->getDb()->serverVersion(),
-        'Database Client Version'    => $faqConfig->getDb()->clientVersion(),
-    );
-    foreach ($systemInformation as $name => $info): ?>
-    <tr>
-        <td class="span3"><strong><?php echo $name ?></strong></td>
-        <td><?php echo $info ?></td>
-    </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+    <div class="row">
+        <div class="col-lg-12">
+            <table class="table table-striped">
+                <tbody>
+                <?php
+                $systemInformation = array(
+                    'phpMyFAQ Version'           => $faqSystem->getVersion(),
+                    'Server Software'            => $_SERVER['SERVER_SOFTWARE'],
+                    'Server Document root'       => $_SERVER['DOCUMENT_ROOT'],
+                    'phpMyFAQ installation path' => dirname(dirname($_SERVER['SCRIPT_FILENAME'])),
+                    'PHP Version'                => PHP_VERSION,
+                    'Webserver Interface'        => strtoupper(PHP_SAPI),
+                    'PHP Extensions'             => implode(', ', get_loaded_extensions()),
+                    'PHP Session path'           => session_save_path(),
+                    'Database Server'            => PMF_Db::getType(),
+                    'Database Server Version'    => $faqConfig->getDb()->serverVersion(),
+                    'Database Client Version'    => $faqConfig->getDb()->clientVersion(),
+                );
+                foreach ($systemInformation as $name => $info): ?>
+                    <tr>
+                        <td class="col-lg-2"><strong><?php echo $name ?></strong></td>
+                        <td><?php echo $info ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 <?php
 } else {
     echo $PMF_LANG['err_NotAuth'];
