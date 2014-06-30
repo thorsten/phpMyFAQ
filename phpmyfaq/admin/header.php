e6ba18f89e68a0b9b9e466ba00577396ca2b268a
TonyChyi <tonychee1989@gmail.com>
2014-06-30 13:47:09 +0800

    bug fix

--- phpmyfaq/admin/header.php
+++ phpmyfaq/admin/header.php
@@ -2,7 +2,7 @@
 /**
  * Header of the admin area
  *
- * PHP Version 5.3
+ * PHP Version 5.4
  *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
@@ -17,6 +17,8 @@
  * @since     2003-02-26
  */
 
+use PMF\Helper\AdminMenuBuilder;
+
 if (!defined('IS_VALID_PHPMYFAQ')) {
     $protocol = 'http';
     if (isset($_SERVER['HTTPS']) && strtoupper($_SERVER['HTTPS']) === 'ON'){
@@ -30,32 +32,47 @@ $httpHeader = new PMF_Helper_Http();
 $httpHeader->setContentType('text/html');
 $httpHeader->addHeader();
 
-$secLevelEntries   = '';
-$dashboardPage     = true;
-$contentPage       = false;
-$userPage          = false;
-$statisticsPage    = false;
-$exportsPage       = false;
-$backupPage        = false;
-$configurationPage = false;
-$edAutosave        = (('editentry' === $action) && $faqConfig->get('records.autosaveActive'));
+$templateVars = array(
+    'PMF_LANG'              => $PMF_LANG,
+    'appleTouchIcon'        => '../assets/template/' . PMF_Template::getTplSetName() . '/apple-touch-icon.png',
+    'baseUrl'               => $faqConfig->get('main.referenceURL') . '/admin/',
+    'editorAutosaveActive'  => ('editentry' === $action) && $faqConfig->get('records.autosaveActive'),
+    'editorAutosaveSeconds' => $faqConfig->get('records.autosaveSecs'),
+    'gravatarActive'        => $faqConfig->get('main.enableGravatarSupport'),
+    'isAuthenticated'       => isset($auth) && in_array(true, $permission),
+    'pmfVersion'            => $faqConfig->get('main.currentVersion'),
+    'secLevelEntries'       => '',
+    'shortcutIcon'          => '../assets/template/' . PMF_Template::getTplSetName() . '/favicon.ico',
+    'time'                  => time(),
+    'titleFAQ'              => $faqConfig->get('main.titleFAQ'),
+    'userDisplayName'       => isset($user) ? $user->getUserData('display_name'): '',
+    'userTooltip'           => isset($user) ? $PMF_LANG['ad_user_loggedin'] . $user->getLogin() : '',
+    'userEmail'             => isset($user) ? $user->getUserData('email') : ''
+);
 
-$adminHelper = new PMF_Helper_Administration();
-$adminHelper->setPermission($permission);
+if (isset($user) && $faqConfig->get('main.enableGravatarSupport')) {
+    $avatar = new PMF_Services_Gravatar($faqConfig);
+    $templateVars['gravatarImage'] = $avatar->getImage($user->getUserData('email'), array('size' => 30));
+    unset($avatar);
+} else {
+    $templateVars['gravatarImage'] = '';
+}
+
+$adminMenuBuilder = new AdminMenuBuilder($twig);
+$adminMenuBuilder->setUser($user);
 
 switch ($action) {
     case 'user':
     case 'group':
     case 'passwd':
     case 'cookies':
-        $secLevelHeader = $PMF_LANG['admin_mainmenu_users'];
-        $secLevelEntries .= $adminHelper->addMenuEntry('adduser+edituser+deluser', 'user', 'ad_menu_user_administration', $action);
+        $adminMenuBuilder->setHeader($PMF_LANG['admin_mainmenu_users']);
+        $adminMenuBuilder->addMenuEntry('adduser+edituser+deluser', 'user', 'ad_menu_user_administration', $action);
         if ($faqConfig->get('security.permLevel') != 'basic') {
-            $secLevelEntries .= $adminHelper->addMenuEntry('addgroup+editgroup+delgroup', 'group', 'ad_menu_group_administration', $action);
+            $adminMenuBuilder->addMenuEntry('addgroup+editgroup+delgroup', 'group', 'ad_menu_group_administration', $action);
         }
-        $secLevelEntries .= $adminHelper->addMenuEntry('passwd', 'passwd', 'ad_menu_passwd', $action);
-        $dashboardPage    = false;
-        $userPage         = true;
+        $adminMenuBuilder->addMenuEntry('passwd', 'passwd', 'ad_menu_passwd', $action);
+        $templateVars['activePage'] = 'user';
         break;
     case 'content':
     case 'category':
@@ -92,18 +109,17 @@ switch ($action) {
     case 'takequestion':
     case 'comments':
     case 'attachments':
-        $secLevelHeader   = $PMF_LANG['admin_mainmenu_content'];
-        $secLevelEntries .= $adminHelper->addMenuEntry('addcateg+editcateg+delcateg', 'category', 'ad_menu_categ_edit', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('addbt', 'editentry', 'ad_entry_add', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('editbt+delbt', 'view', 'ad_menu_entry_edit', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('editbt+delbt', 'searchfaqs', 'ad_menu_searchfaqs', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('delcomment', 'comments', 'ad_menu_comments', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('delquestion', 'question', 'ad_menu_open', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('addglossary+editglossary+delglossary', 'glossary', 'ad_menu_glossary', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('addnews+editnews+delnews', 'news', 'ad_menu_news_edit', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('addattachment+editattachment+delattachment', 'attachments', 'ad_menu_attachments', $action);
-        $dashboardPage    = false;
-        $contentPage      = true;
+        $adminMenuBuilder->setHeader($PMF_LANG['admin_mainmenu_content']);
+        $adminMenuBuilder->addMenuEntry('addcateg+editcateg+delcateg', 'category', 'ad_menu_categ_edit', $action);
+        $adminMenuBuilder->addMenuEntry('addbt', 'editentry', 'ad_entry_add', $action);
+        $adminMenuBuilder->addMenuEntry('editbt+delbt', 'view', 'ad_menu_entry_edit', $action);
+        $adminMenuBuilder->addMenuEntry('editbt+delbt', 'searchfaqs', 'ad_menu_searchfaqs', $action);
+        $adminMenuBuilder->addMenuEntry('delcomment', 'comments', 'ad_menu_comments', $action);
+        $adminMenuBuilder->addMenuEntry('delquestion', 'question', 'ad_menu_open', $action);
+        $adminMenuBuilder->addMenuEntry('addglossary+editglossary+delglossary', 'glossary', 'ad_menu_glossary', $action);
+        $adminMenuBuilder->addMenuEntry('addnews+editnews+delnews', 'news', 'ad_menu_news_edit', $action);
+        $adminMenuBuilder->addMenuEntry('addattachment+editattachment+delattachment', 'attachments', 'ad_menu_attachments', $action);
+        $templateVars['activePage'] = 'content';
         break;
     case 'statistics':
     case 'viewsessions':
@@ -113,26 +129,23 @@ switch ($action) {
     case 'searchstats':
     case 'reports':
     case 'reportview':
-        $secLevelHeader   = $PMF_LANG['admin_mainmenu_statistics'];
-        $secLevelEntries .= $adminHelper->addMenuEntry('viewlog', 'statistics', 'ad_menu_stat', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('viewlog', 'viewsessions', 'ad_menu_session', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('adminlog', 'adminlog', 'ad_menu_adminlog', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('viewlog', 'searchstats', 'ad_menu_searchstats', $action);
-        $secLevelEntries .= $adminHelper->addMenuEntry('reports', 'reports', 'ad_menu_reports', $action);
-        $dashboardPage    = false;
-        $statisticsPage   = true;
+        $adminMenuBuilder->setHeader($PMF_LANG['admin_mainmenu_statistics']);
+        $adminMenuBuilder->addMenuEntry('viewlog', 'statistics', 'ad_menu_stat', $action);
+        $adminMenuBuilder->addMenuEntry('viewlog', 'viewsessions', 'ad_menu_session', $action);
+        $adminMenuBuilder->addMenuEntry('adminlog', 'adminlog', 'ad_menu_adminlog', $action);
+        $adminMenuBuilder->addMenuEntry('viewlog', 'searchstats', 'ad_menu_searchstats', $action);
+        $adminMenuBuilder->addMenuEntry('reports', 'reports', 'ad_menu_reports', $action);
+        $templateVars['activePage'] = 'statistics';
         break;
     case 'export':
-        $secLevelHeader   = $PMF_LANG['admin_mainmenu_exports'];
-        $secLevelEntries .= $adminHelper->addMenuEntry('', 'export', 'ad_menu_export', $action);
-        $dashboardPage    = false;
-        $exportsPage      = true;
+        $adminMenuBuilder->setHeader($PMF_LANG['admin_mainmenu_exports']);
+        $adminMenuBuilder->addMenuEntry('', 'export', 'ad_menu_export', $action);
+        $templateVars['activePage'] = 'exports';
         break;
     case 'backup':
-        $secLevelHeader   = $PMF_LANG['admin_mainmenu_backup'];
-        $secLevelEntries .= $adminHelper->addMenuEntry('', 'backup', 'ad_menu_export', $action);
-        $dashboardPage    = false;
-        $backupPage       = true;
+        $adminMenuBuilder->setHeader($PMF_LANG['admin_mainmenu_backup']);
+        $adminMenuBuilder->addMenuEntry('', 'backup', 'ad_menu_export', $action);
+        $templateVars['activePage'] = 'backup';
         break;
     case 'config':
     case 'stopwordsconfig':
@@ -142,178 +155,27 @@ switch ($action) {
     case 'upgrade':
     case 'instances':
     case 'system':
-        $secLevelHeader    = $PMF_LANG['admin_mainmenu_configuration'];
-        $secLevelEntries  .= $adminHelper->addMenuEntry('editconfig', 'config', 'ad_menu_editconfig', $action);
-        $secLevelEntries  .= $adminHelper->addMenuEntry('', 'system', 'ad_system_info', $action, false);
-        $secLevelEntries  .= $adminHelper->addMenuEntry('editinstances+addinstances+delinstances', 'instances', 'ad_menu_instances', $action);
-        $secLevelEntries  .= $adminHelper->addMenuEntry('editconfig', 'stopwordsconfig', 'ad_menu_stopwordsconfig', $action);
-        $secLevelEntries  .= $adminHelper->addMenuEntry('edittranslation+addtranslation+deltranslation', 'translist', 'ad_menu_translations', $action);
-        $dashboardPage     = false;
-        $configurationPage = true;
+        $adminMenuBuilder->setHeader($PMF_LANG['admin_mainmenu_configuration']);
+        $adminMenuBuilder->addMenuEntry('editconfig', 'config', 'ad_menu_editconfig', $action);
+        $adminMenuBuilder->addMenuEntry('', 'system', 'ad_system_info', $action, false);
+        $adminMenuBuilder->addMenuEntry('editinstances+addinstances+delinstances', 'instances', 'ad_menu_instances', $action);
+        $adminMenuBuilder->addMenuEntry('editconfig', 'stopwordsconfig', 'ad_menu_stopwordsconfig', $action);
+        $adminMenuBuilder->addMenuEntry('edittranslation+addtranslation+deltranslation', 'translist', 'ad_menu_translations', $action);
+        $templateVars['activePage'] = 'configuration';
         break;
     default:
-        $secLevelHeader   = $PMF_LANG['admin_mainmenu_home'];
-        $secLevelEntries .= $adminHelper->addMenuEntry('addcateg+editcateg+delcateg', 'category', 'ad_menu_categ_edit');
-        $secLevelEntries .= $adminHelper->addMenuEntry('addbt', 'editentry', 'ad_quick_record');
-        $secLevelEntries .= $adminHelper->addMenuEntry('editbt+delbt', 'view', 'ad_menu_entry_edit');
-        $secLevelEntries .= $adminHelper->addMenuEntry('delquestion', 'question', 'ad_menu_open');
-        $secLevelEntries .= $adminHelper->addMenuEntry('', 'system', 'ad_system_info', $action, false);
-        $dashboardPage    = true;
+        $adminMenuBuilder->setHeader($PMF_LANG['admin_mainmenu_home']);
+        $adminMenuBuilder->addMenuEntry('addcateg+editcateg+delcateg', 'category', 'ad_menu_categ_edit');
+        $adminMenuBuilder->addMenuEntry('addbt', 'editentry', 'ad_quick_record');
+        $adminMenuBuilder->addMenuEntry('editbt+delbt', 'view', 'ad_menu_entry_edit');
+        $adminMenuBuilder->addMenuEntry('delquestion', 'question', 'ad_menu_open');
+        $adminMenuBuilder->addMenuEntry('', 'system', 'ad_system_info', $action, false);
+        $templateVars['activePage'] = 'dashboard';
         break;
 }
-?>
-<!DOCTYPE html>
-<!--[if lt IE 7 ]> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html lang="<?php echo $PMF_LANG['metaLanguage']; ?>" class="no-js"> <!--<![endif]-->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-    <title><?php echo $faqConfig->get('main.titleFAQ'); ?> - powered by phpMyFAQ <?php echo $faqConfig->get('main.currentVersion'); ?></title>
-    <base href="<?php echo $faqSystem->getSystemUri($faqConfig) ?>admin/" />
+$templateVars['sideNavigation'] = $adminMenuBuilder->render();
 
-    <meta name="description" content="Only Chuck Norris can divide by zero.">
-    <meta name="author" content="phpMyFAQ Team">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="application-name" content="phpMyFAQ <?php echo $faqConfig->get('main.currentVersion'); ?>">
-    <meta name="copyright" content="(c) 2001-2014 phpMyFAQ Team">
-    <meta name="publisher" content="phpMyFAQ Team">
-    <meta name="MSSmartTagsPreventParsing" content="true">
+$twig->loadTemplate('header.twig')->display($templateVars);
 
-    <link rel="stylesheet" href="assets/css/style.css?v=1">
-    <link rel="stylesheet" href="../assets/js/plugins/datePicker/datePicker.css" type="text/css">
-
-    <script src="../assets/js/libs/modernizr.min.js"></script>
-    <script src="../assets/js/libs/jquery.min.js"></script>
-    <script src="../assets/js/phpmyfaq.js"></script>
-
-    <script src="../assets/js/plugins/datePicker/date.js"></script>
-    <script src="../assets/js/plugins/datePicker/jquery.datePicker.js"></script>
-
-    <script src="kindeditor/kindeditor-all.js"></script>
-    <script src="kindeditor/lang/zh-CN.js"></script>
-
-<?php if ($edAutosave): ?>
-    <script>var pmfAutosaveInterval = <?php echo $faqConfig->get('records.autosaveSecs') ?>;</script>
-    <script src="../assets/js/autosave.js"></script>
-<?php endif; ?>
-
-    <link rel="shortcut icon" href="../assets/template/<?php echo PMF_Template::getTplSetName(); ?>/favicon.ico">
-    <link rel="apple-touch-icon" href="../assets/template/<?php echo PMF_Template::getTplSetName(); ?>/apple-touch-icon.png">
-</head>
-<body dir="<?php echo $PMF_LANG["dir"]; ?>">
-
-<div class="navbar navbar-fixed-top">
-    <div class="navbar-inner">
-        <div class="container-fluid">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-            <a class="brand" href="../index.php" rel="tooltip" title="<?php echo $faqConfig->get('main.titleFAQ'); ?>">
-                phpMyFAQ
-            </a>
-            <div class="nav-collapse">
-                <?php if (isset($auth) && in_array(true, $permission)): ?>
-                <ul class="nav">
-                    <li<?php echo ($dashboardPage ? ' class="active"' : ''); ?>>
-                        <a href="index.php">
-                            <i class="icon-home icon-white"></i> <?php echo $PMF_LANG['admin_mainmenu_home']; ?>
-                        </a>
-                    </li>
-                    <li<?php echo ($userPage ? ' class="active"' : ''); ?>>
-                        <a href="index.php?action=user">
-                            <i class="icon-user icon-white"></i> <?php echo $PMF_LANG['admin_mainmenu_users']; ?>
-                        </a>
-                    </li>
-                    <li<?php echo ($contentPage ? ' class="active"' : ''); ?>>
-                        <a href="index.php?action=content">
-                            <i class="icon-pencil icon-white"></i> <?php echo $PMF_LANG['admin_mainmenu_content']; ?>
-                        </a>
-                    </li>
-                    <li<?php echo ($statisticsPage ? ' class="active"' : ''); ?>>
-                        <a href="index.php?action=statistics">
-                            <i class="icon-tasks icon-white"></i> <?php echo $PMF_LANG['admin_mainmenu_statistics']; ?>
-                        </a>
-                    </li>
-                    <li<?php echo ($exportsPage ? ' class="active"' : ''); ?>>
-                        <a href="index.php?action=export">
-                            <i class="icon-book icon-white"></i> <?php echo $PMF_LANG['admin_mainmenu_exports']; ?>
-                        </a>
-                    </li>
-                    <li<?php echo ($backupPage ? ' class="active"' : ''); ?>>
-                        <a href="index.php?action=backup">
-                            <i class="icon-download-alt icon-white"></i> <?php echo $PMF_LANG['admin_mainmenu_backup']; ?>
-                        </a>
-                    </li>
-                    <li<?php echo ($configurationPage ? ' class="active"' : ''); ?>>
-                        <a href="index.php?action=config">
-                            <i class="icon-wrench icon-white"></i> <?php echo $PMF_LANG['admin_mainmenu_configuration']; ?>
-                        </a>
-                    </li>
-                </ul>
-                <ul class="nav pull-right">
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            <span title="<?php echo $PMF_LANG['ad_user_loggedin'] . $user->getLogin(); ?>">
-                            <?php echo $user->getUserData('display_name'); ?>
-                            </span>
-                            <b class="caret"></b>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li class="divider"></li>
-                            <li>
-                                <a href="index.php?action=logout">
-                                    <i class="icon-off"></i> <?php echo $PMF_LANG['admin_mainmenu_logout']; ?>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-                <?php else: ?>
-                <ul class="nav">
-                    <li></li>
-                </ul>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div id="content">
-    <div class="container-fluid">
-        <div class="row-fluid">
-            <?php if (isset($auth) && in_array(true, $permission)) { ?>
-            <div class="span2">
-                <div class="userpanel">
-                    <?php
-                    if ($faqConfig->get('main.enableGravatarSupport')) {
-                        $avatar = new PMF_Services_Gravatar($faqConfig);
-                        echo $avatar->getImage($user->getUserData('email'), array('size' => 30));
-                    }
-                    printf(
-                        '%s<br><small>%s</small>',
-                        $user->getUserData('display_name'),
-                        $user->getUserData('email')
-                    );
-                    ?>
-                </div>
-                <hr>
-                <div class="well categories">
-                    <ul class="nav nav-list">
-                        <li class="nav-header"><?php echo $secLevelHeader; ?></li>
-                        <?php echo $secLevelEntries; ?>
-                        <li class="nav-header">Admin worklog</li>
-                        <li><span id="saving_data_indicator"></span></li>
-                        <li class="nav-header"><?php echo $PMF_LANG['ad_session_expiration']; ?></li>
-                        <li><span id="sessioncounter"><img src="images/indicator.gif"> Loading...</span></li>
-                    </ul>
-                </div>
-            </div>
-            <?php } ?>
-
-            <div class="span10">
+unset($templateVars);
