798f23328dae5628450bb52c374f7d9a607ccbcf
Thorsten Rinne <thorsten@phpmyfaq.de>
2014-07-13 12:10:23 +0200

    Fixed Twitter OAuth repo, closes #881

--- scripts/moveVendors.sh
+++ scripts/moveVendors.sh
@@ -4,5 +4,5 @@ cwd=`pwd`
 
 mkdir -p $cwd/phpmyfaq/inc/libs/phpseclib/Crypt
 cp -r $cwd/vendor/phpseclib/phpseclib/Crypt $cwd/phpmyfaq/inc/libs/phpseclib
-cp -r $cwd/vendor/twitteroauth/twitteroauth $cwd/phpmyfaq/inc/libs/twitteroauth
+cp -r $cwd/vendor/thorsten/twitteroauth/twitteroauth $cwd/phpmyfaq/inc/libs/twitteroauth
 cp -r $cwd/vendor/symfony/class-loader/* $cwd/phpmyfaq/inc/libs/
