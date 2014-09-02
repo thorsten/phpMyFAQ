ce2952a9791ba339019593324ee8643e4f1a85b4
Thorsten Rinne <t.rinne@yatego.com>
2014-09-02 20:35:58 +0200

    Added missing CSRF check

--- phpmyfaq/admin/instances.php
+++ phpmyfaq/admin/instances.php
@@ -117,6 +117,7 @@ if ($user->perm->checkRight($user->getUserId(), 'editinstances')) {
             <td>
                 <?php if ($currentInstance->getConfig('isMaster') !== true): ?>
                 <a href="javascript:;" id="delete-instance-<?php print $site->id ?>"
+                   data-csrf-token="<?php echo $user->getCsrfTokenFromSession() ?>"
                    class="btn btn-danger pmf-instance-delete"><i class="fa fa-trash-o"></i>
                 </a>
                 <?php endif; ?>
@@ -249,10 +250,11 @@ if ($user->perm->checkRight($user->getUserId(), 'editinstances')) {
             event.preventDefault();
             var targetId = event.target.id.split('-');
             var id = targetId[2];
+            var csrf = this.getAttribute('data-csrf-token');
 
             if (confirm('Are you sure?')) {
                 $.get('index.php',
-                    { action: 'ajax', ajax: 'config', ajaxaction: 'delete_instance', instanceId: id },
+                    { action: 'ajax', ajax: 'config', ajaxaction: 'delete_instance', instanceId: id, csrf: csrf },
                     function(data) {
                         if (typeof(data.deleted) === 'undefined') {
                             $('.table').after(
