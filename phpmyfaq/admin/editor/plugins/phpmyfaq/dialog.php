7daea4855a48c0363e6156b5f650b1f5800a084c
Thorsten Rinne <thorsten@phpmyfaq.de>
2014-09-25 19:59:25 +0200

    Fixed Q button in editor, closes #905

--- phpmyfaq/admin/editor/plugins/phpmyfaq/dialog.php
+++ phpmyfaq/admin/editor/plugins/phpmyfaq/dialog.php
@@ -6,17 +6,29 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta http-equiv="content-language" content="en">
     <meta name="application-name" content="phpMyFAQ">
-    <meta name="copyright" content="(c) 2001-2010 phpMyFAQ Team">
+    <meta name="copyright" content="(c) 2001-2014 phpMyFAQ Team">
     <script type="text/javascript" src="../../../../assets/js/libs/jquery.min.js"></script>
     <script type="text/javascript" src="../../tiny_mce_popup.js"></script>
     <script type="text/javascript" src="js/dialog.js"></script>
 </head>
 <body>
 
+<?php
+
+define('PMF_ROOT_DIR', dirname(dirname(dirname(dirname(__DIR__)))));
+define('IS_VALID_PHPMYFAQ', null);
+
+require PMF_ROOT_DIR . '/inc/Bootstrap.php';
+
+$user = PMF_User_CurrentUser::getFromSession($faqConfig);
+
+?>
+
 <form onsubmit="phpmyfaqDialog.insert(); return false;" action="post">
+    <input type="hidden" name="csrf" id="csrf" value="<?php print $user->getCsrfTokenFromSession(); ?>">
     
     <div class="title">{#phpmyfaq_dlg.title}</div>
-    <input id="suggestbox" type="text" name="search" value="" size="64">
+    <input id="suggestbox" type="text" name="search" value="" size="64" autocomplete="off">
     
     <div id="suggestions">
     
