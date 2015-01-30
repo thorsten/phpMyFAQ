3b14e406766c7387467f4ea7b5424e278b177841
Thorsten Rinne <thorsten@phpmyfaq.de>
2015-01-30 16:56:32 +0100

    Updated dependencies, now using stable release of SwiftMailer

--- scripts/moveVendors.sh
+++ scripts/moveVendors.sh
@@ -7,4 +7,4 @@ mkdir -p $cwd/phpmyfaq/inc/libs/swiftmailer
 cp -r $cwd/vendor/phpseclib/phpseclib/Crypt $cwd/phpmyfaq/inc/libs/phpseclib
 cp -r $cwd/vendor/thorsten/twitteroauth/twitteroauth $cwd/phpmyfaq/inc/libs/twitteroauth
 cp -r $cwd/vendor/symfony/class-loader/* $cwd/phpmyfaq/inc/libs/
-cp -r $cwd/vendor/swiftmailer/lib/* $cwd/phpmyfaq/inc/libs/swiftmailer
+cp -r $cwd/vendor/swiftmailer/swiftmailer/lib/* $cwd/phpmyfaq/inc/libs/swiftmailer
