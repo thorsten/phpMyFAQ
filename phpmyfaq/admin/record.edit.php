4d23e01b9c88d3176aacf0dcd4dab575aa075e05
Thorsten Rinne <thorsten@phpmyfaq.de>
2015-01-31 10:46:45 +0100

    Revert "Fixed issue with overridden category permissions"
    This reverts commit 2e0fa29bace897f5a69494b9ec3d8e499445d78b.

--- phpmyfaq/admin/record.edit.php
+++ phpmyfaq/admin/record.edit.php
@@ -688,7 +688,7 @@ if (($permission['editbt']|| $permission['addbt']) && !PMF_Db::checkOnEmptyTable
         $('#tags').focus(function() { showHelp('tags'); });
 
         // Override FAQ permissions with Category permission to avoid confused users
-        $('#phpmyfaq-categories option:selected').each(function() {
+        $('#phpmyfaq-categories').click(function() {
             var categories = $('#phpmyfaq-categories option:selected').map(function() {
                 return $(this).val();
             }).get();
