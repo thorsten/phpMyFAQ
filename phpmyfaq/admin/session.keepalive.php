94140f64da18a7d4b923f409771dafb53229d825
Thorsten Rinne <thorsten@phpmyfaq.de>
2015-06-04 11:58:02 +0200

    Fixed check for cookie in session counter (#1024)

--- phpmyfaq/admin/session.keepalive.php
+++ phpmyfaq/admin/session.keepalive.php
@@ -47,7 +47,10 @@ if (!is_null($language) && PMF_Language::isASupportedLanguage($language)) {
 //
 PMF_String::init($language);
 
-$user        = PMF_User_CurrentUser::getFromSession($faqConfig);
+$user = PMF_User_CurrentUser::getFromCookie($faqConfig);
+if (! $user instanceof PMF_User_CurrentUser) {
+    $user = PMF_User_CurrentUser::getFromSession($faqConfig);
+}
 $refreshTime = (PMF_AUTH_TIMEOUT - PMF_AUTH_TIMEOUT_WARNING) * 60;
 ?>
 <!DOCTYPE html>
