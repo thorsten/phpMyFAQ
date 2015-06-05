1853ad9930144e0796aaaa853b1c523708f88a3b
Thorsten Rinne <thorsten@phpmyfaq.de>
2015-06-05 13:08:40 +0200

    Switched back to Symfony 2.6

--- scripts/moveVendors.sh
+++ scripts/moveVendors.sh
@@ -5,14 +5,13 @@ cwd=`pwd`
 mkdir -p $cwd/phpmyfaq/inc/libs/parsedown
 mkdir -p $cwd/phpmyfaq/inc/libs/phpseclib/Crypt
 mkdir -p $cwd/phpmyfaq/inc/libs/swiftmailer
-mkdir -p $cwd/phpmyfaq/inc/libs/Symfony
 mkdir -p $cwd/phpmyfaq/inc/libs/tcpdf
 
 cp -r $cwd/vendor/erusev/parsedown/Parsedown.php $cwd/phpmyfaq/inc/libs/parsedown/Parsedown.php
 cp -r $cwd/vendor/erusev/parsedown-extra/ParsedownExtra.php $cwd/phpmyfaq/inc/libs/parsedown/ParsedownExtra.php
 cp -r $cwd/vendor/phpseclib/phpseclib/Crypt $cwd/phpmyfaq/inc/libs/phpseclib
 cp -r $cwd/vendor/thorsten/twitteroauth/twitteroauth $cwd/phpmyfaq/inc/libs/twitteroauth
-cp -r $cwd/vendor/symfony/class-loader/* $cwd/phpmyfaq/inc/libs/Symfony/
+cp -r $cwd/vendor/symfony/class-loader/* $cwd/phpmyfaq/inc/libs/
 cp -r $cwd/vendor/swiftmailer/swiftmailer/lib/* $cwd/phpmyfaq/inc/libs/swiftmailer
 
 # TCPDF
