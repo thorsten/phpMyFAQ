793d2ae5204a4f6fd09831661351667aa1445c53
Thorsten Rinne <thorsten@phpmyfaq.de>
2015-12-23 14:10:38 +0100

    Updated build scripts

--- scripts/bumpcopyright.sh
+++ scripts/bumpcopyright.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-find . -name "*.php" -exec perl -pi -w -e 's#(copyright.*-20)([0-9]{2})#${1}15#;' {} \;
+find . -name "*.php" -exec perl -pi -w -e 's#(copyright.*-20)([0-9]{2})#${1}16#;' {} \;
+find . -name "*.js" -exec perl -pi -w -e 's#(copyright.*-20)([0-9]{2})#${1}16#;' {} \;
+find . -name "*.less" -exec perl -pi -w -e 's#(copyright.*-20)([0-9]{2})#${1}16#;' {} \;
--- scripts/createHashes.php
+++ scripts/createHashes.php
@@ -4,19 +4,18 @@
  * This scripts iterates recursively through the whole phpMyFAQ project and
  * creates SHA-1 keys for all files
  *
- * PHP Version 5.2.3
+ * PHP Version 5.5
  *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/.
  *
- * @category phpMyFAQ
- * @package Scripts
- * @author Thorsten Rinne <thorsten@phpmyfaq.de>
- * @copyright 2012 phpMyFAQ Team
- * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
- * @link http://www.phpmyfaq.de
- * @since 2012-04-11
+ * @category  phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2012-2015 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      http://www.phpmyfaq.de
+ * @since     2012-04-11
  */
 
 define('PMF_ROOT_DIR', dirname(__DIR__) . '/phpmyfaq');
