d4502975e448796a15872be5e027957fb4de3999
Thorsten Rinne <thorsten@phpmyfaq.de>
2016-03-28 11:45:05 +0200

    Fixed broken PostgreSQL setup queries, closes #1110

--- phpmyfaq/inc/PMF/Instance/Database/Pgsql.php
+++ phpmyfaq/inc/PMF/Instance/Database/Pgsql.php
@@ -40,7 +40,7 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             usr INT4 NOT NULL,
             `text` TEXT NOT NULL,
             ip VARCHAR(64) NOT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id))',
 
         'faqattachment' => 'CREATE TABLE %sfaqattachment (
             id SERIAL NOT NULL,
@@ -53,12 +53,12 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             filesize INT4 NOT NULL,
             encrypted INT4 NOT NULL DEFAULT 0,
             mime_type VARCHAR(255) NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id))',
 
         'faqattachment file' => 'CREATE TABLE %sfaqattachment_file (
             virtual_hash CHAR(32) NOT NULL,
             contents BLOB NOT NULL,
-            PRIMARY KEY (virtual_hash)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (virtual_hash))',
 
         'faqcaptcha' => 'CREATE TABLE %sfaqcaptcha (
             id VARCHAR(6) NOT NULL,
@@ -66,7 +66,7 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             language VARCHAR(5) NOT NULL,
             ip VARCHAR(64) NOT NULL,
             captcha_time INT4 NOT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id))',
 
         'faqcategories' => 'CREATE TABLE %sfaqcategories (
             id SERIAL NOT NULL,
@@ -77,7 +77,7 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             user_id SERIAL NOT NULL,
             group_id SERIAL NOT NULL DEFAULT -1,
             active INT4 NULL DEFAULT 1,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang))',
 
         'faqcategoryrelations' => 'CREATE TABLE %sfaqcategoryrelations (
             category_id INT4 NOT NULL,
@@ -85,17 +85,17 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             record_id SERIAL NOT NULL,
             record_lang VARCHAR(5) NOT NULL,
             PRIMARY KEY (category_id, category_lang, record_id, record_lang)),
-            KEY idx_records (record_id, record_lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            KEY idx_records (record_id, record_lang))',
 
         'faqcategory_group' => 'CREATE TABLE %sfaqcategory_group (
             category_id INT4 NOT NULL,
             group_id SERIAL NOT NULL,
-            PRIMARY KEY (category_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (category_id, group_id))',
 
         'faqcategory_user' => 'CREATE TABLE %sfaqcategory_user (
             category_id INT4 NOT NULL,
             user_id SERIAL NOT NULL,
-            PRIMARY KEY (category_id, user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (category_id, user_id))',
 
         'faqchanges' => 'CREATE TABLE %sfaqchanges (
             id SERIAL NOT NULL,
@@ -105,7 +105,7 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             usr INT4 NOT NULL ,
             datum INT4 NOT NULL,
             what text DEFAULT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang))',
 
         'faqcomments' => 'CREATE TABLE %sfaqcomments (
             id_comment SERIAL NOT NULL,
@@ -116,12 +116,12 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             comment text NOT NULL,
             datum VARCHAR(64) NOT NULL,
             helped text DEFAULT NULL,
-            PRIMARY KEY (id_comment)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id_comment))',
 
         'faqconfig' => 'CREATE TABLE %sfaqconfig (
             config_name VARCHAR(255) NOT NULL default \'\',
             config_value VARCHAR(255) DEFAULT NULL,
-            PRIMARY KEY (config_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (config_name))',
 
         'faqdata' => 'CREATE TABLE %sfaqdata (
             id SERIAL NOT NULL,
@@ -143,7 +143,7 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             date_end VARCHAR(14) NOT NULL DEFAULT \'99991231235959\',
             created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             notes text DEFAULT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang))',
 
         'faqdata_revisions' => 'CREATE TABLE %sfaqdata_revisions (
             id SERIAL NOT NULL,
@@ -165,29 +165,29 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             date_end VARCHAR(14) NOT NULL DEFAULT \'99991231235959\',
             created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             notes text DEFAULT NULL,
-            PRIMARY KEY (id, lang, solution_id, revision_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang, solution_id, revision_id))',
 
         'faqdata_group' => 'CREATE TABLE %sfaqdata_group (
             record_id INT4 NOT NULL,
             group_id INT4 NOT NULL,
-            PRIMARY KEY (record_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (record_id, group_id))',
 
         'faqdata_tags' => 'CREATE TABLE %sfaqdata_tags (
             record_id INT4 NOT NULL,
             tagging_id INT4 NOT NULL,
-            PRIMARY KEY (record_id, tagging_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (record_id, tagging_id))',
 
         'faqdata_user' => 'CREATE TABLE %sfaqdata_user (
             record_id INT4 NOT NULL,
             user_id INT4 NOT NULL,
-            PRIMARY KEY (record_id, user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (record_id, user_id))',
 
         'faqglossary' => 'CREATE TABLE %sfaqglossary (
             id SERIAL NOT NULL ,
             lang VARCHAR(5) NOT NULL ,
             item VARCHAR(255) NOT NULL ,
             definition text NOT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang))',
 
         'faqgroup' => 'CREATE TABLE %sfaqgroup (
             group_id SERIAL NOT NULL,
@@ -195,12 +195,12 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             description text NULL,
             auto_join INT4 NULL,
             PRIMARY KEY(group_id)),
-            KEY idx_name (name) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            KEY idx_name (name)',
 
         'faqgroup_right' => 'CREATE TABLE %sfaqgroup_right (
             group_id INT4 NOT NULL,
             right_id INT4 NOT NULL,
-            PRIMARY KEY(group_id, right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY(group_id, right_id))',
 
         'faqinstances' => 'CREATE TABLE %sfaqinstances (
             id INT4 NOT NULL,
@@ -209,13 +209,13 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             comment TEXT NULL,
             created DATETIME NOT NULL,
             modified DATETIME NOT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id))',
 
         'faqinstances_config' => 'CREATE TABLE %sfaqinstances_config (
             instance_id INT4 NOT NULL,
             config_name VARCHAR(255) NOT NULL default \'\',
             config_value VARCHAR(255) DEFAULT NULL,
-            PRIMARY KEY (instance_id, config_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (instance_id, config_name))',
 
         'faqnews' => 'CREATE TABLE %sfaqnews (
             id SERIAL NOT NULL,
@@ -232,7 +232,7 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             link VARCHAR(255) DEFAULT NULL,
             linktitel VARCHAR(255) DEFAULT NULL,
             target VARCHAR(255) NOT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id))',
 
         'faqquestions' => 'CREATE TABLE %sfaqquestions (
             id SERIAL NOT NULL,
@@ -244,7 +244,7 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             created VARCHAR(20) NOT NULL,
             is_visible char(1) default \'Y\',
             answer_id SERIAL NOT NULL DEFAULT 0,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id))',
 
         'faqright' => 'CREATE TABLE %sfaqright (
             right_id SERIAL NOT NULL,
@@ -252,21 +252,21 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             description text NULL,
             for_users INT4 NULL DEFAULT 1,
             for_groups INT4 NULL DEFAULT 1,
-            PRIMARY KEY (right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (right_id))',
 
         'faqsearches' => 'CREATE TABLE %sfaqsearches (
             id SERIAL NOT NULL ,
             lang VARCHAR(5) NOT NULL ,
             searchterm VARCHAR(255) NOT NULL ,
             searchdate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang))',
 
         'faqsessions' => 'CREATE TABLE %sfaqsessions (
             sid SERIAL NOT NULL,
             user_id SERIAL NOT NULL,
             ip VARCHAR(64) NOT NULL,
             time INT4 NOT NULL,
-            PRIMARY KEY (sid)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (sid))',
 
         'faqsessions_idx' => 'CREATE INDEX idx_time ON %sfaqsessions (time)',
 
@@ -274,12 +274,12 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             id SERIAL NOT NULL,
             lang VARCHAR(5) NOT NULL,
             stopword VARCHAR(64) NOT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang))',
 
         'faqtags' => 'CREATE TABLE %sfaqtags (
             tagging_id SERIAL NOT NULL,
             tagging_name VARCHAR(255) NOT NULL ,
-            PRIMARY KEY (tagging_id, tagging_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (tagging_id, tagging_name))',
 
         'faquser' => 'CREATE TABLE %sfaquser (
             user_id SERIAL NOT NULL,
@@ -293,7 +293,7 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
             member_since VARCHAR(14) NULL,
             remember_me VARCHAR(150) NULL,
             success INT(1) NULL DEFAULT 1,
-            PRIMARY KEY (user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (user_id))',
 
         'faquserdata' => 'CREATE TABLE %sfaquserdata (
             user_id SERIAL NOT NULL,
@@ -304,24 +304,24 @@ class PMF_Instance_Database_Pgsql extends PMF_Instance_Database implements PMF_I
         'faquserlogin' => 'CREATE TABLE %sfaquserlogin (
             login VARCHAR(128) NOT NULL,
             pass VARCHAR(80) NULL,
-            PRIMARY KEY (login)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (login))',
 
         'faquser_group' => 'CREATE TABLE %sfaquser_group (
             user_id INT4 NOT NULL,
             group_id INT4 NOT NULL,
-            PRIMARY KEY (user_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (user_id, group_id))',
 
         'faquser_right' => 'CREATE TABLE %sfaquser_right (
             user_id INT4 NOT NULL,
             right_id INT4 NOT NULL,
-            PRIMARY KEY (user_id, right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (user_id, right_id))',
 
         'faqvisits' => 'CREATE TABLE %sfaqvisits (
             id SERIAL NOT NULL,
             lang VARCHAR(5) NOT NULL,
             visits SMALLINT NOT NULL,
             last_visit INT4 NOT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang))',
 
         'faqvoting' => 'CREATE TABLE %sfaqvoting (
             id SERIAL NOT NULL,
