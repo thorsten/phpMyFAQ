731d0fbaa4508e2ac1796c275529b4463131d659
Thorsten Rinne <thorsten@phpmyfaq.de>
2016-05-12 21:19:02 +0200

    2.8.27 -> 2.8.28

--- phpmyfaq/inc/PMF/System.php
+++ phpmyfaq/inc/PMF/System.php
@@ -43,7 +43,7 @@ class PMF_System
     /**
      * Patchlevel
      */
-    const VERSION_PATCHLEVEL = 27;
+    const VERSION_PATCHLEVEL = 28;
 
     /**
      * Pre-release version
--- scripts/version.sh
+++ scripts/version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 if [ "x${PMF_VERSION}" = "x" ]; then
-    PMF_VERSION="2.8.27"
+    PMF_VERSION="2.8.28"
 fi
