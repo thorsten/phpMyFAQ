5d6b256e126c855f00dcc6ce2882d8e2b391e0b5
Thorsten Rinne <thorsten@phpmyfaq.de>
2018-02-16 17:07:26 +0100

    Fixed non visible link to FAQ, closes #1412

--- phpmyfaq/admin/record.edit.php
+++ phpmyfaq/admin/record.edit.php
@@ -572,7 +572,7 @@ if (($user->perm->checkRight($user->getUserId(), 'editbt') ||
 
             <!-- sidebar -->
             <div class="col-lg-4">
-                <?php if ('' !== $faqData['dateEnd'] && 'copyentry' !== $action) {
+                <?php if (0 !== $faqData['id'] && 'copyentry' !== $action) {
                     $url = sprintf(
                         '%sindex.php?action=artikel&cat=%s&id=%d&artlang=%s',
                         $faqConfig->getDefaultUrl(),
