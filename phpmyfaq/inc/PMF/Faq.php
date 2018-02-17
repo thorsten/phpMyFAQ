8d950422de7e19b759ef9781ef18a7ea3a298f9b
Thorsten Rinne <thorsten@phpmyfaq.de>
2018-02-17 14:45:43 +0100

    Fixed multiple entries in FAQ entries, closes #1423

--- phpmyfaq/inc/PMF/Faq.php
+++ phpmyfaq/inc/PMF/Faq.php
@@ -1422,6 +1422,7 @@ class PMF_Faq
      */
     public function getAllRecords($sortType = FAQ_SORTING_TYPE_CATID_FAQID, Array $condition = null, $sortOrder = 'ASC')
     {
+        $solutionIds = [];
         $where = '';
         if (!is_null($condition)) {
             $num = count($condition);
@@ -1549,26 +1550,30 @@ class PMF_Faq
                 $content = $this->pmf_lang['err_expiredArticle'];
             }
 
-            $this->faqRecords[] = [
-                'id' => $row->id,
-                'category_id' => $row->category_id,
-                'lang' => $row->lang,
-                'solution_id' => $row->solution_id,
-                'revision_id' => $row->revision_id,
-                'active' => $row->active,
-                'sticky' => $row->sticky,
-                'keywords' => $row->keywords,
-                'title' => $row->thema,
-                'content' => $content,
-                'author' => $row->author,
-                'email' => $row->email,
-                'comment' => $row->comment,
-                'updated' => PMF_Date::createIsoDate($row->updated, 'Y-m-d H:i:s'),
-                'dateStart' => $row->date_start,
-                'dateEnd' => $row->date_end,
-                'created' => $row->created,
-                'notes' => $row->notes
-            ];
+            if (!in_array($row->solution_id, $solutionIds)) {
+                array_push($solutionIds, $row->solution_id);
+
+                $this->faqRecords[] = [
+                    'id' => $row->id,
+                    'category_id' => $row->category_id,
+                    'lang' => $row->lang,
+                    'solution_id' => $row->solution_id,
+                    'revision_id' => $row->revision_id,
+                    'active' => $row->active,
+                    'sticky' => $row->sticky,
+                    'keywords' => $row->keywords,
+                    'title' => $row->thema,
+                    'content' => $content,
+                    'author' => $row->author,
+                    'email' => $row->email,
+                    'comment' => $row->comment,
+                    'updated' => PMF_Date::createIsoDate($row->updated, 'Y-m-d H:i:s'),
+                    'dateStart' => $row->date_start,
+                    'dateEnd' => $row->date_end,
+                    'created' => $row->created,
+                    'notes' => $row->notes
+                ];
+            }
         }
     }
 
