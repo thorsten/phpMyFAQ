d00e256701dc9b554a69a79cf789b0d0f6ef4889
Maghin <adrien.estanove@gmail.com>
2018-05-08 13:55:02 +0200

    add some comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -83,12 +83,12 @@ fi
 #=== Set recommanded opcache settings ===
 # see https://secure.php.net/manual/en/opcache.installation.php
 {
-		echo "opcache.memory_consumption=128"
-		echo "opcache.interned_strings_buffer=8"
-		echo "opcache.max_accelerated_files=4000"
-		echo "opcache.revalidate_freq=2"
-		echo "opcache.fast_shutdown=1"
-		echo "opcache.enable_cli=1"
+  echo "opcache.memory_consumption=128"
+  echo "opcache.interned_strings_buffer=8"
+  echo "opcache.max_accelerated_files=4000"
+  echo "opcache.revalidate_freq=2"
+  echo "opcache.fast_shutdown=1"
+  echo "opcache.enable_cli=1"
 } | tee $PHP_INI_DIR/conf.d/opcache-recommended.ini
 
 docker-php-entrypoint "$@"
