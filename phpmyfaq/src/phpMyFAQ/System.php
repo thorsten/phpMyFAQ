097c050c1ae732071749abaece0e4a4cc0778a9f
Thorsten Rinne <thorsten@phpmyfaq.de>
2018-12-22 11:27:04 +0100

    3.0.0-alpha3 -> 3.0.0-alpha.4
    * updated Bootstrap
    * updated TinyMCE

--- phpmyfaq/src/phpMyFAQ/System.php
+++ phpmyfaq/src/phpMyFAQ/System.php
@@ -52,7 +52,7 @@ class System
     /**
      * Pre-release version.
      */
-    const VERSION_PRERELEASE = 'alpha.3';
+    const VERSION_PRERELEASE = 'alpha.4';
 
     /**
      * API version.
--- scripts/version.sh
+++ scripts/version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 if [ "x${PMF_VERSION}" = "x" ]; then
-    PMF_VERSION="3.0.0-alpha.3"
+    PMF_VERSION="3.0.0-alpha.4"
 fi
