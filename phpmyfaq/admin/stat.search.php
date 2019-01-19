866eaf254344a5f36973042fbd35e61f54396d51
Thorsten Rinne <thorsten@phpmyfaq.de>
2019-01-19 14:12:06 +0100

    Fixed issue with missing URL encoding
    resolves #1515

--- phpmyfaq/admin/stat.search.php
+++ phpmyfaq/admin/stat.search.php
@@ -2,8 +2,6 @@
 /**
  * Frontend for search log statistics.
  *
- *
- *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/.
@@ -32,24 +30,25 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
     exit();
 }
 ?>
-        <header class="row">
-            <div class="col-lg-12">
-                <h2 class="page-header">
-                    <i aria-hidden="true" class="fas fa-tasks"></i> <?= $PMF_LANG['ad_menu_searchstats'] ?>
-                    <div class="float-right">
-                        <a class="btn btn-danger" href="?action=truncatesearchterms">
-                            <i aria-hidden="true" class="fas fa-trash-o fa-fw"></i> <?= $PMF_LANG['ad_searchterm_del'] ?>
-                        </a>
-                    </div>
-                </h2>
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 class="h2">
+            <i aria-hidden="true" class="fas fa-tasks"></i>
+              <?= $PMF_LANG['ad_menu_searchstats'] ?>
+          </h1>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group mr-2">
+              <a class="btn btn-sm btn-outline-danger" href="?action=truncatesearchterms">
+                <i aria-hidden="true" class="fas fa-trash"></i> <?= $PMF_LANG['ad_searchterm_del'] ?>
+              </a>
             </div>
-        </header>
+          </div>
+        </div>
 
         <div class="row">
-            <div class="col-lg-12">
+          <div class="col-lg-12">
 <?php
 if ($user->perm->checkRight($user->getUserId(), 'viewlog')) {
-    $perpage = 15;
+    $perPage = 15;
     $pages = Filter::filterInput(INPUT_GET, 'pages', FILTER_VALIDATE_INT);
     $page = Filter::filterInput(INPUT_GET, 'page', FILTER_VALIDATE_INT, 1);
 
@@ -59,7 +58,7 @@ if ($user->perm->checkRight($user->getUserId(), 'viewlog')) {
         if ($search->deleteAllSearchTerms()) {
             printf('<p class="alert alert-success">%s</p>', $PMF_LANG['ad_searchterm_del_suc']);
         } else {
-            printf('<p class="alert alert-success">%s</p>', $PMF_LANG['ad_searchterm_del_err']);
+            printf('<p class="alert alert-danger">%s</p>', $PMF_LANG['ad_searchterm_del_err']);
         }
     }
 
@@ -67,11 +66,11 @@ if ($user->perm->checkRight($user->getUserId(), 'viewlog')) {
     $searchesList = $search->getMostPopularSearches($searchesCount + 1, true);
 
     if (is_null($pages)) {
-        $pages = round((count($searchesList) + ($perpage / 3)) / $perpage, 0);
+        $pages = round((count($searchesList) + ($perPage / 3)) / $perPage, 0);
     }
 
-    $start = ($page - 1) * $perpage;
-    $ende = $start + $perpage;
+    $start = ($page - 1) * $perPage;
+    $end = $start + $perPage;
 
     $baseUrl = sprintf(
         '%s?action=searchstats&amp;page=%d',
@@ -80,39 +79,37 @@ if ($user->perm->checkRight($user->getUserId(), 'viewlog')) {
     );
 
     // Pagination options
-    $options = array(
+    $options = [
         'baseUrl' => $baseUrl,
         'total' => count($searchesList),
-        'perPage' => $perpage,
+        'perPage' => $perPage,
         'pageParamName' => 'page',
-    );
+    ];
     $pagination = new Pagination($faqConfig, $options);
-    ?>
-                <div id="ajaxresponse"></div>
-                <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th><?= $PMF_LANG['ad_searchstats_search_term'] ?></th>
-                    <th><?= $PMF_LANG['ad_searchstats_search_term_count'] ?></th>
-                    <th><?= $PMF_LANG['ad_searchstats_search_term_lang'] ?></th>
-                    <th colspan="2"><?= $PMF_LANG['ad_searchstats_search_term_percentage'] ?></th>
-                    <th>&nbsp;</th>
-                </tr>
-                </thead>
-                <tfoot>
-                    <tr>
-                        <td colspan="6"><?= $pagination->render();
-    ?></td>
-                    </tr>
-                </tfoot>
-                <tbody>
-<?php 
-
+?>
+          <div id="ajaxresponse"></div>
+          <table class="table table-striped">
+            <thead>
+            <tr>
+              <th><?= $PMF_LANG['ad_searchstats_search_term'] ?></th>
+              <th><?= $PMF_LANG['ad_searchstats_search_term_count'] ?></th>
+              <th><?= $PMF_LANG['ad_searchstats_search_term_lang'] ?></th>
+              <th colspan="2"><?= $PMF_LANG['ad_searchstats_search_term_percentage'] ?></th>
+              <th>&nbsp;</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+              <td colspan="6"><?= $pagination->render() ?></td>
+            </tr>
+            </tfoot>
+            <tbody>
+<?php
     $counter = $displayedCounter = 0;
     $self = substr(__FILE__, strlen($_SERVER['DOCUMENT_ROOT']));
 
     foreach ($searchesList as $searchItem) {
-        if ($displayedCounter >= $perpage) {
+        if ($displayedCounter >= $perPage) {
             ++$displayedCounter;
             continue;
         }
@@ -125,58 +122,51 @@ if ($user->perm->checkRight($user->getUserId(), 'viewlog')) {
 
         $num = round(($searchItem['number'] * 100 / $searchesCount), 2);
         ?>
-                    <tr class="row_search_id_<?= $searchItem['id'] ?>">
-                        <td><?= Strings::htmlspecialchars($searchItem['searchterm']);
-        ?></td>
-                        <td><?= $searchItem['number'] ?></td>
-                        <td><?= $languageCodes[Strings::strtoupper($searchItem['lang'])] ?></td>
-                        <td><meter max="100" value="<?= $num;
-        ?>"></td>
-                        <td><?= $num;
-        ?>%</td>
-                        <td>
-                            <a class="btn btn-danger" href="javascript:;" title="<?= $PMF_LANG['ad_news_delete'];
-        ?>"
-                               onclick="deleteSearchTerm('<?= $searchItem['searchterm'] ?>', <?= $searchItem['id'] ?>); return false;">
-                              <i aria-hidden="true" class="fas fa-trash"></i>
-                            </a>
-                        </td>
-                    </tr>
+              <tr class="row_search_id_<?= $searchItem['id'] ?>">
+                  <td><?= Strings::htmlspecialchars($searchItem['searchterm']) ?></td>
+                  <td><?= $searchItem['number'] ?></td>
+                  <td><?= $languageCodes[Strings::strtoupper($searchItem['lang'])] ?></td>
+                  <td><meter max="100" value="<?= $num ?>"></td>
+                  <td><?= $num ?>%</td>
+                  <td>
+                      <a class="btn btn-danger" href="#" title="<?= $PMF_LANG['ad_news_delete'] ?>"
+                         onclick="deleteSearchTerm('<?= urlencode($searchItem['searchterm']) ?>', <?= $searchItem['id'] ?>); return false;">
+                        <i aria-hidden="true" class="fas fa-trash"></i>
+                      </a>
+                  </td>
+              </tr>
 <?php
-
     }
-    ?>
-                </tbody>
-                </table>
-                <script>
-                /**
-                 * Ajax call to delete search term
-                 *
-                 * @param searchterm
-                 * @param searchId
-                 */
-                function deleteSearchTerm(searchterm, searchId)
-                {
-                    if (confirm('<?= $PMF_LANG['ad_user_del_3'] ?>')) {
-                        $.getJSON("index.php?action=ajax&ajax=search&ajaxaction=delete_searchterm&searchterm=" + searchterm,
-                        function(response) {
-                            if (response == 1) {
-                                $('#ajaxresponse').
-                                    html('<?php printf('<p class="alert alert-success">%s</p>', $PMF_LANG['ad_search_delsuc']) ?>');
-                                $('.row_search_id_' + searchId).fadeOut('slow');
-                            } else {
-                                $('#ajaxresponse').
-                                    html('<?php printf('<p class="alert alert-danger">%s</p>', $PMF_LANG['ad_search_delfail']) ?>');
-                            }
-                        });
+?>
+            </tbody>
+          </table>
+          <script>
+            /**
+             * Ajax call to delete search term
+             *
+             * @param searchterm
+             * @param searchId
+             */
+            function deleteSearchTerm(searchterm, searchId) {
+              if (confirm('<?= $PMF_LANG['ad_user_del_3'] ?>')) {
+                $.getJSON("index.php?action=ajax&ajax=search&ajaxaction=delete_searchterm&searchterm=" + searchterm,
+                  (response) => {
+                    if (response === 1) {
+                      $('#ajaxresponse').html('<?php printf('<p class="alert alert-success">%s</p>',
+                          $PMF_LANG['ad_search_delsuc']) ?>');
+                      $('.row_search_id_' + searchId).fadeOut('slow');
+                    } else {
+                      $('#ajaxresponse').html('<?php printf('<p class="alert alert-danger">%s</p>',
+                          $PMF_LANG['ad_search_delfail']) ?>');
                     }
-                }
-                </script>
+                  });
+              }
+            }
+          </script>
 <?php
-
 } else {
     echo $PMF_LANG['err_NotAuth'];
 }
 ?>
-            </div>
         </div>
+      </div>
