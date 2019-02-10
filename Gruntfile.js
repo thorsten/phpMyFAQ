2a91296c3859efca3e741dc08973640d5db6c9e3
aherking <a.herking@shopware.com>
2019-02-10 15:19:01 +0000

    Fixed loading of floating label scss file with @import it in style.scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -236,7 +236,6 @@ module.exports = function (grunt) {
           'phpmyfaq/admin/assets/css/style.css': 'phpmyfaq/admin/assets/scss/style.scss',
           //'phpmyfaq/admin/assets/css/style.rtl.css': 'phpmyfaq/admin/assets/scss/style.rtl.scss',
           'phpmyfaq/assets/themes/default/css/style.css': 'phpmyfaq/assets/themes/default/scss/style.scss',
-          'phpmyfaq/assets/themes/default/css/floating-labels.css': 'phpmyfaq/assets/scss/themes/default/floating-labels.scss'
           //'phpmyfaq/assets/themes/default/css/style.rtl.css': 'phpmyfaq/assets/themes/default/scss/style.rtl.scss'
         }
       },
@@ -245,7 +244,6 @@ module.exports = function (grunt) {
           'phpmyfaq/admin/assets/css/style.css': 'phpmyfaq/admin/assets/scss/style.scss',
           //'phpmyfaq/admin/assets/css/style.rtl.css': 'phpmyfaq/admin/assets/scss/style.rtl.scss',
           'phpmyfaq/assets/themes/default/css/style.css': 'phpmyfaq/assets/themes/default/scss/style.scss',
-          'phpmyfaq/assets/themes/default/css/floating-labels.css': 'phpmyfaq/assets/themes/default/scss/floating-labels.scss'
           //'phpmyfaq/assets/themes/default/css/style.rtl.css': 'phpmyfaq/assets/themes/default/scss/style.rtl.scss'
         },
         compress: true
