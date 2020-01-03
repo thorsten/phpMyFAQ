775120e86c573a14d64109c44582a49cd8b6d196
Thorsten Rinne <thorsten@phpmyfaq.de>
2020-01-03 19:46:59 +0100

    Removed debug output

--- phpmyfaq/src/Bootstrap.php
+++ phpmyfaq/src/Bootstrap.php
@@ -27,7 +27,7 @@ use phpMyFAQ\Init;
 // - false      debug mode disabled
 // - true       debug mode enabled
 //
-define('DEBUG', true);
+define('DEBUG', false);
 if (DEBUG) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
