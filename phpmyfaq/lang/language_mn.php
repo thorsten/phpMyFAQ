227db73c2117a78ea781f59964ab46f925448fa0
Thorsten Rinne <thorsten@phpmyfaq.de>
2020-01-06 17:03:48 +0100

    Fixed wrong behaviour of icon changes when open a category

--- phpmyfaq/lang/language_mn.php
+++ phpmyfaq/lang/language_mn.php
@@ -8,7 +8,7 @@
  *
  * @category  phpMyFAQ
  * @author    khaidaw@gmail.com
- * @copyright 2019 phpMyFAQ Team
+ * @copyright 2019-2020 phpMyFAQ Team
  * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
  * @link      http://www.phpmyfaq.de
  * @since     2019-03-24
