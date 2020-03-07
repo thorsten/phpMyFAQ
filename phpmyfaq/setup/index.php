bf9e9ae9e3290c5404ba2ac332874d3cc332d7ce
Thorsten Rinne <thorsten@phpmyfaq.de>
2020-03-07 19:01:53 +0100

    Added new wizard stepper for setup

--- phpmyfaq/setup/index.php
+++ phpmyfaq/setup/index.php
@@ -2,7 +2,6 @@
 
 /**
  * The main phpMyFAQ Setup.
- *
  * This script checks the complete environment, writes the database connection
  * parameters into the file config/database.php and the configuration into the database.
  * This Source Code Form is subject to the terms of the Mozilla Public License,
@@ -24,7 +23,7 @@
 
 use Composer\Autoload\ClassLoader;use phpMyFAQ\Installer;use phpMyFAQ\Strings;use phpMyFAQ\System;
 
-define('COPYRIGHT', '&copy; 2001-2020 <a href="https://www.phpmyfaq.de/">phpMyFAQ Team</a> | Follow us on <a href="http://twitter.com/phpMyFAQ">Twitter</a> ');
+define('COPYRIGHT', '&copy; 2001-2020 <a href="https://www.phpmyfaq.de/">phpMyFAQ Team</a>');
 define('PMF_ROOT_DIR', dirname(dirname(__FILE__)));
 define('PMF_SRC_DIR', PMF_ROOT_DIR . '/src');
 define('IS_VALID_PHPMYFAQ', null);
@@ -80,10 +79,10 @@ $loader->register();
               <a class="nav-link" target="_blank" href="https://www.phpmyfaq.de/documentation">Documentation</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" target="_blank" href="https://www.phpmyfaq.de/support">Support</a>
+              <a class="nav-link" target="_blank" href="https://www.phpmyfaq.de/support">Support</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" target="_blank" href="http://forum.phpmyfaq.de/">Forums</a>
+              <a class="nav-link" target="_blank" href="http://forum.phpmyfaq.de/">Forums</a>
             </li>
           </ul>
         </div>
@@ -106,29 +105,58 @@ $loader->register();
       </div>
     </div>
 
-    <div class="container">
-      <div class="row">
+    <div class="container mb-3">
+
+      <div class="pmf-setup-stepwizard">
+        <div class="pmf-setup-stepwizard-row setup-panel">
+          <div class="pmf-setup-stepwizard-step">
+            <a href="#step-1" type="button" class="btn btn-primary pmf-setup-stepwizard-btn-circle">1</a>
+            <p>Database setup</p>
+          </div>
+          <div class="pmf-setup-stepwizard-step">
+            <a href="#step-2" type="button" class="btn btn-secondary pmf-setup-stepwizard-btn-circle"
+            disabled="disabled">
+            2
+            </a>
+            <p>LDAP setup</p>
+          </div>
+          <div class="pmf-setup-stepwizard-step">
+            <a href="#step-3" type="button" class="btn btn-secondary pmf-setup-stepwizard-btn-circle"
+            disabled="disabled">
+            3
+            </a>
+            <p>Elasticsearch setup</p>
+          </div>
+          <div class="pmf-setup-stepwizard-step">
+            <a href="#step-4" type="button" class="btn btn-secondary pmf-setup-stepwizard-btn-circle"
+            disabled="disabled">
+            4
+            </a>
+            <p>Admin user setup</p>
+          </div>
+        </div>
+      </div>
+
 <?php
 //
 // Initialize static string wrapper
 //
 Strings::init('en');
-$installer = new Installer();
 $system = new System();
+$installer = new Installer();
 $installer->checkBasicStuff();
 $installer->checkFilesystemPermissions();
 // not yet POSTed
 if (!isset($_POST['sql_server']) && !isset($_POST['sql_user']) && !isset($_POST['sql_db'])) {
-    $installer->checkNoncriticalSettings()
-    ?>
-        </div>
+  $installer->checkNoncriticalSettings()
+?>
 
-        <form  action="index.php" method="post" id="phpmyfaq-setup-form">
-        <div class="row">
-            <div class="col-sm">
-                <fieldset>
-                <legend>Database setup</legend>
-                    <div class="form-group row">
+        <form action="index.php" method="post" id="phpmyfaq-setup-form" class="needs-validation" novalidate>
+
+          <div class="row setup-content" id="step-1">
+            <div class="col-12">
+                <h3 class="mb-3"> Step 1: Database setup</h3>
+                <div class="form-group row">
                         <label class="col-sm-3 col-form-label" for="sql_type">Server:</label>
                         <div class="col-sm-9">
                             <select name="sql_type" id="sql_type" class="form-control">
@@ -143,7 +171,7 @@ if (!isset($_POST['sql_server']) && !isset($_POST['sql_user']) && !isset($_POST[
                             <label class="col-sm-3 col-form-label" for="sql_server">Host/Socket:</label>
                             <div class="col-sm-9">
                                 <input type="text" name="sql_server" id="sql_server" class="form-control"
-                                    placeholder="e.g. 127.0.0.1">
+                                    placeholder="e.g. 127.0.0.1" required>
                                 <small class="form-text text-muted">
                                   Please enter the host or path to the socket of your database server.
                                 </small>
@@ -153,28 +181,28 @@ if (!isset($_POST['sql_server']) && !isset($_POST['sql_user']) && !isset($_POST[
                             <label class="col-sm-3 col-form-label" for="sql_port">Port:</label>
                             <div class="col-sm-9">
                                 <input type="number" name="sql_port" id="sql_port" class="form-control"
-                                    value="3306">
+                                    value="3306" required>
                                 <small class="form-text text-muted">Please enter the port your database server.</small>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label" for="sql_user">User:</label>
                             <div class="col-sm-9">
-                                <input type="text" name="sql_user" id="sql_user" class="form-control">
+                                <input type="text" name="sql_user" id="sql_user" class="form-control" required>
                                 <small class="form-text text-muted">Please enter your database user.</small>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label" for="sql_password">Password:</label>
                             <div class="col-sm-9">
-                                <input name="sql_password" type="password" id="sql_password" class="form-control">
+                                <input name="sql_password" type="password" id="sql_password" class="form-control" required>
                                 <small class="form-text text-muted">Please enter your database password.</small>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-3 col-form-label" for="sql_db">Database:</label>
                             <div class="col-sm-9">
-                                <input type="text" name="sql_db" id="sql_db" class="form-control">
+                                <input type="text" name="sql_db" id="sql_db" class="form-control" required>
                                 <small class="form-text text-muted">Please enter your existing database name.</small>
                             </div>
                         </div>
@@ -185,7 +213,7 @@ if (!isset($_POST['sql_server']) && !isset($_POST['sql_user']) && !isset($_POST[
                             <label class="col-sm-3 col-form-label" for="sql_sqlitefile">SQLite database file:</label>
                             <div class="col-sm-9">
                                 <input type="text" name="sql_sqlitefile" id="sql_sqlitefile" class="form-control"
-                                       value="<?= dirname(__DIR__) ?>">
+                                       value="<?= dirname(__DIR__) ?>" required>
                                 <small class="form-text text-muted">
                                     Please enter the full path to your SQLite datafile which should be outside your
                                     docroot.
@@ -204,13 +232,15 @@ if (!isset($_POST['sql_server']) && !isset($_POST['sql_user']) && !isset($_POST[
                             </small>
                         </div>
                     </div>
-                </fieldset>
-            </div>
+                <button class="btn btn-primary btn-next btn-lg pull-right" type="button">Next: LDAP setup</button>
+              </div>
+          </div>
 
-            <?php if (extension_loaded('ldap')) : ?>
-            <div class="col-sm">
-                <fieldset>
-                <legend>LDAP setup</legend>
+
+          <div class="row setup-content" id="step-2">
+            <div class="col-12">
+                <h3 class="mb-3"> Step 2: LDAP setup</h3>
+                <?php if (extension_loaded('ldap')) : ?>
                     <div class="form-group">
                         <div class="form-check">
                           <input id="ldap_enabled" class="form-check-input" type="checkbox" name="ldap_enabled"
@@ -261,18 +291,66 @@ if (!isset($_POST['sql_server']) && !isset($_POST['sql_user']) && !isset($_POST[
                     <p class="alert alert-info">
                         You can add additional LDAP configuration in the admin configuration panel.
                     </p>
-
-                </fieldset>
-            </div>
-        </div>
-
-        <div class="row">
             <?php endif; ?>
 
-            <div class="col-sm">
-                <fieldset>
-                <legend>phpMyFAQ setup</legend>
+                <button class="btn btn-primary btn-next btn-lg pull-right" type="button">Next: Elasticsearch setup</button>
+              </div>
+        </div>
+
+
+
+          <div class="row setup-content" id="step-3">
+            <div class="col-12">
+                <h3 class="mb-3"> Step 2: Elasticsearch setup</h3>
+                <?php if (extension_loaded('curl')) : ?>
+                  <div class="form-group">
+                        <div class="form-check">
+                          <input id="elasticsearch_enabled" class="form-check-input" type="checkbox"
+                                 name="elasticsearch_enabled" value="yes">
+                          <label class="form-check-label" for="elasticsearch_enabled">
+                            Enable Elasticsearch support?
+                          </label>
+                        </div>
+                    </div>
                     <div class="form-group row">
+                        <label class="col-sm-3 col-form-label" for="elasticsearch_server">Server(s):</label>
+                        <div class="col-sm-9">
+                            <div class="input-group" id="elasticsearch_server-wrapper">
+                                <input type="text" name="elasticsearch_server[]" id="elasticsearch_server"
+                                       class="form-control" placeholder="127.0.0.1:9200">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                      <a title="Add more Elasticsearch hosts" style="cursor: pointer;" data-action="add"
+                                         class="pmf-add-elasticsearch-host">
+                                        <i aria-hidden="true" class="fa fa-plus-circle"></i>
+                                    </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <small class="form-text text-muted">
+                                Please enter the host (domain or IP) with port number of your Elasticsearch server.
+                            </small>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-3 col-form-label"for="elasticsearch_index">Index name:</label>
+                        <div class="col-sm-9">
+                            <input type="text" name="elasticsearch_index" id="elasticsearch_index" class="form-control">
+                            <small class="form-text text-muted">Please enter your Elasticsearch index name.</small>
+                        </div>
+                    </div>
+                <?php endif; ?>
+                <button class="btn btn-primary btn-next btn-lg pull-right" type="button">Next: Admin user setup</button>
+              </div>
+        </div>
+
+
+
+          <div class="row setup-content" id="step-4">
+            <div class="col-12">
+                <h3 class="mb-3"> Step 2: Admin user setup</h3>
+
+                      <div class="form-group row">
                         <label class="col-sm-3 col-form-label" for="language">Default language:</label>
                         <div class="col-sm-9">
                             <select name="language" id="language" class="form-control">
@@ -316,98 +394,41 @@ if (!isset($_POST['sql_server']) && !isset($_POST['sql_user']) && !isset($_POST[
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-sm-3 col-form-label"for="realname">Your name:</label>
+                        <label class="col-sm-3 col-form-label" for="realname">Your name:</label>
                         <div class="col-sm-9">
                             <input type="text" name="realname" id="realname" class="form-control" required>
                             <small class="form-text text-muted">Please enter your real name.</small>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-sm-3 col-form-label"for="email">Your email address:</label>
+                        <label class="col-sm-3 col-form-label" for="email">Your email address:</label>
                         <div class="col-sm-9">
                             <input type="email" name="email" id="email" class="form-control" required>
                             <small class="form-text text-muted">Please enter your email adress.</small>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-sm-3 col-form-label"for="loginname">Your login name:</label>
+                        <label class="col-sm-3 col-form-label" for="loginname">Your login name:</label>
                         <div class="col-sm-9">
                             <input type="text" name="loginname" id="loginname" class="form-control" required>
                             <small class="form-text text-muted">Please enter your login name.</small>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-sm-3 col-form-label"for="password">Your password:</label>
+                        <label class="col-sm-3 col-form-label" for="password">Your password:</label>
                         <div class="col-sm-9">
                             <input type="password" name="password" id="password" class="form-control" required>
                             <small class="form-text text-muted">Please enter your password.</small>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-sm-3 col-form-label"for="password_retype">Retype password:</label>
+                        <label class="col-sm-3 col-form-label" for="password_retype">Retype password:</label>
                         <div class="col-sm-9">
                             <input type="password" name="password_retyped" id="password_retype" class="form-control"
                                    required>
                             <small class="form-text text-muted">Please retype your password.</small>
                         </div>
                     </div>
-                </fieldset>
-            </div>
-
-            <?php if (extension_loaded('curl')) : ?>
-            <div class="col-sm">
-                <fieldset>
-                    <legend>Elasticsearch setup</legend>
-                    <div class="form-group">
-                        <div class="form-check">
-                          <input id="elasticsearch_enabled" class="form-check-input" type="checkbox" name="elasticsearch_enabled" value="yes">
-                          <label class="form-check-label" for="elasticsearch_enabled">
-                            Enable Elasticsearch support?
-                          </label>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-3 col-form-label" for="elasticsearch_server">Server(s):</label>
-                        <div class="col-sm-9">
-                            <div class="input-group" id="elasticsearch_server-wrapper">
-                                <input type="text" name="elasticsearch_server[]" id="elasticsearch_server"
-                                       class="form-control" placeholder="127.0.0.1:9200">
-                                <div class="input-group-append">
-                                    <div class="input-group-text">
-                                      <a title="Add more Elasticsearch hosts" style="cursor: pointer;" data-action="add"
-                                         class="pmf-add-elasticsearch-host">
-                                        <i aria-hidden="true" class="fa fa-plus-circle"></i>
-                                    </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <small class="form-text text-muted">
-                                Please enter the host (domain or IP) with port number of your Elasticsearch server.
-                            </small>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-3 col-form-label"for="elasticsearch_index">Index name:</label>
-                        <div class="col-sm-9">
-                            <input type="text" name="elasticsearch_index" id="elasticsearch_index" class="form-control">
-                            <small class="form-text text-muted">Please enter your Elasticsearch index name.</small>
-                        </div>
-                    </div>
-                </fieldset>
-            </div>
-            <?php endif; ?>
-        </div>
-
-        <div class="row">
-          <div class="col-sm">
-              <div class="form-group row justify-content-center">
-                  <button class="btn btn-primary btn-lg" type="submit">
-                         Click to install phpMyFAQ <?= System::getVersion() ?>
-                  </button>
-              </div>
-            </div>
-        </div>
-
         <div class="row">
           <div class="col-sm">
             <p class="alert alert-info text-center">
@@ -417,6 +438,11 @@ if (!isset($_POST['sql_server']) && !isset($_POST['sql_user']) && !isset($_POST[
             </p>
             </div>
         </div>
+
+                <button class="btn btn-primary btn-lg pull-right" type="submit" >Click to install phpMyFAQ <?= System::getVersion() ?></button>
+              </div>
+        </div>
+
         </form>
     <?php
     System::renderFooter();
