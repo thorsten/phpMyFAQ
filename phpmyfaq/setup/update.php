d9042d5e6dca7d2e57122c42603bad7da5056ba2
Thorsten Rinne <thorsten@phpmyfaq.de>
2020-03-27 16:12:16 +0100

    feat(user): added new flag if user data should be visible (#1696)

--- phpmyfaq/setup/update.php
+++ phpmyfaq/setup/update.php
@@ -698,6 +698,15 @@ if ($step == 3) {
         }
     }
 
+    if (version_compare($version, '3.1.0-alpha', '<=')) {
+        // Add isVisible flag for user data
+        if ('sqlite3' === $DB['type']) {
+            $query[] = 'ALTER TABLE ' . $prefix . 'faquserdata ADD COLUMN is_visible INT(1) DEFAULT 0';
+        } else {
+            $query[] = 'ALTER TABLE '.$prefix.'faquserdata ADD is_visible INTEGER DEFAULT 0';
+        }
+    }
+
     // Always the last step: Update version number
     if (version_compare($version, System::getVersion(), '<')) {
         $faqConfig->update(['main.currentApiVersion' => System::getApiVersion()]);
