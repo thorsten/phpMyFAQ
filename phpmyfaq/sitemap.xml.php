07b00b9c9a962a7bcd998c579f9ebc6bd3d44e10
Gerardo <gejobj@gmail.com>
2020-11-10 09:21:06 +0100

    Update sitemap.xml.php
    Fixed Google Search Console date format error. lastmod date changed from +0100 to +01:00

--- phpmyfaq/sitemap.xml.php
+++ phpmyfaq/sitemap.xml.php
@@ -64,6 +64,13 @@ function buildSiteMapNode($location, $lastModified = null)
     if (empty($lastModified)) {
         $lastModified = Date::createIsoDate($_SERVER['REQUEST_TIME'], 'Y-m-d', false);
     }
+    if (preg_match('/^[1|2][0-9]{3}-[0|1][0-9]-[0|1|2|3][0-9]$/', $lastModified)) {
+        $lastModified .= 'T'.date('H:i:sP');
+    } else if ( preg_match('/([\+|\-][0-9]{2})([0-9]{2})$/', $lastModified, $arrayFind) ) {
+        if (isset($arrayFind[1]) && isset($arrayFind[2])) {
+            $lastModified = str_replace($arrayFind[0], $arrayFind[1].':'.$arrayFind[2], $lastModified);
+        }
+    }
     $node =
         '<url>'
         . '<loc>' . Strings::htmlspecialchars($location) . '</loc>'
