9487a5d9ef401afd22015c4902d69860b750c4ed
Thorsten Rinne <thorsten@phpmyfaq.de>
2020-12-23 19:00:54 +0100

    chore: improve build script

--- scripts/git2package.sh
+++ scripts/git2package.sh
@@ -16,16 +16,16 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 #
-# @package phpMyFAQ
-# @author Matteo Scaramuccia <matteo@scaramuccia.com>
-# @author Thorsten Rinne <thorsten@phpmyfaq.de>
-# @author Rene Treffer <treffer+phpmyfaq@measite.de>
-# @author David Soria Parra <dsp@php.net>
-# @author Florian Anderiasch <florian@phpmyfaq.de>
+# @package   phpMyFAQ
+# @author    Matteo Scaramuccia <matteo@scaramuccia.com>
+# @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+# @author    Rene Treffer <treffer+phpmyfaq@measite.de>
+# @author    David Soria Parra <dsp@php.net>
+# @author    Florian Anderiasch <florian@phpmyfaq.de>
 # @copyright 2008-2020 phpMyFAQ Team
-# @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
-# @link https://www.phpmyfaq.de
-# @version 2008-09-10
+# @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+# @link      https://www.phpmyfaq.de
+# @version   2008-09-10
 
 # phpMyFAQ Version
 . scripts/version.sh
@@ -43,42 +43,42 @@ if [ "x${PMF_PACKAGE_FOLDER}" = "x" ]; then
     PMF_PACKAGE_FOLDER="phpmyfaq-${PMF_VERSION}"
 fi
 
-cwd=`pwd`
+cwd=$(pwd) || exit
 
-git checkout-index -f -a --prefix=$cwd/build/checkout/${PMF_PACKAGE_FOLDER}/
+printf "\n 🚀 Checkout code into build/ folder\n";
+git checkout-index -f -a --prefix="$cwd"/build/checkout/"${PMF_PACKAGE_FOLDER}"/
 
-# Add missing directories
-mkdir -p $cwd/build/package/${PMF_PACKAGE_FOLDER}/
+printf "\n 🚀 Add missing directories\n";
+mkdir -p "$cwd"/build/package/"${PMF_PACKAGE_FOLDER}"/
 
-cd $cwd/build/checkout/${PMF_PACKAGE_FOLDER}/
+cd "$cwd"/build/checkout/"${PMF_PACKAGE_FOLDER}"/ || exit
 
-# add dependencies
+printf "\n 🚀 Add dependencies\n";
 composer install --no-dev --prefer-dist
 yarn install
 yarn build
 
-# Remove fonts from TCPDF
-rm -rf $cwd/build/checkout/${PMF_PACKAGE_FOLDER}/phpmyfaq/src/libs/tecnickcom/tcpdf/fonts
+printf "\n 🚀 Remove fonts from TCPDF\n"
+rm -rf "$cwd"/build/checkout/"${PMF_PACKAGE_FOLDER}"/phpmyfaq/src/libs/tecnickcom/tcpdf/fonts
 
-# create md5 hashes for file verification
-php scripts/createHashes.php > $cwd/hashes-${PMF_VERSION}.json
+printf "\n 🚀 Create md5 hashes for file verification\n"
+php scripts/createHashes.php > "$cwd"/hashes-"${PMF_VERSION}".json
 
-# prepare packaging
-cd $cwd
-mv $cwd/build/checkout/${PMF_PACKAGE_FOLDER}/phpmyfaq $cwd/build/package/${PMF_PACKAGE_FOLDER}
+printf "\n 🚀 Prepare packaging\n"
+cd "$cwd" || exit
+mv "$cwd"/build/checkout/"${PMF_PACKAGE_FOLDER}"/phpmyfaq "$cwd"/build/package/"${PMF_PACKAGE_FOLDER}"
 
-# build packages
-tar cfvz ${PMF_PACKAGE_FOLDER}.tar.gz -C $cwd/build/package/${PMF_PACKAGE_FOLDER} phpmyfaq
-cd $cwd/build/package/${PMF_PACKAGE_FOLDER}
-zip -r $cwd/${PMF_PACKAGE_FOLDER}.zip phpmyfaq
-cd $cwd
+printf "\n 🚀 Build packages\n"
+tar cfvz "${PMF_PACKAGE_FOLDER}".tar.gz -C "$cwd"/build/package/"${PMF_PACKAGE_FOLDER}" phpmyfaq
+cd "$cwd"/build/package/"${PMF_PACKAGE_FOLDER}" || exit
+zip -r "$cwd"/"${PMF_PACKAGE_FOLDER}".zip phpmyfaq
+cd "$cwd" || exit
 
-# md5sum
+printf "\n 🚀 Create md5sum\n"
 $MD5BIN "${PMF_PACKAGE_FOLDER}.tar.gz" > "${PMF_PACKAGE_FOLDER}.tar.gz.md5"
 $MD5BIN "${PMF_PACKAGE_FOLDER}.zip" > "${PMF_PACKAGE_FOLDER}.zip.md5"
 
-# clean up
-rm -rf $cwd/build/checkout/${PMF_PACKAGE_FOLDER}
-rm -rf $cwd/build/package/${PMF_PACKAGE_FOLDER}
+printf "\n 🚀 Clean up\n"
+rm -rf "$cwd"/build
 
-echo "done.\n";
+printf "\n 🚀 done.\n";
