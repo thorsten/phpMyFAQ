427874cbf70efb93db0df903c15f7ea5df76a276
Everton Leite <eleite@cecierj.edu.br>
2021-08-11 09:50:41 -0300

    Adapting the landEditor to the proper standards
    - Removing jQuery dependancy. It was very short used, so the js code was converted to native js.
    - Using short array initializers.
    - Adapting variables and functions names to camelCase.
    - Using MPL 2.0 to document the file (hope I did it right).

--- scripts/langEditor.php
+++ scripts/langEditor.php
@@ -1,17 +1,23 @@
 <?php
 	
-/**
+ /**
  * This file is intended to make it easier to translate the language files. 
  * It makes visible the strings already translated, the ones that exist on 
  * a file but not on the original, and the ones that are not translated yet.
  *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ *
  * @package   phpMyFAQ
- * @author    Everton Leite <etcholeite@gmail.com>
+ * @author    Everton Leite <etcholeite@gmail>
+ * @copyright 2012-2021 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2021-08-11
  */
 	
 ?>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 <style>
 	body {
@@ -59,47 +65,47 @@
 
 <?php
 
-$PMF_LANG_source = array();
-$LANG_CONF_source = array();
-$PMF_LANG_target = array();
-$LANG_CONF_target = array();
-$index_source = array();
-$index_target = array();
+$pmfLangSource = [];
+$langConfSource = [];
+$pmfLangTarget = [];
+$langConfTarget= [];
+$indexSource= [];
+$indexTarget = [];
 
-function render_target_table_line($index, &$PMF_LANG_source, &$PMF_LANG_target, &$LANG_CONF_source, &$LANG_CONF_target) {
-	eval('@$source_entry = ' . str_replace('$PMF_LANG', '$PMF_LANG_source', str_replace('$LANG_CONF', '$LANG_CONF_source', $index)) . ';');
-	eval('@$target_entry = ' . str_replace('$PMF_LANG', '$PMF_LANG_target', str_replace('$LANG_CONF', '$LANG_CONF_target', $index)) . ';');
-	$key_founded = $source_entry !== null;
-	$class = $key_founded ? "green" : "yellow";
+function renderTargetTableLine($index, &$pmfLangSource, &$pmfLangTarget, &$langConfSource, &$langConfTarget) {
+	eval('@$sourceEntry = ' . str_replace('$PMF_LANG', '$pmfLangSource', str_replace('$LANG_CONF', '$langConfSource', $index)) . ';');
+	eval('@$targetEntry = ' . str_replace('$PMF_LANG', '$pmfLangTarget', str_replace('$LANG_CONF', '$langConfTarget', $index)) . ';');
+	$keyFounded = $sourceEntry !== null;
+	$class = $keyFounded ? "green" : "yellow";
 	echo '
 		<tr class="' . $class . '">
 			<td class="nowrap key">' . $index . '</td>
 			<td style="width: 50%;" class="source">';
-	if ($key_founded) {
-		echo htmlspecialchars($source_entry);
+	if ($keyFounded) {
+		echo htmlspecialchars($sourceEntry);
 	} else {
 		echo '<em><strong>String not found in source file</strong></em>';
 	}
 	echo '
 			</td>
 			<td style="width: 50%">
-				<textarea style="width: 100%;" class="target ' . $class . '">' . htmlspecialchars($target_entry) . '</textarea>
+				<textarea style="width: 100%;" class="target ' . $class . '">' . htmlspecialchars($targetEntry) . '</textarea>
 			</td>
 		</tr>';
 }
 
-function render_source_table_line($index, &$PMF_LANG_source, &$PMF_LANG_target, &$LANG_CONF_source, &$LANG_CONF_target) {
-	eval('@$source_entry = ' . str_replace('$PMF_LANG', '$PMF_LANG_source', str_replace('$LANG_CONF', '$LANG_CONF_source', $index)) . ';');
-	eval('@$target_entry = ' . str_replace('$PMF_LANG', '$PMF_LANG_target', str_replace('$LANG_CONF', '$LANG_CONF_target', $index)) . ';');
-	$key_founded = $target_entry !== null;
-	if ($key_founded){
+function renderSourceTableLine($index, &$pmfLangSource, &$pmfLangTarget, &$langConfSource, &$langConfTarget) {
+	eval('@$sourceEntry = ' . str_replace('$PMF_LANG', '$pmfLangSource', str_replace('$LANG_CONF', '$langConfSource', $index)) . ';');
+	eval('@$targetEntry = ' . str_replace('$PMF_LANG', '$pmfLangTarget', str_replace('$LANG_CONF', '$langConfTarget', $index)) . ';');
+	$keyFounded = $targetEntry !== null;
+	if ($keyFounded){
 		return;
 	}
 	$class = "red";
 	echo '
 		<tr class="' . $class . '">
 			<td class="nowrap key">' . $index . '</td>
-			<td style="width: 50%;" class="source">' . htmlspecialchars($source_entry) . '</td>
+			<td style="width: 50%;" class="source">' . htmlspecialchars($sourceEntry) . '</td>
 			<td style="width: 50%">
 				<textarea style="width: 100%;" class="target ' . $class . '"></textarea>
 			</td>
@@ -113,33 +119,33 @@ if ($_POST) {
 	if ($source === null || $target === null || $source["name"] === "" || $target["name"] === "") {
 		echo 'You must select both files first';
 	} else {
-		$file_source = fopen($source["tmp_name"], "r");
-		$file_target = fopen($target["tmp_name"], "r");
-		while (($line_source = fgets($file_source)) !== false) {
-			if (strpos($line_source, '$PMF_LANG') === 0) {
-				eval(str_replace('$PMF_LANG', '$PMF_LANG_source', $line_source));
-				$index_source[] = trim(substr($line_source, 0, strpos($line_source, '=')));
+		$fileSource = fopen($source["tmp_name"], "r");
+		$fileTarget = fopen($target["tmp_name"], "r");
+		while (($lineSource = fgets($fileSource)) !== false) {
+			if (strpos($lineSource, '$PMF_LANG') === 0) {
+				eval(str_replace('$PMF_LANG', '$pmfLangSource', $lineSource));
+				$indexSource[] = trim(substr($lineSource, 0, strpos($lineSource, '=')));
 			}
-			if (strpos($line_source, '$LANG_CONF') === 0) {
-				eval(str_replace('$LANG_CONF', '$LANG_CONF_source', $line_source));
-				$index_source[] = trim(substr($line_source, 0, strpos($line_source, '=')));
-				$line_source = str_replace('$LANG_CONF', '$LANG_CONF_source', $line_source);
-				$idx = trim(substr($line_source, 0, strpos($line_source, '=')));
-				$val = substr(str_replace("'", "\'", trim(substr($line_source, strpos($line_source, '=') + 1, strlen($line_source)))), 0, -1);
+			if (strpos($lineSource, '$LANG_CONF') === 0) {
+				eval(str_replace('$LANG_CONF', '$langConfSource', $lineSource));
+				$indexSource[] = trim(substr($lineSource, 0, strpos($lineSource, '=')));
+				$lineSource = str_replace('$LANG_CONF', '$langConfSource', $lineSource);
+				$idx = trim(substr($lineSource, 0, strpos($lineSource, '=')));
+				$val = substr(str_replace("'", "\'", trim(substr($lineSource, strpos($lineSource, '=') + 1, strlen($lineSource)))), 0, -1);
 				eval($idx . " = '$val';");
 			}
 		}
-		while (($line_target = fgets($file_target)) !== false) {
-			if (strpos($line_target, '$PMF_LANG') === 0) {
-				eval(str_replace('$PMF_LANG', '$PMF_LANG_target', $line_target));
-				$index_target[] = trim(substr($line_target, 0, strpos($line_target, '=')));
+		while (($lineTarget = fgets($fileTarget)) !== false) {
+			if (strpos($lineTarget, '$PMF_LANG') === 0) {
+				eval(str_replace('$PMF_LANG', '$pmfLangTarget', $lineTarget));
+				$indexTarget[] = trim(substr($lineTarget, 0, strpos($lineTarget, '=')));
 			}
-			if (strpos($line_target, '$LANG_CONF') === 0) {
-				eval(str_replace('$LANG_CONF', '$LANG_CONF_target', $line_target));
-				$index_target[] = trim(substr($line_target, 0, strpos($line_target, '=')));
-				$line_target = str_replace('$LANG_CONF', '$LANG_CONF_target', $line_target);
-				$idx = trim(substr($line_target, 0, strpos($line_target, '=')));
-				$val = substr(str_replace("'", "\'", trim(substr($line_target, strpos($line_target, '=') + 1, strlen($line_target)))), 0, -1);
+			if (strpos($lineTarget, '$LANG_CONF') === 0) {
+				eval(str_replace('$LANG_CONF', '$langConfTarget', $lineTarget));
+				$indexTarget[] = trim(substr($lineTarget, 0, strpos($lineTarget, '=')));
+				$lineTarget = str_replace('$LANG_CONF', '$langConfTarget', $lineTarget);
+				$idx = trim(substr($lineTarget, 0, strpos($lineTarget, '=')));
+				$val = substr(str_replace("'", "\'", trim(substr($lineTarget, strpos($lineTarget, '=') + 1, strlen($lineTarget)))), 0, -1);
 				eval($idx . " = '$val';");
 			}
 		}
@@ -158,8 +164,8 @@ if ($_POST) {
 			</thead>
 			<tbody>
 				<?php 
-					foreach ($index_target as $index) { 
-						render_target_table_line($index, $PMF_LANG_source, $PMF_LANG_target, $LANG_CONF_source, $LANG_CONF_target);
+					foreach ($indexTarget as $index) { 
+						renderTargetTableLine($index, $pmfLangSource, $pmfLangTarget, $langConfSource, $langConfTarget);
 					}
 					echo '
 						<tr>
@@ -169,8 +175,8 @@ if ($_POST) {
 							<th colspan="3">Strings not found on target file</td>
 						</tr>
 					';
-					foreach ($index_source as $index) {
-						render_source_table_line($index, $PMF_LANG_source, $PMF_LANG_target, $LANG_CONF_source, $LANG_CONF_target);
+					foreach ($indexSource as $index) {
+						renderSourceTableLine($index, $pmfLangSource, $pmfLangTarget, $langConfSource, $langConfTarget);
 					}
 				?>
 			</tbody>
@@ -181,23 +187,31 @@ if ($_POST) {
 		<textarea id="output" placeholder="The content of your translated file will appear here"></textarea>
 		
 		<script>
-			$(document).ready(function() {
-				$("#generate").on("click", function() {
-					var textarea = $("#output");
+			var ready = (callback) => {
+				if (document.readyState != "loading") {
+					callback();
+				} else {
+					document.addEventListener("DOMContentLoaded", callback);
+				}
+			}
+
+			ready(() => { 
+				document.querySelector("#generate").addEventListener("click", (e) => { 
+					var textarea = document.querySelector("#output");
 					var output = '';
-					$(".target").each(function(a, target) {
-						var value = $.trim($(target).val().replaceAll("\n", ""));
+					document.querySelectorAll(".target").forEach(target => {
+						var value = target.value.replaceAll("\n", "").trim();
 						if (value.length === 0) {
 							return;
 						}
-						var key = $(target).closest("tr").find(".key").html();
+						var key = target.parentElement.parentElement.querySelector(".key").innerHTML;
 						var type = key.indexOf("$PMF_LANG") === 0 ? "PMF_LANG" : "LANG_CONF";
 						if (type === "PMF_LANG") {
-							value = "'" + $.trim(value.replaceAll("'", "\\'")) + "'";
+							value = "'" + value.replaceAll("'", "\\'").trim() + "'";
 						}
 						output += key + " = " + value + ";\n";
-					});
-					textarea.val(output);
+					})
+					textarea.value = output;
 				});
 			});
 		</script>
@@ -219,4 +233,5 @@ if ($_POST) {
 	<input type="submit" value="compare" />
 </form>
 
-<?php }
+<?php 
+}
