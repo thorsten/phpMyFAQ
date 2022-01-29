23f1d6cc570840cd4b3c9809d414e1e969a1968b
Thorsten Rinne <thorsten@phpmyfaq.de>
2022-01-29 11:38:49 +0100

    chore: disabled debug mode

--- phpmyfaq/src/Bootstrap.php
+++ phpmyfaq/src/Bootstrap.php
@@ -7,12 +7,12 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/.
  *
- * @package phpMyFAQ
- * @author Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
  * @copyright 2012-2022 phpMyFAQ Team
- * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
- * @link https://www.phpmyfaq.de
- * @since 2012-03-07
+ * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2012-03-07
  */
 
 use Elasticsearch\ClientBuilder;
@@ -25,7 +25,7 @@ use phpMyFAQ\Init;
 // Debug mode:
 // - false debug mode disabled
 // - true  debug mode enabled
-define('DEBUG', true);
+const DEBUG = true;
 if (DEBUG) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
