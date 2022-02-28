e278260bc63328aa0c0799d91a94d0fd64434226
Thorsten Rinne <thorsten@phpmyfaq.de>
2022-02-28 20:17:05 +0100

    fix: added missing check if element is valid

--- phpmyfaq/admin/assets/src/instance.js
+++ phpmyfaq/admin/assets/src/instance.js
@@ -5,92 +5,94 @@ export const handleInstances = () => {
   const addInstance = document.querySelector('.pmf-instance-add');
   const deleteInstance = document.querySelectorAll('.pmf-instance-delete');
   const container = document.getElementById('pmf-modal-add-instance');
-  const modal = new Modal(container);
 
-  addInstance.addEventListener('click', (event) => {
-    event.preventDefault();
-    const csrf = document.querySelector('#csrf').value;
-    const url = document.querySelector('#url').value;
-    const instance = document.querySelector('#instance').value;
-    const comment = document.querySelector('#comment').value;
-    const email = document.querySelector('#email').value;
-    const admin = document.querySelector('#admin').value;
-    const password = document.querySelector('#password').value;
+  if (addInstance) {
+    const modal = new Modal(container);
+    addInstance.addEventListener('click', (event) => {
+      event.preventDefault();
+      const csrf = document.querySelector('#csrf').value;
+      const url = document.querySelector('#url').value;
+      const instance = document.querySelector('#instance').value;
+      const comment = document.querySelector('#comment').value;
+      const email = document.querySelector('#email').value;
+      const admin = document.querySelector('#admin').value;
+      const password = document.querySelector('#password').value;
 
-    fetch('index.php?action=ajax&ajax=config&ajaxaction=add-instance', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        csrf: csrf,
-        url: url,
-        instance: instance,
-        comment: comment,
-        email: email,
-        admin: admin,
-        password: password,
-      }),
-    })
-      .then(async (response) => {
-        if (response.status === 200) {
-          return response.json();
-        }
-        throw new Error('Network response was not ok.');
+      fetch('index.php?action=ajax&ajax=config&ajaxaction=add-instance', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          csrf: csrf,
+          url: url,
+          instance: instance,
+          comment: comment,
+          email: email,
+          admin: admin,
+          password: password,
+        }),
       })
-      .then((response) => {
-        const table = document.querySelector('.table tbody');
-        const row = addElement('tr', { id: `row-instance-${response.added}` }, [
-          addElement('td', { innerText: response.added }),
-          addElement('td', {}, [
-            addElement('a', {
-              href: response.url,
-              target: '_blank',
-              innerText: response.url,
-            }),
-          ]),
-          addElement('td', { innerText: instance }),
-          addElement('td', { innerText: comment }),
-          addElement('td', {}, [
-            addElement(
-              'a',
-              {
-                href: `?action=edit-instance&instance_id=${response.added}`,
-                classList: 'btn btn-info',
-              },
-              [addElement('i', { classList: 'fa fa-pencil', ariaHidden: true })]
-            ),
-          ]),
-          addElement('td', {}, [
-            addElement(
-              'button',
-              {
-                classList: 'btn btn-danger',
-                'data-delete-instance-id': `${response.added}`,
-                type: 'button',
-              },
-              [
-                addElement('i', {
-                  ariaHidden: true,
-                  classList: 'fa fa-trash',
+        .then(async (response) => {
+          if (response.status === 200) {
+            return response.json();
+          }
+          throw new Error('Network response was not ok.');
+        })
+        .then((response) => {
+          const table = document.querySelector('.table tbody');
+          const row = addElement('tr', { id: `row-instance-${response.added}` }, [
+            addElement('td', { innerText: response.added }),
+            addElement('td', {}, [
+              addElement('a', {
+                href: response.url,
+                target: '_blank',
+                innerText: response.url,
+              }),
+            ]),
+            addElement('td', { innerText: instance }),
+            addElement('td', { innerText: comment }),
+            addElement('td', {}, [
+              addElement(
+                'a',
+                {
+                  href: `?action=edit-instance&instance_id=${response.added}`,
+                  classList: 'btn btn-info',
+                },
+                [addElement('i', { classList: 'fa fa-pencil', ariaHidden: true })]
+              ),
+            ]),
+            addElement('td', {}, [
+              addElement(
+                'button',
+                {
+                  classList: 'btn btn-danger',
                   'data-delete-instance-id': `${response.added}`,
-                }),
-              ]
-            ),
-          ]),
-        ]);
-        table.appendChild(row);
-        modal.hide();
-      })
-      .catch((error) => {
-        const table = document.querySelector('.table');
-        table.insertAdjacentElement(
-          'afterend',
-          addElement('div', { classList: 'alert alert-danger', innerText: error })
-        );
-      });
-  });
+                  type: 'button',
+                },
+                [
+                  addElement('i', {
+                    ariaHidden: true,
+                    classList: 'fa fa-trash',
+                    'data-delete-instance-id': `${response.added}`,
+                  }),
+                ]
+              ),
+            ]),
+          ]);
+          table.appendChild(row);
+          modal.hide();
+        })
+        .catch((error) => {
+          const table = document.querySelector('.table');
+          table.insertAdjacentElement(
+            'afterend',
+            addElement('div', { classList: 'alert alert-danger', innerText: error })
+          );
+        });
+    });
+  }
 
   if (deleteInstance) {
     deleteInstance.forEach((element) => {
