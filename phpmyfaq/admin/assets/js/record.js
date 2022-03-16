b0d3461192289418dcf4b027e610e5c71b420371
Thorsten Rinne <thorsten@phpmyfaq.de>
2022-03-16 17:06:31 +0100

    fix: corrected fetching value from hidden input, closes (#1996)

--- phpmyfaq/admin/assets/js/record.js
+++ phpmyfaq/admin/assets/js/record.js
@@ -5,12 +5,12 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/.
  *
- * @package phpMyFAQ
- * @author Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
  * @copyright 2013-2022 phpMyFAQ Team
- * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
- * @link https://www.phpmyfaq.de
- * @since 2013-11-17
+ * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2013-11-17
  */
 
 $(function () {
@@ -40,9 +40,9 @@ $(function () {
     getCategoryPermissions(categories);
   });
 
-  const faqId = document.getElementById('record_id').value;
+  const faqId = document.getElementById('record_id');
   if (faqId > 0) {
-    getFaqPermissions(faqId);
+    getFaqPermissions(faqId.value);
   }
 });
 
