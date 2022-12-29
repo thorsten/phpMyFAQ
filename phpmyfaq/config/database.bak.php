2db87be0baa354d94c53548c48e778975f22c578
Thorsten Rinne <thorsten@phpmyfaq.de>
2022-12-29 13:43:08 +0100

    fix: removed untracked files

--- phpmyfaq/config/database.bak.php
+++
@@ -1,8 +0,0 @@
-<?php
-$DB['server'] = 'db';
-$DB['port'] = '3306';
-$DB['user'] = 'phpmyfaq';
-$DB['password'] = 'phpmyfaq';
-$DB['db'] = 'phpmyfaq';
-$DB['prefix'] = '';
-$DB['type'] = 'mysqli';
--- phpmyfaq/config/elasticsearch.bak.php
+++
@@ -1,4 +0,0 @@
-<?php
-$PMF_ES['hosts'] = ['elasticsearch:9200'];
-$PMF_ES['index'] = 'phpmyfaq';
-$PMF_ES['type'] = 'faqs';
