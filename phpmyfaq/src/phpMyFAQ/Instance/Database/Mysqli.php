6dadf5f1ad0808e97b518ff5d152148356d3b12c
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-01-10 16:50:16 +0100

    fix: force tables to use InnoDB

--- phpmyfaq/src/phpMyFAQ/Instance/Database/Mysqli.php
+++ phpmyfaq/src/phpMyFAQ/Instance/Database/Mysqli.php
@@ -34,7 +34,7 @@ class Mysqli extends Database implements Driver
             usr INT(11) NOT NULL,
             text TEXT NOT NULL,
             ip VARCHAR(64) NOT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqattachment' => 'CREATE TABLE %sfaqattachment (
             id INT(11) NOT NULL,
@@ -47,12 +47,12 @@ class Mysqli extends Database implements Driver
             filesize INT(11) NOT NULL,
             encrypted INT(11) NOT NULL DEFAULT 0,
             mime_type VARCHAR(255) NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqattachment_file' => 'CREATE TABLE %sfaqattachment_file (
             virtual_hash CHAR(32) NOT NULL,
             contents BLOB NOT NULL,
-            PRIMARY KEY (virtual_hash)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (virtual_hash)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqbackup' => 'CREATE TABLE %sfaqbackup (
             id INT(11) NOT NULL,
@@ -60,7 +60,7 @@ class Mysqli extends Database implements Driver
             authkey VARCHAR(255) NOT NULL,
             authcode VARCHAR(255) NOT NULL,
             created timestamp NOT NULL,
-            PRIMARY KEY (id))',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqcaptcha' => 'CREATE TABLE %sfaqcaptcha (
             id VARCHAR(6) NOT NULL,
@@ -68,7 +68,7 @@ class Mysqli extends Database implements Driver
             language VARCHAR(5) NOT NULL,
             ip VARCHAR(64) NOT NULL,
             captcha_time INT(11) NOT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqcategories' => 'CREATE TABLE %sfaqcategories (
             id INT(11) NOT NULL,
@@ -81,12 +81,12 @@ class Mysqli extends Database implements Driver
             active INT(11) NULL DEFAULT 1,
             image VARCHAR(255) DEFAULT NULL,
             show_home INT(1) DEFAULT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqcategory_news' => 'CREATE TABLE %sfaqcategory_news (
             category_id INT(11) NOT NULL,
             news_id INT(11) NOT NULL,
-            PRIMARY KEY (category_id, news_id))',
+            PRIMARY KEY (category_id, news_id)) ENGINE = InnoDB',
 
         'faqcategoryrelations' => 'CREATE TABLE %sfaqcategoryrelations (
             category_id INT(11) NOT NULL,
@@ -94,22 +94,23 @@ class Mysqli extends Database implements Driver
             record_id INT(11) NOT NULL,
             record_lang VARCHAR(5) NOT NULL,
             PRIMARY KEY (category_id, category_lang, record_id, record_lang),
-            KEY idx_records (record_id, record_lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            KEY idx_records (record_id, record_lang)) 
+            DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqcategory_group' => 'CREATE TABLE %sfaqcategory_group (
             category_id INT(11) NOT NULL,
             group_id INT(11) NOT NULL,
-            PRIMARY KEY (category_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (category_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqcategory_user' => 'CREATE TABLE %sfaqcategory_user (
             category_id INT(11) NOT NULL,
             user_id INT(11) NOT NULL,
-            PRIMARY KEY (category_id, user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (category_id, user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqcategory_order' => 'CREATE TABLE %sfaqcategory_order (
             category_id int(11) NOT NULL,
             position int(11) NOT NULL,
-            PRIMARY KEY (category_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (category_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqchanges' => 'CREATE TABLE %sfaqchanges (
             id INT(11) NOT NULL,
@@ -119,7 +120,7 @@ class Mysqli extends Database implements Driver
             usr INT(11) NOT NULL ,
             datum INT(11) NOT NULL,
             what text DEFAULT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqcomments' => 'CREATE TABLE %sfaqcomments (
             id_comment INT(11) NOT NULL,
@@ -130,12 +131,12 @@ class Mysqli extends Database implements Driver
             comment text NOT NULL,
             datum VARCHAR(64) NOT NULL,
             helped text DEFAULT NULL,
-            PRIMARY KEY (id_comment)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id_comment)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqconfig' => 'CREATE TABLE %sfaqconfig (
             config_name VARCHAR(255) NOT NULL default \'\',
             config_value VARCHAR(255) DEFAULT NULL,
-            PRIMARY KEY (config_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (config_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqdata' => 'CREATE TABLE %sfaqdata (
             id INT(11) NOT NULL,
@@ -158,7 +159,7 @@ class Mysqli extends Database implements Driver
             created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             notes TEXT DEFAULT NULL,
             FULLTEXT (keywords,thema,content),
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqdata_revisions' => 'CREATE TABLE %sfaqdata_revisions (
             id INT(11) NOT NULL,
@@ -180,29 +181,30 @@ class Mysqli extends Database implements Driver
             date_end VARCHAR(14) NOT NULL DEFAULT \'99991231235959\',
             created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             notes text DEFAULT NULL,
-            PRIMARY KEY (id, lang, solution_id, revision_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang, solution_id, revision_id)) 
+            DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqdata_group' => 'CREATE TABLE %sfaqdata_group (
             record_id INT(11) NOT NULL,
             group_id INT(11) NOT NULL,
-            PRIMARY KEY (record_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (record_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqdata_tags' => 'CREATE TABLE %sfaqdata_tags (
             record_id INT(11) NOT NULL,
             tagging_id INT(11) NOT NULL,
-            PRIMARY KEY (record_id, tagging_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (record_id, tagging_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqdata_user' => 'CREATE TABLE %sfaqdata_user (
             record_id INT(11) NOT NULL,
             user_id INT(11) NOT NULL,
-            PRIMARY KEY (record_id, user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (record_id, user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqglossary' => 'CREATE TABLE %sfaqglossary (
             id INT(11) NOT NULL ,
             lang VARCHAR(5) NOT NULL ,
             item VARCHAR(255) NOT NULL ,
             definition text NOT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqgroup' => 'CREATE TABLE %sfaqgroup (
             group_id INT(11) NOT NULL,
@@ -210,12 +212,12 @@ class Mysqli extends Database implements Driver
             description text NULL,
             auto_join INT(11) NULL,
             PRIMARY KEY(group_id),
-            KEY idx_name (name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            KEY idx_name (name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqgroup_right' => 'CREATE TABLE %sfaqgroup_right (
             group_id INT(11) NOT NULL,
             right_id INT(11) NOT NULL,
-            PRIMARY KEY(group_id, right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY(group_id, right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqinstances' => 'CREATE TABLE %sfaqinstances (
             id INT NOT NULL,
@@ -224,13 +226,13 @@ class Mysqli extends Database implements Driver
             comment TEXT NULL,
             created TIMESTAMP DEFAULT \'1977-04-07 14:47:00\',
             modified DATETIME NOT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqinstances_config' => 'CREATE TABLE %sfaqinstances_config (
             instance_id INT NOT NULL,
             config_name VARCHAR(255) NOT NULL default \'\',
             config_value VARCHAR(255) DEFAULT NULL,
-            PRIMARY KEY (instance_id, config_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (instance_id, config_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqmeta' => 'CREATE TABLE %sfaqmeta (
             id INT(11) unsigned NOT NULL,
@@ -238,7 +240,7 @@ class Mysqli extends Database implements Driver
             page_id VARCHAR(48) DEFAULT NULL,
             type VARCHAR(48) DEFAULT NULL,
             content TEXT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqnews' => 'CREATE TABLE %sfaqnews (
             id INT(11) NOT NULL,
@@ -255,7 +257,7 @@ class Mysqli extends Database implements Driver
             link VARCHAR(255) DEFAULT NULL,
             linktitel VARCHAR(255) DEFAULT NULL,
             target VARCHAR(255) NOT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqquestions' => 'CREATE TABLE %sfaqquestions (
             id INT(11) NOT NULL,
@@ -267,7 +269,7 @@ class Mysqli extends Database implements Driver
             created VARCHAR(20) NOT NULL,
             is_visible char(1) default \'Y\',
             answer_id INT(11) NOT NULL DEFAULT 0,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqright' => 'CREATE TABLE %sfaqright (
             right_id INT(11) NOT NULL,
@@ -276,20 +278,20 @@ class Mysqli extends Database implements Driver
             for_users INT(11) NULL DEFAULT 1,
             for_groups INT(11) NULL DEFAULT 1,
             for_sections INT(11) NULL DEFAULT 1,
-            PRIMARY KEY (right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqsearches' => 'CREATE TABLE %sfaqsearches (
             id INT(11) NOT NULL ,
             lang VARCHAR(5) NOT NULL ,
             searchterm VARCHAR(255) NOT NULL ,
             searchdate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqsections' => 'CREATE TABLE %sfaqsections (
             id INT(11) NOT NULL,
             name VARCHAR(255) NOT NULL,
             description VARCHAR(255) DEFAULT NULL,
-            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqsection_category' => 'CREATE TABLE %sfaqsection_category (
             section_id INT(11) NOT NULL,
@@ -311,7 +313,7 @@ class Mysqli extends Database implements Driver
             user_id INT(11) NOT NULL,
             ip VARCHAR(64) NOT NULL,
             time INT(11) NOT NULL,
-            PRIMARY KEY (sid)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (sid)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqsessions_idx' => 'CREATE INDEX idx_time ON %sfaqsessions (time)',
 
@@ -319,12 +321,12 @@ class Mysqli extends Database implements Driver
             id INT(11) NOT NULL,
             lang VARCHAR(5) NOT NULL,
             stopword VARCHAR(64) NOT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqtags' => 'CREATE TABLE %sfaqtags (
             tagging_id INT(11) NOT NULL,
             tagging_name VARCHAR(255) NOT NULL ,
-            PRIMARY KEY (tagging_id, tagging_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (tagging_id, tagging_name)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faquser' => 'CREATE TABLE %sfaquser (
             user_id INT(11) NOT NULL,
@@ -344,7 +346,7 @@ class Mysqli extends Database implements Driver
             access_token TEXT NULL DEFAULT NULL,
             code_verifier VARCHAR(255) NULL DEFAULT NULL,
             jwt TEXT NULL DEFAULT NULL,
-            PRIMARY KEY (user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faquserdata' => 'CREATE TABLE %sfaquserdata (
             user_id INT(11) NOT NULL,
@@ -357,24 +359,24 @@ class Mysqli extends Database implements Driver
             login VARCHAR(128) NOT NULL,
             pass VARCHAR(80) NULL,
             domain VARCHAR(255) NULL,
-            PRIMARY KEY (login)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (login)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faquser_group' => 'CREATE TABLE %sfaquser_group (
             user_id INT(11) NOT NULL,
             group_id INT(11) NOT NULL,
-            PRIMARY KEY (user_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (user_id, group_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faquser_right' => 'CREATE TABLE %sfaquser_right (
             user_id INT(11) NOT NULL,
             right_id INT(11) NOT NULL,
-            PRIMARY KEY (user_id, right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (user_id, right_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqvisits' => 'CREATE TABLE %sfaqvisits (
             id INT(11) NOT NULL,
             lang VARCHAR(5) NOT NULL,
             visits INT(11) NOT NULL,
             last_visit INT(11) NOT NULL,
-            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
+            PRIMARY KEY (id, lang)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
 
         'faqvoting' => 'CREATE TABLE %sfaqvoting (
             id INT(11) NOT NULL,
@@ -383,7 +385,7 @@ class Mysqli extends Database implements Driver
             usr INT(11) NOT NULL,
             datum VARCHAR(20) DEFAULT \'\',
             ip VARCHAR(15) DEFAULT \'\',
-            PRIMARY KEY (id))',
+            PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB',
     ];
 
     /**
