66a1ec67275827059c308b4f6fa5cf0be34993fa
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-01-15 21:25:14 +0100

    fix: improved error message handling

---
+++ phpmyfaq/assets/src/api/forms.js
@@ -0,0 +1,49 @@
+/**
+ * phpMyFAQ Forms API
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne
+ * @copyright 2023 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2023-01-06
+ */
+
+import { addElement, serialize } from '../utils';
+
+export const saveFormData = (action) => {
+  const form = document.querySelector('#formValues');
+  const loader = document.getElementById('loader');
+  const formData = new FormData(form);
+
+  loader.classList.remove('d-none');
+  fetch(`ajaxservice.php?action=${action}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(serialize(formData)),
+  })
+    .then(async (response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Network response was not ok: ', { cause: { response } });
+    })
+    .then((response) => {
+      console.info(response);
+    })
+    .catch(async (error) => {
+      const message = document.getElementById('faqs');
+      const errorMessage = await error.cause.response.json();
+      message.insertAdjacentElement(
+        'afterend',
+        addElement('div', { classList: 'alert alert-danger', innerText: errorMessage })
+      );
+    });
+};
