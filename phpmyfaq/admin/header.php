edf0f6f90d4deaf46b4fd97ae92f16c1e10a2635
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-01-23 07:21:45 +0100

    fix: added missing conversion to HTML entities

--- phpmyfaq/admin/header.php
+++ phpmyfaq/admin/header.php
@@ -442,7 +442,7 @@ switch ($action) {
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                    aria-haspopup="true" aria-expanded="false">
                   <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                      <?= $user->getUserData('display_name'); ?>
+                      <?= Strings::htmlentities($user->getUserData('display_name')) ?>
                   </span>
                     <?php
                     if ($faqConfig->get('main.enableGravatarSupport')) {
