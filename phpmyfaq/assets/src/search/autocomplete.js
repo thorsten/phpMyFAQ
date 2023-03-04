42a26ee7128dbb539354ae666c628d847a526f46
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-03-04 11:13:13 +0100

    fix: check on search input

--- phpmyfaq/assets/src/search/autocomplete.js
+++ phpmyfaq/assets/src/search/autocomplete.js
@@ -19,28 +19,31 @@ import { addElement } from '../utils';
 
 export const handleAutoComplete = () => {
   const autoCompleteInput = document.getElementById('pmf-search-autocomplete');
-  autocomplete({
-    debounceWaitMs: 200,
-    preventSubmit: true,
-    disableAutoSelect: false,
-    input: autoCompleteInput,
-    container: addElement('ul', { classList: 'list-group bg-dark' }),
-    fetch: async (searchString, update) => {
-      searchString = searchString.toLowerCase();
-      const fetchedData = await fetchAutoCompleteData(searchString);
-      const suggestions = fetchedData.filter((item) => item.question.search(searchString));
-      update(suggestions);
-    },
-    onSelect: (event) => {
-      window.location.href = event.url;
-    },
-    render: (item, currentValue) => {
-      return addElement('li', { classList: 'list-group-item d-flex justify-content-between align-items-start' }, [
-        addElement('div', { classList: 'ms-2 me-auto' }, [
-          addElement('div', { classList: 'fw-bold', innerText: item.category }),
-          addElement('span', { classList: 'pmf-searched-question', textContent: item.question }),
-        ]),
-      ]);
-    },
-  });
+
+  if (autoCompleteInput) {
+    autocomplete({
+      debounceWaitMs: 200,
+      preventSubmit: true,
+      disableAutoSelect: false,
+      input: autoCompleteInput,
+      container: addElement('ul', { classList: 'list-group bg-dark' }),
+      fetch: async (searchString, update) => {
+        searchString = searchString.toLowerCase();
+        const fetchedData = await fetchAutoCompleteData(searchString);
+        const suggestions = fetchedData.filter((item) => item.question.search(searchString));
+        update(suggestions);
+      },
+      onSelect: (event) => {
+        window.location.href = event.url;
+      },
+      render: (item, currentValue) => {
+        return addElement('li', { classList: 'list-group-item d-flex justify-content-between align-items-start' }, [
+          addElement('div', { classList: 'ms-2 me-auto' }, [
+            addElement('div', { classList: 'fw-bold', innerText: item.category }),
+            addElement('span', { classList: 'pmf-searched-question', textContent: item.question }),
+          ]),
+        ]);
+      },
+    });
+  }
 };
