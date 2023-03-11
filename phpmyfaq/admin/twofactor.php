dae9a2f2275cc8061816a5a21bbeb102ebf06627
modelrailroader <96944229+modelrailroader@users.noreply.github.com>
2023-03-11 19:37:31 +0100

    Fixed wrong email-adress

--- phpmyfaq/admin/twofactor.php
+++ phpmyfaq/admin/twofactor.php
@@ -10,7 +10,7 @@
  * @package phpMyFAQ
  * @author Thorsten Rinne <thorsten@phpmyfaq.de>
  * @author Alexander M. Turek <me@derrabus.de>
- * @author Jan Harms <model_railroader@gmx.de>
+ * @author Jan Harms <model_railroader@gmx-topmail.de>
  * @copyright 2005-2023 phpMyFAQ Team
  * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
  * @link https://www.phpmyfaq.de
