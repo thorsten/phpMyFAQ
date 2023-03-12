ff399ca72e9396043d1cb08900b82e081c98b8ce
modelrailroader <96944229+modelrailroader@users.noreply.github.com>
2023-03-12 12:56:16 +0100

    PHP CS corrections

--- phpmyfaq/src/phpMyFAQ/Twofactor.php
+++ phpmyfaq/src/phpMyFAQ/Twofactor.php
@@ -26,58 +26,57 @@ use phpMyFAQ\Configuration;
 use RobThree\Auth\TwoFactorAuth;
 use phpMyFAQ\User\CurrentUser;
 
-class Twofactor 
+class Twofactor
 {
     private $config = null;
-    
-    public function __construct(Configuration $config) 
+
+    public function __construct(Configuration $config)
     {
         $this->config = $config;
     }
-    
+
   // Generates and returns a new secret without saving
-    public function generateSecret(): string 
+    public function generateSecret(): string
     {
         $tfa = new TwoFactorAuth();
         $secret = $tfa->createSecret();
         return $secret;
     }
-    
+
   // Saves a given secret to the current user from the session
-    public function saveSecret($secret) 
+    public function saveSecret($secret)
     {
         $user = CurrentUser::getFromSession($this->config);
         $user->setUserData(['secret' => $secret]);
         return true;
     }
-    
+
   // Returns the secret of the current user
-    public function getSecret($user) 
+    public function getSecret($user)
     {
         return $user->getUserData('secret');
     }
-    
+
   // Validates a given token. Returns true if the token is correct.
-    public function validateToken($token, $userid) 
+    public function validateToken($token, $userid)
     {
         $user = new CurrentUser($this->config);
         $user->getUserById($userid);
         $secret = $user->getUserData('secret');
         $tfa = new TwoFactorAuth();
         $result = $tfa->verifyCode($secret, $token);
-        if($result == true) {
+        if ($result == true) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
    
   // Returns a QR-Code to a given secret for transmitting the secret to the Authentificator-App
-    public function getQrCode($secret): string 
+    public function getQrCode($secret): string
     {
         $tfa = new TwoFactorAuth();
         $result = $tfa->getQRCodeImageAsDataUri("phpMyFAQ", $secret);
         return $result;
-    }    
+    }
 }
