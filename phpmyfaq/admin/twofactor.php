2f3ff0b23d522a70f7364a159d1bbf733e172480
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-03-19 12:22:05 +0100

    style: improved layout for 2FA

--- phpmyfaq/admin/twofactor.php
+++ phpmyfaq/admin/twofactor.php
@@ -5,17 +5,18 @@
  *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/.
+ * obtain one at https://mozilla.org/MPL/2.0/.
  *
- * @package phpMyFAQ
- * @author Thorsten Rinne <thorsten@phpmyfaq.de>
- * @author Alexander M. Turek <me@derrabus.de>
- * @author Jan Harms <model_railroader@gmx-topmail.de>
+ * @package   phpMyFAQ
+ * @author    Jan Harms <model_railroader@gmx-topmail.de>
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
  * @copyright 2005-2023 phpMyFAQ Team
- * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
- * @link https://www.phpmyfaq.de
- * @since 2023-03-11
+ * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2023-03-11
  */
+
+use phpMyFAQ\Translation;
 
 if (isset($error) && 0 < strlen((string) $error)) {
     $message = sprintf(
@@ -28,13 +29,14 @@ if (isset($error) && 0 < strlen((string) $error)) {
 } else {
     $message = '';
 }
-if ($action == 'logout') {
+
+if ($action === 'logout') {
     $message = sprintf(
         '<p class="alert alert-success alert-dismissible fade show mt-3">%s%s</p>',
         '<button type="button" class="close" data-dismiss="alert">' .
         '<span aria-hidden="true">&times;</span>' .
         '</button>',
-        $PMF_LANG['ad_logout']
+        Translation::get('ad_logout')
     );
 }
 if ((isset($_SERVER['HTTPS']) && strtoupper((string) $_SERVER['HTTPS']) === 'ON') || !$faqConfig->get('security.useSslForLogins')) {
@@ -47,8 +49,12 @@ if ((isset($_SERVER['HTTPS']) && strtoupper((string) $_SERVER['HTTPS']) === 'ON'
         <div class="col-lg-6 mx-auto">
           <div class="card rounded-0" id="login-form">
             <div class="card-header">
-              <h3 class="mb-0"><?= $PMF_LANG['msgTwofactorEnabled'] ?></h3>
-                <?= $message ?>
+              <h3 class="mb-0">
+                  <?= Translation::get('msgTwofactorEnabled') ?>
+              </h3>
+
+              <?= $message ?>
+
             </div>
             <div class="card-body">
               <form action="<?= $faqSystem->getSystemUri($faqConfig) ?>admin/index.php" method="post"
@@ -56,13 +62,17 @@ if ((isset($_SERVER['HTTPS']) && strtoupper((string) $_SERVER['HTTPS']) === 'ON'
                 <input type="hidden" name="userid" id="userid" value="<?= $userid ?>">
                 <input type="hidden" name="redirect-action" value="<?= $action ?>">
                 <div class="form-group">
-                  <label for="token"><?= $PMF_LANG['msgEnterTwofactorToken'] ?></label>
-                  <input type="text" class="form-control form-control-lg rounded-0" name="token" id="token"
-                         autocomplete="off" required>
+                  <label for="token"><?= Translation::get('msgEnterTwofactorToken') ?></label>
+                  <div class="col-4 mx-auto my-2">
+                    <input type="text" class="form-control form-control-lg text-center rounded-0" name="token"
+                           id="token" autocomplete="off" maxlength="6" autofocus required>
+                  </div>
                 </div>
-                <button type="submit" class="btn btn-success btn-lg float-right" id="btnLogin">
-                    <?= $PMF_LANG['msgTwofactorCheck'] ?>
-                </button>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                  <button type="submit" class="btn btn-success btn-lg float-right" id="btnLogin">
+                    <?= Translation::get('msgTwofactorCheck') ?>
+                  </button>
+                </div>
               </form>
             </div>
           </div>
@@ -78,6 +88,6 @@ if ((isset($_SERVER['HTTPS']) && strtoupper((string) $_SERVER['HTTPS']) === 'ON'
         '<p><a href="https://%s%s">%s</a></p>',
         $_SERVER['HTTP_HOST'],
         $_SERVER['REQUEST_URI'],
-        $PMF_LANG['msgSecureSwitch']
+        Translation::get('msgSecureSwitch')
     );
 }
