ac70a72b61a0297047b419b12526fdd5fc87ada7
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-05-10 17:31:46 +0200

    chore: added Rector dry run script, added Symfony rules to Rector

--- rector.php
+++ rector.php
@@ -7,6 +7,7 @@ use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -25,6 +26,9 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
         SetList::DEAD_CODE,
-        PHPUnitSetList::PHPUNIT_100
+        SymfonySetList::SYMFONY_62,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+        PHPUnitSetList::PHPUNIT_100,
     ]);
 };
