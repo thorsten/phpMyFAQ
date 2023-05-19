23f10f622c1d21173eef2dbb05cab2b5aa610e85
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-05-19 16:51:00 +0200

    feat(template): migrated system page to Twig template

--- phpmyfaq/admin/system.php
+++ phpmyfaq/admin/system.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * phpMyFAQ system information.
+ * phpMyFAQ System Information page.
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at https://mozilla.org/MPL/2.0/.
@@ -19,10 +20,14 @@ use Elastic\Elasticsearch\Exception\ClientResponseException;
 use Elastic\Elasticsearch\Exception\ServerResponseException;
 use Elastic\Transport\Exception\NoNodeAvailableException;
 use phpMyFAQ\Component\Alert;
+use phpMyFAQ\Configuration;
 use phpMyFAQ\Database;
 use phpMyFAQ\System;
 use phpMyFAQ\Translation;
+use phpMyFAQ\User\CurrentUser;
 use Symfony\Component\HttpFoundation\Request;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
     http_response_code(400);
@@ -30,6 +35,15 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
 }
 
 $request = Request::createFromGlobals();
+$faqConfig = Configuration::getConfigurationInstance();
+$user = CurrentUser::getCurrentUser($faqConfig);
+
+$loader = new FilesystemLoader('./assets/templates');
+$twig = new Environment($loader, [
+    'cache' => './assets/cache',
+    'debug' => DEBUG,
+]);
+$template = $twig->load('system.twig');
 
 if ($user->perm->hasPermission($user->getUserId(), 'editconfig')) {
     $faqSystem = new System();
@@ -38,7 +52,7 @@ if ($user->perm->hasPermission($user->getUserId(), 'editconfig')) {
         try {
             $esFullInformation = $faqConfig->getElasticsearch()->info();
             $esInformation = $esFullInformation['version']['number'];
-        } catch (ClientResponseException|ServerResponseException|NoNodeAvailableException $e) {
+        } catch (ClientResponseException | ServerResponseException | NoNodeAvailableException $e) {
             $faqConfig->getLogger()->error('Error while fetching Elasticsearch information', [$e->getMessage()]);
             $esInformation = 'n/a';
             echo Alert::danger('ad_entryins_fail');
@@ -46,49 +60,27 @@ if ($user->perm->hasPermission($user->getUserId(), 'editconfig')) {
     } else {
         $esInformation = 'n/a';
     }
-    ?>
-    <div
-        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">
-            <i class="fa fa-info-circle" aria-hidden="true"></i>
-            <?= Translation::get('ad_system_info') ?>
-        </h1>
-    </div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <table class="table table-striped align-middle">
-                <tbody>
-                <?php
+    $templateVars = [
+        'adminHeaderSystemInfo' => Translation::get('ad_system_info'),
+        'systemInformation' => [
+            'phpMyFAQ Version' => $faqSystem->getVersion(),
+            'phpMyFAQ API Version' => $faqSystem->getApiVersion(),
+            'phpMyFAQ Installation Path' => dirname((string) $request->server->get('SCRIPT_FILENAME'), 2),
+            'Web server software' => $request->server->get('SERVER_SOFTWARE'),
+            'Web server document root' => $request->server->get('DOCUMENT_ROOT'),
+            'Web server Interface' => strtoupper(PHP_SAPI),
+            'PHP Version' => PHP_VERSION,
+            'PHP Extensions' => implode(', ', get_loaded_extensions()),
+            'PHP Session path' => session_save_path(),
+            'Database Server' => Database::getType(),
+            'Database Server Version' => $faqConfig->getDb()->serverVersion(),
+            'Database Client Version' => $faqConfig->getDb()->clientVersion(),
+            'Elasticsearch Version' => $esInformation
+        ]
+    ];
 
-                $systemInformation = [
-                    'phpMyFAQ Version' => $faqSystem->getVersion(),
-                    'phpMyFAQ API Version' => $faqSystem->getApiVersion(),
-                    'phpMyFAQ Installation Path' => dirname((string) $request->server->get('SCRIPT_FILENAME'), 2),
-                    'Web server software' => $request->server->get('SERVER_SOFTWARE'),
-                    'Web server document root' => $request->server->get('DOCUMENT_ROOT'),
-                    'Web server Interface' => strtoupper(PHP_SAPI),
-                    'PHP Version' => PHP_VERSION,
-                    'PHP Extensions' => implode(', ', get_loaded_extensions()),
-                    'PHP Session path' => session_save_path(),
-                    'Database Server' => Database::getType(),
-                    'Database Server Version' => $faqConfig->getDb()->serverVersion(),
-                    'Database Client Version' => $faqConfig->getDb()->clientVersion(),
-                    'Elasticsearch Version' => $esInformation
-                ];
-                foreach ($systemInformation as $name => $info) : ?>
-                    <tr>
-                        <td class="col-2 align-text-top"><strong><?= $name ?></strong></td>
-                        <td><?= $info ?></td>
-                    </tr>
-                    <?php
-                endforeach;
-                ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <?php
+    echo $template->render($templateVars);
 } else {
     echo Translation::get('err_NotAuth');
 }
