0ca942934d764a8d038354f5a5a7f0f69bb3725d
Jan <96944229+modelrailroader@users.noreply.github.com>
2023-07-19 21:32:25 +0200

    fix: PHP CS

--- phpmyfaq/src/phpMyFAQ/Instance/Database/Mysqli.php
+++ phpmyfaq/src/phpMyFAQ/Instance/Database/Mysqli.php
@@ -66,7 +66,7 @@ class Mysqli extends Database implements Driver
         'faqbookmarks' => 'CREATE TABLE %sfaqbookmarks (
             userid INT(11) DEFAULT NULL,
             faqid INT(11) DEFAULT NULL',
- 
+
         'faqcaptcha' => 'CREATE TABLE %sfaqcaptcha (
             id VARCHAR(6) NOT NULL,
             useragent VARCHAR(255) NOT NULL,
