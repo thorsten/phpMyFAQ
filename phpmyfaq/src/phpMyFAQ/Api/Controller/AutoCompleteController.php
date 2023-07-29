d209bce2da898fedc864b86ffa2c1825783380c7
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-07-29 14:02:27 +0200

    refactor: added file header

--- phpmyfaq/src/phpMyFAQ/Api/Controller/AutoCompleteController.php
+++ phpmyfaq/src/phpMyFAQ/Api/Controller/AutoCompleteController.php
@@ -1,10 +1,24 @@
 <?php
 
+/**
+ * The Autocomplete Controller
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2023 phpMyFAQ Team
+ * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2023-07-29
+ */
+
 namespace phpMyFAQ\Api\Controller;
 
 use phpMyFAQ\Category;
 use phpMyFAQ\Configuration;
-use phpMyFAQ\Core\Exception;
 use phpMyFAQ\Faq\FaqPermission;
 use phpMyFAQ\Filter;
 use phpMyFAQ\Helper\SearchHelper;
