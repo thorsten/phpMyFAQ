b218d5f46cdee9ccd22897603188b63b1d407162
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-12-30 11:27:15 +0100

    feat: added new page for category management (#2405)

---
+++ phpmyfaq/admin/category.overview.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * List all categories in the admin section with drag and drop support for sorting.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2023 phpMyFAQ Team
+ * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2013-12-30
+ */
+
+use phpMyFAQ\Configuration;
+use phpMyFAQ\Template\TwigWrapper;
+use phpMyFAQ\Translation;
+use Symfony\Component\HttpFoundation\Request;
+use Twig\Extension\DebugExtension;
+
+if (!defined('IS_VALID_PHPMYFAQ')) {
+    http_response_code(400);
+    exit();
+}
+
+
+$request = Request::createFromGlobals();
+$faqConfig = Configuration::getConfigurationInstance();
+
+
+$twig = new TwigWrapper(PMF_ROOT_DIR . '/assets/templates');
+$twig->addExtension(new DebugExtension());
+$template = $twig->loadTemplate('./admin/content/category.overview.twig');
+
+$templateVars = [
+    'msgHeaderCategoryOverview' => Translation::get('ad_menu_categ_edit'),
+    'msgAddCategory' => Translation::get('ad_kateg_add'),
+    'msgCategoryMatrix' => Translation::get('ad_categ_show'),
+];
+
+echo $template->render($templateVars);
