376f659d68b89b03f483f27111bf44f17b6de44d
Jan <96944229+modelrailroader@users.noreply.github.com>
2024-01-04 19:51:35 +0100

    fix: fix linting

--- phpmyfaq/admin/assets/src/content/stickyfaqs.js
+++ phpmyfaq/admin/assets/src/content/stickyfaqs.js
@@ -18,54 +18,54 @@ import { addElement } from '../../../../assets/src/utils';
 import { pushErrorNotification, pushNotification } from '../utils';
 
 export const handleStickyFaqs = () => {
-    const stickyFAQs = document.getElementById('stickyFAQs');
-    if (stickyFAQs) {
-        Sortable.create(stickyFAQs, {
-            animation: 100,
-            draggable: '.list-group-item',
-            handle: '.list-group-item',
-            sort: true,
-            filter: '.sortable-disabled',
-            dataIdAttr: 'data-pmf-faqid',
-            onEnd: async (event) => {
-                const currentOrder = Array.from(event.from.children).map(function (item) {
-                    return item.getAttribute('data-pmf-faqid');
-                });
-                await saveStatus(currentOrder);
-            }
+  const stickyFAQs = document.getElementById('stickyFAQs');
+  if (stickyFAQs) {
+    Sortable.create(stickyFAQs, {
+      animation: 100,
+      draggable: '.list-group-item',
+      handle: '.list-group-item',
+      sort: true,
+      filter: '.sortable-disabled',
+      dataIdAttr: 'data-pmf-faqid',
+      onEnd: async (event) => {
+        const currentOrder = Array.from(event.from.children).map(function (item) {
+          return item.getAttribute('data-pmf-faqid');
         });
-    }
+        await saveStatus(currentOrder);
+      },
+    });
+  }
 };
 
 const saveStatus = async (currentOrder) => {
-    const stickyFAQs = document.getElementById('stickyFAQs');
-    const card = document.getElementById('mainCardStickyFAQs');
-    const successAlert = document.getElementById('successAlert');
-    const csrf = stickyFAQs.getAttribute('data-csrf');
-    if (successAlert) {
-        successAlert.remove();
-    }
-    try {
-        const response = await fetch('./api/faqs/sticky/order', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                faqIds: currentOrder,
-                csrf: csrf
-            })
-        });
+  const stickyFAQs = document.getElementById('stickyFAQs');
+  const card = document.getElementById('mainCardStickyFAQs');
+  const successAlert = document.getElementById('successAlert');
+  const csrf = stickyFAQs.getAttribute('data-csrf');
+  if (successAlert) {
+    successAlert.remove();
+  }
+  try {
+    const response = await fetch('./api/faqs/sticky/order', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        faqIds: currentOrder,
+        csrf: csrf,
+      }),
+    });
 
-        if (response.ok) {
-            const jsonResponse = await response.json();
-            pushNotification(jsonResponse.success)
-        } else {
-            const errorResponse = await response.json();
-            throw new Error('Network response was not ok: ' + JSON.stringify(errorResponse));
-        }
-    } catch (error) {
-        pushErrorNotification(error.message);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      pushNotification(jsonResponse.success);
+    } else {
+      const errorResponse = await response.json();
+      throw new Error('Network response was not ok: ' + JSON.stringify(errorResponse));
     }
+  } catch (error) {
+    pushErrorNotification(error.message);
+  }
 };
