7a4b00442f8908f05fdf80f1f458939f748f4998
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-01-21 15:57:58 +0100

    fix: avoid null values, closes #2809

--- phpmyfaq/admin/category.edit.php
+++ phpmyfaq/admin/category.edit.php
@@ -207,7 +207,7 @@ if ($user->perm->hasPermission($user->getUserId(), 'editcateg')) {
                           </label>
                       </div>
                       <select name="restricted_users" id="restricted_users" class="form-select">
-                          <?= $userHelper->getAllUserOptions($userPermission[0]) ?>
+                          <?= $userHelper->getAllUserOptions($categoryData->getUserId()) ?>
                       </select>
                   </div>
               </div>
