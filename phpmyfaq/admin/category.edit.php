afd926645a6ff6154c334864a2a8d45a348721c6
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-02-04 17:08:48 +0100

    refactor: re-formatted file

--- phpmyfaq/admin/category.edit.php
+++ phpmyfaq/admin/category.edit.php
@@ -2,7 +2,6 @@
 
 /**
  * Edits a category.
- *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at https://mozilla.org/MPL/2.0/.
@@ -61,167 +60,183 @@ if ($user->perm->hasPermission($user->getUserId(), 'editcateg')) {
         $restrictedGroups = true;
     }
 
-    $header = Translation::get('ad_categ_edit_1') . ' "' . Strings::htmlentities($categoryData->getName()) . '" ' .
-        Translation::get('ad_categ_edit_2');
+    $header = Translation::get('ad_categ_edit_1') . ' "' . Strings::htmlentities(
+            $categoryData->getName()
+        ) . '" ' . Translation::get('ad_categ_edit_2');
     ?>
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">
+    <div
+        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">
             <i aria-hidden="true" class="fa fa-folder"></i> <?= $header ?>
-          </h1>
-        </div>
+        </h1>
+    </div>
 
-        <div class="row mb-2">
-          <div class="col-lg-12">
-          <form enctype="multipart/form-data" action="?action=updatecategory" method="post">
-            <input type="hidden" name="id" value="<?= $categoryId ?>">
-            <input type="hidden" name="catlang" id="catlang" value="<?= $categoryData->getLang() ?>">
-            <input type="hidden" name="parent_id" value="<?= $categoryData->getParentId() ?>">
-              <?= Token::getInstance()->getTokenInput('update-category') ?>
-            <input type="hidden" name="existing_image" value="<?= $categoryData->getImage() ?>"
-                   id="pmf-category-existing-image">
+    <div class="row mb-2">
+        <div class="col-lg-12">
+            <form enctype="multipart/form-data" action="?action=updatecategory" method="post">
+                <input type="hidden" name="id" value="<?= $categoryId ?>">
+                <input type="hidden" name="catlang" id="catlang" value="<?= $categoryData->getLang() ?>">
+                <input type="hidden" name="parent_id" value="<?= $categoryData->getParentId() ?>">
+                <?= Token::getInstance()->getTokenInput('update-category') ?>
+                <input type="hidden" name="existing_image" value="<?= $categoryData->getImage() ?>"
+                       id="pmf-category-existing-image">
 
-            <div class="row mb-2">
-              <label class="col-lg-2 col-form-label" for="name">
-                  <?= Translation::get('ad_categ_titel') ?>
-              </label>
-              <div class="col-lg-4">
-                <input type="text" id="name" name="name" value="<?= Strings::htmlentities($categoryData->getName()) ?>" class="form-control">
-              </div>
-            </div>
+                <div class="row mb-2">
+                    <label class="col-lg-2 col-form-label" for="name">
+                        <?= Translation::get('ad_categ_titel') ?>
+                    </label>
+                    <div class="col-lg-4">
+                        <input type="text" id="name" name="name"
+                               value="<?= Strings::htmlentities($categoryData->getName()) ?>" class="form-control">
+                    </div>
+                </div>
 
-            <div class="row mb-2">
-              <label class="col-lg-2 col-form-label" for="description">
-                  <?= Translation::get('ad_categ_desc') ?>
-              </label>
-              <div class="col-lg-4">
+                <div class="row mb-2">
+                    <label class="col-lg-2 col-form-label" for="description">
+                        <?= Translation::get('ad_categ_desc') ?>
+                    </label>
+                    <div class="col-lg-4">
                 <textarea id="description" name="description" rows="3"
                           class="form-control"><?= Strings::htmlentities($categoryData->getDescription()) ?></textarea>
-              </div>
-            </div>
-
-            <div class="row mb-2">
-              <div class="offset-lg-2 col-lg-4">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="active" id='active' value="1"
-                        <?= (1 === (int)$categoryData->getActive() ? 'checked' : '') ?>>
-                    <label class="form-check-label" for="active"><?= Translation::get('ad_user_active') ?></label>
-                </div>
-              </div>
-            </div>
-
-            <div class="row mb-2">
-              <div class="offset-lg-2 col-lg-4">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="show_home" id='show_home' value="1"
-                        <?= (1 === (int)$categoryData->getShowHome() ? 'checked' : '') ?>>
-                    <label class="form-check-label" for="show_home"><?= Translation::get('ad_user_show_home') ?></label>
-                </div>
-              </div>
-            </div>
-
-
-              <div class="row mb-2">
-                  <label class="col-lg-2 col-form-label" for="pmf-category-image-upload">
-                      <?= Translation::get('ad_category_image') ?>:
-                  </label>
-                  <div class="col-lg-4">
-                    <div class="input-group">
-                      <input class="form-control" type="file" name="image" id="pmf-category-image-upload"
-                             value="<?= $categoryData->getImage() ?>">
-                      <span class="input-group-text" id="pmf-category-image-label"><?= $categoryData->getImage() ?></span>
                     </div>
-                      <div class="input-group mt-2">
-                          <button type="button" class="btn btn-info" id="button-reset-category-image">
-                              Reset category image
-                          </button>
-                      </div>
-                  </div>
-              </div>
+                </div>
 
-            <div class="row mb-2">
-              <label class="col-lg-2 col-form-label" for="user_id">
-                  <?= Translation::get('ad_categ_owner') ?>
-              </label>
-              <div class="col-lg-4">
-                <select id="user_id" name="user_id" class="form-select">
-                    <?= $userHelper->getAllUserOptions($categoryData->getUserId()) ?>
-                </select>
-              </div>
-            </div>
+                <div class="row mb-2">
+                    <div class="offset-lg-2 col-lg-4">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="active" id='active' value="1"
+                                <?= (1 === (int)$categoryData->getActive() ? 'checked' : '') ?>>
+                            <label class="form-check-label" for="active"><?= Translation::get(
+                                    'ad_user_active'
+                                ) ?></label>
+                        </div>
+                    </div>
+                </div>
 
-              <?php if ($faqConfig->get('security.permLevel') != 'basic') { ?>
-                  <div class="row mb-2">
-                      <label class="col-lg-2 col-form-label" for="group_id"><?= Translation::get('ad_categ_moderator') ?>:</label>
-                      <div class="col-lg-4">
-                          <select name="group_id" id="group_id" class="form-select">
-                              <?= $user->perm->getAllGroupsOptions([$categoryData->getGroupId()], $user) ?>
-                          </select>
-                      </div>
-                  </div>
+                <div class="row mb-2">
+                    <div class="offset-lg-2 col-lg-4">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="show_home" id='show_home' value="1"
+                                <?= (1 === (int)$categoryData->getShowHome() ? 'checked' : '') ?>>
+                            <label class="form-check-label" for="show_home"><?= Translation::get(
+                                    'ad_user_show_home'
+                                ) ?></label>
+                        </div>
+                    </div>
+                </div>
 
-                  <div class="row mb-2">
-                      <label class="col-lg-2 col-form-label" for="restricted_groups">
-                          <?= Translation::get('ad_entry_grouppermission') ?>
-                      </label>
-                      <div class="col-lg-4">
-                          <div class="form-check">
-                              <input type="radio" name="grouppermission" id="grouppermission_all" value="all"
-                                     class="form-check-input" <?php echo($allGroups ? 'checked' : '') ?>>
-                              <label class="form-check-label" for="grouppermission_all">
-                                  <?= Translation::get('ad_entry_all_groups') ?>
-                              </label>
-                          </div>
-                          <div class="form-check">
-                              <input type="radio" name="grouppermission" id="grouppermission" value="restricted"
-                                     class="form-check-input" <?php echo($restrictedGroups ? 'checked' : '') ?>>
-                              <label class="form-check-label" for="grouppermission">
-                                  <?= Translation::get('ad_entry_restricted_groups') ?>
-                              </label>
-                          </div>
-                          <select name="restricted_groups[]" id="restricted_groups" size="3" class="form-select"
-                                  multiple>
-                              <?= $user->perm->getAllGroupsOptions($groupPermission, $user) ?>
-                          </select>
-                      </div>
-                  </div>
+                <div class="row mb-2">
+                    <label class="col-lg-2 col-form-label" for="pmf-category-image-upload">
+                        <?= Translation::get('ad_category_image') ?>:
+                    </label>
+                    <div class="col-lg-4">
+                        <div class="input-group">
+                            <input class="form-control" type="file" name="image" id="pmf-category-image-upload"
+                                   value="<?= $categoryData->getImage() ?>">
+                            <span class="input-group-text" id="pmf-category-image-label"><?= $categoryData->getImage(
+                                ) ?></span>
+                        </div>
+                        <div class="input-group mt-2">
+                            <button type="button" class="btn btn-info" id="button-reset-category-image">
+                                Reset category image
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
-              <?php } else { ?>
-                <input type="hidden" name="grouppermission" value="all">
-              <?php } ?>
-              <div class="row mb-2">
-                  <label class="col-lg-2 col-form-label" for="restricted_users">
-                      <?= Translation::get('ad_entry_userpermission') ?>
-                  </label>
-                  <div class="col-lg-4">
-                      <div class="form-check">
-                          <input type="radio" name="userpermission" id="userpermission_all" value="all"
-                                 class="form-check-input" <?= ($allUsers ? 'checked' : '') ?>>
-                          <label class="form-check-label" for="userpermission_all">
-                              <?= Translation::get('ad_entry_all_users') ?>
-                          </label>
-                      </div>
-                      <div class="form-check">
-                          <input type="radio" name="userpermission" id="userpermission" value="restricted" class="form-check-input">
-                          <label class="form-check-label" for="userpermission"<?= ($restrictedUsers ? 'checked' : '') ?>>
-                              <?= Translation::get('ad_entry_restricted_users') ?>
-                          </label>
-                      </div>
-                      <select name="restricted_users" id="restricted_users" class="form-select">
-                          <?= $userHelper->getAllUserOptions($categoryData->getUserId()) ?>
-                      </select>
-                  </div>
-              </div>
+                <div class="row mb-2">
+                    <label class="col-lg-2 col-form-label" for="user_id">
+                        <?= Translation::get('ad_categ_owner') ?>
+                    </label>
+                    <div class="col-lg-4">
+                        <select id="user_id" name="user_id" class="form-select">
+                            <?= $userHelper->getAllUserOptions($categoryData->getUserId()) ?>
+                        </select>
+                    </div>
+                </div>
 
-            <div class="row mb-2">
-              <div class="offset-lg-2 col-lg-4 text-end">
-                <button class="btn btn-primary" type="submit" name="submit">
-                    <?= Translation::get('ad_categ_updatecateg') ?>
-                </button>
-              </div>
-            </div>
-          </form>
-          </div>
+                <?php
+                if ($faqConfig->get('security.permLevel') != 'basic') { ?>
+                    <div class="row mb-2">
+                        <label class="col-lg-2 col-form-label" for="group_id"><?= Translation::get(
+                                'ad_categ_moderator'
+                            ) ?>:</label>
+                        <div class="col-lg-4">
+                            <select name="group_id" id="group_id" class="form-select">
+                                <?= $user->perm->getAllGroupsOptions([$categoryData->getGroupId()], $user) ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row mb-2">
+                        <label class="col-lg-2 col-form-label" for="restricted_groups">
+                            <?= Translation::get('ad_entry_grouppermission') ?>
+                        </label>
+                        <div class="col-lg-4">
+                            <div class="form-check">
+                                <input type="radio" name="grouppermission" id="grouppermission_all" value="all"
+                                       class="form-check-input" <?php
+                                echo($allGroups ? 'checked' : '') ?>>
+                                <label class="form-check-label" for="grouppermission_all">
+                                    <?= Translation::get('ad_entry_all_groups') ?>
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input type="radio" name="grouppermission" id="grouppermission" value="restricted"
+                                       class="form-check-input" <?php
+                                echo($restrictedGroups ? 'checked' : '') ?>>
+                                <label class="form-check-label" for="grouppermission">
+                                    <?= Translation::get('ad_entry_restricted_groups') ?>
+                                </label>
+                            </div>
+                            <select name="restricted_groups[]" id="restricted_groups" size="3" class="form-select"
+                                    multiple>
+                                <?= $user->perm->getAllGroupsOptions($groupPermission, $user) ?>
+                            </select>
+                        </div>
+                    </div>
+
+                <?php
+                } else { ?>
+                    <input type="hidden" name="grouppermission" value="all">
+                <?php
+                } ?>
+                <div class="row mb-2">
+                    <label class="col-lg-2 col-form-label" for="restricted_users">
+                        <?= Translation::get('ad_entry_userpermission') ?>
+                    </label>
+                    <div class="col-lg-4">
+                        <div class="form-check">
+                            <input type="radio" name="userpermission" id="userpermission_all" value="all"
+                                   class="form-check-input" <?= ($allUsers ? 'checked' : '') ?>>
+                            <label class="form-check-label" for="userpermission_all">
+                                <?= Translation::get('ad_entry_all_users') ?>
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" name="userpermission" id="userpermission" value="restricted"
+                                   class="form-check-input">
+                            <label class="form-check-label"
+                                   for="userpermission"<?= ($restrictedUsers ? 'checked' : '') ?>>
+                                <?= Translation::get('ad_entry_restricted_users') ?>
+                            </label>
+                        </div>
+                        <select name="restricted_users" id="restricted_users" class="form-select">
+                            <?= $userHelper->getAllUserOptions($categoryData->getUserId()) ?>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="row mb-2">
+                    <div class="offset-lg-2 col-lg-4 text-end">
+                        <button class="btn btn-primary" type="submit" name="submit">
+                            <?= Translation::get('ad_categ_updatecateg') ?>
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
+    </div>
     <?php
 } else {
     echo Translation::get('err_NotAuth');
