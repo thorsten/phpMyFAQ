1fe66a8aa3a6689a483fea10db761ee67d05693c
Jan <96944229+modelrailroader@users.noreply.github.com>
2024-03-08 22:43:46 +0100

    refactor: moved instances.edit to twig

--- phpmyfaq/admin/instances.edit.php
+++ phpmyfaq/admin/instances.edit.php
@@ -19,76 +19,38 @@ use phpMyFAQ\Enums\PermissionType;
 use phpMyFAQ\Filter;
 use phpMyFAQ\Instance;
 use phpMyFAQ\Strings;
+use phpMyFAQ\Template\TwigWrapper;
 use phpMyFAQ\Translation;
+use Twig\Extension\DebugExtension;
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
     http_response_code(400);
     exit();
 }
-?>
-    <div
-        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">
-            <i aria-hidden="true" class="bi bi-wrench bi-fw"></i>
-            <?= Translation::get('ad_menu_instances'); ?>
-        </h1>
-    </div>
-<?php
+
 if ($user->perm->hasPermission($user->getUserId(), PermissionType::INSTANCE_EDIT->value)) {
     $instanceId = Filter::filterInput(INPUT_GET, 'instance_id', FILTER_VALIDATE_INT);
 
     $instance = new Instance($faqConfig);
-    $instanceData = $instance->getInstanceById($instanceId);
+    $instanceData = $instance->getInstanceById($instanceId, 'array');
 
-    ?>
-  <form action="?action=update-instance" method="post" accept-charset="utf-8">
-    <input type="hidden" name="instance_id" value="<?= $instanceData->id ?>"/>
-    <div class="row mb-2">
-      <label for="url" class="col-lg-2 col-form-label"><?= Translation::get('ad_instance_url') ?>:</label>
-      <div class="col-lg-8">
-        <input type="url" name="url" id="url" class="form-control"
-               value="<?= Strings::htmlentities($instanceData->url, ENT_QUOTES) ?>" required>
-      </div>
-    </div>
-    <div class="row mb-2">
-      <label for="instance" class="col-lg-2 col-form-label"><?= Translation::get('ad_instance_path') ?>:</label>
-      <div class="col-lg-8">
-        <input type="text" name="instance" id="instance" class="form-control" required
-               value="<?= Strings::htmlentities($instanceData->instance, ENT_QUOTES) ?>">
-      </div>
-    </div>
-    <div class="row mb-2">
-      <label for="comment" class="col-lg-2 col-form-label"><?= Translation::get('ad_instance_name') ?>:</label>
-      <div class="col-lg-8">
-        <input type="text" name="comment" id="comment" class="form-control" required
-               value="<?= Strings::htmlentities($instanceData->comment, ENT_QUOTES) ?>">
-      </div>
-    </div>
-    <div class="row mb-2">
-      <label class="col-lg-2 col-form-label" for="config"><?= Translation::get('ad_instance_config') ?>:</label>
-      <div class="col-lg-8">
-            <?php
-            foreach ($instance->getInstanceConfig($instanceData->id) as $key => $config) {
-                printf(
-                    '<input type="text" readonly class="form-control-plaintext" id="config" value="%s">',
-                    $key . ': ' . $config
-                );
-            }
-            ?>
-      </div>
-    </div>
-    <div class="row mb-2">
-      <div class="offset-lg-2 col-lg-4">
-          <a class="btn btn-secondary" href="?action=instances">
-              <?= Translation::get('ad_entry_back') ?>
-          </a>
-        <button class="btn btn-primary" type="submit">
-            <?= Translation::get('ad_instance_button') ?>
-        </button>
-      </div>
-    </div>
-  </form>
-    <?php
+    $templateVars = [
+        'ad_menu_instances' => Translation::get('ad_menu_instances'),
+        'instanceConfig' => $instance->getInstanceConfig($instanceData['id']),
+        'ad_instance_url' => Translation::get('ad_instance_url'),
+        'ad_instance_button' => Translation::get('ad_instance_button'),
+        'ad_instance_path' => Translation::get('ad_instance_path'),
+        'ad_instance_name' => Translation::get('ad_instance_name'),
+        'ad_instance_config' => Translation::get('ad_instance_config'),
+        'ad_entry_back' => Translation::get('ad_entry_back'),
+        'instance' => $instanceData
+    ];
+
+    $twig = new TwigWrapper(PMF_ROOT_DIR . '/assets/templates');
+    $twig->addExtension(new DebugExtension());
+    $template = $twig->loadTemplate('./admin/configuration/instances.edit.twig');
+
+    echo $template->render($templateVars);
 } else {
     require __DIR__ . '/no-permission.php';
 }
