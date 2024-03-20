edfc777241cd8c92c70012b44472ade117369c45
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-03-20 15:59:18 +0100

    test: added more Jest tests

---
+++ phpmyfaq/assets/src/api/register.test.js
@@ -0,0 +1,45 @@
+import { register } from './register';
+import { serialize } from '../utils';
+
+describe('register function', () => {
+  it('creates a registration successfully', async () => {
+    // Mocking fetch function
+    global.fetch = jest.fn().mockResolvedValue({
+      status: 200,
+      json: () => Promise.resolve({ success: 'Registration created successfully' }),
+    });
+
+    // Test data
+    const data = Object.keys({ newUser: 'New User' });
+
+    // Call the function
+    const result = await register(data);
+
+    // Assertions
+    expect(result).toEqual({ success: 'Registration created successfully' });
+    expect(fetch).toHaveBeenCalledWith('api/register', {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(serialize(data)),
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+    });
+  });
+
+  it('throws an error if network response is not ok', async () => {
+    // Mocking fetch function
+    global.fetch = jest.fn().mockResolvedValue({
+      status: 400,
+      json: () => Promise.resolve({ error: 'Something went wrong' }),
+    });
+
+    // Test data
+    const data = Object.keys({ comment: 'Test comment' });
+
+    // Assertions
+    await expect(register(data)).resolves.toEqual({ error: 'Something went wrong' });
+  });
+});
