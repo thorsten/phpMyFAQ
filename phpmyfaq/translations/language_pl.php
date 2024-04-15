fbf101c57a43c88a0c9d9bf4204d99f146f4349b
Chaffinch <bobi@poczta.fm>
2024-04-15 06:20:14 +0200

    Update language_pl.php
    Correction

--- phpmyfaq/translations/language_pl.php
+++ phpmyfaq/translations/language_pl.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The English language file - try to be the best of British and American English
+ * Polish language file
  *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
