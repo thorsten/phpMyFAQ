ca6e7ba00b8ffb108f09e783d69ebca1501f1147
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-06-16 14:43:19 +0200

    refactor: moved footer to Twig

--- phpmyfaq/admin/footer.php
+++ phpmyfaq/admin/footer.php
@@ -18,6 +18,7 @@
 
 use phpMyFAQ\Configuration;
 use phpMyFAQ\System;
+use phpMyFAQ\Template\TwigWrapper;
 use phpMyFAQ\Translation;
 use phpMyFAQ\User\CurrentUser;
 
@@ -26,103 +27,23 @@ if (!defined('IS_VALID_PHPMYFAQ')) {
     exit();
 }
 
-?>
-            </div>
-        </main>
-
-        <div class="toast-container position-fixed top-0 start-50 translate-middle-x mt-5 p-3">
-            <div id="pmf-notification" class="toast align-items-center text-bg-primary shadow border-0" role="alert"
-                 aria-live="assertive" aria-atomic="true">
-                <div class="d-flex">
-                    <div class="toast-body" id="pmf-notification-message">
-                    </div>
-                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
-                            aria-label="Close">
-                    </button>
-                </div>
-            </div>
-            <div id="pmf-notification-error" class="toast align-items-center text-bg-danger shadow border-0"
-                 role="alert" aria-live="assertive" aria-atomic="true">
-                <div class="d-flex">
-                    <div class="toast-body" id="pmf-notification-error-message">
-                    </div>
-                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
-                            aria-label="Close">
-                    </button>
-                </div>
-            </div>
-        </div>
-
-        <!-- Session Warning Modal -->
-        <div class="modal fade" id="sessionWarningModal" tabindex="-1" aria-labelledby="sessionWarningModalLabel"
-             aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="sessionWarningModalLabel">
-                            <?= Translation::get('msgSessionExpiringSoon') ?>
-                        </h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <?= sprintf(Translation::get('ad_session_expiring'), PMF_AUTH_TIMEOUT_WARNING) ?>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                            <?= Translation::get('msgNoLogMeOut') ?>
-                        </button>
-                        <button type="button" class="btn btn-primary" id="pmf-button-reload-page">
-                            <?= Translation::get('msgYesKeepMeLoggedIn') ?>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <footer class="py-4 bg-light mt-auto">
-            <div class="container-fluid px-4">
-                <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">
-                        Proudly <?= System::getPoweredByString() ?> |
-                        <a target="_blank" class="text-decoration-none"
-                           href="https://en.isupportukraine.eu/trombinoscope">
-                            #StandWithUkraine
-                        </a>
-                    </div>
-                    <div>
-                        <a target="_blank" rel="noopener" href="<?= System::getDocumentationUrl() ?>">
-                            Documentation
-                        </a>
-                        &middot;
-                        <a target="_blank" rel="noopener" href="https://www.buymeacoffee.com/thorsten">
-                            Buy us a coffee
-                        </a>
-                        &middot;
-                        <a target="_blank" rel="noopener" href="https://facebook.com/phpMyFAQ">
-                            Facebook
-                        </a>
-                        &middot;
-                        &copy; 2001 - <?= date('Y') ?>
-                        <a target="_blank" rel="noopener" href="<?= System::PHPMYFAQ_URL ?>">
-                            phpMyFAQ Team
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </div>
-</div>
-
-
-<?php
-
 $faqConfig = Configuration::getConfigurationInstance();
 $user = CurrentUser::getCurrentUser($faqConfig);
 
-if ($user->isLoggedIn()) { ?>
-  <iframe id="keepPMFSessionAlive" src="./session.keepalive.php?lang=<?= $faqLangCode ?>" width="0" height="0"
-          style="display: none;" name="keep-phpmyfaq-session-alive"></iframe>
-<?php } ?>
-<script src="../assets/dist/backend.js?<?= time(); ?>"></script>
-</body>
-</html>
+$twig = new TwigWrapper(PMF_ROOT_DIR . '/assets/templates');
+$template = $twig->loadTemplate('./admin/footer.twig');
+
+$templateVars = [
+    'msgSessionExpiringSoon' => Translation::get('msgSessionExpiringSoon'),
+    'msgModalSessionWarning' => sprintf(Translation::get('ad_session_expiring'), PMF_AUTH_TIMEOUT_WARNING),
+    'msgNoLogMeOut' => Translation::get('msgNoLogMeOut'),
+    'msgYesKeepMeLoggedIn' => Translation::get('msgYesKeepMeLoggedIn'),
+    'msgPoweredBy' => System::getPoweredByString(),
+    'documentationUrl' => System::getDocumentationUrl(),
+    'phpMyFaqUrl' => System::PHPMYFAQ_URL,
+    'isUserLoggedIn' => $user->isLoggedIn(),
+    'currentLanguage' => $faqLangCode,
+    'currentTimeStamp' => time(),
+];
+
+echo $template->render($templateVars);
