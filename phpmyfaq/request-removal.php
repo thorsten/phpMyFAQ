e89ed87e0d11c74338beba711e72fa19458dc04e
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-07-02 17:08:37 +0200

    refactor: migrated request removal page to Twig (#2969)

--- phpmyfaq/request-removal.php
+++ phpmyfaq/request-removal.php
@@ -15,40 +15,52 @@
  * @since     2018-02-03
  */
 
-use phpMyFAQ\Core\Exception;
+use phpMyFAQ\Configuration;
 use phpMyFAQ\Session\Token;
 use phpMyFAQ\Strings;
+use phpMyFAQ\Template\TwigWrapper;
 use phpMyFAQ\Translation;
 use phpMyFAQ\User\CurrentUser;
+use Twig\Extension\DebugExtension;
 
 if (!defined('IS_VALID_PHPMYFAQ')) {
     http_response_code(400);
     exit();
 }
 
+$faqConfig = Configuration::getConfigurationInstance();
+$user = CurrentUser::getCurrentUser($faqConfig);
+
 $faqSession->userTracking('request_removal', 0);
 
-$template->parse(
+$twig = new TwigWrapper(PMF_ROOT_DIR . '/assets/templates');
+$twig->addExtension(new DebugExtension());
+$twigTemplate = $twig->loadTemplate('./request-removal.twig');
+
+// Twig template variables
+$templateVars = [
+    'pageHeader' => Translation::get('msgUserRemoval'),
+    'msgContact' => Translation::get('msgContact'),
+    'msgUserRemovalText' => Translation::get('msgUserRemovalText'),
+    'msgContactRemove' => Translation::get('msgContactRemove'),
+    'msgContactPrivacyNote' => Translation::get('msgContactPrivacyNote'),
+    'msgPrivacyNote' => Translation::get('msgPrivacyNote'),
+    'privacyURL' => Strings::htmlentities($faqConfig->get('main.privacyURL')),
+    'msgNewContentName' => Translation::get('msgNewContentName'),
+    'msgNewContentMail' => Translation::get('msgNewContentMail'),
+    'ad_user_loginname' => Translation::get('ad_user_loginname'),
+    'csrf' => Token::getInstance()->getTokenInput('request-removal'),
+    'lang' => $Language->getLanguage(),
+    'userId' => $user->getUserId(),
+    'defaultContentMail' => ($user->getUserId() > 0) ? $user->getUserData('email') : '',
+    'defaultContentName' =>
+        ($user->getUserId() > 0) ? Strings::htmlentities($user->getUserData('display_name')) : '',
+    'defaultLoginName' => ($user->getUserId() > 0) ? Strings::htmlentities($user->getLogin()) : '',
+    'msgMessage' => Translation::get('msgMessage'),
+    'msgS2FButton' => Translation::get('msgS2FButton')
+];
+
+$template->addRenderedTwigOutput(
     'mainPageContent',
-    [
-        'pageHeader' => Translation::get('msgUserRemoval'),
-        'msgContact' => Translation::get('msgContact'),
-        'msgUserRemovalText' => Translation::get('msgUserRemovalText'),
-        'msgContactRemove' => Translation::get('msgContactRemove'),
-        'msgContactPrivacyNote' => Translation::get('msgContactPrivacyNote'),
-        'msgPrivacyNote' => Translation::get('msgPrivacyNote'),
-        'privacyURL' => Strings::htmlentities($faqConfig->get('main.privacyURL')),
-        'msgNewContentName' => Translation::get('msgNewContentName'),
-        'msgNewContentMail' => Translation::get('msgNewContentMail'),
-        'ad_user_loginname' => Translation::get('ad_user_loginname'),
-        'csrf' => Token::getInstance()->getTokenInput('request-removal'),
-        'lang' => $Language->getLanguage(),
-        'userId' => $user->getUserId(),
-        'defaultContentMail' => ($user->getUserId() > 0) ? $user->getUserData('email') : '',
-        'defaultContentName' =>
-            ($user->getUserId() > 0) ? Strings::htmlentities($user->getUserData('display_name')) : '',
-        'defaultLoginName' => ($user->getUserId() > 0) ? Strings::htmlentities($user->getLogin()) : '',
-        'msgMessage' => Translation::get('msgMessage'),
-        'msgS2FButton' => Translation::get('msgS2FButton')
-    ]
+    $twigTemplate->render($templateVars)
 );
