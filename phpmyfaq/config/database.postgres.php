746211518e9ad4a843f3b6c3e62f61aff03a8fb9
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-07-13 10:38:02 +0200

    chore: removed accidently commited file

--- phpmyfaq/config/database.postgres.php
+++
@@ -1,8 +0,0 @@
-<?php
-$DB['server'] = 'postgres';
-$DB['port'] = '5432';
-$DB['user'] = 'phpmyfaq';
-$DB['password'] = 'phpmyfaq';
-$DB['db'] = 'phpmyfaq';
-$DB['prefix'] = '';
-$DB['type'] = 'pgsql';
