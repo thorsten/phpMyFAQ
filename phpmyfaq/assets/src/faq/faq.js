57ed4695227b2be5e1116218d3264bdd1002f497
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-09-20 19:16:13 +0200

    docs: added missing file header

--- phpmyfaq/assets/src/faq/faq.js
+++ phpmyfaq/assets/src/faq/faq.js
@@ -1,3 +1,18 @@
+/**
+ * FAQ functionality
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne
+ * @copyright 2024 phpMyFAQ Team
+ * @license   http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2024-03-16
+ */
+
 import { addElement } from '../utils';
 import { createBookmark, createFaq, deleteBookmark } from '../api';
 import { pushErrorNotification, pushNotification } from '../../../admin/assets/src/utils';
