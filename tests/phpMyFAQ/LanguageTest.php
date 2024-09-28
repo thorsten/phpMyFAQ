9a4a269f237e722ec860dc8ab5ba3f33ff3171f7
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-09-28 12:46:43 +0200

    test: added missing test

--- tests/phpMyFAQ/LanguageTest.php
+++ tests/phpMyFAQ/LanguageTest.php
@@ -3,20 +3,48 @@
 namespace phpMyFAQ;
 
 use phpMyFAQ\Database\Sqlite3;
+use phpMyFAQ\Plugin\PluginException;
 use PHPUnit\Framework\TestCase;
 
 class LanguageTest extends TestCase
 {
     private Language $language;
+
+    private Sqlite3 $dbHandle;
+
+    /**
+     * @throws PluginException
+     */
     protected function setUp(): void
     {
         parent::setUp();
 
         Strings::init();
 
-        $dbHandle = new Sqlite3();
-        $configuration = new Configuration($dbHandle);
+
+        $this->dbHandle = new Sqlite3();
+        $this->dbHandle->connect(PMF_TEST_DIR . '/test.db', '', '');
+        $configuration = new Configuration($this->dbHandle);
         $this->language = new Language($configuration);
+    }
+
+    public function testIsLanguageAvailableWithId(): void
+    {
+        $this->dbHandle->query(
+            'INSERT INTO faqdata (id, lang, solution_id, active, sticky, thema, author, email, updated) VALUES' .
+            '(1, "en", 1001, 1, 1, "Test", "Author", "test@example.org", DATETIME("now", "localtime"))'
+        );
+        $result = $this->language->isLanguageAvailable(1);
+
+        $this->assertEquals(['en'], $result);
+
+        $this->dbHandle->query('DELETE FROM faqdata');
+    }
+
+    public function testIsLanguageAvailableNoResults(): void
+    {
+        $result = $this->language->isLanguageAvailable(1);
+        $this->assertEquals([], $result);
     }
 
     public function testGetLanguageReturnsString(): void
@@ -50,13 +78,13 @@ class LanguageTest extends TestCase
         $this->assertFalse($isSupported);
     }
 
-    public function testSetLanguageReturnsCorrectLanguageForValidConfigLanguage()
+    public function testSetLanguageReturnsCorrectLanguageForValidConfigLanguage(): void
     {
         $language = $this->language->setLanguage(true, 'language_en.php');
         $this->assertEquals('en', $language);
     }
 
-    public function testSetLanguageReturnsCorrectLanguageForValidSessionLanguage()
+    public function testSetLanguageReturnsCorrectLanguageForValidSessionLanguage(): void
     {
         $_SESSION['lang'] = 'fr';
         $language = $this->language->setLanguage(true, 'language_en.php');
@@ -64,7 +92,7 @@ class LanguageTest extends TestCase
         unset($_SESSION['lang']);
     }
 
-    public function testSetLanguageReturnsFallbackLanguageForInvalidConfigLanguageAndNoOtherDetectedLanguages()
+    public function testSetLanguageReturnsFallbackLanguageForInvalidConfigLanguageAndNoOtherDetectedLanguages(): void
     {
         $language = $this->language->setLanguage(true, 'invalid_language.php');
         $this->assertEquals('en', $language);
