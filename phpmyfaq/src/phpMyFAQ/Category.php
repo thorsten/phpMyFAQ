2f178c6a583cdc046a916b805ab43735a9315314
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-10-06 10:11:07 +0200

    refactor: simplified code

--- phpmyfaq/src/phpMyFAQ/Category.php
+++ phpmyfaq/src/phpMyFAQ/Category.php
@@ -22,7 +22,6 @@ namespace phpMyFAQ;
 
 use phpMyFAQ\Entity\CategoryEntity;
 use phpMyFAQ\Helper\LanguageHelper;
-use phpMyFAQ\Language\LanguageCodes;
 
 /**
  * Class Category
@@ -250,20 +249,23 @@ class Category
     /**
      * Get the level of the item id.
      *
-     * @param int $id Entity id
+     * @param int $categoryId Entity id
      */
-    private function getLevelOf(int $id): int
+    private function getLevelOf(int $categoryId): int
     {
-        $alreadyListed = [$id];
+        $alreadyListed = [$categoryId];
         $level = 0;
 
-        while ((isset($this->categoryName[$id]['parent_id'])) && ($this->categoryName[$id]['parent_id'] !== 0)) {
+        while (
+            (isset($this->categoryName[$categoryId]['parent_id'])) &&
+            ((int)$this->categoryName[$categoryId]['parent_id'] !== 0)
+        ) {
             ++$level;
-            $id = (int)$this->categoryName[$id]['parent_id'];
-            if (in_array($id, $alreadyListed)) {
+            $categoryId = (int)$this->categoryName[$categoryId]['parent_id'];
+            if (in_array($categoryId, $alreadyListed)) {
                 break;
             } else {
-                $alreadyListed[] = $id;
+                $alreadyListed[] = $categoryId;
             }
         }
 
@@ -345,8 +347,8 @@ class Category
      */
     public function buildCategoryTree(int $parentId = 0, int $indent = 0): void
     {
-        $tt = [];
-        $x = 0;
+        $temporaryTree = [];
+        $xLevel = 0;
 
         foreach ($this->categories as $categoryId => $n) {
             if (!isset($n['parent_id'])) {
@@ -358,11 +360,11 @@ class Category
             if ($categoryId <= 0) {
                 continue;
             }
-            $tt[$x++] = $categoryId;
+            $temporaryTree[$xLevel++] = $categoryId;
         }
 
-        if ($x != 0) {
-            foreach ($tt as $d) {
+        if ($xLevel != 0) {
+            foreach ($temporaryTree as $d) {
                 $tmp = [];
                 if (isset($this->categories[$d])) {
                     foreach ($this->categories[$d] as $key => $value) {
@@ -386,9 +388,9 @@ class Category
 
         foreach ($categories as $category) {
             if ($category['parent_id'] == $parentId) {
-                $id = $category['id'];
-                $children = $this->buildAdminCategoryTree($categories, $category['id']);
-                $result[$id] = [];
+                $categoryId = $category['id'];
+                $children = $this->buildAdminCategoryTree($categories, $categoryId);
+                $result[$categoryId] = [];
             }
         }
 
@@ -400,84 +402,63 @@ class Category
      * Transforms the linear array in a 1D array in the order of the tree, with
      * the info.
      *
-     * @param int $id Entity id
+     * @param int $categoryId Category ID
      */
-    public function transform(int $id): void
+    public function transform(int $categoryId): void
     {
-        $parentId = 0;
-        $showHome = 0;
+        $category = $this->categoryName[$categoryId] ?? [];
+        $parentId = $category['parent_id'] ?? 0;
+        $children = $this->children[$categoryId] ?? [];
+
+        if ($categoryId > 0) {
+            $category['level'] = $this->getLevelOf($categoryId);
+            $category['children'] = array_keys($children);
+            $category['tree'] = $this->buildTree($categoryId);
+            $category['symbol'] = $this->getSymbol($categoryId, $parentId);
+            $this->treeTab[] = $category;
+        }
+
+        foreach (array_keys($children) as $childId) {
+            $this->transform($childId);
+        }
+    }
+
+    private function buildTree(int $categoryId): array
+    {
+        $ascendants = $this->getNodes($categoryId);
         $tree = [];
-        $tabs = isset($this->children[$id]) ? array_keys($this->children[$id]) : [];
-        $num = count($tabs);
-        $symbol = 'minus';
-        $name = '';
-        $description = '';
-        $active = true;
-        $image = '';
 
-        if ($id > 0) {
-            $active = $this->categoryName[$id]['active'];
-            $description = $this->categoryName[$id]['description'];
-            $name = $this->categoryName[$id]['name'];
-            $parentId = $this->categoryName[$id]['parent_id'];
-            $image = $this->categoryName[$id]['image'];
-            $showHome = $this->categoryName[$id]['show_home'];
-        }
-
-        if ($num > 0) {
-            $temp = isset($this->children[$parentId]) ? array_keys($this->children[$parentId]) : [];
-            if (isset($temp[count($temp) - 1])) {
-                $symbol = ($id == $temp[count($temp) - 1]) ? 'angle' : 'medium';
+        foreach ($ascendants as $i => $ascendantId) {
+            if ($ascendantId == 0) {
+                break;
             }
+            $brothers = $this->getBrothers($ascendantId);
+            $tree[$i] = ($ascendantId == end($brothers)) ? 'space' : 'vertical';
         }
 
-        $ascendants = $this->getNodes($id);
-        $numAscendants = count($ascendants);
+        return $tree;
+    }
 
-        if ($id > 0) {
-            for ($i = 0; $i < $numAscendants; ++$i) {
-                if (0 === (int)$ascendants[$i]) {
-                    break;
-                }
+    private function getSymbol(int $categoryId, int $parentId): string
+    {
+        $siblings = $this->children[$parentId] ?? [];
 
-                $brothers = $this->getBrothers($ascendants[$i]);
-                $tree[$i] = ($ascendants[$i] == $brothers[count($brothers) - 1]) ? 'space' : 'vertical';
-            }
-        }
+        $array = array_keys($siblings);
 
-        if ($id > 0) {
-            $this->treeTab[] = [
-                'id' => $id,
-                'symbol' => $symbol,
-                'name' => $name,
-                'numChilds' => count($tabs),
-                'level' => $this->getLevelOf($id),
-                'parent_id' => $parentId,
-                'childs' => $tabs,
-                'tree' => $tree,
-                'description' => $description,
-                'active' => $active,
-                'image' => $image,
-                'show_home' => $showHome
-            ];
-        }
-
-        foreach ($tabs as $tab) {
-            $this->transform($tab);
-        }
+        return ($categoryId == end($array)) ? 'angle' : 'medium';
     }
 
     /**
      * List in array the root, super-root, ... of the $id.
      */
-    private function getNodes(int $id): array
+    private function getNodes(int $categoryId): array
     {
-        if (($id > 0) && (isset($this->categoryName[$id]['level']))) {
-            $thisLevel = $this->categoryName[$id]['level'];
+        if (($categoryId > 0) && (isset($this->categoryName[$categoryId]['level']))) {
+            $thisLevel = $this->categoryName[$categoryId]['level'];
             $temp = [];
             for ($i = $thisLevel; $i > 0; --$i) {
-                $id = $this->categoryName[$id]['parent_id'];
-                array_unshift($temp, $id);
+                $categoryId = $this->categoryName[$categoryId]['parent_id'];
+                array_unshift($temp, $categoryId);
             }
 
             return $temp;
@@ -489,15 +470,15 @@ class Category
     /**
      * Gets the list of the brothers of $id (include $id).
      *
-     * @param int $id Brothers
+     * @param int $categoryId Brothers
      */
-    private function getBrothers(int $id): array
+    private function getBrothers(int $categoryId): array
     {
-        return $this->getChildren($this->categoryName[$id]['parent_id']);
+        return $this->getChildren($this->categoryName[$categoryId]['parent_id']);
     }
 
     /**
-     * List in a array of the $id of the child.
+     * List in an array of the $id of the child.
      */
     public function getChildren(int $categoryId): array
     {
@@ -505,17 +486,17 @@ class Category
     }
 
     /**
-     * list in a array of the $id of the child.
+     * List in an array of the $id of the child.
      *
-     * @param int $id Entity id
+     * @param int $categoryId Entity id
      * @return array<int>
      */
-    public function getChildNodes(int $id): array
+    public function getChildNodes(int $categoryId): array
     {
         $children = [];
 
-        if (isset($this->children[$id])) {
-            foreach (array_keys($this->children[$id]) as $childId) {
+        if (isset($this->children[$categoryId])) {
+            foreach (array_keys($this->children[$categoryId]) as $childId) {
                 $children = array_merge($children, [$childId]);
                 $children = array_merge($children, $this->getChildNodes($childId));
             }
@@ -527,15 +508,15 @@ class Category
     /**
      * Try to expand from the parent_id to the node $id
      */
-    public function expandTo(int $id): void
+    public function expandTo(int $categoryId): void
     {
         $this->collapseAll();
-        $ascendants = $this->getNodes($id);
-        $ascendants[] = $id;
+        $ascendants = $this->getNodes($categoryId);
+        $ascendants[] = $categoryId;
         $numAscendants = count($ascendants);
         for ($i = 0; $i < $numAscendants; ++$i) {
-            if (isset($this->treeTab[$this->getLineCategory($ascendants[$i])]['numChilds'])) {
-                $numChildren = $this->treeTab[$this->getLineCategory($ascendants[$i])]['numChilds'];
+            if (isset($this->treeTab[$this->getLineCategory($ascendants[$i])]['numChildren'])) {
+                $numChildren = $this->treeTab[$this->getLineCategory($ascendants[$i])]['numChildren'];
                 if ($numChildren > 0) {
                     $this->expand($ascendants[$i]);
                 } else {
@@ -561,13 +542,13 @@ class Category
     /**
      * Get the line number where to find the node $id in the category tree.
      *
-     * @param int $id Entity id
+     * @param int $categoryId Entity id
      */
-    private function getLineCategory(int $id): int
+    private function getLineCategory(int $categoryId): int
     {
         $num = count($this->treeTab);
         for ($i = 0; $i < $num; ++$i) {
-            if (isset($this->treeTab[$i]['id']) && $this->treeTab[$i]['id'] === $id) {
+            if (isset($this->treeTab[$i]['id']) && $this->treeTab[$i]['id'] === $categoryId) {
                 return $i;
             }
         }
@@ -578,11 +559,11 @@ class Category
     /**
      * Expand the node $id.
      *
-     * @param int $id Entity id
+     * @param int $categoryId Entity id
      */
-    public function expand(int $id): void
+    public function expand(int $categoryId): void
     {
-        $this->treeTab[$this->getLineCategory($id)]['symbol'] = 'minus';
+        $this->treeTab[$this->getLineCategory($categoryId)]['symbol'] = 'minus';
     }
 
     /**
@@ -621,20 +602,20 @@ class Category
     /**
      * Gets the path from root to child as breadcrumbs.
      *
-     * @param int $id Entity ID
+     * @param int    $catId Entity ID
      * @param string $separator Path separator
-     * @param bool $renderAsHtml Renders breadcrumbs as HTML
+     * @param bool   $renderAsHtml Renders breadcrumbs as HTML
      * @param string $useCssClass Use CSS class "breadcrumb"
      */
     public function getPath(
-        int $id,
+        int $catId,
         string $separator = ' / ',
         bool $renderAsHtml = false,
         string $useCssClass = 'breadcrumb'
     ): string {
         global $sids;
 
-        $ids = $this->getNodes($id);
+        $ids = $this->getNodes($catId);
 
         $num = count($ids);
         $tempName = [];
@@ -643,18 +624,18 @@ class Category
         $breadcrumb = [];
 
         for ($i = 0; $i < $num; ++$i) {
-            $t = $this->getLineCategory($ids[$i]);
-            if (array_key_exists($t, $this->treeTab)) {
+            $lineCategory = $this->getLineCategory($ids[$i]);
+            if (array_key_exists($lineCategory, $this->treeTab)) {
                 $tempName[] = $this->treeTab[$this->getLineCategory($ids[$i])]['name'];
                 $categoryId[] = $this->treeTab[$this->getLineCategory($ids[$i])]['id'];
                 $description[] = $this->treeTab[$this->getLineCategory($ids[$i])]['description'];
             }
         }
 
-        if (isset($this->treeTab[$this->getLineCategory($id)]['name'])) {
-            $tempName[] = $this->treeTab[$this->getLineCategory($id)]['name'];
-            $categoryId[] = $this->treeTab[$this->getLineCategory($id)]['id'];
-            $description[] = $this->treeTab[$this->getLineCategory($id)]['description'];
+        if (isset($this->treeTab[$this->getLineCategory($catId)]['name'])) {
+            $tempName[] = $this->treeTab[$this->getLineCategory($catId)]['name'];
+            $categoryId[] = $this->treeTab[$this->getLineCategory($catId)]['id'];
+            $description[] = $this->treeTab[$this->getLineCategory($catId)]['description'];
         }
 
         // @todo Maybe this should be done somewhere else ...
@@ -738,7 +719,7 @@ class Category
 
     /**
      * Returns all categories that are related to the given article-id and
-     * the current language $this->language in an unsorted array which consists
+     * the current language $this→language in an unsorted array which consists
      * of associative arrays with the keys 'name', 'id', 'lang',
      * 'parent_id' and 'description'.
      *
@@ -989,23 +970,23 @@ class Category
      */
     public function getCategoryLanguagesTranslated(int $categoryId): array
     {
-        $existingCategoryLanguages = $this->configuration->getLanguage()->isLanguageAvailable(
+        $existingLanguages = $this->configuration->getLanguage()->isLanguageAvailable(
             $categoryId,
             'faqcategories'
         );
-        $translated = [];
 
-        foreach ($existingCategoryLanguages as $existingCategoryLanguage) {
+        $translated = [];
+        foreach ($existingLanguages as $existingLanguage) {
             $query = sprintf(
                 "SELECT name, description FROM %sfaqcategories WHERE %s lang = '%s'",
                 Database::getTablePrefix(),
                 $categoryId === 0 ? '' : 'id = ' . $categoryId . ' AND ',
-                $this->configuration->getDb()->escape($existingCategoryLanguage)
+                $this->configuration->getDb()->escape($existingLanguage)
             );
 
             $result = $this->configuration->getDb()->query($query);
             if ($row = $this->configuration->getDb()->fetchArray($result)) {
-                $translated[$existingCategoryLanguage] =
+                $translated[$existingLanguage] =
                     $row['name'] . ('' == $row['description'] ? '' : '  (' . $row['description'] . ')');
             }
         }
@@ -1016,7 +997,7 @@ class Category
     }
 
     /**
-     * Create all languages which can be used for translation as <option>.
+     * Create all languages that can be used for translation as <option>.
      *
      * @param int $categoryId Entity id
      * @param string $selectedLanguage Selected language
@@ -1024,13 +1005,13 @@ class Category
     public function getCategoryLanguagesToTranslate(int $categoryId, string $selectedLanguage): string
     {
         $output = '';
-        $existingCategoryLanguage = $this->configuration->getLanguage()->isLanguageAvailable(
+        $existingLanguage = $this->configuration->getLanguage()->isLanguageAvailable(
             $categoryId,
             'faqcategories'
         );
 
         foreach (LanguageHelper::getAvailableLanguages() as $lang => $langname) {
-            if (!in_array(strtolower((string)$lang), $existingCategoryLanguage)) {
+            if (!in_array(strtolower((string)$lang), $existingLanguage)) {
                 $output .= "\t<option value=\"" . strtolower((string)$lang) . '"';
                 if ($lang == $selectedLanguage) {
                     $output .= ' selected="selected"';
@@ -1045,7 +1026,7 @@ class Category
 
     /**
      * Gets all categories that are not translated in actual language
-     * to add in this->categories (used in the admin section).
+     * to add in this→categories (used in the admin section).
      */
     public function getMissingCategories(): void
     {
