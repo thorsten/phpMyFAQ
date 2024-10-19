21c98ae59193f19f22304d17817dfdc064ec799c
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-10-19 17:14:41 +0200

    refactor: removed unused code

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,7 @@
 module.exports = {
-  // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // The test environment that will be used for testing
   testEnvironment: 'jsdom',
-
   testMatch: ['**/*.test.js', '!**/*.playwright.test.js'],
 };
