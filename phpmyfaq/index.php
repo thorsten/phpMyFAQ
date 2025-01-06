292140faf31be5a8cd0661781edae921f5eee6db
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-01-06 18:22:56 +0100

    fix: set CSRF token before everything else, otherwise the cookie won't be set

--- phpmyfaq/index.php
+++ phpmyfaq/index.php
@@ -67,6 +67,8 @@ require __DIR__ . '/src/Bootstrap.php';
 $request = Request::createFromGlobals();
 $response = new Response();
 $response->headers->set('Content-Type', 'text/html');
+$csrfLogoutToken = Token::getInstance()->getTokenString('logout');
+
 
 //
 // Service Containers
@@ -639,7 +641,7 @@ if ($user->isLoggedIn() && $user->getUserId() > 0) {
         'msgBookmarks' => Translation::get('msgBookmarks'),
         'msgUserRemoval' => Translation::get('ad_menu_RequestRemove'),
         'msgLogoutUser' => Translation::get('ad_menu_logout'),
-        'csrfLogout' => Token::getInstance()->getTokenString('logout'),
+        'csrfLogout' => $csrfLogoutToken,
     ];
 }
 
