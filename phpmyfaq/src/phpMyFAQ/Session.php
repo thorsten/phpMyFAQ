2071fcb8d1f4038c976cb1892ff4996c7e9bf449
Jan <96944229+modelrailroader@users.noreply.github.com>
2025-01-17 18:17:15 +0100

    fix: remove spaces before exploding

--- phpmyfaq/src/phpMyFAQ/Session.php
+++ phpmyfaq/src/phpMyFAQ/Session.php
@@ -270,7 +270,7 @@ class Session
      */
     private function getBotIgnoreList(): array
     {
-        return explode(',', (string) $this->configuration->get('main.botIgnoreList'));
+        return explode(',', str_replace(' ', '', (string) $this->configuration->get('main.botIgnoreList')));
     }
 
     /**
