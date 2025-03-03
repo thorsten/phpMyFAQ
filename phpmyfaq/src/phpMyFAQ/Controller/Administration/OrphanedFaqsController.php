69c76e5b47243f1c925047ad07ca94f6bda30a41
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-03-03 11:53:48 +0100

    fix: added missing permission check (#3331)

--- phpmyfaq/src/phpMyFAQ/Controller/Administration/OrphanedFaqsController.php
+++ phpmyfaq/src/phpMyFAQ/Controller/Administration/OrphanedFaqsController.php
@@ -1,10 +1,26 @@
 <?php
 
+/**
+ * The Orphaned FAQs Controller
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2025 phpMyFAQ Team
+ * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2025-03-02
+ */
+
 declare(strict_types=1);
 
 namespace phpMyFAQ\Controller\Administration;
 
 use phpMyFAQ\Core\Exception;
+use phpMyFAQ\Enums\PermissionType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -20,6 +36,8 @@ class OrphanedFaqsController extends AbstractAdministrationController
     #[Route('/orphaned-faqs', name: 'admin.content.orphaned-faqs', methods: ['GET'])]
     public function index(Request $request): Response
     {
+        $this->userHasPermission(PermissionType::FAQ_EDIT);
+
         $faq = $this->container->get('phpmyfaq.faq');
 
         return $this->render(
