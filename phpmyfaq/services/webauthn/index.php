e7a88bdefb580a4371dd4ea933c4f265a19578a3
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-03-17 10:55:08 +0100

    fix: corrected Webauthn route

--- phpmyfaq/services/webauthn/index.php
+++ phpmyfaq/services/webauthn/index.php
@@ -1,7 +1,21 @@
 <?php
 
+/**
+ * Login handler for WebAuthn
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    Thorsten Rinne <thorsten@phpmyfaq.de>
+ * @copyright 2024-2025 phpMyFAQ Team
+ * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ * @since     2024-09-11
+ */
+
 use phpMyFAQ\Application;
-use phpMyFAQ\Configuration;
 use phpMyFAQ\Controller\WebAuthnController;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -25,7 +39,7 @@ try {
 $routes = new RouteCollection();
 $routes->add(
     'public.webauthn.index',
-    new Route('/', ['_controller' => [WebAuthnController::class, 'index']])
+    new Route('/services/webauthn', ['_controller' => [WebAuthnController::class, 'index']])
 );
 
 $app = new Application($container);
