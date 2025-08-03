061c2ddad1a25c8c5d948d9cf539283ad0896ede
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-08-03 17:33:24 +0200

    test: increased test coverage for UpdateCommand

---
+++ tests/phpMyFAQ/Command/UpdateCommandTest.php
@@ -0,0 +1,329 @@
+<?php
+
+namespace phpMyFAQ\Command;
+
+use DateTime;
+use Exception;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use phpMyFAQ\Administration\Api;
+use phpMyFAQ\Configuration;
+use phpMyFAQ\Setup\Upgrade;
+use phpMyFAQ\System;
+use phpMyFAQ\Translation;
+use ReflectionClass;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Test class for UpdateCommand
+ */
+class UpdateCommandTest extends TestCase
+{
+    private UpdateCommand $command;
+    private CommandTester $commandTester;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Create real command instance for testing
+        $this->command = new UpdateCommand();
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    public function testCommandConfiguration(): void
+    {
+        $this->assertEquals('phpmyfaq:update', $this->command->getName());
+        $this->assertEquals('Executes the phpMyFAQ update process', $this->command->getDescription());
+
+        $definition = $this->command->getDefinition();
+        $this->assertTrue($definition->hasArgument('version'));
+        $this->assertFalse($definition->getArgument('version')->isRequired());
+        $this->assertEquals('Requested version for the update', $definition->getArgument('version')->getDescription());
+    }
+
+    public function testConstructorInitializesProperties(): void
+    {
+        $command = new UpdateCommand();
+
+        // Use reflection to check private properties
+        $reflection = new ReflectionClass($command);
+
+        $configProperty = $reflection->getProperty('configuration');
+        $configProperty->setAccessible(true);
+        $this->assertInstanceOf(Configuration::class, $configProperty->getValue($command));
+
+        $systemProperty = $reflection->getProperty('system');
+        $systemProperty->setAccessible(true);
+        $this->assertInstanceOf(System::class, $systemProperty->getValue($command));
+    }
+
+    public function testExecuteWithoutUpdate(): void
+    {
+        // This test may require specific environment setup
+        // For now, we test that execute returns an integer result
+        $exitCode = $this->commandTester->execute([]);
+
+        $this->assertIsInt($exitCode);
+        $this->assertContains($exitCode, [Command::SUCCESS, Command::FAILURE]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertStringContainsString('Start automatic phpMyFAQ update', $output);
+    }
+
+    public function testExecuteWithVersionArgument(): void
+    {
+        $exitCode = $this->commandTester->execute(['version' => '4.0.0']);
+
+        $this->assertIsInt($exitCode);
+        $output = $this->commandTester->getDisplay();
+        $this->assertStringContainsString('Start automatic phpMyFAQ update', $output);
+    }
+
+    public function testExecuteHandlesExceptionsGracefully(): void
+    {
+        // Create a command that will throw an exception
+        $command = new class extends UpdateCommand {
+            protected function execute($input, $output): int
+            {
+                throw new RuntimeException('Test exception');
+            }
+        };
+
+        $commandTester = new CommandTester($command);
+
+        // Expect the exception to be thrown and caught
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Test exception');
+
+        $commandTester->execute([]);
+    }
+
+    public function testTaskHealthCheckMethodExists(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $this->assertTrue($reflection->hasMethod('taskHealthCheck'));
+
+        $method = $reflection->getMethod('taskHealthCheck');
+        $this->assertTrue($method->isPrivate());
+
+        $parameters = $method->getParameters();
+        $this->assertCount(1, $parameters);
+        $this->assertEquals('symfonyStyle', $parameters[0]->getName());
+    }
+
+    public function testTaskUpdateCheckMethodExists(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $this->assertTrue($reflection->hasMethod('taskUpdateCheck'));
+
+        $method = $reflection->getMethod('taskUpdateCheck');
+        $this->assertTrue($method->isPrivate());
+
+        $parameters = $method->getParameters();
+        $this->assertCount(1, $parameters);
+        $this->assertEquals('symfonyStyle', $parameters[0]->getName());
+    }
+
+    public function testAllTaskMethodsExist(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+
+        $expectedMethods = [
+            'taskHealthCheck',
+            'taskUpdateCheck',
+            'taskDownloadPackage',
+            'taskExtractPackage',
+            'taskCreateTemporaryBackup',
+            'taskInstallPackage',
+            'taskUpdateDatabase',
+            'taskCleanup'
+        ];
+
+        foreach ($expectedMethods as $methodName) {
+            $this->assertTrue(
+                $reflection->hasMethod($methodName),
+                sprintf('Method %s should exist', $methodName)
+            );
+
+            $method = $reflection->getMethod($methodName);
+            $this->assertTrue(
+                $method->isPrivate(),
+                sprintf('Method %s should be private', $methodName)
+            );
+        }
+    }
+
+    public function testTaskMethodsReturnInteger(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+
+        $taskMethods = [
+            'taskHealthCheck',
+            'taskUpdateCheck'
+        ];
+
+        foreach ($taskMethods as $methodName) {
+            $method = $reflection->getMethod($methodName);
+            $returnType = $method->getReturnType();
+
+            $this->assertNotNull($returnType, sprintf('Method %s should have return type', $methodName));
+            $this->assertEquals('int', $returnType->getName(), sprintf('Method %s should return int', $methodName));
+        }
+    }
+
+    public function testCommandExtendsSymfonyCommand(): void
+    {
+        $this->assertInstanceOf(Command::class, $this->command);
+    }
+
+    public function testCommandUsesCorrectNamespace(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $this->assertEquals('phpMyFAQ\Command', $reflection->getNamespaceName());
+    }
+
+    public function testCommandHasCorrectImports(): void
+    {
+        // Test that the command file contains necessary imports
+        $commandFile = '/Users/thorsten/htdocs/phpMyFAQ/phpmyfaq/src/phpMyFAQ/Command/UpdateCommand.php';
+        $this->assertFileExists($commandFile);
+
+        $content = file_get_contents($commandFile);
+
+        $expectedImports = [
+            'use Symfony\Component\Console\Command\Command;',
+            'use Symfony\Component\Console\Input\InputInterface;',
+            'use Symfony\Component\Console\Output\OutputInterface;',
+            'use Symfony\Component\Console\Style\SymfonyStyle;',
+            'use phpMyFAQ\Configuration;',
+            'use phpMyFAQ\System;'
+        ];
+
+        foreach ($expectedImports as $import) {
+            $this->assertStringContainsString($import, $content);
+        }
+    }
+
+    public function testExecuteMethodSignature(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $method = $reflection->getMethod('execute');
+
+        $this->assertTrue($method->isProtected());
+
+        $parameters = $method->getParameters();
+        $this->assertCount(2, $parameters);
+
+        $this->assertEquals('input', $parameters[0]->getName());
+        $this->assertEquals('output', $parameters[1]->getName());
+
+        $returnType = $method->getReturnType();
+        $this->assertNotNull($returnType);
+        $this->assertEquals('int', $returnType->getName());
+    }
+
+    public function testConfigureMethodSignature(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $method = $reflection->getMethod('configure');
+
+        $this->assertTrue($method->isProtected());
+
+        $returnType = $method->getReturnType();
+        $this->assertNotNull($returnType);
+        $this->assertEquals('void', $returnType->getName());
+    }
+
+    public function testVersionPropertyExists(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $this->assertTrue($reflection->hasProperty('version'));
+
+        $property = $reflection->getProperty('version');
+        $this->assertTrue($property->isPrivate());
+
+        $type = $property->getType();
+        $this->assertNotNull($type);
+        $this->assertEquals('string', $type->getName());
+    }
+
+    public function testConfigurationPropertyExists(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $this->assertTrue($reflection->hasProperty('configuration'));
+
+        $property = $reflection->getProperty('configuration');
+        $this->assertTrue($property->isPrivate());
+    }
+
+    public function testSystemPropertyExists(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $this->assertTrue($reflection->hasProperty('system'));
+
+        $property = $reflection->getProperty('system');
+        $this->assertTrue($property->isPrivate());
+    }
+
+    public function testDefaultNameProperty(): void
+    {
+        $reflection = new ReflectionClass($this->command);
+        $this->assertTrue($reflection->hasProperty('defaultName'));
+
+        $property = $reflection->getProperty('defaultName');
+        $this->assertTrue($property->isStatic());
+        $this->assertTrue($property->isProtected());
+
+        // Use reflection to access protected static property
+        $property->setAccessible(true);
+        $this->assertEquals('phpmyfaq:update', $property->getValue());
+    }
+
+    public function testCommandRegistration(): void
+    {
+        // Test that command can be properly registered
+        $application = new \Symfony\Component\Console\Application();
+        $application->add($this->command);
+
+        $this->assertTrue($application->has('phpmyfaq:update'));
+        $registeredCommand = $application->find('phpmyfaq:update');
+        $this->assertSame($this->command, $registeredCommand);
+    }
+
+    public function testExecuteOutputFormat(): void
+    {
+        $exitCode = $this->commandTester->execute([]);
+        $output = $this->commandTester->getDisplay();
+
+        // Test that output contains expected sections
+        $this->assertStringContainsString('Start automatic phpMyFAQ update', $output);
+
+        // Output should be properly formatted (contains dashes for title)
+        $this->assertStringContainsString('===', $output);
+    }
+
+    public function testExecuteWithVerboseOutput(): void
+    {
+        $exitCode = $this->commandTester->execute([], ['verbosity' => OutputInterface::VERBOSITY_VERBOSE]);
+        $output = $this->commandTester->getDisplay();
+
+        $this->assertIsInt($exitCode);
+        $this->assertStringContainsString('Start automatic phpMyFAQ update', $output);
+    }
+
+    public function testExecuteWithQuietOutput(): void
+    {
+        $exitCode = $this->commandTester->execute([], ['verbosity' => OutputInterface::VERBOSITY_QUIET]);
+
+        $this->assertIsInt($exitCode);
+        // Quiet mode should produce minimal output
+        $output = $this->commandTester->getDisplay();
+        $this->assertIsString($output);
+    }
+}
