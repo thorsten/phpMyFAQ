337ef470a5fdcd5863ce61122906fe6672fbced3
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-08-04 19:44:37 +0200

    test: improved code coverage

--- tests/phpMyFAQ/Administration/ApiTest.php
+++ tests/phpMyFAQ/Administration/ApiTest.php
@@ -10,6 +10,8 @@ use ReflectionClass;
 use Symfony\Component\HttpClient\Exception\TransportException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
@@ -18,6 +20,7 @@ class ApiTest extends TestCase
 {
     private Api $api;
     private Configuration $configuration;
+    private System $system;
     private HttpClientInterface $httpClient;
 
     /**
@@ -26,9 +29,10 @@ class ApiTest extends TestCase
     protected function setUp(): void
     {
         $this->configuration = $this->createMock(Configuration::class);
+        $this->system = $this->createMock(System::class);
         $this->httpClient = $this->createMock(HttpClientInterface::class);
 
-        $this->api = new Api($this->configuration, new System());
+        $this->api = new Api($this->configuration, $this->system);
         $this->api->setHttpClient($this->httpClient);
     }
 
@@ -102,6 +106,10 @@ class ApiTest extends TestCase
         $this->api->isVerified();
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws \JsonException
+     */
     public function testIsVerifiedTransportException(): void
     {
         $this->httpClient->expects($this->once())
@@ -141,5 +149,254 @@ class ApiTest extends TestCase
 
         $expected = ['hash2' => 'def456'];
         $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @throws DecodingExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws Exception
+     */
+    public function testGetVersionsWithHttpError(): void
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getStatusCode')->willReturn(Response::HTTP_NOT_FOUND);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with('GET', 'https://api.phpmyfaq.de/versions')
+            ->willReturn($response);
+
+        $this->configuration->method('getVersion')->willReturn('4.0.0');
+
+        $expected = [
+            'installed' => '4.0.0',
+            'stable' => 'n/a',
+            'development' => 'n/a',
+            'nightly' => 'n/a'
+        ];
+
+        $this->assertEquals($expected, $this->api->getVersions());
+    }
+
+    /**
+     * @throws DecodingExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function testGetVersionsWithTransportException(): void
+    {
+        $transportException = new TransportException('Network error');
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->willThrowException($transportException);
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Network error');
+
+        $this->api->getVersions();
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws Exception
+     */
+    public function testIsVerifiedWithInvalidJson(): void
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getContent')->willReturn('invalid json content');
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
+
+        $this->expectException(\JsonException::class);
+        $this->api->isVerified();
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws Exception
+     */
+    public function testIsVerifiedWithEmptyResponse(): void
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getContent')->willReturn('');
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
+
+        $this->expectException(\JsonException::class);
+        $this->api->isVerified();
+    }
+
+    public function testIsVerifiedWithNonArrayJson(): void
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getContent')->willReturn('"just a string"');
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
+
+        $result = $this->api->isVerified();
+
+        $this->assertFalse($result);
+    }
+
+    public function testIsVerifiedWithServerException(): void
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getContent')->willThrowException(
+            $this->createMock(ServerExceptionInterface::class)
+        );
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('phpMyFAQ Verification API is not available:');
+
+        $this->api->isVerified();
+    }
+
+    public function testGetVerificationIssuesWithNullRemoteHashes(): void
+    {
+        $this->system->method('createHashes')->willReturn('{"hash1": "abc"}');
+
+        $this->api->setRemoteHashes(null);
+
+        $this->expectException(\JsonException::class);
+        $this->api->getVerificationIssues();
+    }
+
+    public function testGetVerificationIssuesWithInvalidRemoteHashes(): void
+    {
+        $this->system->method('createHashes')->willReturn('{"hash1": "abc"}');
+
+        $this->api->setRemoteHashes('invalid json');
+
+        $this->expectException(\JsonException::class);
+        $this->api->getVerificationIssues();
+    }
+
+    public function testGetVerificationIssuesWithInvalidSystemHashes(): void
+    {
+        $this->system->method('createHashes')->willReturn('invalid json');
+
+        $this->api->setRemoteHashes('{"hash1": "abc"}');
+
+        $this->expectException(\JsonException::class);
+        $this->api->getVerificationIssues();
+    }
+
+    public function testGetVerificationIssuesWithIdenticalHashes(): void
+    {
+        $hashes = '{"hash1": "abc", "hash2": "def"}';
+
+        $this->system->method('createHashes')->willReturn($hashes);
+        $this->api->setRemoteHashes($hashes);
+
+        $result = $this->api->getVerificationIssues();
+
+        $this->assertEmpty($result);
+    }
+
+    public function testGetVerificationIssuesWithEmptyHashes(): void
+    {
+        $this->system->method('createHashes')->willReturn('{}');
+        $this->api->setRemoteHashes('{}');
+
+        $result = $this->api->getVerificationIssues();
+
+        $this->assertEmpty($result);
+    }
+
+    public function testSetHttpClient(): void
+    {
+        $newHttpClient = $this->createMock(HttpClientInterface::class);
+
+        $this->api->setHttpClient($newHttpClient);
+
+        // Verify the client was set by making a request
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getStatusCode')->willReturn(Response::HTTP_OK);
+        $response->method('toArray')->willReturn(['stable' => '1.0.0', 'development' => '1.1.0', 'nightly' => '1.2.0']);
+
+        $newHttpClient->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
+
+        $this->configuration->method('getVersion')->willReturn('1.0.0');
+
+        $this->api->getVersions();
+    }
+
+    public function testSetRemoteHashesReturnsInstance(): void
+    {
+        $result = $this->api->setRemoteHashes('{"test": "hash"}');
+
+        $this->assertInstanceOf(Api::class, $result);
+        $this->assertSame($this->api, $result);
+    }
+
+    public function testSetRemoteHashesWithNull(): void
+    {
+        $result = $this->api->setRemoteHashes(null);
+
+        $this->assertInstanceOf(Api::class, $result);
+    }
+
+    public function testConstructorInitializesHttpClient(): void
+    {
+        // Test that constructor creates HttpClient with correct configuration
+        $api = new Api($this->configuration, $this->system);
+
+        // Use reflection to verify the HttpClient is properly initialized
+        $reflection = new ReflectionClass($api);
+        $property = $reflection->getProperty('httpClient');
+        $property->setAccessible(true);
+        $httpClient = $property->getValue($api);
+
+        $this->assertInstanceOf(HttpClientInterface::class, $httpClient);
+    }
+
+    public function testApiUrlIsCorrect(): void
+    {
+        // Verify that the API URL is correctly set
+        $reflection = new ReflectionClass($this->api);
+        $property = $reflection->getProperty('apiUrl');
+        $property->setAccessible(true);
+        $apiUrl = $property->getValue($this->api);
+
+        $this->assertEquals('https://api.phpmyfaq.de/', $apiUrl);
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws Exception
+     * @throws \JsonException
+     */
+    public function testIsVerifiedSetsRemoteHashes(): void
+    {
+        $expectedHashes = '{"hash1": "abc", "hash2": "def"}';
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getContent')->willReturn($expectedHashes);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
+
+        $this->api->isVerified();
+
+        // Verify that remoteHashes was set
+        $reflection = new ReflectionClass($this->api);
+        $property = $reflection->getProperty('remoteHashes');
+        $property->setAccessible(true);
+        $remoteHashes = $property->getValue($this->api);
+
+        $this->assertEquals($expectedHashes, $remoteHashes);
     }
 }
