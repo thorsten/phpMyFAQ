b5145e497d546ccf54b75873e134aafa5ab92ac7
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-08-09 16:06:46 +0200

    test: improved code coverage

---
+++ tests/phpMyFAQ/Database/MysqliTest.php
@@ -0,0 +1,244 @@
+<?php
+
+/**
+ * Tests for phpMyFAQ\Database\Mysqli
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    GitHub Copilot
+ * @copyright 2025 phpMyFAQ Team
+ * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ */
+
+namespace phpMyFAQ\Database;
+
+use mysqli_result;
+use PHPUnit\Framework\TestCase;
+use phpMyFAQ\Core\Exception;
+use stdClass;
+
+/**
+ * Class MysqliTest
+ */
+class MysqliTest extends TestCase
+{
+    private Mysqli $mysqli;
+
+    protected function setUp(): void
+    {
+        $this->mysqli = new Mysqli();
+    }
+
+    public function testImplementsDatabaseDriver(): void
+    {
+        $this->assertInstanceOf(DatabaseDriver::class, $this->mysqli);
+    }
+
+    public function testInitialState(): void
+    {
+        $this->assertEquals([], $this->mysqli->tableNames);
+        $this->assertEquals('', $this->mysqli->log());
+    }
+
+    public function testFetchArrayWithMockResult(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+        $expectedData = ['id' => 1, 'name' => 'test'];
+
+        $resultMock->expects($this->once())
+            ->method('fetch_assoc')
+            ->willReturn($expectedData);
+
+        $result = $this->mysqli->fetchArray($resultMock);
+        $this->assertEquals($expectedData, $result);
+    }
+
+    public function testFetchArrayNull(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        $resultMock->expects($this->once())
+            ->method('fetch_assoc')
+            ->willReturn(null);
+
+        $result = $this->mysqli->fetchArray($resultMock);
+        $this->assertNull($result);
+    }
+
+    public function testFetchRowWithData(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        $resultMock->expects($this->once())
+            ->method('fetch_row')
+            ->willReturn(['test_value', 'other_value']);
+
+        $result = $this->mysqli->fetchRow($resultMock);
+        $this->assertEquals('test_value', $result);
+    }
+
+    public function testFetchRowEmpty(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        $resultMock->expects($this->once())
+            ->method('fetch_row')
+            ->willReturn([]);
+
+        $result = $this->mysqli->fetchRow($resultMock);
+        $this->assertFalse($result);
+    }
+
+    public function testFetchRowNull(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        $resultMock->expects($this->once())
+            ->method('fetch_row')
+            ->willReturn(null);
+
+        $result = $this->mysqli->fetchRow($resultMock);
+        $this->assertFalse($result);
+    }
+
+    public function testFetchObject(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+        $expectedObject = new stdClass();
+        $expectedObject->id = 1;
+        $expectedObject->name = 'test';
+
+        $resultMock->expects($this->once())
+            ->method('fetch_object')
+            ->willReturn($expectedObject);
+
+        $result = $this->mysqli->fetchObject($resultMock);
+        $this->assertEquals($expectedObject, $result);
+    }
+
+    public function testFetchObjectNull(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        $resultMock->expects($this->once())
+            ->method('fetch_object')
+            ->willReturn(null);
+
+        $result = $this->mysqli->fetchObject($resultMock);
+        $this->assertNull($result);
+    }
+
+    public function testNumRowsWithMysqliResult(): void
+    {
+        $nonMysqliObject = new stdClass();
+        $result = $this->mysqli->numRows($nonMysqliObject);
+        $this->assertEquals(0, $result);
+
+        $result = $this->mysqli->numRows('not_a_result');
+        $this->assertEquals(0, $result);
+
+        $result = $this->mysqli->numRows(['data']);
+        $this->assertEquals(0, $result);
+    }
+
+    public function testNumRowsWithNonMysqliResult(): void
+    {
+        $result = $this->mysqli->numRows('invalid_result');
+        $this->assertEquals(0, $result);
+    }
+
+    public function testNumRowsWithNull(): void
+    {
+        $result = $this->mysqli->numRows(null);
+        $this->assertEquals(0, $result);
+    }
+
+    public function testNumRowsWithFalse(): void
+    {
+        $result = $this->mysqli->numRows(false);
+        $this->assertEquals(0, $result);
+    }
+
+    public function testTableNamesProperty(): void
+    {
+        $this->assertEquals([], $this->mysqli->tableNames);
+
+        $this->mysqli->tableNames = ['table1', 'table2'];
+        $this->assertEquals(['table1', 'table2'], $this->mysqli->tableNames);
+    }
+
+    public function testFetchAllWithValidResult(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        $object1 = new stdClass();
+        $object1->id = 1;
+        $object2 = new stdClass();
+        $object2->id = 2;
+
+        $resultMock->expects($this->exactly(3))
+            ->method('fetch_object')
+            ->willReturnOnConsecutiveCalls($object1, $object2, false);
+
+        $result = $this->mysqli->fetchAll($resultMock);
+
+        $this->assertCount(2, $result);
+        $this->assertEquals($object1, $result[0]);
+        $this->assertEquals($object2, $result[1]);
+    }
+
+    public function testFetchAllWithFalseResult(): void
+    {
+        $mysqli = new class extends Mysqli {
+            public function error(): string
+            {
+                return 'Test error message';
+            }
+        };
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Error while fetching result: Test error message');
+
+        $mysqli->fetchAll(false);
+    }
+
+    public function testFetchAllWithEmptyResult(): void
+    {
+        $resultMock = $this->createMock(mysqli_result::class);
+
+        $resultMock->expects($this->once())
+            ->method('fetch_object')
+            ->willReturn(false);
+
+        $result = $this->mysqli->fetchAll($resultMock);
+        $this->assertEquals([], $result);
+    }
+
+    public function testConnectExceptionHandling(): void
+    {
+        $mysqli = new Mysqli();
+
+        $this->assertTrue(method_exists($mysqli, 'connect'));
+
+        $this->expectException(Exception::class);
+
+        set_error_handler(function () {
+        }, E_WARNING);
+
+        try {
+            $mysqli->connect('', '', '', '');
+        } finally {
+            restore_error_handler();
+        }
+    }
+
+    public function testLogInitiallyEmpty(): void
+    {
+        $result = $this->mysqli->log();
+        $this->assertEquals('', $result);
+    }
+}
---
+++ tests/phpMyFAQ/Database/PdoMysqlTest.php
@@ -0,0 +1,333 @@
+<?php
+
+/**
+ * Tests for phpMyFAQ\Database\PdoMysql
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    GitHub Copilot
+ * @copyright 2025 phpMyFAQ Team
+ * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ */
+
+namespace phpMyFAQ\Database;
+
+use Error;
+use PDO;
+use PDOStatement;
+use PHPUnit\Framework\TestCase;
+use phpMyFAQ\Core\Exception;
+use ReflectionMethod;
+use stdClass;
+
+/**
+ * Class PdoMysqlTest
+ */
+class PdoMysqlTest extends TestCase
+{
+    private PdoMysql $pdoMysql;
+
+    protected function setUp(): void
+    {
+        $this->pdoMysql = new PdoMysql();
+    }
+
+    public function testImplementsDatabaseDriver(): void
+    {
+        $this->assertInstanceOf(DatabaseDriver::class, $this->pdoMysql);
+    }
+
+    public function testInitialState(): void
+    {
+        $this->assertEquals([], $this->pdoMysql->tableNames);
+        $this->assertEquals('', $this->pdoMysql->log());
+    }
+
+    public function testConnectExceptionHandling(): void
+    {
+        $this->assertTrue(method_exists($this->pdoMysql, 'connect'));
+
+        $this->expectException(Exception::class);
+
+        set_error_handler(function () {
+        }, E_WARNING);
+
+        try {
+            // This should fail and throw Exception
+            $this->pdoMysql->connect('invalid_host', 'invalid_user', 'invalid_password', 'invalid_db', 3306);
+        } finally {
+            restore_error_handler();
+        }
+    }
+
+    public function testEscape(): void
+    {
+        $testString = "test'string";
+        $result = $this->pdoMysql->escape($testString);
+        $this->assertEquals($testString, $result);
+    }
+
+    public function testEscapeWithSpecialCharacters(): void
+    {
+        $testString = 'test"string\'with\\special;chars';
+        $result = $this->pdoMysql->escape($testString);
+        $this->assertEquals($testString, $result);
+    }
+
+    public function testFetchArrayWithMockResult(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+        $expectedData = ['id' => 1, 'name' => 'test'];
+
+        $statementMock->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_ASSOC)
+            ->willReturn($expectedData);
+
+        $result = $this->pdoMysql->fetchArray($statementMock);
+        $this->assertEquals($expectedData, $result);
+    }
+
+    public function testFetchArrayReturnsNull(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_ASSOC)
+            ->willReturn(null);
+
+        $result = $this->pdoMysql->fetchArray($statementMock);
+        $this->assertNull($result);
+    }
+
+    public function testFetchArrayReturnsFalse(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_ASSOC)
+            ->willReturn(false);
+
+        $result = $this->pdoMysql->fetchArray($statementMock);
+        $this->assertFalse($result);
+    }
+
+    public function testFetchRowWithData(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_NUM)
+            ->willReturn(['test_value', 'other_value']);
+
+        $result = $this->pdoMysql->fetchRow($statementMock);
+        $this->assertEquals('test_value', $result);
+    }
+
+    public function testFetchRowEmpty(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_NUM)
+            ->willReturn([]);
+
+        $result = $this->pdoMysql->fetchRow($statementMock);
+        $this->assertFalse($result);
+    }
+
+    public function testFetchRowNull(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_NUM)
+            ->willReturn(null);
+
+        $result = $this->pdoMysql->fetchRow($statementMock);
+        $this->assertFalse($result);
+    }
+
+    public function testFetchObject(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+        $expectedObject = new stdClass();
+        $expectedObject->id = 1;
+        $expectedObject->name = 'test';
+
+        $statementMock->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_OBJ)
+            ->willReturn($expectedObject);
+
+        $result = $this->pdoMysql->fetchObject($statementMock);
+        $this->assertEquals($expectedObject, $result);
+    }
+
+    public function testFetchObjectNull(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_OBJ)
+            ->willReturn(null);
+
+        $result = $this->pdoMysql->fetchObject($statementMock);
+        $this->assertNull($result);
+    }
+
+    public function testFetchAllWithValidResult(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $object1 = new stdClass();
+        $object1->id = 1;
+        $object2 = new stdClass();
+        $object2->id = 2;
+
+        $expectedData = [$object1, $object2];
+
+        $statementMock->expects($this->once())
+            ->method('fetchAll')
+            ->with(PDO::FETCH_OBJ)
+            ->willReturn($expectedData);
+
+        $result = $this->pdoMysql->fetchAll($statementMock);
+        $this->assertEquals($expectedData, $result);
+    }
+
+    public function testFetchAllWithFalseResult(): void
+    {
+        $pdoMysql = new class extends PdoMysql {
+            public function error(): string
+            {
+                return 'Test error message';
+            }
+        };
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Error while fetching result: Test error message');
+
+        $pdoMysql->fetchAll(false);
+    }
+
+    public function testFetchAllWithEmptyResult(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('fetchAll')
+            ->with(PDO::FETCH_OBJ)
+            ->willReturn([]);
+
+        $result = $this->pdoMysql->fetchAll($statementMock);
+        $this->assertEquals([], $result);
+    }
+
+    public function testNumRows(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('rowCount')
+            ->willReturn(5);
+
+        $result = $this->pdoMysql->numRows($statementMock);
+        $this->assertEquals(5, $result);
+    }
+
+    public function testNumRowsZero(): void
+    {
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        $statementMock->expects($this->once())
+            ->method('rowCount')
+            ->willReturn(0);
+
+        $result = $this->pdoMysql->numRows($statementMock);
+        $this->assertEquals(0, $result);
+    }
+
+    public function testTableNamesProperty(): void
+    {
+        $this->assertEquals([], $this->pdoMysql->tableNames);
+
+        $this->pdoMysql->tableNames = ['table1', 'table2'];
+        $this->assertEquals(['table1', 'table2'], $this->pdoMysql->tableNames);
+    }
+
+    public function testLogInitiallyEmpty(): void
+    {
+        $result = $this->pdoMysql->log();
+        $this->assertEquals('', $result);
+    }
+
+    public function testConnectDsnConstruction(): void
+    {
+        $host = 'localhost';
+        $database = 'test_db';
+        $port = 3306;
+
+        // Expected DSN format: mysql:host=localhost;dbname=test_db;port=3306;charset=utf8mb4
+        $expectedDsnPattern = sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8mb4', $host, $database, $port);
+
+        $this->assertTrue(method_exists($this->pdoMysql, 'connect'));
+    }
+
+    public function testErrorMethodExists(): void
+    {
+        $this->assertTrue(method_exists($this->pdoMysql, 'error'));
+    }
+
+    public function testErrorWithNullPdo(): void
+    {
+        try {
+            $result = $this->pdoMysql->error();
+            $this->assertEquals('', $result);
+        } catch (Error $e) {
+            $this->assertStringContainsString('errorInfo', $e->getMessage());
+        }
+    }
+
+    public function testMethodSignatures(): void
+    {
+        $this->assertTrue(method_exists($this->pdoMysql, 'connect'));
+        $this->assertTrue(method_exists($this->pdoMysql, 'error'));
+        $this->assertTrue(method_exists($this->pdoMysql, 'escape'));
+        $this->assertTrue(method_exists($this->pdoMysql, 'fetchArray'));
+        $this->assertTrue(method_exists($this->pdoMysql, 'fetchRow'));
+        $this->assertTrue(method_exists($this->pdoMysql, 'fetchObject'));
+        $this->assertTrue(method_exists($this->pdoMysql, 'fetchAll'));
+        $this->assertTrue(method_exists($this->pdoMysql, 'numRows'));
+        $this->assertTrue(method_exists($this->pdoMysql, 'log'));
+    }
+
+    public function testConnectParameterTypes(): void
+    {
+        $reflection = new ReflectionMethod($this->pdoMysql, 'connect');
+        $parameters = $reflection->getParameters();
+
+        $this->assertCount(5, $parameters);
+        $this->assertEquals('host', $parameters[0]->getName());
+        $this->assertEquals('user', $parameters[1]->getName());
+        $this->assertEquals('password', $parameters[2]->getName());
+        $this->assertEquals('database', $parameters[3]->getName());
+        $this->assertEquals('port', $parameters[4]->getName());
+
+        // Test optional parameters
+        $this->assertTrue($parameters[3]->isOptional());
+        $this->assertTrue($parameters[4]->isOptional());
+        $this->assertEquals('', $parameters[3]->getDefaultValue());
+        $this->assertNull($parameters[4]->getDefaultValue());
+    }
+}
