ceee4fa5a3ff4078587adbeab18f12c3884d6552
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-08-09 15:34:26 +0200

    test: improved code coverage

--- tests/phpMyFAQ/Category/OrderTest.php
+++ tests/phpMyFAQ/Category/OrderTest.php
@@ -1,132 +1,316 @@
 <?php
 
+/**
+ * Tests for phpMyFAQ\Category\Order
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package   phpMyFAQ
+ * @author    GitHub Copilot
+ * @copyright 2025 phpMyFAQ Team
+ * @license   https://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link      https://www.phpmyfaq.de
+ */
+
 namespace phpMyFAQ\Category;
 
-use phpMyFAQ\Configuration;
-use phpMyFAQ\Database\Sqlite3;
-use phpMyFAQ\System;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use phpMyFAQ\Configuration;
+use phpMyFAQ\Database;
+use phpMyFAQ\Database\DatabaseDriver;
+use stdClass;
 
+/**
+ * Class OrderTest
+ */
 class OrderTest extends TestCase
 {
-    private Order $categoryOrder;
+    private Order $order;
+    private Configuration|MockObject $configurationMock;
+    private DatabaseDriver|MockObject $databaseMock;
 
     protected function setUp(): void
     {
-        $dbHandle = new Sqlite3();
-        $dbHandle->connect(PMF_TEST_DIR . '/test.db', '', '');
-        $configuration = new Configuration($dbHandle);
-        $configuration->set('main.currentVersion', System::getVersion());
-        $this->categoryOrder = new Order($configuration);
+        $this->databaseMock = $this->createMock(DatabaseDriver::class);
+        $this->configurationMock = $this->createMock(Configuration::class);
+
+        $this->configurationMock->expects($this->any())
+            ->method('getDb')
+            ->willReturn($this->databaseMock);
+
+        $this->order = new Order($this->configurationMock);
     }
 
-    protected function tearDown(): void
+    public function testConstructor(): void
     {
-        #echo 'teardown';
-        $dbHandle = new Sqlite3();
-        $dbHandle->connect(PMF_TEST_DIR . '/test.db', '', '');
-        $dbHandle->query('DELETE FROM faqcategory_order');
+        $this->assertInstanceOf(Order::class, $this->order);
     }
 
-    public function testAdd(): void
+    public function testAddSuccess(): void
     {
-        $this->categoryOrder->add(1, 0);
-        $this->categoryOrder->add(2, 0);
+        $categoryId = 123;
+        $parentId = 456;
+        $nextId = 10;
 
-        $actual = $this->categoryOrder->getAllCategories();
+        $this->databaseMock->expects($this->once())
+            ->method('nextId')
+            ->with('faqcategory_order', 'position')
+            ->willReturn($nextId);
 
-        $expected = [
-            [
-                'category_id' => 1,
-                'parent_id' => 0,
-                'position' => 1,
-            ],
-            [
-                'category_id' => 2,
-                'parent_id' => 0,
-                'position' => 2,
-            ],
+        $this->databaseMock->expects($this->once())
+            ->method('query')
+            ->with($this->stringContains('INSERT INTO faqcategory_order'))
+            ->willReturn(true);
+
+        $result = $this->order->add($categoryId, $parentId);
+        $this->assertTrue($result);
+    }
+
+    public function testAddFailure(): void
+    {
+        $categoryId = 123;
+        $parentId = 456;
+        $nextId = 10;
+
+        $this->databaseMock->expects($this->once())
+            ->method('nextId')
+            ->with('faqcategory_order', 'position')
+            ->willReturn($nextId);
+
+        $this->databaseMock->expects($this->once())
+            ->method('query')
+            ->willReturn(false);
+
+        $result = $this->order->add($categoryId, $parentId);
+        $this->assertFalse($result);
+    }
+
+    public function testRemoveSuccess(): void
+    {
+        $categoryId = 123;
+
+        $this->databaseMock->expects($this->once())
+            ->method('query')
+            ->with($this->stringContains('DELETE FROM faqcategory_order WHERE category_id = 123'))
+            ->willReturn(true);
+
+        $result = $this->order->remove($categoryId);
+        $this->assertTrue($result);
+    }
+
+    public function testRemoveFailure(): void
+    {
+        $categoryId = 123;
+
+        $this->databaseMock->expects($this->once())
+            ->method('query')
+            ->willReturn(false);
+
+        $result = $this->order->remove($categoryId);
+        $this->assertFalse($result);
+    }
+
+    public function testSetCategoryTreeWithEmptyTree(): void
+    {
+        $this->databaseMock->expects($this->once())
+            ->method('query')
+            ->with($this->stringContains('DELETE FROM faqcategory_order'));
+
+        $this->order->setCategoryTree([]);
+    }
+
+    public function testSetCategoryTreeWithSimpleTree(): void
+    {
+        $category1 = new stdClass();
+        $category1->id = 1;
+        $category1->children = [];
+
+        $category2 = new stdClass();
+        $category2->id = 2;
+        $category2->children = [];
+
+        $categoryTree = [$category1, $category2];
+
+        // Expect DELETE query first, then INSERT queries
+        $this->databaseMock->expects($this->exactly(3))
+            ->method('query');
+
+        $this->order->setCategoryTree($categoryTree);
+    }
+
+    public function testSetCategoryTreeWithNestedTree(): void
+    {
+        $childCategory = new stdClass();
+        $childCategory->id = 2;
+        $childCategory->children = [];
+
+        $parentCategory = new stdClass();
+        $parentCategory->id = 1;
+        $parentCategory->children = [$childCategory];
+
+        $categoryTree = [$parentCategory];
+
+        // Expect DELETE query first, then INSERT queries for parent and child
+        $this->databaseMock->expects($this->exactly(3))
+            ->method('query');
+
+        $this->order->setCategoryTree($categoryTree);
+    }
+
+    public function testSetCategoryTreeWithInvalidId(): void
+    {
+        $category = new stdClass();
+        $category->id = 0; // Invalid ID
+        $category->children = [];
+
+        $categoryTree = [$category];
+
+        // Only expect DELETE query, no INSERT because ID is invalid
+        $this->databaseMock->expects($this->once())
+            ->method('query')
+            ->with($this->stringContains('DELETE FROM faqcategory_order'));
+
+        $this->order->setCategoryTree($categoryTree);
+    }
+
+    public function testGetCategoryTreeEmpty(): void
+    {
+        $result = $this->order->getCategoryTree([], 0);
+        $this->assertEquals([], $result);
+    }
+
+    public function testGetCategoryTreeWithCategories(): void
+    {
+        $categories = [
+            ['category_id' => '1', 'parent_id' => '0'],
+            ['category_id' => '2', 'parent_id' => '1'],
+            ['category_id' => '3', 'parent_id' => '0'],
+            ['category_id' => '4', 'parent_id' => '2'],
         ];
 
-        $this->assertEquals($expected, $actual);
+        $result = $this->order->getCategoryTree($categories, 0);
+
+        $this->assertArrayHasKey('1', $result);
+        $this->assertArrayHasKey('3', $result);
+        $this->assertArrayHasKey('2', $result['1']);
+        $this->assertArrayHasKey('4', $result['1']['2']);
     }
 
-    public function testRemove(): void
+    public function testGetCategoryTreeWithSpecificParent(): void
     {
-        $this->categoryOrder->add(1, 0);
-        $this->categoryOrder->add(2, 0);
-        $this->categoryOrder->remove(1);
-
-        $actual = $this->categoryOrder->getAllCategories();
-
-        $expected = [
-            [
-                'category_id' => 2,
-                'parent_id' => 0,
-                'position' => 2,
-            ],
+        $categories = [
+            ['category_id' => '1', 'parent_id' => '0'],
+            ['category_id' => '2', 'parent_id' => '1'],
+            ['category_id' => '3', 'parent_id' => '1'],
         ];
 
-        $this->assertEquals($expected, $actual);
+        $result = $this->order->getCategoryTree($categories, 1);
+
+        $this->assertArrayHasKey('2', $result);
+        $this->assertArrayHasKey('3', $result);
+        $this->assertArrayNotHasKey('1', $result);
     }
 
-    public function testGetCategoryTree(): void
+    public function testGetParentIdFound(): void
     {
-        $this->categoryOrder->add(1, 0);
-        $this->categoryOrder->add(2, 0);
-        $this->categoryOrder->add(3, 1);
+        $childCategory = new stdClass();
+        $childCategory->id = 2;
+        $childCategory->children = [];
 
-        $actual = $this->categoryOrder->getAllCategories();
+        $parentCategory = new stdClass();
+        $parentCategory->id = 1;
+        $parentCategory->children = [$childCategory];
 
-        $expected = [
-            [
-                'category_id' => 1,
-                'parent_id' => 0,
-                'position' => 1,
-            ],
-            [
-                'category_id' => 2,
-                'parent_id' => 0,
-                'position' => 2,
-            ],
-            [
-                'category_id' => 3,
-                'parent_id' => 1,
-                'position' => 3,
-            ],
-        ];
+        $categoryTree = [$parentCategory];
 
-        $this->assertEquals($expected, $actual);
+        $result = $this->order->getParentId($categoryTree, 2);
+        $this->assertEquals(1, $result);
     }
 
-    public function testGetParentId(): void
+    public function testGetParentIdNotFound(): void
     {
-        $categoryTree = [
-            (object)[
-                'id' => '1',
-                'children' => [
-                    (object)[
-                        'id' => '6',
-                        'children' => [
-                            (object)['id' => '7', 'children' => []],
-                            (object)['id' => '8', 'children' => []],
-                        ],
-                    ],
-                    (object)['id' => '9', 'children' => []],
-                    (object)['id' => '10', 'children' => []],
-                    (object)['id' => '12', 'children' => []],
-                ],
-            ],
-            (object)['id' => '3', 'children' => []],
-            (object)['id' => '2', 'children' => []],
-            (object)['id' => '4', 'children' => []],
-            (object)['id' => '5', 'children' => []],
+        $category = new stdClass();
+        $category->id = 1;
+        $category->children = [];
+
+        $categoryTree = [$category];
+
+        $result = $this->order->getParentId($categoryTree, 999);
+        $this->assertNull($result);
+    }
+
+    public function testGetParentIdTopLevel(): void
+    {
+        $category = new stdClass();
+        $category->id = 1;
+        $category->children = [];
+
+        $categoryTree = [$category];
+
+        $result = $this->order->getParentId($categoryTree, 1);
+        $this->assertEquals(0, $result);
+    }
+
+    public function testGetParentIdDeepNesting(): void
+    {
+        $deepChild = new stdClass();
+        $deepChild->id = 3;
+        $deepChild->children = [];
+
+        $child = new stdClass();
+        $child->id = 2;
+        $child->children = [$deepChild];
+
+        $parent = new stdClass();
+        $parent->id = 1;
+        $parent->children = [$child];
+
+        $categoryTree = [$parent];
+
+        $result = $this->order->getParentId($categoryTree, 3);
+        $this->assertEquals(2, $result);
+    }
+
+    public function testGetAllCategoriesEmpty(): void
+    {
+        $this->databaseMock->expects($this->once())
+            ->method('query')
+            ->with($this->stringContains('SELECT category_id, parent_id, position FROM faqcategory_order ORDER BY position'))
+            ->willReturn('mock_result');
+
+        $this->databaseMock->expects($this->exactly(1))
+            ->method('fetchArray')
+            ->with('mock_result')
+            ->willReturn(false);
+
+        $result = $this->order->getAllCategories();
+        $this->assertEquals([], $result);
+    }
+
+    public function testGetAllCategoriesWithData(): void
+    {
+        $mockData = [
+            ['category_id' => '1', 'parent_id' => '0', 'position' => '1'],
+            ['category_id' => '2', 'parent_id' => '1', 'position' => '2'],
         ];
 
-        $actual = $this->categoryOrder->getParentId($categoryTree, 7);
+        $this->databaseMock->expects($this->once())
+            ->method('query')
+            ->willReturn('mock_result');
 
-        $expected = 6;
+        $this->databaseMock->expects($this->exactly(3))
+            ->method('fetchArray')
+            ->with('mock_result')
+            ->willReturnOnConsecutiveCalls(
+                $mockData[0],
+                $mockData[1],
+                false
+            );
 
-        $this->assertEquals($expected, $actual);
+        $result = $this->order->getAllCategories();
+        $this->assertEquals($mockData, $result);
     }
 }
