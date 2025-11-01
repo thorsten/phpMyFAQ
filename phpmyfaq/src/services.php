02b441670b51dfc55bc88cd52e802b74287d64f4
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-11-01 15:57:16 +0100

    test: added tests for service container configuration

--- phpmyfaq/src/services.php
+++ phpmyfaq/src/services.php
@@ -21,7 +21,8 @@ use phpMyFAQ\Administration\Backup;
 use phpMyFAQ\Administration\Category;
 use phpMyFAQ\Administration\Changelog;
 use phpMyFAQ\Administration\Faq as AdminFaq;
-use phpMyFAQ\Administration\LatestUsers;use phpMyFAQ\Administration\RatingData;
+use phpMyFAQ\Administration\LatestUsers;
+use phpMyFAQ\Administration\RatingData;
 use phpMyFAQ\Administration\Session as AdminSession;
 use phpMyFAQ\Attachment\AttachmentCollection;
 use phpMyFAQ\Auth;
@@ -75,121 +76,130 @@ use phpMyFAQ\User\TwoFactor;
 use phpMyFAQ\User\UserSession;
 use phpMyFAQ\Visits;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container): void {
     // Parameters
     $container->parameters();
 
-    // Services
+    // Services builder
     $services = $container->services();
 
-    $services->set('session', Session::class);
+    // Apply defaults to reduce args boilerplate
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure();
 
+    // ========== Core Symfony framework services ==========
+    $services->set('session', Session::class);
+    $services->alias(SessionInterface::class, 'session');
+
+    // ========== phpMyFAQ services ==========
     $services->set('phpmyfaq.admin.api', Api::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
-            new Reference('phpmyfaq.system')
+            service('phpmyfaq.configuration'),
+            service('phpmyfaq.system')
         ]);
 
     $services->set('phpmyfaq.admin.admin-log', AdminLog::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.admin.backup', Backup::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
-            new Reference('phpmyfaq.database.helper')
+            service('phpmyfaq.configuration'),
+            service('phpmyfaq.database.helper')
         ]);
 
 
     $services->set('phpmyfaq.admin.category', Category::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.admin.changelog', Changelog::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.admin.helper', Helper::class);
 
     $services->set('phpmyfaq.admin.faq', AdminFaq::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.admin.latest-users', LatestUsers::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.admin.rating-data', RatingData::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.admin.session', AdminSession::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
 
     $services->set('phpmyfaq.attachment-collection', AttachmentCollection::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.auth', Auth::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.backup', Backup::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
-            new Reference('phpmyfaq.database.helper')
+            service('phpmyfaq.configuration'),
+            service('phpmyfaq.database.helper')
         ]);
 
     $services->set('phpmyfaq.bookmark', Bookmark::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
-            new Reference('phpmyfaq.user.current_user')
+            service('phpmyfaq.configuration'),
+            service('phpmyfaq.user.current_user')
         ]);
 
     $services->set('phpmyfaq.captcha', Captcha::class)
         ->factory([Captcha::class, 'getInstance'])
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.captcha.helper.captcha_helper', CaptchaHelper::class)
         ->factory([CaptchaHelper::class, 'getInstance'])
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.category.image', Image::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.category.order', Order::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.category.permission', Permission::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.comments', Comments::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.configuration', Configuration::class)
@@ -197,212 +207,207 @@ return static function (ContainerConfigurator $container): void {
 
     $services->set('phpmyfaq.date', Date::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.faq', Faq::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.faq.metadata', MetaData::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.faq.permission', Faq\Permission::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.faq.statistics', Statistics::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.database.helper', DatabaseHelper::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.forms', Forms::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.glossary', Glossary::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.helper.category-helper', CategoryHelper::class);
 
     $services->set('phpmyfaq.helper.faq', FaqHelper::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.helper.search', SearchHelper::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.helper.statistics', StatisticsHelper::class)
         ->args([
-            new Reference('phpmyfaq.admin.session'),
-            new Reference('phpmyfaq.visits'),
-            new Reference('phpmyfaq.date')
+            service('phpmyfaq.admin.session'),
+            service('phpmyfaq.visits'),
+            service('phpmyfaq.date')
         ]);
 
     $services->set('phpmyfaq.helper.user-helper', UserHelper::class)
         ->args([
-            new Reference('phpmyfaq.user.current_user')
+            service('phpmyfaq.user.current_user')
         ]);
 
     $services->set('phpmyfaq.instance', Instance::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.instance.client', Instance\Client::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.instance.elasticsearch', Elasticsearch::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.language', Language::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
-            new Reference('session')
+            service('phpmyfaq.configuration'),
+            service('session')
         ]);
 
     $services->set('phpmyfaq.language.plurals', Plurals::class);
 
     $services->set('phpmyfaq.mail', Mail::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.news', News::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.notification', Notification::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.plugin.plugin-manager', PluginManager::class);
 
     $services->set('phpmyfaq.question', Question::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.rating', Rating::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
+            service('phpmyfaq.configuration'),
         ]);
 
     $services->set('phpmyfaq.search', Search::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.seo', Seo::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.session.token', Token::class)
         ->factory([Token::class, 'getInstance'])
         ->args([
-            new Reference('session')
+            service('session')
         ]);
 
     $services->set('phpmyfaq.session.wrapper', SessionWrapper::class)
         ->args([
-            new Reference('session')
+            service('session')
         ]);
 
     $services->set('phpmyfaq.sitemap', Sitemap::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.services.gravatar', Gravatar::class);
 
-    $services->set('phpmyfaq.seo', Seo::class)
-        ->args([
-            new Reference('phpmyfaq.configuration')
-        ]);
-
     $services->set('phpmyfaq.setup.environment_configurator', EnvironmentConfigurator::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.setup.update', Update::class)
         ->args([
-            new Reference('phpmyfaq.system'),
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.system'),
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.setup.upgrade', Upgrade::class)
         ->args([
-            new Reference('phpmyfaq.system'),
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.system'),
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.stop-words', StopWords::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.system', System::class);
 
     $services->set('phpmyfaq.tags', Tags::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.user', User::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.user.current_user', CurrentUser::class)
         ->factory([CurrentUser::class, 'getCurrentUser'])
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.user.session', UserSession::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.user.two-factor', TwoFactor::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
-            new Reference('phpmyfaq.user.current_user')
+            service('phpmyfaq.configuration'),
+            service('phpmyfaq.user.current_user')
         ]);
 
     $services->set('phpmyfaq.visits', Visits::class)
         ->args([
-            new Reference('phpmyfaq.configuration')
+            service('phpmyfaq.configuration')
         ]);
 
     $services->set('phpmyfaq.service.mcp-server', PhpMyFaqMcpServer::class)
         ->args([
-            new Reference('phpmyfaq.configuration'),
-            new Reference('phpmyfaq.language'),
-            new Reference('phpmyfaq.search'),
-            new Reference('phpmyfaq.faq')
+            service('phpmyfaq.configuration'),
+            service('phpmyfaq.language'),
+            service('phpmyfaq.search'),
+            service('phpmyfaq.faq')
         ]);
 };
---
+++ tests/phpMyFAQ/ServicesConfigurationTest.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpMyFAQ;
+
+use PHPUnit\Framework\TestCase;
+
+final class ServicesConfigurationTest extends TestCase
+{
+    /** @var string */
+    private string $servicesFile;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->servicesFile = dirname(__DIR__, 2) . '/phpmyfaq/src/services.php';
+
+        $composerAutoload = dirname(__DIR__, 2) . '/phpmyfaq/src/libs/autoload.php';
+        if (is_file($composerAutoload)) {
+            require_once $composerAutoload;
+        }
+    }
+
+    public function test_service_configuration_classes_exist_and_factories_are_callable(): void
+    {
+        $contents = file_get_contents($this->servicesFile);
+        $this->assertIsString($contents, 'services.php could not be read');
+
+        // Build alias map from use statements
+        preg_match_all('/^use\s+([^;]+);/mi', $contents, $useMatches);
+        $aliases = [];
+        foreach ($useMatches[1] ?? [] as $useLine) {
+            $useLine = trim($useLine);
+            if (preg_match('/^([A-Za-z0-9_\\\\]+)\s+as\s+([A-Za-z0-9_]+)$/', $useLine, $m)) {
+                $aliases[$m[2]] = ltrim($m[1], '\\');
+            } else {
+                $fq = ltrim($useLine, '\\');
+                $parts = explode('\\', $fq);
+                $alias = end($parts);
+                if ($alias !== false && $alias !== '') {
+                    $aliases[$alias] = $fq;
+                }
+            }
+        }
+
+        // Find all Foo\Bar::class occurrences
+        preg_match_all('/([\\\\A-Za-z0-9_]+)::class/', $contents, $classConstMatches);
+        $fqSymbols = [];
+        foreach ($classConstMatches[1] ?? [] as $raw) {
+            $fqSymbols[] = $this->resolveToFqcn($raw, $aliases);
+        }
+
+        // Find all factory arrays like [Foo\Bar::class, 'method']
+        preg_match_all('/\[\s*([\\\\A-Za-z0-9_]+)::class\s*,\s*[\'\"]([A-Za-z_][A-Za-z0-9_]*)[\'\"]\s*\]/', $contents, $factoryMatches);
+        $factories = [];
+        $count = count($factoryMatches[1] ?? []);
+        for ($i = 0; $i < $count; $i++) {
+            $class = $this->resolveToFqcn($factoryMatches[1][$i], $aliases);
+            $method = $factoryMatches[2][$i] ?? null;
+            if ($method) {
+                $factories[] = [$class, $method];
+            }
+        }
+
+        $fqSymbols = array_values(array_unique($fqSymbols));
+
+        $missing = [];
+        foreach ($fqSymbols as $fqcn) {
+            if (!class_exists($fqcn) && !interface_exists($fqcn) && !(function_exists('enum_exists') && enum_exists($fqcn)) && !trait_exists($fqcn)) {
+                $missing[] = $fqcn;
+            }
+        }
+        $this->assertEmpty($missing, 'Missing classes in services.php: ' . implode(', ', $missing));
+
+        $badFactories = [];
+        foreach ($factories as [$class, $method]) {
+            if (!method_exists($class, $method)) {
+                $badFactories[] = $class . '::' . $method . ' (method missing)';
+            }
+        }
+        $this->assertEmpty($badFactories, 'Invalid factory methods in services.php: ' . implode(', ', $badFactories));
+    }
+
+    private function resolveToFqcn(string $raw, array $aliases): string
+    {
+        if (str_starts_with($raw, '\\')) {
+            return ltrim($raw, '\\');
+        }
+
+        $parts = explode('\\', $raw);
+        $first = $parts[0] ?? '';
+        if (isset($aliases[$first])) {
+            $parts[0] = $aliases[$first];
+            return implode('\\', $parts);
+        }
+
+        return $raw;
+    }
+}
