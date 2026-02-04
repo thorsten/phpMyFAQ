a0a283027caa5d21eabf1949359edef4376480bf
d0ubIeU <github@wadewitz.it>
2026-02-04 18:31:48 +0100

    Fix: formatting

--- phpmyfaq/src/phpMyFAQ/Controller/Frontend/QuestionsController.php
+++ phpmyfaq/src/phpMyFAQ/Controller/Frontend/QuestionsController.php
@@ -66,7 +66,7 @@ final class QuestionsController extends AbstractFrontController
             'isCloseQuestionEnabled' => $this->configuration->get('records.enableCloseQuestion'),
             'userHasPermissionToAnswer' =>
                 $this->currentUser->perm->hasPermission($this->currentUser->getUserId(), PermissionType::FAQ_ADD->value)
-                || $this->configuration->get('records.allowNewFaqsForGuests'),
+                    || $this->configuration->get('records.allowNewFaqsForGuests'),
             'msgQuestionsWaiting' => Translation::get(key: 'msgQuestionsWaiting'),
             'msgNoQuestionsAvailable' => Translation::get(key: 'msgNoQuestionsAvailable'),
             'msg2answerFAQ' => Translation::get(key: 'msg2answerFAQ'),
