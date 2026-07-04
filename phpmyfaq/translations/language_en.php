8895b47c5c8e44691007bfe2a9e4016424861caa
Thorsten Rinne <thorsten@phpmyfaq.de>
2026-07-04 17:57:54 +0200

    feat(admin): add translation keys for redesigned group administration page

--- phpmyfaq/translations/language_en.php
+++ phpmyfaq/translations/language_en.php
@@ -1813,4 +1813,11 @@ $PMF_LANG['msgCustomizeDashboard'] = 'Customize';
 $PMF_LANG['msgDashboardDone'] = 'Done';
 $PMF_LANG['msgResetDashboard'] = 'Reset layout';
 
+$PMF_LANG['ad_group_filter'] = 'Filter groups…';
+$PMF_LANG['ad_group_search_users'] = 'Search users to add…';
+$PMF_LANG['ad_group_select_group'] = 'Select a group from the list to view and edit its settings.';
+$PMF_LANG['ad_group_remove_member'] = 'Remove member';
+$PMF_LANG['ad_group_permission_filter'] = 'Filter permissions…';
+$PMF_LANG['ad_group_restrictions_saved'] = 'Category restrictions saved.';
+
 return $PMF_LANG;
