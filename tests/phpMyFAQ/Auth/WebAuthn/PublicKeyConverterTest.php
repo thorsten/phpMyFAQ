a316f100bb287acc7735fd16126d0281ecd53d18
Thorsten Rinne <thorsten@phpmyfaq.de>
2026-09-12 16:29:38 +0200

    test: cover the RS256 happy path of PublicKeyConverter

--- tests/phpMyFAQ/Auth/WebAuthn/PublicKeyConverterTest.php
+++ tests/phpMyFAQ/Auth/WebAuthn/PublicKeyConverterTest.php
@@ -3,6 +3,7 @@
 namespace phpMyFAQ\Auth\WebAuthn;
 
 use CBOR\CBOREncoder;
+use CBOR\Types\CBORByteString;
 use phpMyFAQ\Core\Exception;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
@@ -47,6 +48,50 @@ class PublicKeyConverterTest extends TestCase
         $this->expectExceptionMessage('RSA Modulus missing');
 
         PublicKeyConverter::fromCoseToPkcs($encoded);
+    }
+
+    public function testFromCoseToPkcsThrowsWhenRsaExponentOrModulusIsNotAByteString(): void
+    {
+        $encoded = CBOREncoder::encode([3 => -257, -2 => 1, -1 => 1]);
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Cannot decode key response for RSA exponent or modulus');
+
+        PublicKeyConverter::fromCoseToPkcs($encoded);
+    }
+
+    /**
+     * Exercises the full RS256 path, including the phpseclib BigInteger and
+     * PublicKeyLoader calls, by round-tripping an openssl-generated RSA key.
+     *
+     * @throws Exception
+     */
+    public function testFromCoseToPkcsConvertsRs256KeyToPem(): void
+    {
+        $key = openssl_pkey_new(['private_key_type' => OPENSSL_KEYTYPE_RSA, 'private_key_bits' => 2048]);
+        $this->assertNotFalse($key);
+        $details = openssl_pkey_get_details($key);
+        $this->assertIsArray($details);
+
+        $encoded = CBOREncoder::encode([
+            1 => 3,
+            3 => -257,
+            -1 => new CBORByteString($details['rsa']['n']),
+            -2 => new CBORByteString($details['rsa']['e']),
+        ]);
+
+        $pem = PublicKeyConverter::fromCoseToPkcs($encoded);
+
+        $this->assertIsString($pem);
+        $this->assertStringStartsWith('-----BEGIN PUBLIC KEY-----', $pem);
+
+        $publicKey = openssl_pkey_get_public($pem);
+        $this->assertNotFalse($publicKey);
+        $publicKeyDetails = openssl_pkey_get_details($publicKey);
+        $this->assertIsArray($publicKeyDetails);
+        $this->assertSame(OPENSSL_KEYTYPE_RSA, $publicKeyDetails['type']);
+        $this->assertSame($details['rsa']['n'], $publicKeyDetails['rsa']['n']);
+        $this->assertSame($details['rsa']['e'], $publicKeyDetails['rsa']['e']);
     }
 
     public function testFromCoseToPkcsThrowsWhenCurveIsMissing(): void
